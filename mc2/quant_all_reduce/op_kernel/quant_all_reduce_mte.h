ee7c08a47d21626d1ff1f6d10ef01ca6cabc987a
andingfangt <miaominghao@huawei.com>
2025-12-05 19:02:00 +0800

    change Cast RoundMode to RINT
    Co-authored-by: andingfangt<miaominghao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3461 merge master into master
    
    change Cast RoundMode to RINT
    
    Created-by: andingfangt
    Commit-by: andingfangt
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!3461

--- mc2/quant_all_reduce/op_kernel/quant_all_reduce_mte.h
+++ mc2/quant_all_reduce/op_kernel/quant_all_reduce_mte.h
@@ -361,7 +361,7 @@ __aicore__ inline void QuantAllReduceMte<TemplateQuantAllReduceType>::ReadRomote
             if constexpr (AscendC::IsSameType<OutputType, float>::value) {
                 DataCopy(xOutTensor_, sumTensor_, SINGLE_NUM);
             } else {
-                Cast(xOutTensor_, sumTensor_, RoundMode::CAST_ROUND, SINGLE_NUM);
+                Cast(xOutTensor_, sumTensor_, RoundMode::CAST_RINT, SINGLE_NUM);
             }
             xOutQueue_.EnQue(xOutTensor_);
             xOutTensor_ = xOutQueue_.DeQue<OutputType>();
