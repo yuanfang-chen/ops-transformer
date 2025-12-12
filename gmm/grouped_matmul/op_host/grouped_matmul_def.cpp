65b73fa5ebf8cff677bb5bfdd1ee848f242e9744
chen-shuai <chenshuai96@huawei.com>
2025-12-12 15:56:19 +0800

    gmm
    copyright
    
    OAT

--- gmm/grouped_matmul/op_host/grouped_matmul_def.cpp
+++ gmm/grouped_matmul/op_host/grouped_matmul_def.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -734,7 +734,8 @@ public:
             .PrecisionReduceFlag(true)
             .ExtendCfgInfo("prebuildPattern.value", "Opaque")
             .ExtendCfgInfo("coreType.value", "AiCore")
-            .ExtendCfgInfo("aclnnSupport.value", "support_aclnn");
+            .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
+            .ExtendCfgInfo("opFile.value","grouped_matmul_apt");
         this->AICore().AddConfig("ascend910_95", config91095);
 
         OpAICoreConfig config_kirin = GetKirinCoreConfig();
