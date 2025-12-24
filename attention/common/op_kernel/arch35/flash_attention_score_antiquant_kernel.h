13a8e9050c85f6a17d07af105a5dd935ec56af59
jinyingbo <jinying22@huawei.com>
2025-12-24 14:27:35 +0800

    FD场景PA_NZ KVs对齐处理
    Co-authored-by: number-2-bo-bo<jinying22@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4534 merge master into master
    
    FD场景PA_NZ KVs对齐处理
    
    Created-by: jinyingbo
    Commit-by: number-2-bo-bo
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!4534

--- attention/common/op_kernel/arch35/flash_attention_score_antiquant_kernel.h
+++ attention/common/op_kernel/arch35/flash_attention_score_antiquant_kernel.h
@@ -317,7 +317,13 @@ __aicore__ inline void FlashAttentionScoreAntiquantKernel<AntiquantCubeBlockType
         this->constInfo.splitKVNum = inputParamsRegbase.kvSplitPart;
         this->constInfo.sInnerLoopSize = CeilDivision(this->constInfo.s2Size, this->constInfo.splitKVNum);
         if constexpr (PAGE_ATTENTION_ANTIQUANT) {
-            this->constInfo.sInnerLoopSize = AlignUp32((uint64_t)this->constInfo.sInnerLoopSize);
+            this->constInfo.sInnerLoopSize = AlignUp32(static_cast<uint64_t>(this->constInfo.sInnerLoopSize));
+        }
+        if constexpr (isPa) {
+            bool isNz = inputParamsRegbase.paLayoutType == static_cast<uint32_t>(KvCacheLayout::KV_CACHE_NZ);
+            if (isNz) {
+                this->constInfo.sInnerLoopSize = AlignUp32(static_cast<uint64_t>(this->constInfo.sInnerLoopSize));
+            }
         }
     }
     this->constInfo.isRowInvalid = inputParamsRegbase.isRowInvalid;
