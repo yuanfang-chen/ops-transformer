9597bf7411db1881a0928bdfcda9ec79efa501ac
wuzhongxin <wuzhongxin1@huawei.com>
2025-12-31 15:11:07 +0800

    ut适配soc整改，npuArch替代socversion
    Co-authored-by: wuzhongxin<wuzhongxin1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4819 merge npuarch into master
    
    ut适配soc整改，npuArch替代socversion
    
    Created-by: wuzhongxin
    Commit-by: wuzhongxin
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!4819

--- tests/ut/framework_normal/common/tiling_case_executor.cpp
+++ tests/ut/framework_normal/common/tiling_case_executor.cpp
@@ -124,19 +124,25 @@
     if (tilingContextPara.socInfoString_ != "") {                                                                      \
         compileInfoString = tilingContextPara.socInfoString_;                                                          \
     }                                                                                                                  \
-    map<string, string> socToArch = {                                                                                  \
-        {"Ascend310P", "2002"},                                                                                        \
-        {"Ascend910B", "2201"},                                                                                        \
-        {"Ascend910_93", "2201"},                                                                                      \
-        {"Ascend910_95", "3510"}                                                                                       \
-    };                                                                                                                 \
     map<string, string> socInfos;                                                                                      \
     map<string, string> aicoreSpec;                                                                                    \
     map<string, string> intrinsics;                                                                                    \
-    map<string, string> versions = {                                                                                   \
-        {"NpuArch", socToArch[tilingContextPara.socVersion_]},                                                         \
-        {"Short_SoC_version", tilingContextPara.socVersion_}                                                           \
-    };                                                                                                                 \
+    map<string, string> versions;                                                                                      \
+    string version = tilingContextPara.socVersion_;                                                                    \
+    if (isNpuArchString(version)) {                                                                                    \
+        versions = {{"NpuArch", tilingContextPara.socVersion_}};                                                       \
+    } else {                                                                                                           \
+        map<string, string> socToArch = {                                                                              \
+            {"Ascend310P", "2002"},                                                                                    \
+            {"Ascend910B", "2201"},                                                                                    \
+            {"Ascend910_93", "2201"},                                                                                  \
+            {"Ascend910_95", "3510"}                                                                                   \
+        };                                                                                                             \
+        versions = {                                                                                                   \
+            {"NpuArch", socToArch[tilingContextPara.socVersion_]},                                                     \
+            {"Short_SoC_version", tilingContextPara.socVersion_}                                                       \
+        };                                                                                                             \
+    }                                                                                                                  \
     GetPlatFormInfos(compileInfoString.c_str(), socInfos, aicoreSpec, intrinsics);                                     \
     auto tilingContext = contextHolder.GetContext();                                                                   \
     tilingContext->GetPlatformInfo()->SetPlatformRes("SoCInfo", socInfos);                                             \
@@ -149,6 +155,20 @@
     auto tilingFunc = spaceRegistry->GetOpImpl(tilingContextPara.opName_.c_str())->tiling;                             \
     /* 4. check tiling func */                                                                                         \
     auto tilingRet = tilingFunc(tilingContext);
+
+static bool isNpuArchString(string version)
+{
+    if(version.empty()){
+        return false;
+    }
+
+    for(char c : version) {
+        if (!std::isdigit(static_cast<unsigned char>(c))) {
+            return false;
+        }
+    }
+    return true;
+}
 
 template <typename T>
 static string to_string(void* buf, size_t size, unordered_set<size_t> mask={})
