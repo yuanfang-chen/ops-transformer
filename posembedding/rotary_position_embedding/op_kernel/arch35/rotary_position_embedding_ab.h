1b66b90eef7ccefb72acf0af5b6bb1cfd3f46f9f
daiwei18 <daiwei18@huawei.com>
2026-01-10 15:09:01 +0800

    update posembedding
    Co-authored-by: daiwei18<daiwei18@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5292 merge master into master
    
    update posembedding
    
    Created-by: daiwei18
    Commit-by: daiwei18
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!5292

--- posembedding/rotary_position_embedding/op_kernel/arch35/rotary_position_embedding_ab.h
+++ posembedding/rotary_position_embedding/op_kernel/arch35/rotary_position_embedding_ab.h
@@ -17,7 +17,7 @@
 
 #include "op_kernel/math_util.h"
 #include "op_kernel/platform_util.h"
-#include "../../inc/load_store_utils.h"
+#include "op_kernel/load_store_utils.h"
 #if __has_include("../../apply_rotary_pos_emb/arch35/apply_rotary_pos_emb_common.h")
 #include "../../apply_rotary_pos_emb/arch35/apply_rotary_pos_emb_common.h"
 #else
