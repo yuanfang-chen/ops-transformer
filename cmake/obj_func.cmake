8df3325d572b653f98a5ea3445c390b1744a70cb
jiangbo96 <jiangbo74@huawei.com>
2026-01-12 15:45:51 +0800

    fix allreduce ut
    Co-authored-by: 江波<jiangbo74@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5150 merge br_personal/j00600688/fix_allreduce_ut_0106 into master
    
    fix allreduce ut
    
    Created-by: jiangbo96
    Commit-by: 江波
    Merged-by: cann-robot
    Description: ## 描述
    修复matmulAllreduce类算子UT
    （1）tilingKey整改后，UT tiling部分校验的tilingKey，预期需要更改适配；
    （2）tbe tiling场景下，会走到legacy包里的GenTiling函数。其中getSocVersion的函数获取结果失败，会导致tbe tiling的结果异常，最终因校验而用例fail。
    这部分实现在canndev仓里，不方便打桩getSocVersion的函数，直接打桩了GenTiling函数。通过修改编译工程，所有需要tbe Gentiling的UT，编进去的都是同名桩函数，保证tbe tiling结果的正常。
    
    
    
    See merge request: cann/ops-transformer-dev!5150

--- cmake/obj_func.cmake
+++ cmake/obj_func.cmake
@@ -300,7 +300,7 @@ macro(add_mc2_modules_sources)
       ${OPS_TRANSFORMER_DIR}/mc2/common/src/mc2_tiling_utils.cpp
       ${OPS_TRANSFORMER_DIR}/mc2/common/src/mc2_matmul_tiling_cfg.cpp
       ${OPS_TRANSFORMER_DIR}/mc2/common/src/mc2_log.cpp
-      ${OPS_TRANSFORMER_DIR}/mc2/3rd/ops_legacy/op_tiling/op_cache_tiling.cpp
+      $<$<NOT:$<OR:$<BOOL:${UT_TEST_ALL}>,$<BOOL:${OP_HOST_UT}>>>:${OPS_TRANSFORMER_DIR}/mc2/3rd/ops_legacy/op_tiling/op_cache_tiling.cpp>
       ${OPS_TRANSFORMER_DIR}/mc2/3rd/ops_legacy/op_tiling/runtime_kb_api.cpp
     )
   endif()
