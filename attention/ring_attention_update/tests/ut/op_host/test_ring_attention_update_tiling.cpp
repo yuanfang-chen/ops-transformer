3a865ee7a8dc7bf10c04102e818ea936d7d0b946
sanhaonvqingnian <wujiali7@huawei.com>
2026-01-15 11:03:58 +0800

    ring_attention_update ascend950 opensource

--- attention/ring_attention_update/tests/ut/op_host/test_ring_attention_update_tiling.cpp
+++ attention/ring_attention_update/tests/ut/op_host/test_ring_attention_update_tiling.cpp
@@ -57,3 +57,109 @@ TEST_F(RingAttentionUpdateTiling, test_ring_attention_update_success) {
     std::vector<size_t> expectWorkspaces = {16777216};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
+
+TEST_F(RingAttentionUpdateTiling, test_ring_attention_update_regbase_sbh_fp32) {
+    RingAttentionUpdateCompileInfo compileInfo = {};
+    std::string input_layout = "SBH";
+    std::string input_softmax_layout = "SBH";
+    
+    gert::TilingContextPara tilingContextPara("RingAttentionUpdate",
+                                              {
+                                                // input info
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // output info
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // attr
+                                                {"input_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_layout)},
+                                                {"input_softmax_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_softmax_layout)},
+                                              },
+                                              &compileInfo,
+                                              "Ascend910_95",
+                                              56, 262144);
+    uint64_t expectTilingKey = 100;
+    string expectTilingData = "2 1024 12 128 8 56 7 3 147 142 74 128 ";
+    std::vector<size_t> expectWorkspaces = {4294967295};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(RingAttentionUpdateTiling, test_ring_attention_update_regbase_sbh_fp16) {
+    RingAttentionUpdateCompileInfo compileInfo = {};
+    std::string input_layout = "SBH";
+    std::string input_softmax_layout = "SBH";
+    
+    gert::TilingContextPara tilingContextPara("RingAttentionUpdate",
+                                              {
+                                                // input info
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // output info
+                                                {{{1024, 2, 1536}, {1024, 2, 1536}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{2, 12, 1024, 8}, {2, 12, 1024, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // attr
+                                                {"input_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_layout)},
+                                                {"input_softmax_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_softmax_layout)},
+                                              },
+                                              &compileInfo,
+                                              "Ascend910_95",
+                                              56, 262144);
+    uint64_t expectTilingKey = 100;
+    string expectTilingData = "2 1024 12 128 8 56 7 3 147 142 132 128 ";
+    std::vector<size_t> expectWorkspaces = {4294967295};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
+TEST_F(RingAttentionUpdateTiling, test_ring_attention_update_regbase_softmaxtnd_fp16) {
+    RingAttentionUpdateCompileInfo compileInfo = {};
+    std::string input_layout = "TND";
+    std::string input_softmax_layout = "TND";
+    
+    gert::TilingContextPara tilingContextPara("RingAttentionUpdate",
+                                              {
+                                                // input info
+                                                {{{12000, 2, 768}, {12000, 2, 768}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{12000, 2, 768}, {12000, 2, 768}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{18,}, {18,}}, ge::DT_INT64, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // output info
+                                                {{{12000, 2, 768}, {12000, 2, 768}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {{{12000, 2, 8}, {12000, 2, 8}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                              },
+                                              {
+                                                // attr
+                                                {"input_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_layout)},
+                                                {"input_softmax_layout", Ops::Transformer::AnyValue::CreateFrom<string>(input_softmax_layout)},
+                                              },
+                                              &compileInfo,
+                                              "Ascend910_95",
+                                              56, 262144);
+    uint64_t expectTilingKey = 110;
+    string expectTilingData = "12000 2 768 8 17 56 429 405 27 768 ";
+    std::vector<size_t> expectWorkspaces = {4294967295};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
