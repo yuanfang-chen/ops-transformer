930cef27957d0588f335905d1612afc558369c81
qq_52874943 <gaomingyuan3@h-partners.com>
2026-01-15 19:51:22 +0800

    xinzeng

--- attention/recurrent_gated_delta_rule/op_host/recurrent_gated_delta_rule_def.cpp
+++ attention/recurrent_gated_delta_rule/op_host/recurrent_gated_delta_rule_def.cpp
@@ -90,6 +90,7 @@ public:
             .ExtendCfgInfo("softsync.flag", "true");
         this->AICore().AddConfig("ascend910b", aicConfig);
         this->AICore().AddConfig("ascend910_93", aicConfig);
+        this->AICore().AddConfig("ascend910_95", aicConfig);
     }
 };
 
