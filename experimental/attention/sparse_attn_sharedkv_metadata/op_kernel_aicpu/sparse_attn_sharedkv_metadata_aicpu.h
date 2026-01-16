0d70c8e86756a45595e11aab3b494539b9b9300b
qq_32807861 <handongchen2@huawei.com>
2026-01-16 17:19:14 +0800

    参数适配A5

--- experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
+++ experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
@@ -46,6 +46,12 @@ enum class SparseMode : uint8_t {
     SPARSE_BUTT,
 };
 
+enum class ValidSocVersion {
+    ASCEND910B = 0,
+    ASCEND910D,
+    RESERVED_VERSION = 99999
+};
+
 template<class T>
 using Range = std::pair<T, T>;
 
@@ -230,7 +236,8 @@ private:
   bool ParamsInit(uint32_t cmpRatio_, uint32_t topK_);
   bool BalanceSchedule();
   bool GenMetaData();
-  
+  ValidSocVersion ProcessSocVersion();
+
   // util
   uint32_t GetS1SeqSize(uint32_t bIdx);
   uint32_t GetS2SeqSize(uint32_t bIdx);
@@ -301,6 +308,8 @@ private:
   std::string layoutKV_ = "PA_ND";
   bool hasOriKV_ = true;
   bool hasCmpKV_ = true;
+  uint32_t aicCoreNum_ = 24U;
+  uint32_t aivCoreNum_ = 48U;
 
   // attr
   uint32_t coreNum_ = 24U; // new
@@ -312,7 +321,7 @@ private:
   uint32_t s2BaseSize_ = 0;
   uint32_t gS1BaseSizeOfFd_ = 0;
   bool isS1G_ = true;
-  SplitResult splitRes_ {24, 2};
+  SplitResult splitRes_ {36, 2};
   bool isCFA = false;
   bool isSCFA = false;
   bool supportFd = false;
