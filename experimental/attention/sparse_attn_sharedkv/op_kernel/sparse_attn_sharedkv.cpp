56d2fcae7245c9afd8dee8b8f006f4d539c8dad6
张海杰 <zhanghaijie4@hisilicon.com>
2026-01-18 12:53:11 +0800

    add swa dedode support

--- experimental/attention/sparse_attn_sharedkv/op_kernel/sparse_attn_sharedkv.cpp
+++ experimental/attention/sparse_attn_sharedkv/op_kernel/sparse_attn_sharedkv.cpp
@@ -17,7 +17,7 @@
 #include "lib/matmul_intf.h"
 #include "sparse_attn_sharedkv_template_tiling_key.h"
 #include "arch32/sparse_attn_sharedkv_scfa_kernel.h"
-// #include "arch32/sparse_attn_sharedkv_swa_kernel.h"
+#include "arch32/sparse_attn_sharedkv_swa_kernel.h"
 #include "sparse_attn_sharedkv_metadata.h"
 // #include "sparse_attn_sharedkv_cfa.h"
 
@@ -75,8 +75,8 @@ sparse_attn_sharedkv(__gm__ uint8_t *query, __gm__ uint8_t *oriKV, __gm__ uint8_
             SAS_OP_IMPL(SparseAttnSharedkvScfa, SparseAttnSharedkvTilingData, half, half, half,
                 FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
         } else {
-            // SAS_OP_IMPL(SparseAttnSharedkvSwa, SparseAttnSharedkvTilingData, half, half, half,
-            //     FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
+            SAS_OP_IMPL(SparseAttnSharedkvSwa, SparseAttnSharedkvTilingData, half, half, half,
+                FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
         }
     }
     if constexpr (ORIG_DTYPE_Q == DT_BF16 && ORIG_DTYPE_ORI_KV == DT_BF16 && ORIG_DTYPE_ATTN_OUT == DT_BF16) {
@@ -84,8 +84,8 @@ sparse_attn_sharedkv(__gm__ uint8_t *query, __gm__ uint8_t *oriKV, __gm__ uint8_
             SAS_OP_IMPL(SparseAttnSharedkvScfa, SparseAttnSharedkvTilingData, bfloat16_t, bfloat16_t, bfloat16_t,
                 FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
         } else {
-            // SAS_OP_IMPL(SparseAttnSharedkvSwa, SparseAttnSharedkvTilingData, bfloat16_t, bfloat16_t, bfloat16_t,
-            //     FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
+            SAS_OP_IMPL(SparseAttnSharedkvSwa, SparseAttnSharedkvTilingData, bfloat16_t, bfloat16_t, bfloat16_t,
+                FLASH_DECODE, static_cast<SAS_LAYOUT>(LAYOUT_T), static_cast<SAS_LAYOUT>(KV_LAYOUT_T));
         }
 
     }
