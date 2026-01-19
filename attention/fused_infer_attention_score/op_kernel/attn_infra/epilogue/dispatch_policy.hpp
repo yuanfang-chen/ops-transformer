76f7c5acad53ffe958658efc3322ce80820fe3e4
j60100428 <jingsong5@h-partners.com>
2026-01-19 11:53:40 +0800

    opensource synchronization

--- attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/dispatch_policy.hpp
+++ attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/dispatch_policy.hpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: j60100428 jingsong5@h-partners.com
+ * @Date: 2026-01-19 11:01:43
+ * @LastEditors: j60100428 jingsong5@h-partners.com
+ * @LastEditTime: 2026-01-19 11:52:02
+ * @FilePath: \ops-transformer_synchronize\attention\fused_infer_attention_score\op_kernel\attn_infra\epilogue\dispatch_policy.hpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /**
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
@@ -19,14 +27,20 @@ namespace NpuArch::Epilogue
 
 enum class LseMode {NONE = 0, OUT_ONLY = 1};
 enum class SinkMode {DISABLE = 0, ENABLE = 1};
-
+enum class MaskMode {
+    NO_MASK = 0,
+    MASK_CAUSAL = 1,
+    MASK_SPEC = 2,
+    MASK_SWA = 4
+};
 // For AtlasA2, FA Infer online Softmax
-template <LseMode LSE_MODE_, SinkMode SINK_MODE_, typename SM_DTYPE_>
+template <LseMode LSE_MODE_, SinkMode SINK_MODE_, MaskMode MASK_MODE_, typename SM_DTYPE_>
 struct EpilogueAtlasA2OnlineSoftmax {
     using ArchTag = Arch::AtlasA2;
     using IntermPrec = SM_DTYPE_;
     static constexpr LseMode LSE_MODE = LSE_MODE_;
     static constexpr SinkMode SINK_MODE = SINK_MODE_;
+    static constexpr MaskMode MASK_MODE = MASK_MODE_;
 };
 
 // For AtlasA2, FA Infer RescaleO
