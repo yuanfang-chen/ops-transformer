f6f07a73d15a03392d08f9a6fe287a18fd9dc615
yuhao_ <yuhao93@huawei.com>
2026-01-21 16:42:02 +0800

    bug fix
    Signed-off-by: yuhao_ <yuhao93@huawei.com>

--- attention/flash_attention_score/op_kernel/arch32/flash_attention_score_tiling.h
+++ attention/flash_attention_score/op_kernel/arch32/flash_attention_score_tiling.h
@@ -779,6 +779,7 @@ public:
     uint8_t implMode = 0;
     uint8_t sparseType = 0;
     uint8_t needDropMaskOp = 0;
+    uint8_t needSinkOp = 0;
     uint8_t pseEncodeType = 0;
     uint8_t rsv = 0;
     uint8_t needL1Carry = 0;
@@ -792,8 +793,7 @@ public:
     int64_t qStartIdx = 0;
     int64_t kvStartIdx = 0;
     uint8_t tndSoftmaxOut = 0;
-    uint8_t needSinkOp = 0;
-    uint8_t InputParamsPH[6] = {0};
+    uint8_t InputParamsPH[7] = {0};
 
     // ========================
     // Getter & Setter 方法
