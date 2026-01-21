4823971b2838a4b37af2e248431e0aa611dd10a9
何梓洋_蓝区 <heziyang5@h-partners.com>
2026-01-21 10:15:57 +0800

    delete_hccl_struct_v5

--- mc2/matmul_allto_all/op_kernel/arch32/matmul_allto_all.h
+++ mc2/matmul_allto_all/op_kernel/arch32/matmul_allto_all.h
@@ -21,7 +21,11 @@ using namespace AscendC;
 #include "kernel_operator.h"
 #include "kernel_tiling/kernel_tiling.h"
 #include "matmul_allto_all_tiling.h"
-#include "moe_distribute_base.h"
+#if __has_include("../../common/inc/kernel/moe_distribute_base.h")
+#include "../../common/inc/kernel/moe_distribute_base.h"
+#else
+#include "../../../common/inc/kernel/moe_distribute_base.h"
+#endif
 #include "matmul_allto_all_util.h"
 
 #include "../../3rd/template_linear_algebra/include/template_linear_algebra/catlass.hpp"
