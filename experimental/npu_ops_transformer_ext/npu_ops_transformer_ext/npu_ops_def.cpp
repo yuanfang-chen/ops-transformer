995f14111fd502165bf940258e2b8daad26ee30e
huang-chuhong <huangchuhong1@h-partners.com>
2026-01-22 15:06:49 +0800

    同步 exp/npu

--- experimental/npu_ops_transformer_ext/npu_ops_transformer_ext/npu_ops_def.cpp
+++ experimental/npu_ops_transformer_ext/npu_ops_transformer_ext/npu_ops_def.cpp
@@ -36,8 +36,8 @@ namespace npu_ops_transformer_ext {
 
 TORCH_LIBRARY(npu_ops_transformer_ext, m)
 {
-    // set def like the rule below: m.def("dummy(Tensor x) -> Tensor");
-    m.def("rope_matrix(Tensor x, Tensor y, Tensor sin, Tensor cos) -> Tensor");
+     // set def like the rule below: m.def("dummy(Tensor x) -> Tensor");	 
+     m.def("rope_matrix(Tensor x, Tensor y, Tensor sin, Tensor cos) -> Tensor");
 }
 
 } // namespace npu_ops_transformer_ext
