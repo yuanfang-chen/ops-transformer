70854d78df7af8244145edb875c58895edd19e78
kknan <xukenan@huawei.com>
2026-01-23 11:14:38 +0800

    sycn dev code

--- gmm/grouped_matmul_swiglu_quant_v2/op_kernel/grouped_matmul_swiglu_quant_v2_apt.cpp
+++ gmm/grouped_matmul_swiglu_quant_v2/op_kernel/grouped_matmul_swiglu_quant_v2_apt.cpp
@@ -19,30 +19,33 @@
 #include "lib/matmul_intf.h"
 #include "arch35/grouped_matmul_swiglu_quant_v2_mxquant.h"
 #include "arch35/grouped_matmul_swiglu_quant_v2_tiling_key.h"
+
+#define FLOAT_OVERFLOW_MODE_CTRL 60
+
 using namespace AscendC;
 using namespace matmul;
 
 template <int8_t QUANT_B_TRANS, int8_t QUANT_A_TRANS, int8_t KERNEL_TYPE>
-__global__ __aicore__ void grouped_matmul_swiglu_quant_v2(GM_ADDR x, GM_ADDR xScale, GM_ADDR groupList,
-                                                                     GM_ADDR weight, GM_ADDR weightScale,
-                                                                     GM_ADDR weightAssistanceMatrix, GM_ADDR bias,
-                                                                     GM_ADDR smoothScale, GM_ADDR y, GM_ADDR yScale,
-                                                                     GM_ADDR workspace, GM_ADDR tiling)
+__global__ __aicore__ void grouped_matmul_swiglu_quant_v2(GM_ADDR x, GM_ADDR xScale, GM_ADDR groupList, GM_ADDR weight,
+                                                          GM_ADDR weightScale, GM_ADDR weightAssistanceMatrix,
+                                                          GM_ADDR bias, GM_ADDR smoothScale, GM_ADDR y, GM_ADDR yScale,
+                                                          GM_ADDR workspace, GM_ADDR tiling)
 {
     TPipe tPipe;
     GM_ADDR userWorkspace = GetUserWorkspace(workspace);
-
-    if (QUANT_B_TRANS == GMM_SWIGLU_QUANT_NO_TRANS && QUANT_A_TRANS == GMM_SWIGLU_QUANT_NO_TRANS
-        && KERNEL_TYPE == GMM_SWIGLU_QUANT_DEQUANT_FIXP) { // transX = false, transW = false
-        GmmSwigluAswt<Act::Gemm::layout::RowMajor, Act::Gemm::layout::RowMajor>(x, weight, weightScale,
-                                                                                xScale, weightAssistanceMatrix,
-                                                                                smoothScale, groupList, y,
-                                                                                yScale, workspace, tiling);
-    } else if (QUANT_B_TRANS == GMM_SWIGLU_QUANT_TRANS && QUANT_A_TRANS == GMM_SWIGLU_QUANT_NO_TRANS
-        && KERNEL_TYPE == GMM_SWIGLU_QUANT_DEQUANT_FIXP) { // transX = false, transW = true
-        GmmSwigluAswt<Act::Gemm::layout::RowMajor, Act::Gemm::layout::ColumnMajor>(x, weight, weightScale,
-                                                                                   xScale, weightAssistanceMatrix,
-                                                                                   smoothScale, groupList, y,
-                                                                                   yScale, workspace, tiling);
+    int64_t oriOverflowMode = AscendC::GetCtrlSpr<FLOAT_OVERFLOW_MODE_CTRL, FLOAT_OVERFLOW_MODE_CTRL>();
+    // enable overflow mode to avoid nan/inf value
+    AscendC::SetCtrlSpr<FLOAT_OVERFLOW_MODE_CTRL, FLOAT_OVERFLOW_MODE_CTRL>(0);
+    if (QUANT_B_TRANS == GMM_SWIGLU_QUANT_NO_TRANS && QUANT_A_TRANS == GMM_SWIGLU_QUANT_NO_TRANS &&
+        KERNEL_TYPE == GMM_SWIGLU_QUANT_DEQUANT_FIXP) { // transX = false, transW = false
+        GmmSwigluAswt<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::RowMajor>(
+            x, weight, weightScale, xScale, weightAssistanceMatrix, smoothScale, groupList, y, yScale, workspace,
+            tiling);
+    } else if (QUANT_B_TRANS == GMM_SWIGLU_QUANT_TRANS && QUANT_A_TRANS == GMM_SWIGLU_QUANT_NO_TRANS &&
+               KERNEL_TYPE == GMM_SWIGLU_QUANT_DEQUANT_FIXP) { // transX = false, transW = true
+        GmmSwigluAswt<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::ColumnMajor>(
+            x, weight, weightScale, xScale, weightAssistanceMatrix, smoothScale, groupList, y, yScale, workspace,
+            tiling);
     }
+    AscendC::SetCtrlSpr<FLOAT_OVERFLOW_MODE_CTRL, FLOAT_OVERFLOW_MODE_CTRL>(oriOverflowMode);
 }
