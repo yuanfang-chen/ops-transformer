fce90a55d323a22ac0a13dd9fd6e04cea5ce41f1
qq_32807861 <handongchen2@huawei.com>
2026-01-24 15:59:09 +0800

    kv_quant_metadata适配新接口

--- experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_api/aclnn_kv_quant_sparse_attn_sharedkv_metadata.h
+++ experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_api/aclnn_kv_quant_sparse_attn_sharedkv_metadata.h
@@ -20,7 +20,11 @@ extern "C" {
 
 __attribute__((visibility("default"))) aclnnStatus
 aclnnKVQuantSparseAttnSharedkvMetadataGetWorkspaceSize(
+    const aclTensor* q,
     const aclTensor* cuSeqLensQOptional,
+    const aclTensor* cuSeqLensOriKvOptional,
+    const aclTensor* cuSeqLensCmpKvOptional,
+    const aclTensor* sequsedQOptional,
     const aclTensor* sequsedKvOptional,
     int64_t numHeadsQ,
     int64_t numHeadsKv,
@@ -28,7 +32,11 @@ aclnnKVQuantSparseAttnSharedkvMetadataGetWorkspaceSize(
     int64_t batchSizeOptional,
     int64_t maxSeqlenQOptional,
     int64_t maxSeqlenKvOptional,
-    int64_t topKOptional,
+    int64_t oriTopKOptional,
+    int64_t cmpTopKOptional,
+    int64_t kv_quant_mode,
+    int64_t tile_size,
+    int64_t rope_head_dim,
     int64_t cmpRatioOptional,
     int64_t oriMaskModeOptional,
     int64_t cmpMaskModeOptional,
