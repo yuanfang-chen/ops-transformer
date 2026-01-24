1d728b73d754c32b9655f4c3009cd25622376bd7
shuangxili <shuangxili@noreply.gitcode.com>
2026-01-24 16:19:04 +0800

    attention worker

---
+++ attention/attention_worker_combine/op_host/op_tiling/attention_worker_combine_tiling.h
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file attention_worker_combine_tiling.h
+ * \brief
+ */
+#ifndef OPS_BUILT_IN_OP_TILING_RUNTIME_ATTENTION_WORKER_COMBINE_H_
+#define OPS_BUILT_IN_OP_TILING_RUNTIME_ATTENTION_WORKER_COMBINE_H_
+
+#include "log/log.h"
+#include "platform/platform_info.h"
+#include "register/op_impl_registry.h"
+#include "tiling_base/tiling_base.h"
+#include "tiling_base/tiling_templates_registry.h"
+#include "util/math_util.h"
+#include "util/platform_util.h"
+#include "util/shape_util.h"
+#include "attention/attention_update/op_host/decode_update_tiling.h"
+
+namespace optiling {
+BEGIN_TILING_DATA_DEF(AttentionWorkerCombineTilingData)
+  TILING_DATA_FIELD_DEF(int64_t, usedCoreNum); // 使用的核数
+  TILING_DATA_FIELD_DEF(int64_t, BS); // batchSize
+  TILING_DATA_FIELD_DEF(int64_t, K); // TopK 选出的专家数
+  TILING_DATA_FIELD_DEF(int64_t, H); // hiddenSize
+  TILING_DATA_FIELD_DEF(int64_t, needSchedule); // 是否需要扫描 schedule_context
+  TILING_DATA_FIELD_DEF(int64_t, BsSplitFactor); // bs轴核内切分
+  TILING_DATA_FIELD_DEF(int64_t, BsSplitCoreNum); // bs轴核间切分
+  TILING_DATA_FIELD_DEF(int64_t, mainCoreBsLoopNum); // 主核 bs 循环次数
+  TILING_DATA_FIELD_DEF(int64_t, tailCoreBsLoopNum); // 尾核 bs 循环次数
+  TILING_DATA_FIELD_DEF(int64_t, HSplitFactor); // hidden 轴核内切分
+  TILING_DATA_FIELD_DEF(int64_t, HSplitTailFactor); // hidden 轴核内尾块
+  TILING_DATA_FIELD_DEF(int64_t, HSplitCoreNum); // hidden 轴核间切分
+  TILING_DATA_FIELD_DEF(int64_t, mainCoreHLoopNum); // 主核 H 循环次数
+  TILING_DATA_FIELD_DEF(int64_t, tailCoreHLoopNum); // 尾核 H 循环次数
+  TILING_DATA_FIELD_DEF(int64_t, KSplitFactor); // k 轴核内切分
+  TILING_DATA_FIELD_DEF(int64_t, KSplitTailFactor); // k 轴尾块切分
+  TILING_DATA_FIELD_DEF(int64_t, KSplitLoopNum); // k 轴循环次数
+END_TILING_DATA_DEF;
+
+REGISTER_TILING_DATA_CLASS(AttentionWorkerCombine, AttentionWorkerCombineTilingData)
+
+struct AttentionWorkerCombineCompileInfo {
+  int64_t coreNum = 0;
+  int64_t ubSize = 0;
+};
+
+class AttentionWorkerCombineTiling : public TilingBaseClass {
+  public:
+    explicit AttentionWorkerCombineTiling(gert::TilingContext* context_) : TilingBaseClass(context_) {
+    }
+    ~AttentionWorkerCombineTiling() override {
+    }
+
+    uint64_t coreNum_ = 0;
+    uint64_t ubSize_ = 0;
+  
+  protected:
+    bool IsCapable() override;
+    ge::graphStatus DoOpTiling() override;
+    ge::graphStatus DoLibApiTiling() override;
+    uint64_t GetTilingKey() const override;
+    ge::graphStatus GetPlatformInfo() override;
+    ge::graphStatus GetShapeAttrsInfo() override;
+    ge::graphStatus GetWorkspaceSize() override;
+    ge::graphStatus PostTiling() override;
+
+  void DoOpTilingFullK(int64_t batchSize, int64_t bsCoreNum, int64_t hAlign, int64_t kIn);
+  void SelectBestHCore(int64_t hAlign, int64_t hInSplitK, int64_t &bsCoreNum, int64_t &lastCoreNum, int64_t &lastBestHCore);
+
+private:
+  int64_t tokenDtype_ = 0;
+  AttentionWorkerCombineTilingData tilingData_;
+};
+
+}  // namespace optiling
+
+#endif  // OPS_BUILT_IN_OP_TILING_RUNTIME_ATTENTION_WORKER_COMBINE_H_
---
+++ ffn/ffn_worker_batching/README.md
@@ -0,0 +1,116 @@
+# SwinAttentionFFN
+
+## 产品支持情况
+
+<table style="undefined;table-layout: fixed; width: 700px"><colgroup>
+<col style="width: 600px">
+<col style="width: 100px">
+</colgroup>
+<thead>
+  <tr>
+    <th style="text-align: center;">产品</th>
+    <th style="text-align: center;">是否支持</th>
+  </tr></thead>
+<tbody>
+  <tr>
+    <td>Ascend 950PR/Ascend 950DT</td>
+    <td style="text-align: center;">×</td>
+  </tr>
+  <tr>
+    <td>Atlas A3 训练系列产品/Atlas A3 推理系列产品</td>
+    <td style="text-align: center;">×</td>
+  </tr>
+  <tr>
+    <td>Atlas A2 训练系列产品/Atlas A2 推理系列产品</td>
+    <td style="text-align: center;">√</td>
+  </tr>
+  <tr>
+    <td>Atlas 200I/500 A2 推理产品</td>
+    <td style="text-align: center;">×</td>
+  </tr>
+  <tr>
+    <td>Atlas 推理系列加速卡产品</td>
+    <td style="text-align: center;">×</td>
+  </tr>
+  <tr>
+    <td>Atlas 训练系列产品</td>
+    <td style="text-align: center;">×</td>
+  </tr>
+</tbody>
+</table>
+
+## 功能说明
+
+- 算子功能：全量推理场景的FlashAttention算子，支持sparse优化、支持actualSeqLengthsKv优化、支持int8量化功能，支持高精度或者高性能模式选择。
+
+- 计算公式：
+
+
+    $$
+    y=x1*x2+bias +x3
+    $$
+
+
+## 参数说明
+
+<table style="undefined;table-layout: fixed; width: 900px"><colgroup>
+<col style="width: 180px">
+<col style="width: 120px">
+<col style="width: 200px">
+<col style="width: 300px">
+<col style="width: 100px">
+</colgroup>
+<thead>
+  <tr>
+    <th>参数名</th>
+    <th>输入/输出</th>
+    <th>描述</th>
+    <th>数据类型</th>
+    <th>数据格式</th>
+  </tr></thead>
+<tbody>
+  <tr>
+    <td>x1</td>
+    <td>输入</td>
+    <td>公式中的输入Q。</td>
+    <td>FLOAT16</td>
+    <td>ND</td>
+  </tr>
+  <tr>
+    <td>x2</td>
+    <td>输入</td>
+    <td>公式中的输入K。</td>
+    <td>FLOAT16</td>
+    <td>ND</td>
+  </tr>
+  <tr>
+    <td>bias</td>
+    <td>输入</td>
+    <td>公式中的输入V。</td>
+    <td>FLOAT16</td>
+    <td>ND</td>
+  </tr>
+  <tr>
+    <td>x3</td>
+    <td>输入</td>
+    <td>公式中的输入V。</td>
+    <td>FLOAT16</td>
+    <td>ND</td>
+  </tr>  
+  <tr>
+    <td>y</td>
+    <td>输出</td>
+    <td>公式中的输出。</td>
+    <td>FLOAT16</td>
+    <td>ND</td>
+  </tr>
+</tbody>
+</table>
+
+
+
+## 约束说明
+
+  当前不支持用户直接调用
+
+
