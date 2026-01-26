1011643c95a92850ca4e0a656b1fda4d557066f1
江波 <jiangbo74@huawei.com>
2026-01-26 09:39:21 +0800

    addMatmulReduceScatterV2

--- mc2/matmul_reduce_scatter/op_host/op_tiling/matmul_reduce_scatter_tiling.cpp
+++ mc2/matmul_reduce_scatter/op_host/op_tiling/matmul_reduce_scatter_tiling.cpp
@@ -21,7 +21,7 @@
 #include "mc2_hcom_topo_info.h"
 #include "util/math_util.h"
 #include "tiling/matmul_formulaic_tiling.h"
-#include "reduce_scatter_formulaic_tiling.h"
+#include "matmul_reduce_scatter_v2/op_host/op_tiling/reduce_scatter_formulaic_tiling.h"
 #include "../../op_kernel/matmul_reduce_scatter_tiling_key.h"
 #include "../../op_kernel/matmul_reduce_scatter_tiling.h"
 
@@ -476,7 +476,7 @@ struct KFCMsgBody {
     HcclAicpuOpParam msgRcvArea[mc2tiling::AC_MAX_AIV][mc2tiling::AC_MSG_CNT];
 };
 
-static void GetTilingKey(uint64_t& tilingKey, const MatmulReduceScatterTilingData& tilingData)  
+static void GetTilingKey(uint64_t& tilingKey, const MatmulReduceScatterTilingData& tilingData)
 { 
     bool mmReduceScatterFullMesh = true;
     bool mmReduceScatterNd2nzOpt = false;
