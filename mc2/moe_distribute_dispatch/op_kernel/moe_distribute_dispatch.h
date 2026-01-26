837b0a27e5d2dc8163340284ec881922bc520998
weixin_49300047 <lijianhua56@huawei.com>
2026-01-26 21:56:30 +0800

    Replenish

--- mc2/moe_distribute_dispatch/op_kernel/moe_distribute_dispatch.h
+++ mc2/moe_distribute_dispatch/op_kernel/moe_distribute_dispatch.h
@@ -43,13 +43,6 @@ constexpr uint64_t WIN_STATE_OFFSET = 512 * 1024;
 constexpr uint64_t STATE_WIN_OFFSET = 900 * 1024;
 constexpr uint32_t TP_STATE_SIZE = 100 * 1024;
 
-template<AscendC::HardEvent event>
-__aicore__ inline void SyncFunc() {
-    int32_t eventID = static_cast<int32_t>(GetTPipePtr()->FetchEventID(event));
-    AscendC::SetFlag<event>(eventID);
-    AscendC::WaitFlag<event>(eventID);
-}
-
 #define TemplateMC2TypeClass \
     typename XType, \
     typename ExpandXOutType, \
