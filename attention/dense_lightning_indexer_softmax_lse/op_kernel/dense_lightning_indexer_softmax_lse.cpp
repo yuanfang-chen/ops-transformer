28273be96d8a304e0d1a69c3585d1aef394832ba
shilulu <shilulu8@huawei.com>
2026-01-26 10:01:16 +0800

    修改DenseLightningIndexerSoftmaxLse tiling key获取方式

--- attention/dense_lightning_indexer_softmax_lse/op_kernel/dense_lightning_indexer_softmax_lse.cpp
+++ attention/dense_lightning_indexer_softmax_lse/op_kernel/dense_lightning_indexer_softmax_lse.cpp
@@ -14,15 +14,16 @@
  */
 
 #include "dense_lightning_indexer_softmax_lse.h"
+#include "dense_lightning_indexer_softmax_lse_tiling_key.h"
 #include "dense_lightning_indexer_softmax_lse_common.h"
 
 using namespace DenseLISoftmaxLseKernel;
 using namespace DenseLISoftmaxLseCommon;
 using namespace AscendC;
 
-#define DENSE_LI_SOFTMAX_OP_IMPL(templateClass, ...)                                          \
+#define DENSE_LI_SOFTMAX_LSE_OP_IMPL(templateClass, ...)                            \
     do {                                                                                      \
-        templateClass<DenseLISoftmaxLseCommon::DenseLISoftmaxLseType<__VA_ARGS__>> op;              \
+        templateClass<DenseLISoftmaxLseType<__VA_ARGS__>> op;                                    \
         GET_TILING_DATA_WITH_STRUCT(DenseLISoftmaxLseTilingData, tiling_data_in, tiling);        \
         const DenseLISoftmaxLseTilingData *__restrict tiling_data = &tiling_data_in;             \
         op.Init(query, key, weights, actualSeqLengthsQ, actualSeqLengths, softmaxMax,         \
@@ -30,7 +31,8 @@ using namespace AscendC;
         op.Process();                                                                         \
     } while (0)
 
-extern "C" __global__ __aicore__ void dense_lightning_indexer_softmax_lse(__gm__ uint8_t *query, __gm__ uint8_t *key,
+template <int DT_Q, int DT_K, int DT_OUT, int LAYOUT_T>
+__global__ __aicore__ void dense_lightning_indexer_softmax_lse(__gm__ uint8_t *query, __gm__ uint8_t *key,
                                                            __gm__ uint8_t *weights, __gm__ uint8_t *actualSeqLengthsQ,
                                                            __gm__ uint8_t *actualSeqLengths, __gm__ uint8_t *softmaxMax,
                                                            __gm__ uint8_t *softmaxSum, __gm__ uint8_t *workspace,
@@ -42,14 +44,11 @@ extern "C" __global__ __aicore__ void dense_lightning_indexer_softmax_lse(__gm__
     TPipe tPipe;
     __gm__ uint8_t *user = GetUserWorkspace(workspace);
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
-    if (TILING_KEY_IS(0)) {
-        DENSE_LI_SOFTMAX_OP_IMPL(DenseLISoftmaxLse, half, half, float, LAYOUT::BSND);
-    } else if (TILING_KEY_IS(1)) {
-        DENSE_LI_SOFTMAX_OP_IMPL(DenseLISoftmaxLse, bfloat16_t, bfloat16_t, float, LAYOUT::BSND);
-    } else  if (TILING_KEY_IS(10)) {
-        DENSE_LI_SOFTMAX_OP_IMPL(DenseLISoftmaxLse, half, half, float, LAYOUT::TND);
-    } else if (TILING_KEY_IS(11)) {
-        DENSE_LI_SOFTMAX_OP_IMPL(DenseLISoftmaxLse, bfloat16_t, bfloat16_t, float, LAYOUT::TND);
+
+    if constexpr (DT_Q == TPL_FP16 && DT_K == TPL_FP16 && DT_OUT == TPL_FP32) {
+        DENSE_LI_SOFTMAX_LSE_OP_IMPL(DenseLISoftmaxLse, half, half, float, LAYOUT(LAYOUT_T));
+    } else {
+        DENSE_LI_SOFTMAX_LSE_OP_IMPL(DenseLISoftmaxLse, bfloat16_t, bfloat16_t, float, LAYOUT(LAYOUT_T));
     }
 #endif
 }
