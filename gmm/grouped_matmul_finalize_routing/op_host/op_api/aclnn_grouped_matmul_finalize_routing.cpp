85a51e37caf0678221307251e7b70cb0667d565b
tooslow <zhangzhizhuo1@huawei.com>
2026-01-27 14:34:20 +0800

    修改910_95

--- gmm/grouped_matmul_finalize_routing/op_host/op_api/aclnn_grouped_matmul_finalize_routing.cpp
+++ gmm/grouped_matmul_finalize_routing/op_host/op_api/aclnn_grouped_matmul_finalize_routing.cpp
@@ -26,7 +26,7 @@
 #include "aclnn_kernels/transpose.h"
 #include "aclnn_kernels/contiguous.h"
 #include "aclnn_kernels/reshape.h"
-#include "aclnn_grouped_matmul_finalize_routing_910_95_checker.h"
+#include "aclnn_grouped_matmul_finalize_routing_MX_checker.h"
 #include "../../../grouped_matmul/op_host/op_api/aclnn_grouped_matmul_910_95_checker.h"
 
 using namespace op;
