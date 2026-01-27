2c772fbb4783ea35f92eb08ec1dcb4c9727ff0e5
Hou_jialin <houjialin3@huawei.com>
2026-01-27 15:20:44 +0800

    transformer support aicpu kernel compile
    Co-authored-by: Hou_jialin<houjialin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1068 merge br_aicpu_support into master
    
    transformer support aicpu kernel compile
    
    Created-by: Hou_jialin
    Commit-by: Hou_jialin
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    - ops-transformer仓支持aicpu算子kernel编译工程能力
    - aicpu算子样例add_example算子的op_kernel_aicpu新增
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    NA
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1068

--- cmake/modules/Findaicpu.cmake
+++ cmake/modules/Findaicpu.cmake
@@ -18,6 +18,7 @@ include(FindPackageHandleStandardArgs)
 if(BUILD_WITH_INSTALLED_DEPENDENCY_CANN_PKG)
   set(AICPU_INC_DIRS
     ${ASCEND_DIR}/${SYSTEM_PREFIX}/include/experiment
+    ${ASCEND_DIR}/${SYSTEM_PREFIX}/include/experiment/cce
     ${ASCEND_DIR}/${SYSTEM_PREFIX}/include/experiment/msprof
     ${ASCEND_DIR}/${SYSTEM_PREFIX}/pkg_inc/aicpu_common/context
     ${ASCEND_DIR}/${SYSTEM_PREFIX}/pkg_inc/aicpu_common/context/common
---
+++ cmake/third_party/eigen.cmake
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# ---------------------------------------------------------------------------------------------------------
+if(POLICY CMP0135)
+    cmake_policy(SET CMP0135 NEW)
+endif()
+
+if (IS_DIRECTORY "${CANN_3RD_LIB_PATH}/eigen")
+    set(REQ_URL "${CANN_3RD_LIB_PATH}/eigen")
+else()
+    set(REQ_URL "https://gitcode.com/cann-src-third-party/eigen/releases/download/3.4.0/eigen-3.4.0.tar.gz")
+endif()
+
+include(ExternalProject)
+ExternalProject_Add(external_eigen_transformer
+        URL               ${REQ_URL}
+        DOWNLOAD_DIR      download/eigen
+        PREFIX            third_party
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND     ""
+        INSTALL_COMMAND   ""
+)
+
+ExternalProject_Get_Property(external_eigen_transformer SOURCE_DIR)
+
+add_library(EigenTransformer INTERFACE)
+target_compile_options(EigenTransformer INTERFACE -w)
+
+set_target_properties(EigenTransformer PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${SOURCE_DIR}"
+)
+add_dependencies(EigenTransformer external_eigen_transformer)
+
+add_library(Eigen3::EigenTransformer ALIAS EigenTransformer)
