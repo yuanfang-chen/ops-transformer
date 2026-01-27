e9ac348ce67c372c3782a640f7375a4939a776fa
fengxingpan1 <fengxingpan@h-partners.com>
2026-01-27 19:40:12 +0800

    fix

--- gmm/grouped_matmul/op_host/op_api/aclnn_grouped_matmul.cpp
+++ gmm/grouped_matmul/op_host/op_api/aclnn_grouped_matmul.cpp
@@ -709,9 +709,7 @@ static aclnnStatus CheckNoQuantUnusedParams(const gmm::GroupedMatmulParams &gmmP
 static aclnnStatus CheckNonQuantMatmulDataType(const gmm::GroupedMatmulParams &gmmParams, const DataType weightDtype) {
   DataType biasDtype = gmmParams.xDtype == DataType::DT_BF16 ? DataType::DT_FLOAT : gmmParams.xDtype;
   // 910_95支持bf16的bias
-  if (op::GetCurrentPlatformInfo().GetCurNpuArch() == NpuArch::DAV_3510) {
-    CHECK_COND(gmmParams.xDtype != DataType::DT_FLOAT, ACLNN_ERR_PARAM_INVALID,
-               "float32 do not supported on Ascend910_95");
+  if (GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910_95) {
     if (gmmParams.biasOptional != nullptr) {
       biasDtype = (*gmmParams.biasOptional)[0]->GetDataType();
       CHECK_COND(biasDtype == gmmParams.xDtype || biasDtype == DataType::DT_FLOAT, ACLNN_ERR_PARAM_INVALID,
