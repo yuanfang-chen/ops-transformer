703d5e610dae5efdf855510ea1741ad399aedc00
dingfeng <dingfeng18@huawei.com>
2026-01-27 15:08:37 +0800

    fix getseqlen

--- experimental/attention/compressor/op_kernel/compressor_kernel.h
+++ experimental/attention/compressor/op_kernel/compressor_kernel.h
@@ -460,7 +460,7 @@ __aicore__ inline void CompressorKernel<COMP>::CalcParams(RunInfo &info) {
     info.scStart = curScEnd;
 
     // sEnd到了seq末尾，切换到下一个batch
-    curActSeqLength = GetSeqLength(curBStart);
+    GetSeqLength(curBStart);
     // 跳batch处理，batch=0且seq=0时不跳
     if (curSStart == curActSeqLength && (curBStart != 0 || curActSeqLength != 0)) {
         curBStart++;
