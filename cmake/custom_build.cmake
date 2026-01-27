20e5441b1f865ae39e97de383334003e780b2d9c
xubinglin <xubinglin@huawei.com>
2026-01-27 21:50:25 +0800

    fix: remove common/act
    Co-authored-by: x00444734<xubinglin@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1086 merge feature_remove_common_act into master
    
    fix: remove common/act
    
    Created-by: xubinglin
    Commit-by: x00444734
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    删除common/act
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    [#588](https://gitcode.com/cann/ops-transformer/issues/588) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    自验证
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1086

--- cmake/custom_build.cmake
+++ cmake/custom_build.cmake
@@ -695,6 +695,9 @@ install(DIRECTORY ${OPS_ADV_UTILS_KERNEL_INC}/
         DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common
 )
 
+install(DIRECTORY ${OPS_ADV_DIR}/gmm/common/cgmct
+        DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common
+)
 install(DIRECTORY ${OPS_ADV_DIR}/mc2/common/inc/kernel
         DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common/inc
 )
