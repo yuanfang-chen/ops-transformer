3226617787245db2ad3d618078377bc961f7578f
Le_666 <yuanle2@huawei.com>
2026-01-28 17:14:49 +0800

    fix error

--- experimental/attention/compressor/op_host/compressor_def.cpp
+++ experimental/attention/compressor/op_host/compressor_def.cpp
@@ -13,10 +13,10 @@
 namespace ops {
 class Compressor : public OpDef {
 public:
-    constexpr uint32_t ROPE_HEAD_DIM_VALUE = 64;
-    constexpr uint32_t CMP_RATIO_VALUE = 4;
-    constexpr uint32_t COFF_VALUE = 1;
-    constexpr uint32_t ROTARY_MODE_VALUE = 1;
+    static constexpr uint32_t ROPE_HEAD_DIM_VALUE = 64;
+    static constexpr uint32_t CMP_RATIO_VALUE = 4;
+    static constexpr uint32_t COFF_VALUE = 1;
+    static constexpr uint32_t ROTARY_MODE_VALUE = 1;
 
     explicit Compressor(const char *name) : OpDef(name)
     {
