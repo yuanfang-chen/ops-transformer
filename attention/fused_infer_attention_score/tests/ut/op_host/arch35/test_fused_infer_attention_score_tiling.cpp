f7a6290d2929f85b01322faeefbaa9772243c5fd
arcticius <hanyufei4@huawei.com>
2026-01-28 18:02:06 +0800

    Decode MLA支持TND_NTD

--- attention/fused_infer_attention_score/tests/ut/op_host/arch35/test_fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/tests/ut/op_host/arch35/test_fused_infer_attention_score_tiling.cpp
@@ -816,7 +816,7 @@ TEST_F(FusedInferAttentionScoreTiling, FusedInferAttentionScoreTiling_tiling_11)
             {"out_dtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
         },
         &compileInfo, "Ascend910_95", 64, 262144, 16384);
-    int64_t expectTilingKey = 132382980;
+    int64_t expectTilingKey = 400819204;
     std::string expectTilingData = "";
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData);
 }
