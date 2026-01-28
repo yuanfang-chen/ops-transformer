509a8a95c2fce56abf2028137c59d028214b6170
xujiachen8 <xujiachen8@h-partners.com>
2026-01-28 17:11:02 +0800

    修复MXFP8量化Cast时SAT_MODE不生效导致的精度差异

--- moe/moe_init_routing_v3/op_kernel/arch35/moe_v3_common.h
+++ moe/moe_init_routing_v3/op_kernel/arch35/moe_v3_common.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -20,7 +20,10 @@
 
 namespace MoeInitRoutingV3 {
 using namespace AscendC;
+
 constexpr int64_t SIMT_THREAD_NUM = 2048;
+constexpr int8_t OVERFLOW_MODE_CTRL = 60;
+
 constexpr int64_t SPLIT_N = 0;
 constexpr int64_t SPLIT_K = 1;
 constexpr float MIN_FP32 = -3.4e38f;
