f5c828321e9cc9677e376bbd04065d7bd1d410ac
huang-chuhong <huangchuhong1@h-partners.com>
2026-01-28 10:54:41 +0800

    pip

--- install_deps.sh
+++ install_deps.sh
@@ -142,7 +142,10 @@ install_python_deps() {
         tornado \
         absl-py \
         "decorator>=5.1.0" \
-        --quiet --timeout=60
+        attrs \
+        jinja2 \
+        --timeout=60
+        -i https://pypi.tuna.tsinghua.edu.cn/simple
     echo "CANN Python dependencies installed."
 }
 
