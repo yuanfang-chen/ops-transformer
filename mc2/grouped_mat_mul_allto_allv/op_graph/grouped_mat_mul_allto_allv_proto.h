e49ed6d9c28fdcc1a299bb73e356df5053a2fc23
liusixia <liusixia@h-partners.com>
2026-01-29 17:24:06 +0800

    MC2 GEIR from fusion ops to proto

--- mc2/grouped_mat_mul_allto_allv/op_graph/grouped_mat_mul_allto_allv_proto.h
+++ mc2/grouped_mat_mul_allto_allv/op_graph/grouped_mat_mul_allto_allv_proto.h
@@ -9,11 +9,11 @@
  */
 
 /*!
- * \file fusion_ops.h
+ * \file grouped_mat_mul_allto_allv_proto.h
  * \brief
  */
-#ifndef OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
-#define OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
+#ifndef GROUPED_MAT_MUL_ALLTO_ALLV_PROTO_H_
+#define GROUPED_MAT_MUL_ALLTO_ALLV_PROTO_H_
 
 #include "graph/operator_reg.h"
 
@@ -65,4 +65,4 @@ REG_OP(GroupedMatMulAlltoAllv)
 }  // namespace ge
 
 
-#endif  // OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
+#endif  // GROUPED_MAT_MUL_ALLTO_ALLV_PROTO_H_
