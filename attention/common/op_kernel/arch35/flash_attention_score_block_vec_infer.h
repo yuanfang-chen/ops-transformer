db3309d4164c850c81f127ce659fd9e989402140
changgl <changguangliang1@h-partners.com>
2026-01-29 14:36:33 +0800

    MLA非量化支持sparse3，G泛化

--- attention/common/op_kernel/arch35/flash_attention_score_block_vec_infer.h
+++ attention/common/op_kernel/arch35/flash_attention_score_block_vec_infer.h
@@ -480,7 +480,25 @@ __aicore__ inline void FABlockVecInfer<TEMPLATE_ARGS>::SoftmaxLseCopyOut(
             intriParams1.dstStride = 0;
         }
     }
-    DataCopyPad(this->softmaxLseGm[runInfo.softmaxLseOffset], lseUb, intriParams1);
+    if (isMlaNoQuant && layout == LayOutTypeEnum::LAYOUT_BSH && constInfo.gSize < 32) {
+        int64_t currRowOffset = runInfo.sOuterOffset % constInfo.n2G;
+        int64_t remainDataLen = runInfo.halfS1RealSize;
+        int64_t dealDataLen = 0;
+        int64_t ubLseOffset = 0;
+        int64_t tmpSoftmaxLseOffset = runInfo.softmaxLseOffset;
+        int64_t oSoftmaxLseOffset = tmpSoftmaxLseOffset - constInfo.s1Size * (runInfo.sOuterOffset % constInfo.gSize);
+        while (remainDataLen > 0) {
+            dealDataLen = currRowOffset + remainDataLen < constInfo.n2G ? remainDataLen : constInfo.n2G - currRowOffset;
+            intriParams1.blockCount = dealDataLen;
+            DataCopyPad(this->softmaxLseGm[tmpSoftmaxLseOffset], lseUb[ubLseOffset], intriParams1);
+            remainDataLen -= dealDataLen;
+            ubLseOffset += (dealDataLen * 8);
+            currRowOffset = (currRowOffset + dealDataLen) % constInfo.n2G;
+            tmpSoftmaxLseOffset = ++oSoftmaxLseOffset;
+        }
+    } else {
+        DataCopyPad(this->softmaxLseGm[runInfo.softmaxLseOffset], lseUb, intriParams1);
+    }
     softmaxLseQueue.FreeTensor(lseUb);
 }
 
