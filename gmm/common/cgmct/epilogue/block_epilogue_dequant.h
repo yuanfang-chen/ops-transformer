a5540088d3a3cc095a4239c4a433bbfc1c117230
张磊 <zhanglei121@huawei.com>
2026-01-29 20:06:30 +0800

    adapt 3101 to 3510

--- gmm/common/cgmct/epilogue/block_epilogue_dequant.h
+++ gmm/common/cgmct/epilogue/block_epilogue_dequant.h
@@ -15,7 +15,7 @@
 
 #ifndef EPILOGUE_BLOCK_EPILOGUE_DEQUANT_H
 #define EPILOGUE_BLOCK_EPILOGUE_DEQUANT_H
-#if defined(__NPU_ARCH__) && (__NPU_ARCH__ == 3101)
+#if defined(__NPU_ARCH__) && (__NPU_ARCH__ == 3510)
 #include "kernel_basic_intf.h"
 #include "../utils/common_utils.h"
 #include "../utils/device_utils.h"
