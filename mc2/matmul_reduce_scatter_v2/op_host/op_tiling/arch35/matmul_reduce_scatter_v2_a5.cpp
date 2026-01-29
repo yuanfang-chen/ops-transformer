ad94685c8e9890bcf83da3b5a8f2aabc858ef537
Patrick Liu <liushuye@huawei.com>
2026-01-29 19:44:34 +0800

    tiling 芯片编译隔离

--- mc2/matmul_reduce_scatter_v2/op_host/op_tiling/arch35/matmul_reduce_scatter_v2_a5.cpp
+++ mc2/matmul_reduce_scatter_v2/op_host/op_tiling/arch35/matmul_reduce_scatter_v2_a5.cpp
@@ -9,7 +9,7 @@
  */
 
 /*!
- * \file matmul_reduce_scatter_v2.cpp
+ * \file matmul_reduce_scatter_v2_a5.cpp
  * \brief
  */
 #include "matmul_reduce_scatter_v2_tiling.h"
@@ -20,39 +20,30 @@
 #include "platform/platform_infos_def.h"
 
 namespace optiling {
-ge::graphStatus MatmulReduceScatterTilingV2Func(gert::TilingContext *context);
-ge::graphStatus TilingParseForMatmulReduceScatterV2(gert::TilingParseContext *context);
+ge::graphStatus MatmulReduceScatterTilingV2FuncA5(gert::TilingContext *context);
+ge::graphStatus TilingParseForMatmulReduceScatterV2A5(gert::TilingParseContext *context);
 constexpr uint32_t ATTR_COMMMODE = 10;	
 
-ge::graphStatus MatmulReduceScatterTilingV2Func(gert::TilingContext *context)
+ge::graphStatus MatmulReduceScatterTilingV2FuncA5(gert::TilingContext *context)
 {
     fe::PlatFormInfos *platformInfoPtr = context->GetPlatformInfo();
     fe::PlatFormInfos &platformInfo = *platformInfoPtr;
 
     std::string socVersion;
     (void)platformInfo.GetPlatformResWithLock("version", "Short_SoC_version", socVersion);
-    if (socVersion == "Ascend910B" || socVersion == "Ascend910_93") {
-        auto attrs = context->GetAttrs();
-        auto commModePtr = attrs->GetAttrPointer<char>(static_cast<int>(ATTR_COMMMODE));
-        OP_TILING_CHECK((commModePtr == nullptr || !(std::strcmp(commModePtr, "aiv") == 0)),
-            OP_LOGE(context->GetNodeName(), "AivModeTiling commMode is invalid. commMode is %s", commModePtr), return ge::GRAPH_FAILED);
-        if (std::strcmp(commModePtr, "aiv") == 0) {
-            return MatmulReduceScatterTilingV2AivModeFunc(context);
-        }
-    }
     return Ops::Transformer::OpTiling::TilingRegistryNew::GetInstance().DoTilingImpl(context);
 }
 
 struct MatmulReduceScatterV2CompileInfo {};
-ge::graphStatus TilingParseForMatmulReduceScatterV2(gert::TilingParseContext *context)
+ge::graphStatus TilingParseForMatmulReduceScatterV2A5(gert::TilingParseContext *context)
 {
     (void)context;
     return ge::GRAPH_SUCCESS;
 }
 
 IMPL_OP_OPTILING(MatmulReduceScatterV2)
-    .Tiling(MatmulReduceScatterTilingV2Func)
-    .TilingParse<MatmulReduceScatterV2CompileInfo>(TilingParseForMatmulReduceScatterV2);
+    .Tiling(MatmulReduceScatterTilingV2FuncA5)
+    .TilingParse<MatmulReduceScatterV2CompileInfo>(TilingParseForMatmulReduceScatterV2A5);
 
 }  // namespace optiling
 
