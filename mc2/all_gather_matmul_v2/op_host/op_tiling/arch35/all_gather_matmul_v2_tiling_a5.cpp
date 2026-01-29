2cf051cb9124a9203c3714bb51bf8910393b9b70
Patrick Liu <liushuye@huawei.com>
2026-01-29 19:44:34 +0800

    tiling 芯片编译隔离

--- mc2/all_gather_matmul_v2/op_host/op_tiling/arch35/all_gather_matmul_v2_tiling_a5.cpp
+++ mc2/all_gather_matmul_v2/op_host/op_tiling/arch35/all_gather_matmul_v2_tiling_a5.cpp
@@ -9,11 +9,10 @@
  */
 
 /*!
- * \file all_gather_matmul_v2_tiling.cpp
+ * \file all_gather_matmul_v2_tiling_a5.cpp
  * \brief
  */
 
-#include "all_gather_matmul_tiling_v2.h"
 #include "mc2_log.h"
 #include "tiling_base/tiling_templates_registry.h"
 #include "graph/utils/type_utils.h"
@@ -25,39 +24,30 @@ using namespace ge;
 
 namespace optiling
 {
-ge::graphStatus AllGatherMatmulTilingV2Func(gert::TilingContext* context);
-ge::graphStatus TilingParseForAllGatherMatmulV2(gert::TilingParseContext* context);
+ge::graphStatus AllGatherMatmulTilingV2FuncA5(gert::TilingContext* context);
+ge::graphStatus TilingParseForAllGatherMatmulV2A5(gert::TilingParseContext* context);
 constexpr uint32_t ATTR_COMMMODE = 11;
 
-ge::graphStatus AllGatherMatmulTilingV2Func(gert::TilingContext* context)
+ge::graphStatus AllGatherMatmulTilingV2FuncA5(gert::TilingContext* context)
 {
     fe::PlatFormInfos *platformInfoPtr = context->GetPlatformInfo();
     fe::PlatFormInfos &platformInfo = *platformInfoPtr;
 
     std::string socVersion;
     (void)platformInfo.GetPlatformResWithLock("version", "Short_SoC_version", socVersion);
-    if (socVersion == "Ascend910B" || socVersion == "Ascend910_93") {
-        auto attrs = context->GetAttrs();
-        auto commModePtr = attrs->GetAttrPointer<char>(static_cast<int>(ATTR_COMMMODE));
-        OP_TILING_CHECK((commModePtr == nullptr || !(std::strcmp(commModePtr, "aiv") == 0)),
-            OP_LOGE(context->GetNodeName(), "AivModeTiling commMode is invalid. commMode is %s", commModePtr), return ge::GRAPH_FAILED);
-        if (std::strcmp(commModePtr, "aiv") == 0) {
-            return AllGatherMatmulTilingAIVModeFunc(context);
-        }
-        return Ops::Transformer::OpTiling::TilingRegistryNew::GetInstance().DoTilingImpl(context);
-    }
-    return Ops::Transformer::OpTiling::TilingRegistryArch::GetInstance().DoTilingImpl(context);
+
+    return Ops::Transformer::OpTiling::TilingRegistryNew::GetInstance().DoTilingImpl(context);
 }
 
 struct AllGatherMatmulCompileInfo {
 };
-ge::graphStatus TilingParseForAllGatherMatmulV2(gert::TilingParseContext* context)
+ge::graphStatus TilingParseForAllGatherMatmulV2A5(gert::TilingParseContext* context)
 {
     (void)context;
     return ge::GRAPH_SUCCESS;
 }
 
 IMPL_OP_OPTILING(AllGatherMatmulV2)
-    .Tiling(AllGatherMatmulTilingV2Func)
-    .TilingParse<AllGatherMatmulCompileInfo>(TilingParseForAllGatherMatmulV2);
+    .Tiling(AllGatherMatmulTilingV2FuncA5)
+    .TilingParse<AllGatherMatmulCompileInfo>(TilingParseForAllGatherMatmulV2A5);
 }  // namespace optiling
