3619b1be9b3ae07f8315859db5d92228b84e0141
huangshuai <huangshuai59@huawei.com>
2026-01-29 18:55:39 +0800

    qgmmatav debug
    qgmmatav debug

--- mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv_tiling.h
+++ mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv_tiling.h
@@ -1,23 +1,24 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
 
 /*!
- * \file grouped_mat_mul_allto_allv_tiling.h
- * \brief
- */
+* \file grouped_mat_mul_allto_allv_tiling.h
+* \brief
+*/
 
 #ifndef __GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H__
 #define __GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H__
 
 #include <cstdint>
 #include "kernel_tiling/kernel_tiling.h"
+#include "../../3rd/grouped_matmul/op_kernel/arch35/grouped_matmul_tiling_data_apt.h"
 
 constexpr uint32_t MAX_EXPERT_SIZE = 512U; // 最大通信域专家的数量
 
@@ -54,5 +55,17 @@ public:
     TCubeTiling sharedExpMatmulTiling;
     GmmAlltoAllvAicpuTiling aicpuTilingInfo;
 };
+
+#pragma pack(push, 8)
+struct QuantGroupedMatMulAlltoAllvTilingData {
+    Mc2InitTiling hcclInitTiling;
+    Mc2CcTiling alltoAllvCcTiling;
+    GmmAlltoAllvCommonTilingInfo commonTilingInfo;
+    TCubeTiling matmulTiling;
+    TCubeTiling sharedExpMatmulTiling;
+    GmmAlltoAllvAicpuTiling aicpuTilingInfo;
+    GroupedMatmulTilingData::GMMQuantTilingData* gmmQuantTilingDataList;
+};
+#pragma pack(pop)
 
 #endif // __GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H__
