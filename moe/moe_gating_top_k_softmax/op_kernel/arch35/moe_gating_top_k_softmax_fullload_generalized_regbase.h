563e7a69649da2df58fe6c0145048c5066fe64ef
ZhouChuping <zhouchuping1@huawei.com>
2026-01-29 19:33:14 +0800

    moe_gating_top_k_softmax精度问题处理

--- moe/moe_gating_top_k_softmax/op_kernel/arch35/moe_gating_top_k_softmax_fullload_generalized_regbase.h
+++ moe/moe_gating_top_k_softmax/op_kernel/arch35/moe_gating_top_k_softmax_fullload_generalized_regbase.h
@@ -255,7 +255,8 @@ __aicore__ inline void MoeGatingTopKSoftmaxFullloadGenerlized<T, hasFinished, ne
                 mask = AscendC::MicroAPI::UpdateMask<int32_t>(remain);
                 offset = i * expertCountAlign_ + j * repeatCount;
                 ops::LoadOneTensorForDtypeT<T>(xTensorAddr, vreg0, mask, offset);
-                AscendC::MicroAPI::Max(reduceMidRreg, reduceMidRreg, vreg0, mask);
+                AscendC::MicroAPI::Max<float, AscendC::MicroAPI::MaskMergeMode::MERGING>(reduceMidRreg,
+                    reduceMidRreg, vreg0, mask);
             }
             remain = expertCount_;
             mask = AscendC::MicroAPI::UpdateMask<int32_t>(remain);
