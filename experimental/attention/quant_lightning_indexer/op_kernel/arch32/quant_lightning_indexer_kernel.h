deca54192695024ec6faa160bcba587443c33e9c
zzzyh22 <zhangyaohui22@huawei.com>
2026-01-29 15:35:22 +0800

    metadata解析适配

--- experimental/attention/quant_lightning_indexer/op_kernel/arch32/quant_lightning_indexer_kernel.h
+++ experimental/attention/quant_lightning_indexer/op_kernel/arch32/quant_lightning_indexer_kernel.h
@@ -59,7 +59,7 @@ public:
     __aicore__ inline QLIPreload(){};
     __aicore__ inline void Init(__gm__ uint8_t *query, __gm__ uint8_t *key, __gm__ uint8_t *weights,
                                 __gm__ uint8_t *queryScale, __gm__ uint8_t *keyScale, __gm__ uint8_t *actualSeqLengthsQ,
-                                __gm__ uint8_t *actualSeqLengthsK, __gm__ uint8_t *blockTable, const QliMetaData *__restrict metadata,
+                                __gm__ uint8_t *actualSeqLengthsK, __gm__ uint8_t *blockTable, __gm__ uint8_t *metadata,
                                 __gm__ uint8_t *sparseIndices, __gm__ uint8_t *workspace,
                                 const QLITilingData *__restrict tiling, TPipe *tPipe);
     __aicore__ inline void Process();
@@ -394,7 +394,7 @@ template <typename QLIT>
 __aicore__ inline void QLIPreload<QLIT>::Init(__gm__ uint8_t *query, __gm__ uint8_t *key, __gm__ uint8_t *weights,
                                               __gm__ uint8_t *queryScale, __gm__ uint8_t *keyScale,
                                               __gm__ uint8_t *actualSeqLengthsQ, __gm__ uint8_t *actualSeqLengthsK,
-                                              __gm__ uint8_t *blockTable, const QliMetaData *__restrict metadata, __gm__ uint8_t *sparseIndices,
+                                              __gm__ uint8_t *blockTable, __gm__ uint8_t *metadata, __gm__ uint8_t *sparseIndices,
                                               __gm__ uint8_t *workspace, const QLITilingData *__restrict tiling,
                                               TPipe *tPipe)
 {
