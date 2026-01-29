08097a454d29ed01509ba388769f55c87c16e641
liusixia <liusixia@h-partners.com>
2026-01-29 17:24:06 +0800

    MC2 GEIR from fusion ops to proto

--- mc2/moe_distribute_dispatch_v2/op_graph/moe_distribute_dispatch_v2_proto.h
+++ mc2/moe_distribute_dispatch_v2/op_graph/moe_distribute_dispatch_v2_proto.h
@@ -9,11 +9,11 @@
  */
 
 /*!
- * \file fusion_ops.h
+ * \file moe_distribute_dispatch_v2_proto.h
  * \brief
  */
-#ifndef OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
-#define OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
+#ifndef MOE_DISTRIBUTE_DISPATCH_V2_PROTO_H_
+#define MOE_DISTRIBUTE_DISPATCH_V2_PROTO_H_
 
 #include "graph/operator_reg.h"
 namespace ge {
@@ -92,4 +92,4 @@ REG_OP(MoeDistributeDispatchV2)
     .OP_END_FACTORY_REG(MoeDistributeDispatchV2)
 
 }  // namespace ge
-#endif  // OPS_BUILT_IN_OP_PROTO_INC_FUSION_OPS_H_
+#endif  // MOE_DISTRIBUTE_DISPATCH_V2_PROTO_H_
