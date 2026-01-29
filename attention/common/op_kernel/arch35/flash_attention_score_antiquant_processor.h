2275738ca4286bff84ce59c6dd5bdfcd71e86606
oreo_hw <wangyuanhe2@huawei.com>
2026-01-29 21:18:42 +0800

    fixbug

--- attention/common/op_kernel/arch35/flash_attention_score_antiquant_processor.h
+++ attention/common/op_kernel/arch35/flash_attention_score_antiquant_processor.h
@@ -263,7 +263,7 @@ __aicore__ inline void AntiquantProcessorBaseAPI<ANTIQUANT_TEMPLATE_ARGS, ANTIQU
 
         LocalTensor<Q_T> antiqScaleE8M0Ub = antiqScaleInputQue.template AllocTensor<Q_T>();
         CopyAntiqScaleE8M0Nz(antiqScaleE8M0Ub, antiqScaleGm, scaleOffset / 2, (taskParam.copyTotalS + 31) / 32 * 32, 
-                        grpNum, taskParam.antiqSeqSize);//伪量化参数32byte对齐
+                        grpNum, taskParam.antiqSeqSize);//FP8的伪量化参拷贝至UB,需32byte对齐，即32/sizeof(FP8)个参数对齐
         antiqScaleInputQue.template EnQue(antiqScaleE8M0Ub);
         antiqScaleE8M0Ub = antiqScaleInputQue.DeQue<Q_T>();
 
