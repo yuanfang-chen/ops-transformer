1050843f9a94f815484723e2593cc5514823630d
ChenPengyi <chenpengyi@huawei.com>
2026-01-29 10:02:58 +0800

    MoeInitRoutingV3:非量化场景支持hif8 & quantMode为6支持bf16/fp16输入转hif8输出

--- moe/moe_init_routing_v3/op_host/moe_init_routing_v3_infershape.cpp
+++ moe/moe_init_routing_v3/op_host/moe_init_routing_v3_infershape.cpp
@@ -16,6 +16,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <set>
 #include "register/op_def_registry.h"
 #include "log/log.h"
 #include "util/math_util.h"
@@ -58,7 +59,17 @@ enum QuantMode : int8_t {
     STATIC_QUANT = 0,
     DYNAMIC_QUANT = 1,
     MXQUANT_FP8_E5M2 = 2,
-    MXQUANT_FP8_E4M3FN = 3
+    MXQUANT_FP8_E4M3FN = 3,
+    HIF8_CAST = 6
+};
+
+const std::set<int64_t> validQuantModes = {
+    QuantMode::NON_QUANT,
+    QuantMode::STATIC_QUANT,
+    QuantMode::DYNAMIC_QUANT,
+    QuantMode::MXQUANT_FP8_E5M2,
+    QuantMode::MXQUANT_FP8_E4M3FN,
+    QuantMode::HIF8_CAST
 };
 
 enum ExpertTokenNumType : int8_t {
@@ -236,9 +247,9 @@ static ge::graphStatus GetAndCheckAttrQuantMode(const gert::RuntimeAttrs *attrs,
         return ge::GRAPH_FAILED;
     }
     quantMode = *quantModePtr;
-    if (quantMode < QuantMode::NON_QUANT || quantMode > QuantMode::MXQUANT_FP8_E4M3FN) {
-        OP_LOGE(context, "The quant_mode should be in [%d, %d]. But it is %d.", QuantMode::NON_QUANT,
-                QuantMode::NON_QUANT, QuantMode::MXQUANT_FP8_E4M3FN, quantMode);
+    if (validQuantModes.count(quantMode) == 0) {
+        OP_LOGE(context, "The quant_mode should be in [%d, %d] or %d. But it is %d.", QuantMode::NON_QUANT,
+                QuantMode::NON_QUANT, QuantMode::MXQUANT_FP8_E4M3FN, QuantMode::HIF8_CAST, quantMode);
         return ge::GRAPH_FAILED;
     }
     OP_LOGD(context, "End to do GetAndCheckQuantMode.");
@@ -285,10 +296,11 @@ static ge::graphStatus CheckInputScaleShape(gert::InferShapeContext *context, co
                 OP_LOGE(context, "The scale cannot be none when quant_mode is %ld.", quantMode),
                 return ge::GRAPH_FAILED);
 
-    //  When quant_mode is NON_QUANT/DYNAMIC_QUANT/MXQUANT_FP8_E5M2/MXQUANT_FP8_E4M3FN, scale can be none.
+    //  When quant_mode is NON_QUANT/DYNAMIC_QUANT/MXQUANT_FP8_E5M2/MXQUANT_FP8_E4M3FN/HIF8_CAST, scale can be none.
     OP_CHECK_IF((nullptr == scaleShape &&
                  (QuantMode::NON_QUANT == quantMode || QuantMode::DYNAMIC_QUANT == quantMode ||
-                  QuantMode::MXQUANT_FP8_E5M2 == quantMode || QuantMode::MXQUANT_FP8_E4M3FN == quantMode)),
+                  QuantMode::MXQUANT_FP8_E5M2 == quantMode || QuantMode::MXQUANT_FP8_E4M3FN == quantMode ||
+                  QuantMode::HIF8_CAST == quantMode)),
                 OP_LOGI(context, "When quant_mode is %ld , scale can be none.", quantMode), return ge::GRAPH_SUCCESS);
 
     if (QuantMode::NON_QUANT == quantMode) {
@@ -701,6 +713,15 @@ static ge::graphStatus InferDataType4MoeInitRoutingV3(gert::InferDataTypeContext
         }
         expandedXDtype = (QuantMode::MXQUANT_FP8_E5M2 == quantMode) ? ge::DT_FLOAT8_E5M2 : ge::DT_FLOAT8_E4M3FN;
         expandedScaleDtype = ge::DT_FLOAT8_E8M0;
+    } else if (QuantMode::HIF8_CAST == quantMode) {
+        if (xDtype != ge::DT_FLOAT16 && xDtype != ge::DT_BF16) {
+            OP_LOGE(
+                context,
+                "When quant_mode=%ld, xDtype should be DT_FLOAT16 or DT_BF16. Current got unexpected dtype id of %d.",
+                quantMode, xDtype);
+            return ge::GRAPH_FAILED;
+        }
+        expandedXDtype = ge::DT_HIFLOAT8;
     }
     context->SetOutputDataType(MOE_INIT_ROUTING_V3_OUTPUT_EXPANDED_X, expandedXDtype);
     context->SetOutputDataType(MOE_INIT_ROUTING_V3_OUTPUT_EXPANDED_ROW_IDX, ge::DT_INT32);
