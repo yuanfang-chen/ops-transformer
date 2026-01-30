86b293fc6fdb6f688b645ce6a47d801deb5d0bf0
wuziyu <wuziyu11@huawei.com>
2026-01-30 17:17:00 +0800

    add

--- mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
+++ mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
@@ -23,16 +23,18 @@ using Ops::Base::CeilDiv;
 
 constexpr size_t INDEX_IN_X1 = 0;
 constexpr size_t INDEX_IN_X2 = 1;
-constexpr size_t INDEX_ATTR_GROUP = 0;
 constexpr size_t INDEX_ATTR_WORLD_SIZE = 1;
 constexpr size_t INDEX_ATTR_Y_DTYPE = 3;
-
+constexpr size_t INDEX_ATTR_X1_QUANT_MODE = 4;
+constexpr size_t INDEX_ATTR_X2_QUANT_MODE = 5;
 constexpr size_t INDEX_ATTR_TRANS_X1 = 9;
 constexpr size_t INDEX_ATTR_TRANS_X2 = 10;
 constexpr size_t INDEX_ATTR_ALLTOALL_OUT_FLAG = 12;
 constexpr size_t INDEX_OUT = 0;
 constexpr size_t INDEX_ALLTO_ALL_OUT = 1;
 constexpr uint64_t DIM_TWO = 2;
+constexpr uint64_t X1_QUANT_NUM = 7;
+constexpr uint64_t X2_QUANT_NUM = 2;
 constexpr uint64_t NUM_MINUS_ONE = -1;
 static const char* INNER_DEBUG = "MC2: AlltoAllMatmul InferShape Debug";
 const std::set<int> SUPPORT_RANK_NUM{2, 4, 8, 16};
@@ -60,16 +62,16 @@ static ge::graphStatus CheckShapeForAlltoAllMatmul(const gert::InferShapeContext
     OPS_CHECK_NULL_WITH_CONTEXT(context, x2_shape);
     const auto attrs = context->GetAttrs();
     OPS_CHECK_NULL_WITH_CONTEXT(context, attrs);
-    const bool* is_trans_x1 = attrs->GetAttrPointer<bool>(INDEX_ATTR_TRANS_X1);
+    const bool* isTransX1 = attrs->GetAttrPointer<bool>(INDEX_ATTR_TRANS_X1);
     OPS_CHECK(
-        is_trans_x1 == nullptr || *is_trans_x1, CUBE_INNER_ERR_REPORT(context->GetNodeName(),
+        isTransX1 == nullptr || *isTransX1, CUBE_INNER_ERR_REPORT(context->GetNodeName(),
         "x1 does not support transpose in allto all matmul."), return ge::GRAPH_FAILED);
-    const bool* is_trans_x2 = attrs->GetAttrPointer<bool>(INDEX_ATTR_TRANS_X2);
-    const bool trans_x2 = ((is_trans_x2 != nullptr) && (*is_trans_x2));
+    const bool* isTransX2 = attrs->GetAttrPointer<bool>(INDEX_ATTR_TRANS_X2);
+    const bool transX2 = ((isTransX2 != nullptr) && (*isTransX2));
     shape.m = x1_shape->GetDim(0U);
     shape.k1 = x1_shape->GetDim(1U);
-    shape.n = trans_x2 ? x2_shape->GetDim(0U) : x2_shape->GetDim(1U);
-    shape.k2 = trans_x2 ? x2_shape->GetDim(1U) : x2_shape->GetDim(0U);
+    shape.n = transX2 ? x2_shape->GetDim(0U) : x2_shape->GetDim(1U);
+    shape.k2 = transX2 ? x2_shape->GetDim(1U) : x2_shape->GetDim(0U);
     shape.output_dim = x1_shape->GetDimNum();
     OP_LOGD(INNER_DEBUG, "Matmul m %ld n %ld k1 %ld k2 %ld.", shape.m, shape.n, shape.k1, shape.k2);
     return ge::GRAPH_SUCCESS;
@@ -85,7 +87,7 @@ static ge::graphStatus CheckRankDim(gert::InferShapeContext* context, AlltoAllMa
 {
     const auto attrs = context->GetAttrs();
     const int* rankDim = attrs->GetAttrPointer<int>(INDEX_ATTR_WORLD_SIZE);
-    OPS_CHECK(*rankDim == 0,
+    OPS_CHECK(rankDim == nullptr,
         CUBE_INNER_ERR_REPORT(context->GetNodeName(), "Invalid rank number %zu in matmul allto all.", *rankDim),
         return ge::GRAPH_FAILED);
     OP_TILING_CHECK(SUPPORT_RANK_NUM.find(*rankDim) == SUPPORT_RANK_NUM.end(),
@@ -119,14 +121,17 @@ static ge::graphStatus InferShapeAlltoAllMatmul(gert::InferShapeContext* context
     OPS_CHECK_NULL_WITH_CONTEXT(context, all2all_out_flag);
     auto all2all_out = context->GetOutputShape(INDEX_ALLTO_ALL_OUT);
     OPS_CHECK_NULL_WITH_CONTEXT(context, all2all_out);
+    all2all_out->SetDimNum(shape.output_dim);
     if (all2all_out_flag) {
-        all2all_out->SetDimNum(shape.output_dim);
-        uint64_t all2all_out_first_dim = CeilDiv(shape.m, shape.rankNum);
-        uint64_t all2all_out_second_dim = shape.k1 * shape.rankNum;
-        all2all_out->SetDim(0U, all2all_out_first_dim);
-        all2all_out->SetDim(1U, all2all_out_second_dim);
-        OP_LOGI(
-        INNER_DEBUG, "Allto all matmul alltoallout shape after infer shape, m: %ld n: %ld.", all2all_out_first_dim, all2all_out_second_dim);
+        if (shape.m != NUM_MINUS_ONE) {
+            uint64_t all2all_out_first_dim = CeilDiv(shape.m, shape.rankNum);
+            uint64_t all2all_out_second_dim = shape.k1 * shape.rankNum;
+            all2all_out->SetDim(0U, all2all_out_first_dim);
+            all2all_out->SetDim(1U, all2all_out_second_dim);
+            OP_LOGI(
+            INNER_DEBUG, "Allto all matmul alltoallout shape after infer shape, output_dim: %ld, m: %ld n: %ld.",
+            shape.output_dim, all2all_out_first_dim, all2all_out_second_dim);
+        }
     }
     shape_out->SetDimNum(shape.output_dim);
     if (shape.m == NUM_MINUS_ONE) {
@@ -150,16 +155,30 @@ static ge::graphStatus InferShapeAlltoAllMatmul(gert::InferShapeContext* context
 static ge::graphStatus InferDataTypeAlltoAllMatmul(gert::InferDataTypeContext* context)
 {
     OPS_CHECK(context == nullptr, OP_LOGE(INNER_DEBUG, "Context is null."), return ge::GRAPH_FAILED);
-    OP_LOGD(INNER_DEBUG, "Start to infer datatype of allto all matmul.");
-    ge::DataType y_type = context->GetOutputDataType(0U);
+    OP_LOGD(INNER_DEBUG, "Start to infer datatype of matmul allto all.");
     const auto attrs = context->GetAttrs();
     OPS_CHECK_NULL_WITH_CONTEXT(context, attrs);
+    const int* x1_quant_mode = attrs->GetAttrPointer<int>(INDEX_ATTR_X1_QUANT_MODE);
+    const int* x2_quant_mode = attrs->GetAttrPointer<int>(INDEX_ATTR_X2_QUANT_MODE);
     const int64_t* y_dtype_ptr = attrs->GetInt(INDEX_ATTR_Y_DTYPE);
-    const uint64_t y_dtype = (y_dtype_ptr != nullptr ? *y_dtype_ptr : ge::DataType::DT_UNDEFINED);
-    if (y_dtype != ge::DataType::DT_UNDEFINED) {
-        y_type = static_cast<ge::DataType>(y_dtype);
+    auto y_type = ge::DataType::DT_UNDEFINED;
+    ge::DataType x1_type = context->GetInputDataType(INDEX_IN_X1);
+    if (*x1_quant_mode == 0 && *x2_quant_mode == 0) {
+        if ((y_dtype_ptr != nullptr && *y_dtype_ptr != static_cast<uint64_t>(ge::DataType::DT_UNDEFINED))) {
+            y_type = static_cast<ge::DataType>(*y_dtype_ptr);
+        } else {
+            return ge::GRAPH_FAILED;
+        }
+    } else if (*x1_quant_mode == X1_QUANT_NUM && *x2_quant_mode == X2_QUANT_NUM) {
+        if ((y_dtype_ptr != nullptr && *y_dtype_ptr != static_cast<uint64_t>(ge::DataType::DT_UNDEFINED))) {
+            y_type = static_cast<ge::DataType>(*y_dtype_ptr);
+        } else {
+            return ge::GRAPH_FAILED;
+        }
     }
-    return context->SetOutputDataType(0U, y_type);
+    context->SetOutputDataType(0, y_type);
+    context->SetOutputDataType(1, x1_type);
+    return ge::GRAPH_SUCCESS;
 }
 
 IMPL_OP_INFERSHAPE(AlltoAllMatmul).InferShape(InferShapeAlltoAllMatmul).InferDataType(InferDataTypeAlltoAllMatmul);
