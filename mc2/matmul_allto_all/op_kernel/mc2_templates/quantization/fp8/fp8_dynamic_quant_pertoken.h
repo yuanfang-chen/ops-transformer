4224648a51420eb70def6d7f7e3822d2f7152689
adamlwang <wangjingquan2@huawei.com>
2026-01-30 15:12:44 +0800

    delete no used args

--- mc2/matmul_allto_all/op_kernel/mc2_templates/quantization/fp8/fp8_dynamic_quant_pertoken.h
+++ mc2/matmul_allto_all/op_kernel/mc2_templates/quantization/fp8/fp8_dynamic_quant_pertoken.h
@@ -1,13 +1,12 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
  * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
-BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /*!
  * \file fp8_dynamic_quant_pertoken.h
@@ -236,7 +235,6 @@ __aicore__ inline void Fp8DynamicQuantPertoken<quantInputDataType, quantOutputDa
             {1, static_cast<uint32_t>(this->colNum_ * sizeof(quantInputDataType)), 0, 0, 0}, {false, 0, 0, 0});
         SyncFunc<AscendC::HardEvent::MTE2_V>();
         __local_mem__ quantInputDataType *xAddr = (__local_mem__ quantInputDataType *)rawInputTensor.GetPhyAddr();
-        __local_mem__ quantInputDataType *smoothAddr; // 类型需要调试
         __local_mem__ quantOutputDataType *yAddr = (__local_mem__ quantOutputDataType *)quantOut.GetPhyAddr();
         __local_mem__ float *scaleAddr = (__local_mem__ float *)scaleWorkData.GetPhyAddr();
         __local_mem__ float *maxValueAddr = (__local_mem__ float *)maxValueData.GetPhyAddr();
@@ -281,9 +279,7 @@ __aicore__ inline void Fp8DynamicQuantPertoken<quantInputDataType, quantOutputDa
     __VEC_SCOPE__
     {
         AscendC::MicroAPI::RegTensor<quantInputDataType> vregX;
-        AscendC::MicroAPI::RegTensor<quantInputDataType> vregSmooth;
         AscendC::MicroAPI::RegTensor<float> vregFloatX;
-        AscendC::MicroAPI::RegTensor<float> vregSmoothFp32;
         AscendC::MicroAPI::RegTensor<float> vregAbsX;
         AscendC::MicroAPI::RegTensor<float> vregMaxAbsX;
         AscendC::MicroAPI::RegTensor<float> vregReduceMax;
@@ -352,9 +348,7 @@ __aicore__ inline void Fp8DynamicQuantPertoken<quantInputDataType, quantOutputDa
     __VEC_SCOPE__
     {
         AscendC::MicroAPI::RegTensor<quantInputDataType> vregX;
-        AscendC::MicroAPI::RegTensor<quantInputDataType> vregSmooth;
         AscendC::MicroAPI::RegTensor<float> vregFloatX;
-        AscendC::MicroAPI::RegTensor<float> vregSmoothFp32;
         AscendC::MicroAPI::RegTensor<float> vregScale;
         AscendC::MicroAPI::RegTensor<float> vregScaledX;
         AscendC::MicroAPI::RegTensor<int16_t> vregYInt16;
