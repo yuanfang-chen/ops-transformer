060f0032b742ec59bff03c537bb35ce5f25ec695
halcyon0327 <baijianyu1@huawei.com>
2026-01-30 10:15:52 +0000

    更改了api的V2、V3、V4，增加了V5，修改了kernel核函数入口的两个参数，修改了tiling的参数

--- attention/fused_infer_attention_score/op_host/op_api/aclnn_fused_infer_attention_score_v4.cpp
+++ attention/fused_infer_attention_score/op_host/op_api/aclnn_fused_infer_attention_score_v4.cpp
@@ -123,9 +123,9 @@ aclnnStatus aclnnFusedInferAttentionScoreV4GetMaxWorkspaceSize(
         valueAntiquantScaleOptional, valueAntiquantOffsetOptional, tensorKeySharedPrefixOptional,
         tensorValueSharedPrefixOptional, fakeActualSharedPrefixLenOptional, queryRopeOptional,
         keyRopeOptional, keyRopeAntiquantScaleOptional, dequantScaleQueryOptional, learnableSinkOptional, nullptr, nullptr,
-        numHeads, scaleValue, preTokens, nextTokens,
+        nullptr, numHeads, scaleValue, preTokens, nextTokens,
         inputLayout, numKeyValueHeads, sparseMode, innerPrecise, blockSize, antiquantMode, softmaxLseFlag,
-        keyAntiquantMode, valueAntiquantMode, queryQuantMode, 0, 0, attentionOut, placeHolder, workspaceSize, executor);
+        keyAntiquantMode, valueAntiquantMode, queryQuantMode, 0, 0, false, false, attentionOut, placeHolder, workspaceSize, executor);
     if (softmaxLseFlag == false) {
         aclDestroyTensor(tempTensor);
     }
@@ -190,9 +190,9 @@ aclnnStatus aclnnFusedInferAttentionScoreV4GetWorkspaceSize(
         valueAntiquantScaleOptional, valueAntiquantOffsetOptional, tensorKeySharedPrefixOptional,
         tensorValueSharedPrefixOptional, actualSharedPrefixLenOptional, queryRopeOptional,
         keyRopeOptional, keyRopeAntiquantScaleOptional, dequantScaleQueryOptional, learnableSinkOptional, nullptr, nullptr,
-        numHeads, scaleValue, preTokens, nextTokens,
+        nullptr, numHeads, scaleValue, preTokens, nextTokens,
         inputLayout, numKeyValueHeads, sparseMode, innerPrecise, blockSize, antiquantMode, softmaxLseFlag,
-        keyAntiquantMode, valueAntiquantMode, queryQuantMode, 0, 0, attentionOut, placeHolder, workspaceSize, executor);
+        keyAntiquantMode, valueAntiquantMode, queryQuantMode, 0, 0, false, false, attentionOut, placeHolder, workspaceSize, executor);
     if (softmaxLseFlag == false) {
         aclDestroyTensor(tempTensor);
     }
@@ -200,6 +200,7 @@ aclnnStatus aclnnFusedInferAttentionScoreV4GetWorkspaceSize(
         if (NnopbaseDisableOptionalInput != nullptr) {
             NnopbaseDisableOptionalInput(*executor, 29U); // 29 is input irIndex，占位符
             NnopbaseDisableOptionalInput(*executor, 30U); // 30 is input irIndex，占位符
+            NnopbaseDisableOptionalInput(*executor, 31U); // 31 is input irIndex，占位符
         }
     }
     return ret;
