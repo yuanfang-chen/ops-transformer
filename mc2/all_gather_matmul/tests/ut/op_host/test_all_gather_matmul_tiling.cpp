d1f1a5890148958d3e89408cece3fb97a61099fd
hblnb <huangbolin3@huawei.com>
2026-01-30 16:27:39 +0800

    对mc2算子的ut用例进行代际隔离

--- mc2/all_gather_matmul/tests/ut/op_host/test_all_gather_matmul_tiling.cpp
+++ mc2/all_gather_matmul/tests/ut/op_host/test_all_gather_matmul_tiling.cpp
@@ -145,7 +145,7 @@ TEST_F(AllGatherMatmulTiling, AllGatherMatmulTestTilingBfloat16)
     Mc2ExecuteTestCase(tilingContextPara, hcomTopologyMockValues, ge::GRAPH_SUCCESS, expectTilingKey);
 }
 
-TEST_F(AllGatherMatmulTiling, AllGatherMatmulTestTilingFloat16Testl2cache)
+TEST_F(AllGatherMatmulTiling, AllGatherMatmulTestTilingFloat16TestL2cache)
 {
     // tilingFunc simulate
     struct AllGatherMatmulCompileInfo {} compileInfo;
--- mc2/moe_distribute_combine_v2/tests/ut/op_host/arch32/test_moe_distribute_combine_v2_tiling.cpp
+++ mc2/moe_distribute_combine_v2/tests/ut/op_host/arch32/test_moe_distribute_combine_v2_tiling.cpp
@@ -18,11 +18,11 @@ namespace MoeDistributeCombineV2 {
 class MoeDistributeCombineV2Tiling : public testing::Test {
 protected:
     static void SetUpTestCase() {
-        std::cout << "MoeDistributeCombineV2Tiling SetUp" << std::endl;
+        std::cout << "MoeDistributeCombineV2 TilingUT910 SetUp" << std::endl;
     }
 
     static void TearDownTestCase() {
-        std::cout << "MoeDistributeCombineV2Tiling TearDown" << std::endl;
+        std::cout << "MoeDistributeCombineV2 TilingUT910 TearDown" << std::endl;
     }
 };
 
