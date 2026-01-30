3c5eadc484b6b3c1c5b3f59784117c589684aab7
zhangtianrong <zhangtianrong@h-partners.com>
2026-01-30 16:59:00 +0800

    gmm add empty tensor check

--- gmm/grouped_matmul/op_host/op_api/aclnn_grouped_matmul.cpp
+++ gmm/grouped_matmul/op_host/op_api/aclnn_grouped_matmul.cpp
@@ -1089,7 +1089,7 @@ bool isActivationAllowed(int64_t act_type) {
 bool CheckScaleForInt8Quant(const gmm::GroupedMatmulParams &gmmParams) {
     if (gmmParams.scaleOptional == nullptr || gmmParams.scaleOptional->Size() == 0 ||
         (*gmmParams.scaleOptional)[0] == nullptr) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                 "When the activation function is enabled, Scale should not be null, but now is null.");
         return false;
     }
@@ -1103,7 +1103,7 @@ bool CheckScaleForInt8Quant(const gmm::GroupedMatmulParams &gmmParams) {
     }
     size_t scaleDimNum = scaleShape.GetDimNum();
     if (scaleDimNum != 2) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                 "When the activation function is enabled, the dim of Scale should be 2 for perchannel, but actual is %zu.\n", scaleDimNum);
         return false;
     }
@@ -1114,7 +1114,7 @@ bool CheckScaleForInt8Quant(const gmm::GroupedMatmulParams &gmmParams) {
     int64_t scaleNSize = scaleShape.GetDim(scaleDimNum - 1);
     int64_t scaleGroupNum = scaleShape.GetDim(0);
     if (scaleNSize != weightNSize || scaleGroupNum != weightGroupNum) {
-              OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+              OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                      "When the activation function is enabled, the shape of scale should be (%ld, %ld), but actual"
                      " is (%ld, %ld).\n",
                      weightGroupNum, weightNSize, scaleGroupNum, scaleNSize);
@@ -1137,9 +1137,9 @@ bool CheckInt8DynamicKCQuant(const gmm::GroupedMatmulParams &gmmParams) {
     }
     const op::Shape &perTokenScaleShape = (*gmmParams.perTokenScaleOptional)[0]->GetViewShape();
     DataType perTokenScaleDtype = (*gmmParams.perTokenScaleOptional)[0]->GetDataType();
-    
+
     if (perTokenScaleDtype != DataType::DT_FLOAT) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                 "When the activation function is enabled,"
                 " the dtype of perTokenScale should be float32, but actual is %s.",
                 gmm::dTypeToString(perTokenScaleDtype).c_str());
@@ -1150,12 +1150,12 @@ bool CheckInt8DynamicKCQuant(const gmm::GroupedMatmulParams &gmmParams) {
     size_t perTokenScaleDim = perTokenScaleShape.GetDimNum();
     int64_t perTokenScaleMSize = perTokenScaleShape.GetDim(perTokenScaleDim - 1);
     if (perTokenScaleDim != 1) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                 "When the activation function is enabled, the dim of perTokenScale should be 1, but actual is %ld.\n", perTokenScaleDim);
         return false;
     }
     if (perTokenScaleMSize != mSize) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                 "When the activation function is enabled and the dim of perTokenScale is 1, the shape of perTokenScale should be (%ld,), but actual"
                 " is (%ld,).\n",
                 mSize, perTokenScaleMSize);
@@ -1169,7 +1169,7 @@ bool CheckIsEnabledActive(const gmm::GroupedMatmulParams &gmmParams) {
     auto weightDtype = (*gmmParams.weight)[0]->GetDataType();
     bool isInt8Input = (xDtype == DataType::DT_INT8 && weightDtype == DataType::DT_INT8);
     if (!isInt8Input) {
-          OP_LOGE(ACLNN_ERR_PARAM_INVALID, 
+          OP_LOGE(ACLNN_ERR_PARAM_INVALID,
                   "When the activation function is enabled, the dtype of x and weight should be DT_INT8,"
                   " actual is %s and %s.",
                   op::ToString(xDtype).GetString(),
@@ -1178,7 +1178,7 @@ bool CheckIsEnabledActive(const gmm::GroupedMatmulParams &gmmParams) {
     }
     bool isInt8StaticTCQuant = CheckInt8StaticTCQuant(gmmParams);
     bool isInt8DynamicKCQuant = CheckInt8DynamicKCQuant(gmmParams);
-    bool allowActOnDavid = isInt8Input && (isInt8StaticTCQuant || isInt8DynamicKCQuant) 
+    bool allowActOnDavid = isInt8Input && (isInt8StaticTCQuant || isInt8DynamicKCQuant)
                             && isActivationAllowed(gmmParams.activeType);
     return allowActOnDavid;
 }
@@ -1203,10 +1203,7 @@ static aclnnStatus CheckFunctionParams(const gmm::GroupedMatmulParams &gmmParams
       return gmm::AclnnGroupedMatmulWeightQuant91095Checker(gmmParams).CheckGroupedMatmulWeightQuant91095();
     } else {
       CHECK_COND(isNoActivation, ACLNN_ERR_PARAM_INVALID, "When input is No-Quant, activation is not supported on this platforms."
- 	                " activeType[%ld] is not supported.", gmmParams.activeType);
-      CHECK_RET(
-          gmm::AclnnGroupedMatmulNoQuantDAV3510Checker(gmmParams).CheckGroupedMatmulFunctionParamsNoQuantDAV3510() ==
-              ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+ 	                  " activeType[%ld] is not supported.", gmmParams.activeType);
     }
   }
   if (gmmParams.xDtype == DataType::DT_INT8 && weightDtype == DataType::DT_INT4) {
@@ -1820,14 +1817,21 @@ static aclnnStatus TransWeightToNz(gmm::GroupedMatmulParams &gmmParams, aclOpExe
 }
 
 static aclnnStatus CheckZeroShape(gmm::GroupedMatmulParams &params, uint64_t *workspaceSize) {
-    bool isEmpty = true;
+    bool isEmptyX = true;
+    bool isEmptyWeight =true;
     for (size_t i = 0; i < params.x->Size(); ++i) {
         if (!((*params.x)[i]->IsEmpty())) {
-            isEmpty = false;
+            isEmptyX = false;
             break;
         }
     }
-    if (isEmpty) {
+    for (size_t i = 0; i < params.weight->Size(); ++i) {
+        if (!((*params.weight)[i]->IsEmpty())) {
+            isEmptyWeight = false;
+            break;
+        }
+    }
+    if (isEmptyX || isEmptyWeight) {
         *workspaceSize = 0UL;
         return ACLNN_ERR_PARAM_INVALID;
     }
@@ -2202,6 +2206,37 @@ static aclnnStatus CheckTransposeStatus(const aclTensorList *x, const aclTensorL
   return ACLNN_SUCCESS;
 }
 
+static aclnnStatus CheckEmptyTensor(const aclTensorList *x, const aclTensorList *weight)
+{
+  // all M or N be zero, get true
+  bool zeroM = true;
+  bool zeroN = true;
+  // exist one K be zero, get true
+  bool zeroK = false;
+  // current view_shape transpose is always false false
+  for (size_t i = 0; i < x->Size(); ++i) {
+    CHECK_COND((*x)[i] != nullptr, ACLNN_ERR_PARAM_INVALID,
+               "GroupedMatmul x tensor should not be null");
+    auto xShape = (*x)[i]->GetViewShape();
+    CHECK_COND(xShape.GetDimNum() >= gmm::MIN_FM_DIM, ACLNN_ERR_PARAM_INVALID,
+               "GroupedMatmul x dim num should larger than 2, but actual %d.", xShape.GetDimNum());
+    zeroM = zeroM && (xShape.GetDim(xShape.GetDimNum() - 2) == 0);
+    zeroK = zeroK || (xShape.GetDim(xShape.GetDimNum() - 1) == 0);
+  }
+  for (size_t i = 0; i < weight->Size(); ++i) {
+    CHECK_COND((*weight)[i] != nullptr, ACLNN_ERR_PARAM_INVALID,
+               "GroupedMatmul weight tensor should not be null");
+    auto wShape = (*weight)[i]->GetViewShape();
+    CHECK_COND(wShape.GetDimNum() >= gmm::MIN_FM_DIM, ACLNN_ERR_PARAM_INVALID,
+               "GroupedMatmul weight dim num should larger than 2, but actual %d.", wShape.GetDimNum());
+    zeroN = zeroN && (wShape.GetDim(wShape.GetDimNum() - 1) == 0);
+  }
+  // if all M or N is zero, do not need to check K
+  CHECK_COND(zeroM || zeroN || !zeroK, ACLNN_ERR_PARAM_INVALID,
+             " GroupedMatmul does not support input dim K being 0 unless all M/N is 0");
+  return ACLNN_SUCCESS;
+}
+
 static aclnnStatus aclnnGroupedMatmulGetWorkspaceSizeCommon(const aclTensorList *x, const aclTensorList *weight,
   const aclTensorList *biasOptional, const aclTensorList *scaleOptional, const aclTensorList *offsetOptional,
   const aclTensorList *antiquantScaleOptional, const aclTensorList *antiquantOffsetOptional,
@@ -2221,6 +2256,8 @@ static aclnnStatus aclnnGroupedMatmulGetWorkspaceSizeCommon(const aclTensorList 
   bool isSingleWeight = (weight->Size() == 1 && groupType != gmm::NO_SPLIT);
   bool transposeX = false;
   bool transposeWeight = false;
+  CHECK_COND(CheckEmptyTensor(x, weight) == ACLNN_SUCCESS,
+             ACLNN_ERR_PARAM_INVALID, "CheckEmptyTensor failed!");
   CHECK_COND(CheckTransposeStatus(x, weight, transposeX, transposeWeight, groupType) == ACLNN_SUCCESS,
              ACLNN_ERR_PARAM_INVALID, "CheckTransposeStatus failed!");
   gmm::GroupedMatmulParams gmmParams{x, weight, biasOptional, groupListOptional, groupTensorOptional, scaleOptional,
