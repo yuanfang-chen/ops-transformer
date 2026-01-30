538524bdf0477cad97f084ca129bee2720143904
SeasonChu <zhuxincheng2@huawei.com>
2026-01-30 11:44:36 +0800

    aicpu

---
+++ mc2/mc2_comm_server/op_host/mc2_comm_server_def.cpp
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file mc2_comm_server_def.cpp
+ * \brief 算子信息库定义
+ */
+#include <register/op_def_registry.h>
+
+namespace ops {
+class MC2CommServer : public OpDef {
+public:
+    explicit MC2CommServer(const char *name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_UNIT64})
+            .FormatList({ge::FORMAT_ND});
+
+        // ascend950 AI处理器定义OpAICoreConfig变量，定制化配置参数
+        OpAICoreConfig aicore_config_950;
+        aicore_config_950.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(true)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false)
+            .PrecisionReduceFlag(true)
+            .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
+            .ExtendCfgInfo("jitCompile.flag", "static_false") 
+            .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
+        this->AICore().AddConfig("ascend910_95", aicore_config_950);
+
+    }
+};
+
+OP_ADD(MC2CommServer);
+
+} // namespace ops
