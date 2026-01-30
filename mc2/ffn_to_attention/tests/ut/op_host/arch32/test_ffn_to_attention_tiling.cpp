699e24f05d9c264302d328c7188a9c7bc49bc7d4
hblnb <huangbolin3@huawei.com>
2026-01-30 16:27:39 +0800

    对mc2算子的ut用例进行代际隔离

--- mc2/ffn_to_attention/tests/ut/op_host/arch32/test_ffn_to_attention_tiling.cpp
+++ mc2/ffn_to_attention/tests/ut/op_host/arch32/test_ffn_to_attention_tiling.cpp
@@ -18,11 +18,11 @@ namespace FFNToAttentionUT {
 class FFNToAttentionTiling : public testing::Test {
 protected:
     static void SetUpTestCase() {
-        std::cout << "FFNToAttentionTiling SetUp" << std::endl;
+        std::cout << "FFNToAttention TilingUT910 SetUp" << std::endl;
     }
 
     static void TearDownTestCase() {
-        std::cout << "FFNToAttentionTiling TearDown" << std::endl;
+        std::cout << "FFNToAttention TilingUT910 TearDown" << std::endl;
     }
 };
 
