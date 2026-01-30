6e9ffa3b71b41adf574a0ec908dbf8701c82259c
SeasonChu <zhuxincheng2@huawei.com>
2026-01-30 11:44:36 +0800

    aicpu

---
+++ mc2/mc2_comm_server/op_host/op_tiling/mc2_comm_server_tiling.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file  mc2_comm_server_tiling.cpp
+ * \brief host侧tiling实现
+ */
+
+ /**
+ * @brief MC2CommServer算子的tiling函数
+ * @param context: 框架根据input，output，attrs等信息生成tiling需要的context
+ * @return
+ */
