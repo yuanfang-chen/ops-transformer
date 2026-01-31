c3bc9a066759c4a406abd32e0404a9bb78143918
sunwenlong <sunwenlong8@huawei.com>
2026-01-31 09:29:36 +0800

    A2/A3支持attention_worker_combine和ffn_worker_batching

---
+++ attention/attention_worker_combine/op_host/attention_worker_combine_def.cpp
@@ -0,0 +1,68 @@
+/* *
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* !
+ * \file attention_worker_combine_def.cpp
+ * \brief AttentionWorkerCombine op host config
+ */
+
+#include <cstdint>
+#include "register/op_def_registry.h"
+
+namespace ops {
+class AttentionWorkerCombine : public OpDef {
+public:
+    explicit AttentionWorkerCombine(const char *name) : OpDef(name)
+    {
+        this->Input("schedule_context")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT8, ge::DT_INT8})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("expert_scales")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("layer_id")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32, ge::DT_INT32})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("next_layer_id")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32, ge::DT_INT32})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Attr("hidden_size").AttrType(REQUIRED).Int(0);
+        this->Attr("token_dtype").AttrType(OPTIONAL).Int(0);
+        this->Attr("need_schedule").AttrType(OPTIONAL).Int(0);
+
+        this->AICore().AddConfig("ascend910_93");
+        this->AICore().AddConfig("ascend910b");
+    }
+};
+
+OP_ADD(AttentionWorkerCombine);
+}  // namespace ops
---
+++ ffn/ffn_worker_batching/op_host/op_tiling/ffn_worker_batching_tiling.h
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file ffn_worker_batching_tiling.h
+ * \brief
+ */
+#ifndef FFN_WORKER_BATCHING_TILING_H
+#define FFN_WORKER_BATCHING_TILING_H
+
+#include "register/tilingdata_base.h"
+#include "tiling/tiling_api.h"
+#include "util/shape_util.h"
+#include "register/op_impl_registry.h"
+#include "log/log.h"
+#include "tiling_base/tiling_templates_registry.h"
+#include "tiling_base/tiling_base.h"
+#include "util/math_util.h"
+#include "platform/platform_info.h"
+
+namespace optiling {
+
+BEGIN_TILING_DATA_DEF(FfnWorkerBatchingTilingData)
+TILING_DATA_FIELD_DEF(int64_t, Y);
+TILING_DATA_FIELD_DEF(int64_t, H);
+TILING_DATA_FIELD_DEF(int64_t, tokenDtype);
+TILING_DATA_FIELD_DEF(int64_t, expertNum);
+TILING_DATA_FIELD_DEF(int64_t, coreNum);
+TILING_DATA_FIELD_DEF(int64_t, ubSize);
+TILING_DATA_FIELD_DEF(int64_t, sortLoopMaxElement);
+TILING_DATA_FIELD_DEF(int64_t, sortNumWorkSpace);
+END_TILING_DATA_DEF;
+
+REGISTER_TILING_DATA_CLASS(FfnWorkerBatching, FfnWorkerBatchingTilingData)
+
+struct FfnWorkerBatchingCompileInfo {
+};
+
+class FfnWorkerBatchingTiling
+{
+public:
+    explicit FfnWorkerBatchingTiling(gert::TilingContext* context) : context_(context){};
+    ge::graphStatus RunFfnWorkerBatchingTiling();
+
+private:
+    ge::graphStatus GetPlatformInfo();
+    ge::graphStatus CheckInputParam();
+    ge::graphStatus SetBatchingWorkspaceSize(int64_t expertNum);
+    ge::graphStatus GetAttrsInfo();
+    void SetBatchingTilingKey(int64_t sortLoopMaxElement);
+
+private:
+    FfnWorkerBatchingTilingData tilingData_;
+    gert::TilingContext *context_ = nullptr;
+
+    int64_t A_ = 0;
+    int64_t BS_ = 0;
+    int64_t K_ = 0;
+    int64_t Y_ = 0;
+    int64_t needSchedule_ = 0;
+    int64_t layerNum_ = 0;
+    uint32_t ubBlockSize_ = 0;
+};
+
+} // namespace optiling
+#endif
