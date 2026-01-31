18a5911cd2386237d4d1625867edcf40a10c4ff8
wuyi_huawei <wuyi36@hisilicon.com>
2026-01-31 18:42:40 +0800

    解决stepka不等于stepkb的精度问题

--- gmm/common/cgmct/kernel/kernel_qgmm_mx.h
+++ gmm/common/cgmct/kernel/kernel_qgmm_mx.h
@@ -231,17 +231,13 @@ __aicore__ inline void QuantMmGroupedMx<QGMM_MX_KERNEL_FUN_TEM_PARAMS>::Init(con
     int64_t kL1B = static_cast<int64_t>(params.gmmParams.stepKb) * static_cast<int64_t>(params.gmmParams.baseK);
     int64_t scaleKL1 = AscendC::Std::min(scaleFactorA * kL1A, scaleFactorB * kL1B);
     scaleKL1 = scaleKL1 / AscendC::Std::max(kL1A, kL1B) * AscendC::Std::max(kL1A, kL1B);
-    L1Params tileL12L0{static_cast<uint64_t>(params.gmmParams.stepKa) * static_cast<uint64_t>(params.gmmParams.baseK),
-                       static_cast<uint64_t>(params.gmmParams.stepKb) * static_cast<uint64_t>(params.gmmParams.baseK),
-                       static_cast<uint64_t>(scaleKL1), 2UL}; // 默认开启2buffer
-    // auto mmResPing_ = epilogueOp_.GetL0c2UbPingTensor();
-    // auto mmResPong_ = epilogueOp_.GetL0c2UbPongTensor();
+    L1Params tileL12L0{static_cast<uint64_t>(kL1A), static_cast<uint64_t>(kL1B), static_cast<uint64_t>(scaleKL1),
+                       2UL}; // 默认开启2buffer
+
     Get<MNK_M>(problemShape_) = params.gmmParams.m;
     Get<MNK_N>(problemShape_) = params.gmmParams.n;
     Get<MNK_K>(problemShape_) = params.gmmParams.k;
     mmadOp_.Init(problemShape_, l0Shape, tileL12L0, isBias_, false);
-    // mmadOp_.Init(l0Shape, tileL12L0, &mmResPing_, &mmResPong_);
-    // epilogueOp_.Init(&params.epilogueParams);
 }
 
 QGMM_MX_KERNEL_CLASS_TEM_PARAMS
