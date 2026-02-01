5cb91c023bc6b231f16b165cb3928e71cd4a0d5a
wuziyu <wuziyu11@huawei.com>
2026-02-02 01:29:32 +0800

    add

--- mc2/matmul_allto_all/op_graph/matmul_allto_all_gen_task.cpp
+++ mc2/matmul_allto_all/op_graph/matmul_allto_all_gen_task.cpp
@@ -18,8 +18,8 @@
 
 #ifdef BUILD_OPEN_PROJECT
 #include "mc2_gen_task_ops_utils.h"
-#include "mc2_gen_task_ops_utils_arch35.h"
 #include "register/op_impl_registry.h"
+#include "mc2_gen_task_ops_utils_arch35.h"
 #include "mc2_log.h"
 #endif
 
