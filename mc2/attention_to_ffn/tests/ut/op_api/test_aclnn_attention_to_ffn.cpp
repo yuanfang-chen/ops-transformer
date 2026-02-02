f5b0b733c6638edbaffd24d4bdc702c33e34d11c
hblnb <huangbolin3@huawei.com>
2026-02-02 19:04:21 +0800

    对mc2算子ut文件进行整改：包括命名风格、缩进和花括号使用等格式问题和一些导致构建错误的语法问题
    Co-authored-by: hblnb<huangbolin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1215 merge fix_mc2_ut into master
    
    对mc2算子ut文件进行整改：包括命名风格、缩进和花括号使用等格式问题和一些导致构建错误的语法问题
    
    Created-by: hblnb
    Commit-by: hblnb
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1215

--- mc2/attention_to_ffn/tests/ut/op_api/test_aclnn_attention_to_ffn.cpp
+++ mc2/attention_to_ffn/tests/ut/op_api/test_aclnn_attention_to_ffn.cpp
@@ -1,12 +1,12 @@
 /**
+ * This program is free software, you can redistribute it and/or modify.
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #include <cfloat>
 
@@ -25,82 +25,83 @@ using namespace op;
 using namespace std;
 
 namespace AttentionToFFNUT {
+class AclnnAttentionToFfnTest : public testing::Test {
+protected:
+    static void SetUpTestCase()
+    {
+      op::SetPlatformSocVersion(op::SocVersion::ASCEND910_93);
+      cout << "AclnnAttentionToFfnTest SetUp" << endl;
+    }
 
-class aclnn_attention_to_ffn_test : public testing::Test {
- protected:
-  static void SetUpTestCase()
-  {
-    op::SetPlatformSocVersion(op::SocVersion::ASCEND910_93);
-    cout << "aclnn_attention_to_ffn_test SetUp" << endl;
-  }
-
-  static void TearDownTestCase()
-  {
-    op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
-    cout << "aclnn_attention_to_ffn_test TearDown" << endl;
-  }
+    static void TearDownTestCase()
+    {
+      op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
+      cout << "AclnnAttentionToFfnTest TearDown" << endl;
+    }
 };
 
-TEST_F(aclnn_attention_to_ffn_test, test_attention_to_ffn_no_quant) {
-  TensorDesc x = TensorDesc({1, 16, 7168}, ACL_FLOAT16, ACL_FORMAT_ND);
-  TensorDesc sessionId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc microBatchId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc layerId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc expertIds = TensorDesc({1, 16, 8}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc expertRankTable = TensorDesc({1, 9, 4}, ACL_INT32, ACL_FORMAT_ND);
-  // TensorDesc scales = TensorDesc({9, 7168}, ACL_FLOAT, ACL_FORMAT_ND);
-  TensorDesc activeMask = TensorDesc({1, 16}, ACL_BOOL, ACL_FORMAT_ND);
-  const char* group = "test_attention_to_ffn_group";
-  int64_t worldSize = 16;
-  std::vector<int64_t> ffnTokenInfoTable = {11, 1, 146};
-  std::vector<int64_t> ffnTokenData = {11, 1, 16, 9, 7168};
-  std::vector<int64_t> attnTokenInfoTable = {1, 16, 9};
-  aclIntArray* ffnTokenInfoTableShape = aclCreateIntArray(ffnTokenInfoTable.data(), ffnTokenInfoTable.size());
-  aclIntArray* ffnTokenDataShape = aclCreateIntArray(ffnTokenData.data(), ffnTokenData.size());
-  aclIntArray* attnTokenInfoTableShape = aclCreateIntArray(attnTokenInfoTable.data(), attnTokenInfoTable.size());
-  int64_t moeExpertNum = 8;
-  int64_t quantMode = 0;
-  int64_t syncFlag = 0;
-  int64_t ffnStartRankId = 0;
+TEST_F(AclnnAttentionToFfnTest, TestAttentionToFfnNoQuant)
+{
+    TensorDesc x = TensorDesc({1, 16, 7168}, ACL_FLOAT16, ACL_FORMAT_ND);
+    TensorDesc sessionId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc microBatchId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc layerId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc expertIds = TensorDesc({1, 16, 8}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc expertRankTable = TensorDesc({1, 9, 4}, ACL_INT32, ACL_FORMAT_ND);
+    // TensorDesc scales = TensorDesc({9, 7168}, ACL_FLOAT, ACL_FORMAT_ND);
+    TensorDesc activeMask = TensorDesc({1, 16}, ACL_BOOL, ACL_FORMAT_ND);
+    const char* group = "test_attention_to_ffn_group";
+    int64_t worldSize = 16;
+    std::vector<int64_t> ffnTokenInfoTable = {11, 1, 146};
+    std::vector<int64_t> ffnTokenData = {11, 1, 16, 9, 7168};
+    std::vector<int64_t> attnTokenInfoTable = {1, 16, 9};
+    aclIntArray* ffnTokenInfoTableShape = aclCreateIntArray(ffnTokenInfoTable.data(), ffnTokenInfoTable.size());
+    aclIntArray* ffnTokenDataShape = aclCreateIntArray(ffnTokenData.data(), ffnTokenData.size());
+    aclIntArray* attnTokenInfoTableShape = aclCreateIntArray(attnTokenInfoTable.data(), attnTokenInfoTable.size());
+    int64_t moeExpertNum = 8;
+    int64_t quantMode = 0;
+    int64_t syncFlag = 0;
+    int64_t ffnStartRankId = 0;
 
-  auto ut = OP_API_UT(aclnnAttentionToFFN, INPUT(x, sessionId, microBatchId, layerId, expertIds,
-  expertRankTable, nullptr, activeMask, group, worldSize, ffnTokenInfoTableShape, ffnTokenDataShape,
-  attnTokenInfoTableShape, moeExpertNum, quantMode, syncFlag, ffnStartRankId), OUTPUT());
-  uint64_t workspace_size = 0;
-  aclOpExecutor* executor = nullptr;
-  aclnnStatus aclRet = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspace_size, executor);
-  EXPECT_EQ(aclRet, ACLNN_SUCCESS);
+    auto ut = OP_API_UT(aclnnAttentionToFFN, INPUT(x, sessionId, microBatchId, layerId, expertIds,
+    expertRankTable, nullptr, activeMask, group, worldSize, ffnTokenInfoTableShape, ffnTokenDataShape,
+    attnTokenInfoTableShape, moeExpertNum, quantMode, syncFlag, ffnStartRankId), OUTPUT());
+    uint64_t workspaceSize = 0;
+    aclOpExecutor* executor = nullptr;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
+    EXPECT_EQ(aclRet, ACLNN_SUCCESS);
 }
 
-TEST_F(aclnn_attention_to_ffn_test, test_attention_to_ffn_quant) {
-  TensorDesc x = TensorDesc({1, 16, 7168}, ACL_FLOAT16, ACL_FORMAT_ND);
-  TensorDesc sessionId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc microBatchId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc layerId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc expertIds = TensorDesc({1, 16, 8}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc expertRankTable = TensorDesc({1, 9, 4}, ACL_INT32, ACL_FORMAT_ND);
-  TensorDesc scales = TensorDesc({9, 7168}, ACL_FLOAT, ACL_FORMAT_ND);
-  TensorDesc activeMask = TensorDesc({1, 16}, ACL_BOOL, ACL_FORMAT_ND);
-  const char* group = "test_attention_to_ffn_group";
-  int64_t worldSize = 16;
-  std::vector<int64_t> ffnTokenInfoTable = {11, 1, 146};
-  std::vector<int64_t> ffnTokenData = {11, 1, 16, 9, 7296};
-  std::vector<int64_t> attnTokenInfoTable = {1, 16, 9};
-  aclIntArray* ffnTokenInfoTableShape = aclCreateIntArray(ffnTokenInfoTable.data(), ffnTokenInfoTable.size());
-  aclIntArray* ffnTokenDataShape = aclCreateIntArray(ffnTokenData.data(), ffnTokenData.size());
-  aclIntArray* attnTokenInfoTableShape = aclCreateIntArray(attnTokenInfoTable.data(), attnTokenInfoTable.size());
-  int64_t moeExpertNum = 8;
-  int64_t quantMode = 2;
-  int64_t syncFlag = 0;
-  int64_t ffnStartRankId = 0;
+TEST_F(AclnnAttentionToFfnTest, TestAttentionToFfnQuant)
+{
+    TensorDesc x = TensorDesc({1, 16, 7168}, ACL_FLOAT16, ACL_FORMAT_ND);
+    TensorDesc sessionId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc microBatchId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc layerId = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc expertIds = TensorDesc({1, 16, 8}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc expertRankTable = TensorDesc({1, 9, 4}, ACL_INT32, ACL_FORMAT_ND);
+    TensorDesc scales = TensorDesc({9, 7168}, ACL_FLOAT, ACL_FORMAT_ND);
+    TensorDesc activeMask = TensorDesc({1, 16}, ACL_BOOL, ACL_FORMAT_ND);
+    const char* group = "test_attention_to_ffn_group";
+    int64_t worldSize = 16;
+    std::vector<int64_t> ffnTokenInfoTable = {11, 1, 146};
+    std::vector<int64_t> ffnTokenData = {11, 1, 16, 9, 7296};
+    std::vector<int64_t> attnTokenInfoTable = {1, 16, 9};
+    aclIntArray* ffnTokenInfoTableShape = aclCreateIntArray(ffnTokenInfoTable.data(), ffnTokenInfoTable.size());
+    aclIntArray* ffnTokenDataShape = aclCreateIntArray(ffnTokenData.data(), ffnTokenData.size());
+    aclIntArray* attnTokenInfoTableShape = aclCreateIntArray(attnTokenInfoTable.data(), attnTokenInfoTable.size());
+    int64_t moeExpertNum = 8;
+    int64_t quantMode = 2;
+    int64_t syncFlag = 0;
+    int64_t ffnStartRankId = 0;
 
-  auto ut = OP_API_UT(aclnnAttentionToFFN, INPUT(x, sessionId, microBatchId, layerId, expertIds,
-  expertRankTable, scales, activeMask, group, worldSize, ffnTokenInfoTableShape, ffnTokenDataShape, 
-  attnTokenInfoTableShape, moeExpertNum,quantMode, syncFlag, ffnStartRankId), OUTPUT());
-  uint64_t workspace_size = 0;
-  aclOpExecutor* executor = nullptr;
-  aclnnStatus aclRet = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspace_size, executor);
-  EXPECT_EQ(aclRet, ACLNN_SUCCESS);
+    auto ut = OP_API_UT(aclnnAttentionToFFN, INPUT(x, sessionId, microBatchId, layerId, expertIds,
+    expertRankTable, scales, activeMask, group, worldSize, ffnTokenInfoTableShape, ffnTokenDataShape, 
+    attnTokenInfoTableShape, moeExpertNum,quantMode, syncFlag, ffnStartRankId), OUTPUT());
+    uint64_t workspaceSize = 0;
+    aclOpExecutor* executor = nullptr;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
+    EXPECT_EQ(aclRet, ACLNN_SUCCESS);
 }
 
 } // AttentionToFFNUT
