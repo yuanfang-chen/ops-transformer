599ada73086cd7b8c99d969cab769c27458ad70d
fishing_artist <liuhuansheng1@h-partners.com>
2026-02-02 14:42:19 +0800

    ScatterPaCache fix not roundup problem

--- attention/scatter_pa_kv_cache/op_host/scatter_pa_kv_cache_tiling_arch35.cpp
+++ attention/scatter_pa_kv_cache/op_host/scatter_pa_kv_cache_tiling_arch35.cpp
@@ -288,12 +288,12 @@ ge::graphStatus ScatterPaKvCacheTiling::TemplateRope()
         std::max(kHeadSize_, vHeadSize_) * floatFactor * DIM1 +    // reduce Buf for inputKeyLocal or inputValueLocal
         std::max(kHeadSize_, vHeadSize_) * floatFactor * DIM1 +    // divide Buf
         std::max(kHeadSize_, vHeadSize_) * floatFactor * DIM1;     // cast Buf
-    if (ubThreshold <= maxHandleNumPerLoop) {
-        // tail dim can fully load
-        isFullyLoad_ = FULLY_LOAD;
-        OP_LOGD(context_, "tail dim can fully load.");
-        return ge::GRAPH_SUCCESS;
-    }
+    // if (ubThreshold <= maxHandleNumPerLoop) {
+    //     // tail dim can fully load
+    //     isFullyLoad_ = FULLY_LOAD;
+    //     OP_LOGD(context_, "tail dim can fully load.");
+    //     return ge::GRAPH_SUCCESS;
+    // }
     // can not fully load
     isFullyLoad_ = NOT_FULLY_LOAD;
 
