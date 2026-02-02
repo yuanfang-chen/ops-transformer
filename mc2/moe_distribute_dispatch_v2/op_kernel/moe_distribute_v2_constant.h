f46c3d3c1c4d8ff004ae098e884a1cde7f8315d7
davon <liuwenda4@huawei.com>
2026-02-02 14:26:13 +0800

    Revert "dispatch优化syncall"
    This reverts commit 81ef49227294fd0947577930e3b4b2e18d6a2c0b.

--- mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_v2_constant.h
+++ mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_v2_constant.h
@@ -44,7 +44,6 @@ constexpr uint8_t COMM_NUM = 2;  // 通信域大小
 constexpr uint8_t COMM_EP_IDX = 0;
 constexpr uint8_t COMM_TP_IDX = 1;
 constexpr uint8_t QUANT_PADDING_VALUE = 0;
-constexpr uint32_t FIRST_CORE = 0U;
 // 先写死这个偏移，如果TP固定为2，可直接往起始数据偏移开始读写
 constexpr uint64_t WIN_STATE_OFFSET = 384UL * 1024UL;
 constexpr uint64_t TIMEOUT_DETECTION_THRESHOLD = 50000UL;
