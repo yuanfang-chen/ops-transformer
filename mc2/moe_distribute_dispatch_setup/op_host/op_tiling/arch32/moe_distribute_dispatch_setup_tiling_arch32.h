f1efb35fdbc3df10d54e314b3f5e3eb8733e66b5
陈佳乐 <chenjiale56@huawei.com>
2026-02-02 17:37:04 +0800

    feat: 新增dispatch_setup host侧实现
    feat: 迁移dispatch_setup原A3kernel及tiling实现
    
    fix: 修改soc型号，抽取tiling公共方法

---
+++ mc2/moe_distribute_dispatch_setup/op_host/op_tiling/arch32/moe_distribute_dispatch_setup_tiling_arch32.h
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file moe_distribute_dispatch_setup_tiling_arch32.h
+ * \brief
+ */
+
+#ifndef MOE_DISTRIBUTE_DISPATCH_SETUP_TILING_ARCH32_H_
+#define MOE_DISTRIBUTE_DISPATCH_SETUP_TILING_ARCH32_H_
+
+#include "../moe_distribute_dispatch_setup_tiling_base.h"
+
+namespace optiling {
+class MoeDistributeDispatchSetupTilingA3 : public MoeDistributeDispatchSetupTilingBase
+{
+public:
+    explicit MoeDistributeDispatchSetupTilingA3(gert::TilingContext* context)
+        : MoeDistributeDispatchSetupTilingBase(context)
+    {
+        socTilingName_ = "MoeDistributeDispatchSetupA3";
+    }
+private:
+    ge::graphStatus DoOpTiling() override final;
+    bool IsCapable() override final;
+    
+    ge::graphStatus CheckRequiredAttrValue() override;
+    ge::graphStatus GetRequiredAttrAndSetTilingData() override;
+    ge::graphStatus CheckSharedExpertAttrValue() override;
+    ge::graphStatus CheckOptionalAttrValue() override;
+    ge::graphStatus GetOptionalAttrAndSetTilingData() override;
+    ge::graphStatus GetComplexAttrAndSetTilingData() override;
+
+    ge::graphStatus CheckInputTensorDataType() override;
+    ge::graphStatus CheckOptionalInputTensorDataType() override;
+    ge::graphStatus CheckOutputTensorDataType() override;
+    ge::graphStatus CheckTensorDataType() override;
+    ge::graphStatus CheckTensorDim() override;
+    ge::graphStatus CheckTensorShapeRelation() override;
+    ge::graphStatus CheckComplexTensorShapeSize() override;
+    ge::graphStatus CheckTensorShapeSizeAndSetTilingData() override;
+    ge::graphStatus CheckCalcTensorShapeSizeAndSetTilingData() override;
+
+    ge::graphStatus MoeDistributeDispatchSetupTilingFuncImpl() override;
+    ge::graphStatus CheckInputTensorDim() override;
+    ge::graphStatus CheckOptionalInputTensorDim() override;
+    ge::graphStatus CheckOutputTensorDim() override;
+    ge::graphStatus CheckOneTensorDim(std::string name, TensorType tensortype, uint32_t index, uint32_t dims);
+
+    ge::graphStatus SetWorkspace() override;
+    ge::graphStatus CheckHcclBuffSize() override;
+    void SetTilingKey() override;
+    void SetPlatformInfo() override;
+    void SetHcommCfg() override;
+    void PrintTilingDataInfo() override;
+};
+} // namespace optiling
+#endif // MOE_DISTRIBUTE_DISPATCH_SETUP_TILING_ARCH32_H_
