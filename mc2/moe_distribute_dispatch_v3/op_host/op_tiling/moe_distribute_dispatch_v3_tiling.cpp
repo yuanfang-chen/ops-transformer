fe8664874cca3f4c4ec43baf508c7cb80c5da2d3
GJQ <gaojiaqin@hisilicon.com>
2026-02-02 10:39:30 +0800

    Add context as an input to dispatch/combine

---
+++ mc2/moe_distribute_dispatch_v3/op_host/op_tiling/moe_distribute_dispatch_v3_tiling.cpp
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file moe_distribute_dispatch_v3_tiling.cpp
+ * \brief
+ */
+
+#include <queue>
+#include <vector>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cmath>
+#include <cstdint>
+#include <string>
+
+#include "tiling/mc2_tiling_utils.h"
+#include "register/tilingdata_base.h"
+#include "tiling/tiling_api.h"
+#include "mc2_log.h"
+#include "mc2_exception_dump.h"
+#include "graph/utils/type_utils.h"
+#include "register/op_def_registry.h"
+#include "platform/platform_infos_def.h"
+#include "../../op_kernel/moe_distribute_dispatch_v3_tiling.h"
+#include "../../op_kernel/moe_distribute_dispatch_v3_tiling_key.h"
+#include "../../../moe_distribute_dispatch_v2/op_host/op_tiling/moe_distribute_dispatch_v2_tiling_helper.h"
+#include "mc2_hcom_topo_info.h"
+
+using namespace Mc2Tiling;
+using namespace Mc2Exception;
+using namespace AscendC;
+using namespace ge;
+
+
+namespace optiling {
+static ge::graphStatus MoeDistributeDispatchV3TilingFuncImpl(gert::TilingContext *context)
+{
+    // dispatch V2 does not have context as input
+    MoeDistributeDispatchV2TilingHelper tilingHelper(true);
+    auto ret = tilingHelper.MoeDistributeDispatchA3TilingImpl(context);
+    return ret;
+}
+
+static ge::graphStatus MoeDistributeDispatchV3TilingFunc(gert::TilingContext* context)
+{
+    std::string socVersion = mc2tiling::GetSocVersion(context);
+    ge::graphStatus ret;
+    ret = MoeDistributeDispatchV3TilingFuncImpl(context);
+    return ret;
+}
+
+struct MoeDistributeDispatchCompileInfo {};
+static ge::graphStatus TilingParseForMoeDistributeDispatchV3(gert::TilingParseContext *context)
+{
+    (void)context;
+    return ge::GRAPH_SUCCESS;
+}
+
+IMPL_OP_OPTILING(MoeDistributeDispatchV3)
+    .Tiling(MoeDistributeDispatchV3TilingFunc)
+    .TilingParse<MoeDistributeDispatchCompileInfo>(TilingParseForMoeDistributeDispatchV3);
+
+// Register exception func
+inline void MoeDistributeDispatchV3ExceptionImplWrapper(aclrtExceptionInfo *args, void *userdata)
+{
+    Mc2ExceptionImpl(args, userdata, "MoeDistributeDispatchV3");
+}
+
+IMPL_OP(MoeDistributeDispatchV3)
+    .ExceptionDumpParseFunc(MoeDistributeDispatchV3ExceptionImplWrapper);
+} // namespace optiling
