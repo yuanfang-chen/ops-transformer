9d7f1713ad951cfc28d9e449b4928d194f766335
liyinghao10 <liyinghao10@h-partners.com>
2026-02-02 16:37:58 +0800

    Fixed some compilation issues

--- gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
+++ gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
@@ -253,7 +253,7 @@ FLOAT4_E1M2}, actual is %s.",
     inputParams_.cDtype = yDesc->GetDataType();
     if (inputParams_.cDtype == ge::DT_INT32 && scaleDesc != nullptr) {
         inputParams_.scaleDtype = ge::DT_UINT64;
-        scaleDesc->SetDataType(ge::DT_UINT64);
+        (void)const_cast<gert::CompileTimeTensorDesc*>(scaleDesc)->SetDataType(ge::DT_UINT64);
     }
     isWeightNz_ = inputParams_.bFormat == ge::FORMAT_FRACTAL_NZ;
     if (isWeightNz_) {
