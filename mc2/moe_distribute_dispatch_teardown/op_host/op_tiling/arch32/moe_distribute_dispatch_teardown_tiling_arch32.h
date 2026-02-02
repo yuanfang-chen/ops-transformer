27532ea372ff85d44d671171a430574bee268c8a
陈佳乐 <chenjiale56@huawei.com>
2026-02-02 17:29:44 +0800

    feat: 新增dispatch_tearodwn host侧实现及ut
    feat: 迁移dispatch_teardown原A3kernel及tiling实现
    
    fix: 修改soc型号，抽取tiling公共方法

---
+++ mc2/moe_distribute_dispatch_teardown/op_host/op_tiling/arch32/moe_distribute_dispatch_teardown_tiling_arch32.h
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*!
+ * \file moe_distribute_dispatch_teardown_tiling_arch32.h
+ * \brief
+ */
+
+#ifndef MOE_DISTRIBUTE_DISPATCH_TEARDOWN_TILING_ARCH32_H_
+#define MOE_DISTRIBUTE_DISPATCH_TEARDOWN_TILING_ARCH32_H_
+
+#include "../moe_distribute_dispatch_teardown_tiling_base.h"
+
+namespace optiling {
+class MoeDistributeDispatchTeardownTilingA3 : public MoeDistributeDispatchTeardownTilingBase
+{
+public:
+    explicit MoeDistributeDispatchTeardownTilingA3(gert::TilingContext* context)
+        : MoeDistributeDispatchTeardownTilingBase(context)
+    {
+        socTilingName_ = "MoeDistributeDispatchTeardownA3";
+    }
+};
+} // namespace optiling
+#endif // MOE_DISTRIBUTE_DISPATCH_TEARDOWN_TILING_ARCH32_H_
