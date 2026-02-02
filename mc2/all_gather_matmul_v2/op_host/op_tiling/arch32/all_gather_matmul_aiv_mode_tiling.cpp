c117a3147088b1d7d1af5b2e91e3f2d5cd89839d
zhumingzhe 00880305 <zhumingzhe2@huawei.com>
2026-02-02 19:25:22 +0800

    optimize

--- mc2/all_gather_matmul_v2/op_host/op_tiling/arch32/all_gather_matmul_aiv_mode_tiling.cpp
+++ mc2/all_gather_matmul_v2/op_host/op_tiling/arch32/all_gather_matmul_aiv_mode_tiling.cpp
@@ -126,6 +126,22 @@ void DealTilingParamByBuffSize(CoCTiling &cocTilingData) {
 }
 
 // Tiling Code Function
+void AllGatherV2MatmulNPU910BTwoRankINT8Tiling(CoCTiling &cocTilingData)
+{
+    int32_t code = ALLGATHERV2_MATMUL_NPU910B_TWO_RANK_INT8_CODE_DEFAULT;
+    std::map<int*, TilingValue> TilingParamMap = {
+        {&code,
+         {ALLGATHERV2_MATMUL_NPU910B_TWO_RANK_INT8_CODE_DEFAULT,
+          g_allGatherV2MatmulNPU910BTwoRankINT8CodeMap}}
+    };
+    SetTilingParam(cocTilingData, TilingParamMap);
+
+    AllGatherV2DecodeTilingData(code, cocTilingData);
+
+    cocTilingData.lenPerLoop = cocTilingData.ubMoveNum * cocTilingData.commDataSplit;
+    DealTilingParamByBuffSize(cocTilingData);
+}
+
 void AllGatherV2MatmulNPU910BFourRankINT8Tiling(CoCTiling &cocTilingData)
 {
     int32_t code = ALLGATHERV2_MATMUL_NPU910B_FOUR_RANK_INT8_CODE_DEFAULT;
@@ -380,6 +396,9 @@ void GetUsrWorkSpaceSize(uint32_t nElemAlign, uint32_t elementSize, uint64_t &us
     if (info.quantFlag) {
         userWorkSpaceSize += static_cast<uint64_t>(info.M * info.N * rankSize * sizeof(int32_t));
     }
+    if (info.dequantType == DequantType::PER_TOKEN) {
+        userWorkSpaceSize += static_cast<uint64_t>(info.M * rankSize * sizeof(float32_t));
+    }
 }
 
 static bool CheckDtypeX1(gert::TilingContext *context)
@@ -413,36 +432,52 @@ static bool CheckDtypeX2(gert::TilingContext *context, AllGatherMatmulAIVModeInf
     return false;
 }
 
+bool SetTilingDataA3(CoCTiling &cocTilingData, AllGatherMatmulAIVModeInfo &info, int64_t rankSize) 
+{
+    if (rankSize == RANKSIZE_FOUR && info.quantFlag) {
+        AllGatherV2MatmulNPU91093FourRankINT8Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_FOUR && !info.quantFlag) {
+        AllGatherV2MatmulNPU91093FourRankFP16Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_EIGHT && info.quantFlag) {
+        AllGatherV2MatmulNPU91093EightRankINT8Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_EIGHT && !info.quantFlag) {
+        AllGatherV2MatmulNPU91093EightRankFP16Tiling(cocTilingData);
+        return true;
+    }
+    return false;
+}
+
+bool SetTilingDataA2(CoCTiling &cocTilingData, AllGatherMatmulAIVModeInfo &info, int64_t rankSize) 
+{
+    if (rankSize == RANKSIZE_TWO && info.quantFlag) {
+        AllGatherV2MatmulNPU910BTwoRankINT8Tiling(cocTilingData);
+ 	    return true;
+    } else if (rankSize == RANKSIZE_FOUR && info.quantFlag) {
+        AllGatherV2MatmulNPU910BFourRankINT8Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_FOUR && !info.quantFlag) {
+        AllGatherV2MatmulNPU910BFourRankFP16Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_EIGHT && info.quantFlag) {
+        AllGatherV2MatmulNPU910BEightRankINT8Tiling(cocTilingData);
+        return true;
+    } else if (rankSize == RANKSIZE_EIGHT && !info.quantFlag) {
+        AllGatherV2MatmulNPU910BEightRankFP16Tiling(cocTilingData);
+        return true;
+    }
+    return false;
+}
+
 void SetTilingData(CoCTiling &cocTilingData, AllGatherMatmulAIVModeInfo &info, int64_t rankSize)
 {
-    if (info.is910C) {
-        if (rankSize == RANKSIZE_FOUR && info.quantFlag) {
-            AllGatherV2MatmulNPU91093FourRankINT8Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_FOUR && !info.quantFlag) {
-            AllGatherV2MatmulNPU91093FourRankFP16Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_EIGHT && info.quantFlag) {
-            AllGatherV2MatmulNPU91093EightRankINT8Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_EIGHT && !info.quantFlag) {
-            AllGatherV2MatmulNPU91093EightRankFP16Tiling(cocTilingData);
-            return;
-        }
-    } else {
-        if (rankSize == RANKSIZE_FOUR && info.quantFlag) {
-            AllGatherV2MatmulNPU910BFourRankINT8Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_FOUR && !info.quantFlag) {
-            AllGatherV2MatmulNPU910BFourRankFP16Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_EIGHT && info.quantFlag) {
-            AllGatherV2MatmulNPU910BEightRankINT8Tiling(cocTilingData);
-            return;
-        } else if (rankSize == RANKSIZE_EIGHT && !info.quantFlag) {
-            AllGatherV2MatmulNPU910BEightRankFP16Tiling(cocTilingData);
-            return;
-        }
+    cocTilingData.rankSize = rankSize;
+    if (info.is910C && SetTilingDataA3(cocTilingData, info, rankSize)) {
+        return;
+    } else if (SetTilingDataA2(cocTilingData, info, rankSize)){
+        return;
     }
     // Default Tiling Func
     AllGatherV2MatmulNPU910BEightRankFP16Tiling(cocTilingData);
