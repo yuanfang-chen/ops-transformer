afa1f49a086ffed22bf1b04d460bd6874fd85bbb
yiqiao-wjp <wangjianping48@huawei.com>
2026-02-02 11:29:28 +0800

    fix ut

--- posembedding/rotary_position_embedding/tests/ut/op_host/test_rotary_position_embedding_infershape.cpp
+++ posembedding/rotary_position_embedding/tests/ut/op_host/test_rotary_position_embedding_infershape.cpp
@@ -8,103 +8,156 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include <vector>
-#include <array>
-#include <float.h>
-#include "gtest/gtest.h"
-#include "../../../../op_host/op_api/aclnn_rotary_position_embedding.h"
-#include "../../../../op_host/op_api/aclnn_rotary_position_embedding_v2.h"
-#include "opdev/platform.h"
-#include "op_api_ut_common/tensor_desc.h"
-#include "op_api_ut_common/scalar_desc.h"
-#include "op_api_ut_common/op_api_ut.h"
+#include <gtest/gtest.h>
+#include <iostream>
+#include "infer_shape_context_faker.h"
+#include "infer_shape_case_executor.h"
+#include "base/registry/op_impl_space_registry_v2.h"
 
-using namespace std;
-
-class l2_rotary_position_embedding_test : public testing::Test {
+class RotaryPositionEmbeddingInfershape : public testing::Test {
 protected:
     static void SetUpTestCase()
     {
-        cout << "l2_rotary_position_embedding_test SetUp" << endl;
+        std::cout << "RotaryPositionEmbeddingInfershape Proto Test SetUp" << std::endl;
     }
 
     static void TearDownTestCase()
     {
-        cout << "l2_rotary_position_embedding_test TearDown" << endl;
+        std::cout << "RotaryPositionEmbeddingInfershape Proto Test TearDown" << std::endl;
     }
 };
 
-// dtype fp32
-TEST_F(l2_rotary_position_embedding_test, Ascend910B2_rotary_position_embedding_fp32)
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_infer_shape_fp16)
 {
-    auto x = TensorDesc({8, 1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-10, 10);
-    auto cos = TensorDesc({1, 1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto sin = TensorDesc({1, 1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(0, 8);
-    int64_t mode = 2;
-    auto out = TensorDesc({8, 1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND);
-    auto ut = OP_API_UT(aclnnRotaryPositionEmbedding, INPUT(x, cos, sin, mode), OUTPUT(out));
-    uint64_t workspaceSize = 0;
-    aclOpExecutor *executor = nullptr;
-    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
-    EXPECT_EQ(getWorkspaceResult, ACLNN_SUCCESS);
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{1, 64, 2, 64}, {1, 64, 2, 64}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{1, 64, 2, 64}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
-TEST_F(l2_rotary_position_embedding_test, Ascend910B2_rotary_position_embedding_fp32_tnd)
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_infer_shape_bf16)
 {
-    auto x = TensorDesc({1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-10, 10);
-    auto cos = TensorDesc({1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto sin = TensorDesc({1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(0, 8);
-    int64_t mode = 2;
-    auto out = TensorDesc({1, 1, 64}, ACL_FLOAT, ACL_FORMAT_ND);
-    auto ut = OP_API_UT(aclnnRotaryPositionEmbedding, INPUT(x, cos, sin, mode), OUTPUT(out));
-    uint64_t workspaceSize = 0;
-    aclOpExecutor *executor = nullptr;
-    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
-    EXPECT_EQ(getWorkspaceResult, ACLNN_SUCCESS);
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{1, 64, 2, 64}, {1, 64, 2, 64}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{1, 64, 2, 64}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
-// dtype bf16
-TEST_F(l2_rotary_position_embedding_test, Ascend910B2_rotary_position_embedding_rotate_matrix_bf16_001)
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_infer_shape_fp32)
 {
-    auto x = TensorDesc({1, 24, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-10, 10);
-    auto cos = TensorDesc({1, 1, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto sin = TensorDesc({1, 1, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto rotate = TensorDesc({128, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-1, 1);
-    int64_t mode = 0; 
-    auto out = TensorDesc({1, 24, 32, 128}, ACL_BF16, ACL_FORMAT_ND);
-    auto ut = OP_API_UT(aclnnRotaryPositionEmbeddingV2, INPUT(x, cos, sin, mode, rotate), OUTPUT(out));
-    uint64_t workspaceSize = 0;
-    aclOpExecutor *executor = nullptr;
-    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
-    EXPECT_EQ(getWorkspaceResult, ACLNN_SUCCESS);
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{1, 64, 2, 64}, {1, 64, 2, 64}}, ge::DT_FLOAT, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_FLOAT, ge::FORMAT_ND},
+            {{{1, 64, 1, 64}, {1, 64, 1, 64}}, ge::DT_FLOAT, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{1, 64, 2, 64}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
-TEST_F(l2_rotary_position_embedding_test, Ascend910B2_rotary_position_embedding_rotate_matrix_bf16_002_variable_s)
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_rotate_matrix_bf16_001)
 {
-    auto x = TensorDesc({1, 24, 64, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-10, 10);
-    auto cos = TensorDesc({1, 1, 64, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto sin = TensorDesc({1, 1, 64, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto rotate = TensorDesc({128, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-1, 1);
-    int64_t mode = 0;
-    auto out = TensorDesc({1, 24, 64, 128}, ACL_BF16, ACL_FORMAT_ND);
-    auto ut = OP_API_UT(aclnnRotaryPositionEmbeddingV2, INPUT(x, cos, sin, mode, rotate), OUTPUT(out));
-    uint64_t workspaceSize = 0;
-    aclOpExecutor *executor = nullptr;
-    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
-    EXPECT_EQ(getWorkspaceResult, ACLNN_SUCCESS);
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{1, 24, 32, 128}, {1, 24, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 1, 32, 128}, {1, 1, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+             {{{1, 1, 32, 128}, {1, 1, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{128, 128}, {128, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{1, 24, 32, 128}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
 
-TEST_F(l2_rotary_position_embedding_test, Ascend910B2_rotary_position_embedding_rotate_matrix_bf16_003_multi_batch)
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_rotate_matrix_bf16_002_variable_s)
 {
-    auto x = TensorDesc({2, 24, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-10, 10);
-    auto cos = TensorDesc({1, 1, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto sin = TensorDesc({1, 1, 32, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(0, 8);
-    auto rotate = TensorDesc({128, 128}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-1, 1);
-    int64_t mode = 0;
-    auto out = TensorDesc({2, 24, 32, 128}, ACL_BF16, ACL_FORMAT_ND);
-    auto ut = OP_API_UT(aclnnRotaryPositionEmbeddingV2, INPUT(x, cos, sin, mode, rotate), OUTPUT(out));
-    uint64_t workspaceSize = 0;
-    aclOpExecutor *executor = nullptr;
-    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSizeWithNNopbaseInner(&workspaceSize, executor);
-    EXPECT_EQ(getWorkspaceResult, ACLNN_SUCCESS);
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{1, 24, 64, 128}, {1, 24, 64, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 1, 64, 128}, {1, 1, 64, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 1, 64, 128}, {1, 1, 64, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{128, 128}, {128, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{1, 24, 64, 128}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(RotaryPositionEmbeddingInfershape, RotaryPositionEmbedding_rotate_matrix_bf16_003_multi_batch)
+{
+    gert::InfershapeContextPara infershapeContextPara(
+        "RotaryPositionEmbedding",
+        {
+            // input info
+            {{{2, 24, 32, 128}, {2, 24, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 1, 32, 128}, {1, 1, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{1, 1, 32, 128}, {1, 1, 32, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+            {{{128, 128}, {128, 128}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // output info
+            {{{}, {}}, ge::DT_BF16, ge::FORMAT_ND},
+        },
+        {
+            // attr
+            {"mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {{2, 24, 32, 128}};
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
 }
