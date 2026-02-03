3c70a3c1ed16a6429b4570d86e4b445416ff6cc1
weixin_43499740 <jinzuxin@huawei.com>
2026-02-03 23:01:59 +0800

    check

--- experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
+++ experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
@@ -47,8 +47,8 @@ enum class SparseMode : uint8_t {
 };
 
 enum class ValidSocVersion {
-    ASCEND910B = 0,
-    ASCEND910D,
+    ASCEND910 = 0,
+    ASCEND950,
     RESERVED_VERSION = 99999
 };
 
@@ -233,6 +233,12 @@ public:
 private:
     bool Prepare(CpuKernelContext &ctx);
     bool ParamsCheck();
+    void GetQueryBatchSize(uint32_t &bSize);
+    void GetKvBatchSize(uint32_t &bSize);
+    bool CheckSingleParam();
+    bool CheckExistence();
+    bool CheckConsistency();
+    bool CheckFeature();
     bool ParamsInit();
     bool BalanceSchedule(SplitResult &splitRes);
     bool GenMetaData(SplitResult &splitRes);
@@ -302,7 +308,7 @@ private:
     uint32_t headDim_ = 0;
     uint32_t oriTopK_ = 0;
     uint32_t cmpTopK_ = 0;
-    uint32_t cmpRatio_ = -1;
+    int32_t cmpRatio_ = -1;
     uint32_t oriMaskMode_ = 4;
     uint32_t cmpMaskMode_ = 3;
     int64_t winLeft_ = 127;
