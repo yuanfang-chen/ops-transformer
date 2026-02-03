f0bdf027e26d9ff7fa8eaabe0a3032eb5ea5dd86
steppecat <liangyu62@huawei.com>
2026-02-03 21:30:51 +0800

    a

--- gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
+++ gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
@@ -537,7 +537,8 @@ bool GroupedQbmmTiling::CheckActiveMode(const gert::Shape &wScaleShape, const ge
                     "the shape of perTokenScale should be (%d,), "
                     "actual is (%d,).", inputParams_.mSize, static_cast<uint64_t>(xScaleShape[0])), return false);
     }
-    
+    printf(" !!!!!inputParams_.bQuantMode, %d",inputParams_.bQuantMode);
+    printf(" !!!!!inputParams_.aQuantMode, %d",inputParams_.aQuantMode);
     if (!(wScaleDims == 2 && wScaleShape[wScaleDims - 1] == 1 && inputParams_.nSize == 1)) { // scale为2维且shape为(g,1)时，不做拦截
         OP_CHECK_IF(wScaleDims != 2, OP_LOGE(context_->GetNodeName(), // 在启用激活函数情景下，Scale应该为2维
                     "When the activation function is enabled, the dim of Scale should be 2, "
