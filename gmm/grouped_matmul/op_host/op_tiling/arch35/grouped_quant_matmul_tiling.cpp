f946865bc07c6d753242a5fc58b2364ab3d93050
steppecat <liangyu62@huawei.com>
2026-02-03 21:28:37 +0800

    a

--- gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
+++ gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
@@ -549,6 +549,8 @@ bool GroupedQbmmTiling::CheckActiveMode(const gert::Shape &wScaleShape, const ge
                     "actual is (%d, %d).", inputParams_.groupNum, inputParams_.nSize,
                     static_cast<uint64_t>(wScaleShape[0]), static_cast<uint64_t>(wScaleShape[1])),
                     return false);
+    } else { // scale为2维且shape为(g,1)时，作为
+        inputParams_.bQuantMode = optiling::QuantMode::PERCHANNEL_MODE;
     }
     return true;
 }
