c5127eeb400def2e20c1ad2c0d5a07334249863a
jiangxiuhan1 <jiangxiuhan@huawei.com>
2026-02-03 09:10:47 +0800

    combineAddRnsNorm资料修改

--- mc2/moe_distribute_combine_add_rms_norm/examples/test_aclnn_moe_distribute_combine_add_rms_norm.cpp
+++ mc2/moe_distribute_combine_add_rms_norm/examples/test_aclnn_moe_distribute_combine_add_rms_norm.cpp
@@ -80,6 +80,7 @@ int CreateAclTensor(const std::vector<T> &hostData, const std::vector<int64_t> &
 
 int LaunchOneProcessDispatchAndCombine(Args &args)
 {
+    const char* rankTableFile = std::getenv("RANK_TABLE_FILE");
     int ret = aclrtSetCurrentContext(args.context);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtSetCurrentContext failed, ret %d\n", ret); return ret);
 
@@ -87,8 +88,10 @@ int LaunchOneProcessDispatchAndCombine(Args &args)
     ret = HcclGetCommName(args.hcclEpComm, hcomEpName);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] HcclGetEpCommName failed, ret %d\n", ret); return -1);
     char hcomTpName[128] = {0};
-    ret = HcclGetCommName(args.hcclTpComm, hcomTpName);
-    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] HcclGetTpCommName failed, ret %d\n", ret); return -1);
+    if (!rankTableFile) {
+        ret = HcclGetCommName(args.hcclTpComm, hcomTpName);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] HcclGetTpCommName failed, ret %d\n", ret); return -1);
+    }
     LOG_PRINT(
         "[INFO] rank = %d, hcomEpName = %s, hcomTpName = %s, dispatchStream = %p, combineStream = %p, context = %p\n",
         args.rankId, hcomEpName, hcomTpName, args.dispatchStream, args.combineStream, args.context
@@ -96,17 +99,17 @@ int LaunchOneProcessDispatchAndCombine(Args &args)
 
     int64_t BS = 8;
     int64_t H = 7168;
-    int64_t K = 3;
+    int64_t K = 2;
     int64_t expertShardType = 0;
     int64_t sharedExpertNum = 0;
     int64_t sharedExpertRankNum = 0;
-    int64_t moeExpertNum = 8;
+    int64_t moeExpertNum = 2;
     int64_t quantMode = 0;
     int64_t globalBS = BS * EP_WORLD_SIZE;
-    int64_t expertTokenNumsType = 1;
+    int64_t expertTokenNumsType = 0;
     int64_t outDtype = 0;
     int64_t commQuantMode = 0;
-    int64_t groupList_type = 1;
+    int64_t groupList_type = 0;
     int64_t localExpertNum;
     int64_t A;
     if (args.epRankId < sharedExpertRankNum) {
@@ -434,7 +437,6 @@ int LaunchOneProcessDispatchAndCombine(Args &args)
 
 int main(int argc, char *argv[])
 {
-    // 本样例基于Atlas A3实现，必须在Atlas A3上运行
     int ret = aclInit(nullptr);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclInit failed, ret = %d\n", ret); return ret);
 
