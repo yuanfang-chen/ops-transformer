e1a8cd705b56a547228b77ce1bd139b9b889f88d
yu-xinjie62 <yuxinjie1@huawei.com>
2026-02-03 15:55:22 +0800

    rope supports 310p compile.

--- posembedding/rotary_position_embedding/op_kernel/rotary_position_embedding.cpp
+++ posembedding/rotary_position_embedding/op_kernel/rotary_position_embedding.cpp
@@ -14,21 +14,26 @@
  */
 #include "rotate_half.h"
 #include "rotate_half_bf16.h"
+
+#if !(defined(__CCE_AICORE__) && __CCE_AICORE__ == 200)
 #include "rotate_interleaved_split_s.h"
 #include "rotate_interleaved_split_bs.h"
 #include "rotate_interleaved_split_bsn.h"
 #include "rotate_interleaved_split_s_pad.h"
 #include "rotate_interleaved_split_bs_pad.h"
 #include "rotate_interleaved_split_bsn_pad.h"
+<<<<<<< HEAD
 #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 220
 #include "rotate_matrix.h"
 using namespace RotateMatrix;
 using namespace matmul;
+=======
+using namespace RotateInterleavedN;
+>>>>>>> 6bf23b5d (rope supports 310p compile.)
 #endif
 
 using namespace AscendC;
 using namespace RotateHalfN;
-using namespace RotateInterleavedN;
 
 extern "C" __global__ __aicore__ void rotary_position_embedding(GM_ADDR x, GM_ADDR cos, GM_ADDR sin, GM_ADDR rotate,
                                                                 GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling)
@@ -86,7 +91,7 @@ extern "C" __global__ __aicore__ void rotary_position_embedding(GM_ADDR x, GM_AD
         RotateHalfBf16<half, float> rotateHalfOp;
         rotateHalfOp.Init(x, cos, sin, y, tilingData);
         rotateHalfOp.Process();
-#if !(defined(__NPU_ARCH__) && (__NPU_ARCH__ == 3003 || __NPU_ARCH__ == 3113))
+#if !(((defined(__NPU_ARCH__) && (__NPU_ARCH__ == 3003 || __NPU_ARCH__ == 3113)) || (defined(__CCE_AICORE__) && __CCE_AICORE__ == 200)))
     } else if (TILING_KEY_IS(1013)) {
         RotateHalfBf16<bfloat16_t, float> rotateHalfOp;
         rotateHalfOp.Init(x, cos, sin, y, tilingData);
@@ -115,6 +120,7 @@ extern "C" __global__ __aicore__ void rotary_position_embedding(GM_ADDR x, GM_AD
     }
 
     // mode: rotate_interleaved
+#if !(defined(__CCE_AICORE__) && __CCE_AICORE__ == 200)
     if (TILING_KEY_IS(2000)) {
         TPipe pipe;
         InterleavedSplitS<half> interleavedSplitS;
