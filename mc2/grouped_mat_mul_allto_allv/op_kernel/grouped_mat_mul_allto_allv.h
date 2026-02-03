3af1a076df1445efff07a2bae28aea4286ce2a12
Yuanfeng_HW <yuanfeng24@huawei.com>
2026-02-03 21:35:11 +0800

    空tensor支持

--- mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv.h
+++ mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv.h
@@ -236,7 +236,9 @@ __aicore__ inline void GroupedMatmulAlltoAllv<GMMATAV>::GmmProcessAlltoallv()
         mmWeightOffset[0] = i * axisH_ * axisN1_;
         tokenNum[0] = gmmTokennum[i];
         if ASCEND_IS_AIC {
-            gmmOp.Process(this->rankId_, axisH_, axisN1_, mmInOffset, mmOutOffset, tokenNum, processNum, i);
+            if (tilingData->GmmAlltoAllvCommonTilingInfo.isNeedGmm) {
+                gmmOp.Process(this->rankId_, axisH_, axisN1_, mmInOffset, mmOutOffset, tokenNum, processNum, i);
+            }
         }
         SyncAll<false>();
         if ASCEND_IS_AIV {
