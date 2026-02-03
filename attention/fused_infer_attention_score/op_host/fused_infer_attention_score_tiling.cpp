cb4a14c0871c9e29e9f2bfd5f24446e8634bf55a
wangsong96 <wangsong109@huawei.com>
2026-02-03 15:35:06 +0800

    fix

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -24,6 +24,7 @@
 #include "err/ops_err.h"
 
 #include "tiling/tiling_api.h"
+
 #include "platform/platform_info.h"
 #include "arch32/fused_infer_attention_score_tiling_v3.h"
 #include "flash_attention_infer_tiling.h"
