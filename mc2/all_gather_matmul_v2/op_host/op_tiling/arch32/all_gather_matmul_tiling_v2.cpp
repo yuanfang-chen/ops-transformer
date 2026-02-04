91757ed36357e0299b5beeb0ca86bf83b67532b0
inspirations <wangke201@huawei.com>
2026-02-04 19:10:18 +0800

    (register)replace socversion with npuArch

--- mc2/all_gather_matmul_v2/op_host/op_tiling/arch32/all_gather_matmul_tiling_v2.cpp
+++ mc2/all_gather_matmul_v2/op_host/op_tiling/arch32/all_gather_matmul_tiling_v2.cpp
@@ -224,6 +224,6 @@ AllGatherMatmulTilingV2::AllGatherMatmulTilingV2(gert::TilingContext* context)
 {
 }
 //注册Tiling类
-REGISTER_TILING_TEMPLATE_WITH_SOCVERSION(AllGatherMatmulV2, AllGatherMatmulTilingV2, \
-                                        static_cast<int32_t>(platform_ascendc::SocVersion::ASCEND950), 0);
+REGISTER_TILING_TEMPLATE_WITH_ARCH(AllGatherMatmulV2, AllGatherMatmulTilingV2, \
+                                   static_cast<int32_t>(NpuArch::DAV_3510), 0);
 }  // namespace optiling
