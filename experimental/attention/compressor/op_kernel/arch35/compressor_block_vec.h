f7b87a189adb90da90236c0250f5274e7f1425d6
chenxin_169 <chenxin169@hisilicon.com>
2026-02-04 20:11:09 +0800

    fix ReadState

--- experimental/attention/compressor/op_kernel/arch35/compressor_block_vec.h
+++ experimental/attention/compressor/op_kernel/arch35/compressor_block_vec.h
@@ -764,8 +764,25 @@ __aicore__ inline void CompressorBlockVector<COMP>::ReadState(const LocalTensor<
                     // dDealSize必须为64
                     Duplicate(kvLocal, FLOAT_ZERO, dDealSize, constInfo_.cmpRatio, 1, 2 * dDealSize / 8);
                     Duplicate(scoreLocal, SOFTMAX_MIN_NUM, dDealSize, constInfo_.cmpRatio, 1, 2 * dDealSize / 8);
+                    if (blockInfo.bStartPos) {
+                        uint32_t copySeqCnt = blockInfo.bStartPos;
+                        uint64_t endSeqIdx = blockInfo.bStartPos;
+                        uint64_t startSeqIdx = endSeqIdx - copySeqCnt;
+                        uint64_t srcBaseOffset = dDealSize * constInfo_.cmpRatio * 2;
+                        ReadFromCacheState(kvLocal[srcBaseOffset], kvStateGm_, kvBlockTableGm_, blockInfo.bIdx, startSeqIdx, endSeqIdx, dStartIdx, dDealSize);
+                        ReadFromCacheState(scoreLocal[srcBaseOffset], scoreStateGm_, scoreBlockTableGm_, blockInfo.bIdx, startSeqIdx, endSeqIdx, dStartIdx, dDealSize);
+                    }
                 } else {
                     uint32_t copySeqCnt = constInfo_.cmpRatio + blockInfo.bStartPos % constInfo_.cmpRatio;
+                    uint64_t endSeqIdx = blockInfo.bStartPos;
+                    uint64_t startSeqIdx = endSeqIdx - copySeqCnt;
+                    uint64_t srcBaseOffset = 0;
+                    ReadFromCacheState(kvLocal, kvStateGm_, kvBlockTableGm_, blockInfo.bIdx, startSeqIdx, endSeqIdx, dStartIdx, dDealSize);
+                    ReadFromCacheState(scoreLocal, scoreStateGm_, scoreBlockTableGm_, blockInfo.bIdx, startSeqIdx, endSeqIdx, dStartIdx, dDealSize);
+                }
+            } else if (blockInfo.sIdx == (constInfo_.cmpRatio - (blockInfo.bStartPos % constInfo_.cmpRatio))) {
+                if ((blockInfo.bStartPos % constInfo_.cmpRatio) > 0) {
+                    uint32_t copySeqCnt = blockInfo.bStartPos % constInfo_.cmpRatio;
                     uint64_t endSeqIdx = blockInfo.bStartPos;
                     uint64_t startSeqIdx = endSeqIdx - copySeqCnt;
                     uint64_t srcBaseOffset = 0;
