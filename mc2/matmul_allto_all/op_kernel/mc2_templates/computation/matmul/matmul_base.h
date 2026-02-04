0c4a0c1bc7fb1f1cb9a85428b3403b0f9d95281a
qq_43844249 <fanglin17@huawei.com>
2026-02-04 17:37:22 +0800

    mc2 template mm compute stage refactor.

--- mc2/matmul_allto_all/op_kernel/mc2_templates/computation/matmul/matmul_base.h
+++ mc2/matmul_allto_all/op_kernel/mc2_templates/computation/matmul/matmul_base.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -9,21 +9,37 @@
  */
 
 /*!
- * \file matmul_factory.h
+ * \file matmul_base.h
  * \brief
  */
 
-#ifndef MC2_MATMUL_FACTORY_H
-#define MC2_MATMUL_FACTORY_H 
+#ifndef MC2_MATMUL_BASE_H
+#define MC2_MATMUL_BASE_H
+
+#include "../../../arch35/3rd_head.h"
 
 namespace MC2KernelTemplate {
-//基本输入输出
-struct BaseGmAddrs {
+// 基本输入输出和偏移
+struct MC2MMBaseGmAddrs {
     GM_ADDR aGM;
     GM_ADDR bGM;
     GM_ADDR cGM;
     GM_ADDR biasGM;
+    uint64_t aOffset;
+    uint64_t bOffset;
+    uint64_t cOffset;
 };
-};  // namespace MC2kernelComputation
+
+// matmul计算节点的数据上下文
+template <typename AdditionalGmAddrDataType, typename TilingDataType>
+struct MC2MMContext {
+    // 基本输入输出和偏移
+    MC2MMBaseGmAddrs baseData;
+    // 不同场景下额外的输入输出和偏移
+    AdditionalGmAddrDataType additionalData;
+    // mamtul的tiling
+    TilingDataType* tilingDataPtr;
+};
+}; // namespace MC2KernelTemplate
 
 #endif
