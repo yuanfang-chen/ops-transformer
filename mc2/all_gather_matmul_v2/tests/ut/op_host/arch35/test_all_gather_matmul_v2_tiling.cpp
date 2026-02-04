5d33a7cab370a75b672764c3d83670f5de952093
yangyajie <yangyajie3@huawei.com>
2026-02-04 15:28:15 +0800

    AllgatherMMV2

---
+++ mc2/all_gather_matmul_v2/tests/ut/op_host/arch35/test_all_gather_matmul_v2_tiling.cpp
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include "../all_gather_matmul_v2_host_ut_param.h"
+#include "mc2_tiling_case_executor.h"
+
+namespace all_gather_matmul_v2_ut {
+
+class Arch35TilingTest : public testing::TestWithParam<AllGatherMatmulV2TilingUtParam> {
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "AllGatherMatmulV2 Arch35TilingTest SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "AllGatherMatmulV2 Arch35TilingTest TearDown" << std::endl;
+    }
+};
+
+TEST_P(Arch35TilingTest, param)
+{
+    auto param = GetParam();
+    struct AllGatherMatmulV2CompileInfo {};
+    AllGatherMatmulV2CompileInfo compileInfo;
+    gert::TilingContextPara tilingContextPara(
+        "AllGatherMatmulV2",
+        {
+            param.x1,
+            param.x2,
+            param.bias,
+            param.x1_scale,
+            param.x2_scale,
+            param.quant_scale,
+        },
+        {
+            param.y,
+            param.gather_out,
+            param.amax_out,
+        },
+        {
+            {"group", Ops::Transformer::AnyValue::CreateFrom<std::string>(param.group)},
+            {"is_trans_a", Ops::Transformer::AnyValue::CreateFrom<bool>(param.is_trans_a)},
+            {"is_trans_b", Ops::Transformer::AnyValue::CreateFrom<bool>(param.is_trans_b)},
+            {"gather_index", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.gather_index)},
+            {"comm_turn", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.comm_turn)},
+            {"rank_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.rank_size)},
+            {"block_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.block_size)},
+            {"group_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.group_size)},
+            {"is_gather_out", Ops::Transformer::AnyValue::CreateFrom<bool>(param.is_gather_out)},
+            {"is_amax_out", Ops::Transformer::AnyValue::CreateFrom<bool>(param.is_amax_out)},
+            {"y_dtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.y_dtype)},
+            {"comm_mode", Ops::Transformer::AnyValue::CreateFrom<std::string>(param.comm_mode)}
+        },
+        param.inputInstance, param.outputInstance,
+        &compileInfo,
+        param.soc, param.coreNum, param.ubsize
+    );
+    Mc2Hcom::MockValues hcomTopologyMockValues {
+        {"rankNum", param.rank_size}
+    };
+    Mc2ExecuteTestCase(tilingContextPara, hcomTopologyMockValues, param.expectResult, param.expectTilingKey,
+        param.expectTilingDataHash, {}, MC2_TILING_DATA_RESERVED_LEN, true);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    AllGatherMatmulV2,
+    Arch35TilingTest,
+    testing::ValuesIn(GetCasesFromCsv<AllGatherMatmulV2TilingUtParam>(ReplaceFileExtension2Csv(__FILE__))),
+    GetCaseInfoString<AllGatherMatmulV2TilingUtParam>
+);
+
+} // namespace all_gather_matmul_v2_ut
