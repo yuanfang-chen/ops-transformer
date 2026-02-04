f7e5a81f11565ac2c03f999620dc3b99c0d9da21
fazhenyao <fazhenyao@h-partners.com>
2026-02-04 11:48:23 +0800

    【迁移】inplace_index_add、masked_scatter

--- moe/moe_token_permute_with_routing_map_grad/op_host/moe_token_permute_with_routing_map_grad_def.cpp
+++ moe/moe_token_permute_with_routing_map_grad/op_host/moe_token_permute_with_routing_map_grad_def.cpp
@@ -68,6 +68,13 @@ public:
                 {ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
         this->AICore().AddConfig("ascend910b");
         this->AICore().AddConfig("ascend910_93");
+        OpAICoreConfig config_910_95;
+        config_910_95.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(false)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false);
+        this->AICore().AddConfig("ascend910_95", config_910_95);
     }
 };
 
