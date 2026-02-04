69ae88ed56fa5e101d36dd80b30e7c027471cff9
qq_32807861 <handongchen2@huawei.com>
2026-02-04 15:50:49 +0000

    check更新

--- experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_kernel_aicpu/kv_quant_sparse_attn_sharedkv_metadata_aicpu.cpp
+++ experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_kernel_aicpu/kv_quant_sparse_attn_sharedkv_metadata_aicpu.cpp
@@ -233,10 +233,6 @@ bool KvQuantSparseAttnSharedkvMetadataCpuKernel::CheckFeature() {
         KERNEL_LOG_ERROR("When ori_kv is disabled, oriTopK_ should be 0!");
         return false;
     }
-    if (!hasCmpKv_ && cmpTopK_ != 0) {
-        KERNEL_LOG_ERROR("When cmp_kv is disabled, cmpTopK_ should be 0!");
-        return false;
-    }
     return true;
 }
 
