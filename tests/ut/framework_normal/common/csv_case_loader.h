de416de0df02c66c0e6069e4d08dc0b8d4e96b57
xutianze <xutianze2@huawei.com>
2026-02-05 16:50:32 +0800

    重构matmul_all_reduce opapi用例

--- tests/ut/framework_normal/common/csv_case_loader.h
+++ tests/ut/framework_normal/common/csv_case_loader.h
@@ -8,10 +8,11 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#ifndef MC2_CSV_CASE_LOADER_H
-#define MC2_CSV_CASE_LOADER_H
+#ifndef CSV_CASE_LOADER_H
+#define CSV_CASE_LOADER_H
 
 #include <iostream>
+#include <cstdint>
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -19,6 +20,8 @@
 #include <filesystem>
 #include "tiling_context_faker.h"
 #include "infer_shape_context_faker.h"
+#include "op_api_ut_common/tensor_desc.h"
+#include "opdev/platform.h"
 
 using csv_map = std::unordered_map<std::string, std::string>;
 
@@ -117,6 +120,60 @@ const std::unordered_map<std::string, ge::Format> GE_FORMAT {
     {"NCL", ge::FORMAT_NCL}
 };
 
+const std::unordered_map<std::string, aclDataType> ACL_DTYPE {
+    {"UNDEFINED", ACL_DT_UNDEFINED},
+    {"FLOAT", ACL_FLOAT},
+    {"FLOAT16", ACL_FLOAT16},
+    {"INT8", ACL_INT8},
+    {"INT32", ACL_INT32},
+    {"UINT8", ACL_UINT8},
+    {"INT16", ACL_INT16},
+    {"UINT16", ACL_UINT16},
+    {"UINT32", ACL_UINT32},
+    {"INT64", ACL_INT64},
+    {"UINT64", ACL_UINT64},
+    {"DOUBLE", ACL_DOUBLE},
+    {"BOOL", ACL_BOOL},
+    {"STRING", ACL_STRING},
+    {"COMPLEX64", ACL_COMPLEX64},
+    {"COMPLEX128", ACL_COMPLEX128},
+    {"BF16", ACL_BF16},
+    {"INT4", ACL_INT4},
+    {"UINT1", ACL_UINT1},
+    {"COMPLEX32", ACL_COMPLEX32},
+    {"HIFLOAT8", ACL_HIFLOAT8},
+    {"FLOAT8_E5M2", ACL_FLOAT8_E5M2},
+    {"FLOAT8_E4M3FN", ACL_FLOAT8_E4M3FN},
+    {"FLOAT8_E8M0", ACL_FLOAT8_E8M0},
+    {"FLOAT6_E3M2", ACL_FLOAT6_E3M2},
+    {"FLOAT6_E2M3", ACL_FLOAT6_E2M3},
+    {"FLOAT4_E2M1", ACL_FLOAT4_E2M1},
+    {"FLOAT4_E1M2", ACL_FLOAT4_E1M2}
+};
+
+const std::unordered_map<std::string, aclFormat> ACL_FORMAT {
+    {"UNDEFINED", ACL_FORMAT_UNDEFINED},
+    {"NCHW", ACL_FORMAT_NCHW},
+    {"NHWC", ACL_FORMAT_NHWC},
+    {"ND", ACL_FORMAT_ND},
+    {"NC1HWC0", ACL_FORMAT_NC1HWC0},
+    {"FRACTAL_Z", ACL_FORMAT_FRACTAL_Z},
+    {"NC1HWC0_C04", ACL_FORMAT_NC1HWC0_C04},
+    {"HWCN", ACL_FORMAT_HWCN},
+    {"NDHWC", ACL_FORMAT_NDHWC},
+    {"FRACTAL_NZ", ACL_FORMAT_FRACTAL_NZ},
+    {"NCDHW", ACL_FORMAT_NCDHW},
+    {"NDC1HWC0", ACL_FORMAT_NDC1HWC0},
+    {"FRACTAL_Z_3D", ACL_FRACTAL_Z_3D},
+    {"NC", ACL_FORMAT_NC},
+    {"NCL", ACL_FORMAT_NCL},
+    {"FRACTAL_NZ_C0_16", ACL_FORMAT_FRACTAL_NZ_C0_16},
+    {"FRACTAL_NZ_C0_32", ACL_FORMAT_FRACTAL_NZ_C0_32},
+    {"FRACTAL_NZ_C0_2", ACL_FORMAT_FRACTAL_NZ_C0_2},
+    {"FRACTAL_NZ_C0_4", ACL_FORMAT_FRACTAL_NZ_C0_4},
+    {"FRACTAL_NZ_C0_8", ACL_FORMAT_FRACTAL_NZ_C0_8}
+};
+
 template <typename Map>
 inline typename Map::mapped_type ReadMap(const Map& m, const typename Map::key_type& key,
     const typename Map::mapped_type& defaultValue = typename Map::mapped_type{})
@@ -171,8 +228,41 @@ inline int GetDataType(const csv_map& csvMap, const std::string& dtypeKey, ge::D
     return 1;
 }
 
+inline int GetDataType(const csv_map& csvMap, const std::string& dtypeKey, aclDataType& out)
+{
+    std::string dtypeStr = ReadMap(csvMap, dtypeKey);
+    if (dtypeStr.empty()) return 0;
+
+    out = ReadMap(ACL_DTYPE, dtypeStr, ACL_DT_UNDEFINED);
+    return 1;
+}
+
+inline op::SocVersion GetCaseSocVersion(const csv_map& csvMap, const std::string& socKey)
+{
+    std::string socStr = ReadMap(csvMap, socKey);
+    if (socStr.empty()) {
+        return op::SocVersion::ASCEND910B;
+    }
+    
+    const std::unordered_map<std::string, op::SocVersion> SOC_VERSION {
+        {"Ascend910", op::SocVersion::ASCEND910},
+        {"Ascend910B", op::SocVersion::ASCEND910B},
+        {"Ascend910_93", op::SocVersion::ASCEND910_93},
+        {"Ascend950", op::SocVersion::ASCEND950},
+        {"Ascend910E", op::SocVersion::ASCEND910E},
+        {"Ascend310", op::SocVersion::ASCEND310},
+        {"Ascend310P", op::SocVersion::ASCEND310P},
+        {"Ascend310B", op::SocVersion::ASCEND310B},
+        {"Ascend310C", op::SocVersion::ASCEND310C},
+        {"Ascend610LITE", op::SocVersion::ASCEND610LITE},
+        {"KirinX90", op::SocVersion::KIRINX90},
+        {"Kirin9030", op::SocVersion::KIRIN9030}
+    };
+    return ReadMap(SOC_VERSION, socStr, op::SocVersion::ASCEND910B);
+}
+
 template<typename T>
-inline int GetTensor(const csv_map& csvMap, const std::string& shapeKey, const std::string& dtypeKey,
+inline int GetTensorGE(const csv_map& csvMap, const std::string& shapeKey, const std::string& dtypeKey,
     const std::string& formatKey, T& out)
 {
     std::string shapeStr = ReadMap(csvMap, shapeKey);
@@ -187,6 +277,22 @@ inline int GetTensor(const csv_map& csvMap, const std::string& shapeKey, const s
     ge::Format format = ReadMap(GE_FORMAT, formatStr, ge::FORMAT_NULL);
     out = T(shape, dtype, format);
     return 1;
+}
+
+inline TensorDesc GetTensorACL(const csv_map& csvMap, const std::string& shapeKey, const std::string& dtypeKey,
+    const std::string& formatKey)
+{
+    std::string shapeStr = ReadMap(csvMap, shapeKey);
+    if (shapeStr.empty()) return;
+    std::string dtypeStr = ReadMap(csvMap, dtypeKey);
+    if (dtypeStr.empty()) return;
+    std::string formatStr = ReadMap(csvMap, formatKey);
+    if (formatStr.empty()) return;
+
+    std::vector<int64_t> shape = GetShapeArr(shapeStr);
+    aclDataType dtype = ReadMap(ACL_DTYPE, dtypeStr, ACL_DT_UNDEFINED);
+    aclFormat format = ReadMap(ACL_FORMAT, dtypeStr, ACL_FORMAT_UNDEFINED);
+    return TensorDesc(shape, dtype, format);
 }
 
 inline std::string ReplaceFileExtension2Csv(const char* file)
@@ -243,4 +349,10 @@ std::vector<T> GetCasesFromCsv(const std::string& csvPath)
     return cases;
 }
 
-#endif // MC2_CSV_CASE_LOADER_H
+template<typename T>
+inline std::string PrintCaseInfoString(const testing::TestParamInfo<T>& info)
+{
+    return info.param.case_name;
+}
+
+#endif // CSV_CASE_LOADER_H
