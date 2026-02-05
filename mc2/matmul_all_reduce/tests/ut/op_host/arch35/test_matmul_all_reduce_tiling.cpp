3fa23a4095488822a0857c8a9f30d9210963cbfc
inspirations <wangke201@huawei.com>
2026-02-05 11:07:02 +0800

    test ut

--- mc2/matmul_all_reduce/tests/ut/op_host/arch35/test_matmul_all_reduce_tiling.cpp
+++ mc2/matmul_all_reduce/tests/ut/op_host/arch35/test_matmul_all_reduce_tiling.cpp
@@ -70,7 +70,7 @@ TEST_P(Arch35TilingTest, param)
         {"rankNum", param.ranksize}
     };
     Mc2ExecuteTestCase(tilingContextPara, hcomTopologyMockValues, param.expectResult, param.expectTilingKey,
-        param.expectTilingDataHash, {}, MC2_TILING_DATA_RESERVED_LEN, true);
+        param.expectTilingDataHash, {}, MC2_TILING_DATA_RESERVED_LEN, 123);
 }
 
 INSTANTIATE_TEST_SUITE_P(
