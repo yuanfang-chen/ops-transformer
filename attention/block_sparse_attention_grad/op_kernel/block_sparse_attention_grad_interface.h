cd41124b4977fb372ab4554a4c9bb1142cd4801c
george_zhanglei <george_zhanglei@noreply.gitcode.com>
2026-02-05 20:28:26 +0800

    BlockSparseAttentionGrad 代码框架

--- attention/block_sparse_attention_grad/op_kernel/block_sparse_attention_grad_interface.h
+++ attention/block_sparse_attention_grad/op_kernel/block_sparse_attention_grad_interface.h
@@ -25,21 +25,21 @@ namespace BSA {
     template <
        typename InputDtype = half,
        uint32_t InputLayout = 0>      // 0=TND, 1=BNSD
-    __global__ __aicore__ void BlockSparseAttentionGradInfer(GM_ADDR dout,
-                                                             GM_ADDR q,
-                                                             GM_ADDR k,
-                                                             GM_ADDR v,
-                                                             GM_ADDR out,
-                                                             GM_ADDR softmaxLse,
-                                                             GM_ADDR blockSparseMask,
-                                                             GM_ADDR blockShape,
-                                                             GM_ADDR attentionMask,
-                                                             GM_ADDR actualQseqlen,
-                                                             GM_ADDR actualKvseqlen,
-                                                             GM_ADDR dq,
-                                                             GM_ADDR dk,
-                                                             GM_ADDR dv,
-                                                             GM_ADDR workspace, GM_ADDR tiling)
+    __aicore__ inline void BlockSparseAttentionGradInfer(GM_ADDR dout,
+                                                         GM_ADDR q,
+                                                         GM_ADDR k,
+                                                         GM_ADDR v,
+                                                         GM_ADDR out,
+                                                         GM_ADDR softmaxLse,
+                                                         GM_ADDR blockSparseMask,
+                                                         GM_ADDR blockShape,
+                                                         GM_ADDR attentionMask,
+                                                         GM_ADDR actualQseqlen,
+                                                         GM_ADDR actualKvseqlen,
+                                                         GM_ADDR dq,
+                                                         GM_ADDR dk,
+                                                         GM_ADDR dv,
+                                                         GM_ADDR workspace, GM_ADDR tiling)
     {
         using ArchTag = Arch::AtlasA2;
         using ElementQ = InputDtype;
@@ -48,60 +48,60 @@ namespace BSA {
         using LayoutK = layout::ColumnMajor;
         using ElementV = InputDtype;
         using LayoutV = layout::RowMajor;
-        using ElementS = SoftmaxDtype;
-        using LayoutS = layout::RowMajor;
-        using ElementP = InputDtype;
-        using LayoutP = layout::RowMajor;
-        using ElementO = InputDtype;
-        using LayoutO = layout::RowMajor;
-        using ElementLse = float; 
-        using LayoutLse = layout::RowMajor;
-        using ElementMask = int8_t;
-        using LayoutMask = layout::RowMajor;
-        using ElementOTmp = SoftmaxDtype;
-        using LayoutOTmp = layout::RowMajor;
-        using ElementUpdate = SoftmaxDtype;
-        using LayoutUpdate = layout::RowMajor;
+        // using ElementS = SoftmaxDtype;
+        // using LayoutS = layout::RowMajor;
+        // using ElementP = InputDtype;
+        // using LayoutP = layout::RowMajor;
+        // using ElementO = InputDtype;
+        // using LayoutO = layout::RowMajor;
+        // using ElementLse = float;
+        // using LayoutLse = layout::RowMajor;
+        // using ElementMask = int8_t;
+        // using LayoutMask = layout::RowMajor;
+        // using ElementOTmp = SoftmaxDtype;
+        // using LayoutOTmp = layout::RowMajor;
+        // using ElementUpdate = SoftmaxDtype;
+        // using LayoutUpdate = layout::RowMajor;
 
-        // Use sparse-specific dispatch policies
-        using L1TileShapeQK = GemmShape<Q_TILE_CEIL, 128, 128>;
-        using L0TileShapeQK = GemmShape<128, 128, 128>;
-        using DispatchPolicyQK = Gemm::MmadAtlasA2SFAIQK<false, false>;
-        using QType = Gemm::GemmType<ElementQ, LayoutQ>;
-        using KType = Gemm::GemmType<ElementK, LayoutK>;
-        using SType = Gemm::GemmType<ElementS, LayoutS>;
-        using BlockMmadQK = Gemm::Block::BlockMmad<DispatchPolicyQK, L1TileShapeQK, L0TileShapeQK,
-                                                QType, KType, SType>;
+        // // Use sparse-specific dispatch policies
+        // using L1TileShapeQK = GemmShape<Q_TILE_CEIL, 128, 128>;
+        // using L0TileShapeQK = GemmShape<128, 128, 128>;
+        // using DispatchPolicyQK = Gemm::MmadAtlasA2SFAIQK<false, false>;
+        // using QType = Gemm::GemmType<ElementQ, LayoutQ>;
+        // using KType = Gemm::GemmType<ElementK, LayoutK>;
+        // using SType = Gemm::GemmType<ElementS, LayoutS>;
+        // using BlockMmadQK = Gemm::Block::BlockMmad<DispatchPolicyQK, L1TileShapeQK, L0TileShapeQK,
+        //                                         QType, KType, SType>;
 
-        using L1TileShapePV = GemmShape<128, 128, 256>;
-        using L0TileShapePV = GemmShape<128, 128, 128>;
-        using DispatchPolicyPV = Gemm::MmadAtlasA2SFAIPV<false, false>;
-        using PType = Gemm::GemmType<ElementP, LayoutP>;
-        using VType = Gemm::GemmType<ElementV, LayoutV>;
-        using OTmpType = Gemm::GemmType<ElementOTmp, LayoutOTmp>;
-        using BlockMmadPV = Gemm::Block::BlockMmad<DispatchPolicyPV, L1TileShapePV, L0TileShapePV,
-                                                PType, VType, OTmpType>;
+        // using L1TileShapePV = GemmShape<128, 128, 256>;
+        // using L0TileShapePV = GemmShape<128, 128, 128>;
+        // using DispatchPolicyPV = Gemm::MmadAtlasA2SFAIPV<false, false>;
+        // using PType = Gemm::GemmType<ElementP, LayoutP>;
+        // using VType = Gemm::GemmType<ElementV, LayoutV>;
+        // using OTmpType = Gemm::GemmType<ElementOTmp, LayoutOTmp>;
+        // using BlockMmadPV = Gemm::Block::BlockMmad<DispatchPolicyPV, L1TileShapePV, L0TileShapePV,
+        //                                         PType, VType, OTmpType>;
 
-        // Epilogue policies for sparse attention
-        using DispatchPolicyOnlineSoftmax = Epilogue::EpilogueAtlasA2OnlineSoftmax<lseMode, SoftmaxDtype>;
-        using MaskType = Gemm::GemmType<ElementMask, LayoutMask>;
-        using EpilogueOnlineSoftmax = Epilogue::Block::BlockEpilogue<DispatchPolicyOnlineSoftmax, 
-                                                                    PType, SType, MaskType>;
-        using DispatchPolicyRescaleO = Epilogue::EpilogueAtlasA2RescaleO<lseMode, SoftmaxDtype>;
-        using OType = Gemm::GemmType<ElementO, LayoutO>;
-        using OUpdateType = Gemm::GemmType<ElementUpdate, LayoutUpdate>;
-        using LseType = Gemm::GemmType<ElementLse, LayoutLse>;
-        using EpilogueRescaleO = Epilogue::Block::BlockEpilogue<DispatchPolicyRescaleO, 
-                                                                OType, OTmpType, OUpdateType, LseType>;
+        // // Epilogue policies for sparse attention
+        // using DispatchPolicyOnlineSoftmax = Epilogue::EpilogueAtlasA2OnlineSoftmax<lseMode, SoftmaxDtype>;
+        // using MaskType = Gemm::GemmType<ElementMask, LayoutMask>;
+        // using EpilogueOnlineSoftmax = Epilogue::Block::BlockEpilogue<DispatchPolicyOnlineSoftmax,
+        //                                                             PType, SType, MaskType>;
+        // using DispatchPolicyRescaleO = Epilogue::EpilogueAtlasA2RescaleO<lseMode, SoftmaxDtype>;
+        // using OType = Gemm::GemmType<ElementO, LayoutO>;
+        // using OUpdateType = Gemm::GemmType<ElementUpdate, LayoutUpdate>;
+        // using LseType = Gemm::GemmType<ElementLse, LayoutLse>;
+        // using EpilogueRescaleO = Epilogue::Block::BlockEpilogue<DispatchPolicyRescaleO,
+        //                                                         OType, OTmpType, OUpdateType, LseType>;
 
         // // Kernel instantiation
         // using RainFusionAttentionKernelType = RainFusionAttentionKernel<BlockMmadQK, BlockMmadPV, 
         //                                                                 EpilogueOnlineSoftmax, 
-        //                                                                 EpilogueRescaleO, 
+        //                                                                 EpilogueRescaleO,
         //                                                                 false,           // PAGED_CACHE_FLAG
         //                                                                 QueryLayout,     // QUERY_LAYOUT
         //                                                                 KvCacheLayout>;  // KV_CACHE_LAYOUT
-        // RainFusionAttentionKernelParams params{q, k, v, mask, blockTables, actualQseqlen, actualKvseqlen, 
+        // RainFusionAttentionKernelParams params{q, k, v, mask, blockTables, actualQseqlen, actualKvseqlen,
         //                             selectIdx, selectNumIdx, o, lse, workspace, tiling};
 
         // // Call rain fusion attention kernel
