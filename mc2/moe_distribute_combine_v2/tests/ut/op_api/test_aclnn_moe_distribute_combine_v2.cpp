e8f4678a9d7041590ef39a3595f58d79065a36cb
hblnb <huangbolin3@huawei.com>
2026-02-05 16:45:26 +0800

    修复mc2算子ut的opapi因打桩变更引起的问题

--- mc2/moe_distribute_combine_v2/tests/ut/op_api/test_aclnn_moe_distribute_combine_v2.cpp
+++ mc2/moe_distribute_combine_v2/tests/ut/op_api/test_aclnn_moe_distribute_combine_v2.cpp
@@ -29,14 +29,14 @@ class L2MoeDistributeCombineV2Test : public testing::Test {
 protected:
     static void SetUpTestCase()
     {
-      op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
-      cout << "l2_moe_distribute_combine_test SetUp" << endl;
+        op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
+        cout << "L2MoeDistributeCombineV2Test SetUp" << endl;
     }
 
     static void TearDownTestCase()
     {
-      op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
-      cout << "l2_moe_distribute_combine_test TearDown" << endl;
+        op::SetPlatformSocVersion(op::SocVersion::ASCEND910B);
+        cout << "L2MoeDistributeCombineV2Test TearDown" << endl;
     }
 };
 
