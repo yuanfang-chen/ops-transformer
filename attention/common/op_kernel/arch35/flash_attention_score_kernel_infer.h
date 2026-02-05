649b3f3bdd97934f559856dde52ac5e235779f6e
yangxh1203 <yangxinhao7@huawei.com>
2026-02-05 16:13:24 +0800

    修改了大小写规范

--- attention/common/op_kernel/arch35/flash_attention_score_kernel_infer.h
+++ attention/common/op_kernel/arch35/flash_attention_score_kernel_infer.h
@@ -281,19 +281,19 @@ __aicore__ inline void FlashAttentionScoreKernelInfer<CubeBlockType, VecBlockTyp
     int64_t bnIndex, int64_t gS1Index, RunParamStr<isInfer> &runParam)
 {
     constexpr uint64_t fp8QBlockSize = 128U; // 128 is SOuterSize
-    constexpr uint64_t fp8KVBlockSize = 256U; // 256 is SInnerSize
+    constexpr uint64_t fp8KvBlockSize = 256U; // 256 is SInnerSize
     if constexpr (layout == LayOutTypeEnum::LAYOUT_NTD) {
         if (runParam.boIdx == 0 || runParam.boIdx == 1) {
             this->s1ScaleNumAcc = runParam.boIdx == 0 ? 0 : CeilDiv(this->actualSeqQlenAddr[0], fp8QBlockSize);
-            this->s2ScaleNumAcc = runParam.boIdx == 0 ? 0 : CeilDiv(this->actualSeqKvlenAddr[0], fp8KVBlockSize);
+            this->s2ScaleNumAcc = runParam.boIdx == 0 ? 0 : CeilDiv(this->actualSeqKvlenAddr[0], fp8KvBlockSize);
             this->s1SizeAcc = runParam.boIdx == 0 ? 0 : this->actualSeqQlenAddr[0];
             this->s2SizeAcc = runParam.boIdx == 0 ? 0 : this->actualSeqKvlenAddr[0];
         } else {
             this->s1ScaleNumAcc = CeilDiv(this->actualSeqQlenAddr[0], fp8QBlockSize);
-            this->s2ScaleNumAcc = CeilDiv(this->actualSeqKvlenAddr[0], fp8KVBlockSize);
+            this->s2ScaleNumAcc = CeilDiv(this->actualSeqKvlenAddr[0], fp8KvBlockSize);
             for (uint32_t boIdx = 1; boIdx < runParam.boIdx; boIdx++) {
                 this->s1ScaleNumAcc += CeilDiv(this->actualSeqQlenAddr[boIdx] - this->actualSeqQlenAddr[boIdx - 1], fp8QBlockSize);
-                this->s2ScaleNumAcc += CeilDiv(this->actualSeqKvlenAddr[boIdx] - this->actualSeqKvlenAddr[boIdx - 1], fp8KVBlockSize);
+                this->s2ScaleNumAcc += CeilDiv(this->actualSeqKvlenAddr[boIdx] - this->actualSeqKvlenAddr[boIdx - 1], fp8KvBlockSize);
             }
             this->s1SizeAcc = this->actualSeqQlenAddr[runParam.boIdx - 1];
             this->s2SizeAcc = this->actualSeqKvlenAddr[runParam.boIdx - 1];
