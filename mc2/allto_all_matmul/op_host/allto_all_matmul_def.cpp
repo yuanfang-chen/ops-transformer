05f9d33d336ae03f8036fcef96b54d4e51544b59
w00951525 <wangfuhao7@huawei.com>
2026-02-05 20:00:01 +0800

    atamm算子以A5代码适配A3非量化新特性

--- mc2/allto_all_matmul/op_host/allto_all_matmul_def.cpp
+++ mc2/allto_all_matmul/op_host/allto_all_matmul_def.cpp
@@ -121,8 +121,8 @@ public:
             .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
             .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel")
             .ExtendCfgInfo("opFile.value", "allto_all_matmul_apt");
+        this->AICore().AddConfig("ascend910_93", aicoreConfig_950); // A3芯片复用950代码
         this->AICore().AddConfig("ascend950", aicoreConfig_950);
-
         // 将group配置为该算子的通信域
         this->MC2().HcclGroup("group");
 
@@ -198,7 +198,6 @@ public:
             .ExtendCfgInfo("jitCompile.flag", "static_false")
             .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
         this->AICore().AddConfig("ascend910b", aicore_config_910b);
-        this->AICore().AddConfig("ascend910_93", aicore_config_910b);
         this->MC2().HcclGroup("group");
     }
 };
