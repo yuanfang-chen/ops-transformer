6befc9f72ca206ca016bbf95f5b85b736fe2fb45
hblnb <huangbolin3@huawei.com>
2026-02-05 16:45:26 +0800

    修复mc2算子ut的opapi因打桩变更引起的问题

--- mc2/allto_all_matmul/tests/ut/op_api/test_aclnn_allto_all_quant_matmul.cpp
+++ mc2/allto_all_matmul/tests/ut/op_api/test_aclnn_allto_all_quant_matmul.cpp
@@ -25,11 +25,14 @@ class test_aclnn_allto_all_quant_matmul : public testing::Test {
 protected:
     static void SetUpTestCase()
     {
-        op::SetPlatformSocVersion(op::SocVersion::ASCEND910_95);
+        op::SetPlatformSocVersion(op::SocVersion::ASCEND950);
         cout << "test_aclnn_allto_all_quant_matmul SetUp" << endl;
     }
 
-    static void TearDownTestCase() { cout << "test_aclnn_allto_all_quant_matmul TearDown" << endl; }
+    static void TearDownTestCase()
+    {
+        cout << "test_aclnn_allto_all_quant_matmul TearDown" << endl;
+    }
 };
 
 // ut用例结构体
@@ -142,7 +145,7 @@ static void TestOneParamCase(const AlltoAllQuantMatmulAclnnTestParam& param)
     TensorDesc x2scales = TensorDesc(x2scalesShape, x2scalesDtype, x2_scale_format);
     TensorDesc output = TensorDesc(outputShape, outputDtype, outputFormat);
     // 三个可能为空指针的，需要特殊处理
-    TensorDesc bias = TensorDesc(biasShape, biasDtype, bias_format);
+    TensorDesc bias = TensorDesc(biasShape, biasDtype, biasFormat);
     TensorDesc alltoallout = TensorDesc(alltoalloutShape, alltoalloutDtype, alltoalloutFormat);
     TensorDesc x1scales = TensorDesc(x1scalesShape, x1scalesDtype, x1_scale_format);
     uint64_t workspace_size = 0;
