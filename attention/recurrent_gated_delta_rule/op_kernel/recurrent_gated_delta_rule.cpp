8f8011626deef01fc446fb425ffd74643e9e01d6
Taojiaming <taojiaming3@huawei.com>
2026-02-05 10:59:10 +0800

    support KDA

--- attention/recurrent_gated_delta_rule/op_kernel/recurrent_gated_delta_rule.cpp
+++ attention/recurrent_gated_delta_rule/op_kernel/recurrent_gated_delta_rule.cpp
@@ -31,7 +31,7 @@ recurrent_gated_delta_rule(GM_ADDR query, GM_ADDR key, GM_ADDR value, GM_ADDR be
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
     TPipe pipe;
     RGDR<bfloat16_t, bfloat16_t> op(&tilingData);
-    RGDRInitParams initParams{query, key,     value, g, beta, state, cuSeqlens, ssmStateIndices, numAcceptedTokens,
+    RGDRInitParams initParams{query, key,     value, g, gk, beta, state, cuSeqlens, ssmStateIndices, numAcceptedTokens,
                               out,   stateOut};
     op.Init(initParams, &pipe);
     op.Process();
