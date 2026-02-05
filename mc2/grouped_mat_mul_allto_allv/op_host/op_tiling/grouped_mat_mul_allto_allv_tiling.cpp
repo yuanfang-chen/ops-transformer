3f00b84eacd4de9531e30d43c153095971035b3d
Yuanfeng_HW <yuanfeng24@huawei.com>
2026-02-05 10:55:32 +0800

    空tensor支持
    Co-authored-by: Yuanfeng_HW<yuanfeng24@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1425 merge master into master
    
    空tensor支持
    
    Created-by: Yuanfeng_HW
    Commit-by: Yuanfeng_HW
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1425

--- mc2/grouped_mat_mul_allto_allv/op_host/op_tiling/grouped_mat_mul_allto_allv_tiling.cpp
+++ mc2/grouped_mat_mul_allto_allv/op_host/op_tiling/grouped_mat_mul_allto_allv_tiling.cpp
@@ -280,13 +280,6 @@ static bool CheckRecvCnt(
                     return false);
                 recvSum += recvArray[j] * H;
             }
-            OP_TILING_CHECK(recvSum < RECV_SEND_MIN,
-                OP_LOGE(
-                    C_INNER_DEBUG,
-                    "rank %ld:sum(recvCounts[%ld, %ld]) * H1 * sizeof dtype(gmmx) should be greater than or equal to 2MB,"
-                    "but got %ld Byte!",
-                    i - 1, (i - 1) * eExpert, i * eExpert - 1, 2 * recvSum),
-                return false);
         }
     }
     return true;
@@ -328,13 +321,6 @@ static bool CheckSendCnt(
                     return false);
                 sendSum += sendArray[j] * H;
             }
-            OP_TILING_CHECK(sendSum < RECV_SEND_MIN,
-                OP_LOGE(
-                    C_INNER_DEBUG,
-                    "rank %ld:sum(sendCounts[%ld, %ld]) * H1 * sizeof dtype(gmmx) should be greater than or equal to 2MB,"
-                    "but got %ld Byte!",
-                    i - 1, (i - 1) * eExpert, i * eExpert - 1, 2 * sendSum),
-                return false);
         }
     }
     return true;
@@ -575,7 +561,6 @@ static bool CheckAndSetAttrs(const gert::TilingContext* context, GroupedMatMulAl
         OP_LOGE(C_INNER_DEBUG, "transMmWeightPtr should not be true when mmX is null!");
         return ge::GRAPH_FAILED;
     }
-
     tilingData->commonTilingInfo.epWorldSize = *epWorldSizePtr;
     tilingData->commonTilingInfo.isGmmWeightTrans = *transGmmWeightPtr;
     tilingData->commonTilingInfo.isMmWeightTrans = *transMmWeightPtr;
@@ -758,11 +743,10 @@ static ge::graphStatus ComputeBaseMNK(GroupedMatMulAlltoAllvTilingData* tilingDa
     uint32_t maxBaseM = PLATFORM_SIZE.l0CSize / (baseN_ * sizeof(float));
     baseM_ = std::min<uint32_t>((PLATFORM_SIZE.l0ASize / DOUBLE_BUFFER_L0A_L0B) / (baseK_ * FP16_DATASIZE), maxBaseM);
     baseM_ = SixteenAlign(baseM_);
-    if (baseM_ > maxM) {
+    if (maxM != 0 && baseM_ > maxM) {
         baseM_ = SixteenAlign(maxM, true);
     }
     OP_TILING_CHECK(baseM_ == 0, OP_LOGE(C_INNER_DEBUG, "baseM_ should not be 0."), return ge::GRAPH_FAILED);
-
     return ge::GRAPH_SUCCESS;
 }
 
@@ -865,11 +849,11 @@ static ge::graphStatus SetMatmulTiling(
     OP_TILING_CHECK(
         ComputeBaseMNK(tilingData, PLATFORM_SIZE) != ge::GRAPH_SUCCESS,
         OP_LOGE(C_INNER_DEBUG, "GMM Tiling compute baseMNK failed."), return ge::GRAPH_FAILED);
-
-    OP_TILING_CHECK(
-        DoMatmulApiTiling(tilingData, PLATFORM_SIZE, mmDtype, context) != ge::GRAPH_SUCCESS,
-        OP_LOGE(C_INNER_DEBUG, "GMM Tiling matmul api do tiling failed."), return ge::GRAPH_FAILED);
-
+    if (tilingData->commonTilingInfo.A != 0) {
+        OP_TILING_CHECK(
+            DoMatmulApiTiling(tilingData, PLATFORM_SIZE, mmDtype, context) != ge::GRAPH_SUCCESS,
+            OP_LOGE(C_INNER_DEBUG, "GMM Tiling matmul api do tiling failed."), return ge::GRAPH_FAILED);
+    }
     if (tilingData->commonTilingInfo.isOptionalMatmul) {
         OP_TILING_CHECK(
             ComputeSharedBaseMNK(tilingData, PLATFORM_SIZE) != ge::GRAPH_SUCCESS,
