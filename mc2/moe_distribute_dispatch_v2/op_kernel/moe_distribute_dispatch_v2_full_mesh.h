e75d670b9beae00b8361c59409b665e3dcf97dae
wangchenfeng6 <wangchenfeng6@h-partners.com>
2026-02-05 15:02:17 +0800

    huihuang_test

--- mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2_full_mesh.h
+++ mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2_full_mesh.h
@@ -15,6 +15,7 @@
 #ifndef MOE_DISTRIBUTE_DISPATCH_V2_FULL_MESH_H
 #define MOE_DISTRIBUTE_DISPATCH_V2_FULL_MESH_H
 
+
 #include "basic_api/kernel_basic_intf.h"
 #include "adv_api/math/cumsum.h"
 #include "adv_api/reduce/sum.h"
