75f03190c41bcda00ca088b10ee2befde33f37ce
gcw_asJPsfok <gemengbai@huawei.com>
2026-02-05 09:28:30 +0800

    add ascendC shmem code

---
+++ experimental/mc2/moe_distribute_combine_v2/op_kernel/shmem_context_def.h
@@ -0,0 +1,15 @@
+#include "kernel_operator.h"
+#include "kernel_tiling/kernel_tiling.h"
+#include "../moe_distribute_dispatch/moe_distribute_base.h"
+#include "moe_distribute_dispatch_v2_tiling.h"
+#include "shmem.h"
+
+
+
+__aicore__ inline int64_t GetShmemDataAddr(__gm__ uint8_t *context, int32_t pe) {
+    return (int64_t) aclshmem_ptr(context, pe);
+}
+
+__aicore__ inline int64_t GetShmemSignalAddr(__gm__ uint8_t *context, int32_t pe) {
+    return (int64_t) aclshmem_ptr(context, pe) + 1022 * 1024 * 1024;
+}
