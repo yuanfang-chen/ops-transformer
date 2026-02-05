e3da331c6010c492767a035d22930dac7b035b05
lccchh <linchenghao@huawei.com>
2026-02-05 16:09:49 +0800

    blocksize 拦截修改

--- attention/fused_infer_attention_score/op_host/flash_attention_infer_tiling.h
+++ attention/fused_infer_attention_score/op_host/flash_attention_infer_tiling.h
@@ -86,6 +86,8 @@ namespace optiling{
     const uint32_t BASE_KV_SIZE = 128;
     const uint32_t PRELANCH_NUM = 3;
     const int64_t SPARSE_MODE_INT_MAX = 2147483647;
+    const uint32_t BLOCK_SIZE_ALIGN_16 = 16;
+    const uint32_t MAX_BLOCSIZE = 128;
 
     enum class MaskType : uint32_t {
         NO_MASK = 0,
