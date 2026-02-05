8687dad021e7a7e68116e283793fed61df9ce163
yiqiao-wjp <wangjianping48@huawei.com>
2026-02-05 10:40:12 +0800

    fix

--- posembedding/rotary_position_embedding/op_host/rotary_position_embedding_def.cpp
+++ posembedding/rotary_position_embedding/op_host/rotary_position_embedding_def.cpp
@@ -99,7 +99,7 @@ private:
             .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
             .AutoContiguous();
         config_kirin.Input("rotate")
-            .ParamType(OPTIONAL).Version(ASCEND_STEP_VERSION_TWO)
+            .ParamType(OPTIONAL)
             .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
             .Format({ge::FORMAT_ND, ge::FORMAT_ND})
             .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
