0dee5b0b39def3b0f87d238da4844ea5cb324951
inspirations <wangke201@huawei.com>
2026-02-05 10:07:42 +0800

    (register+matmul)replace socversion with npuArch

--- mc2/3rd/mat_mul_v3/op_host/op_tiling/matmul_v3_tiling.cpp
+++ mc2/3rd/mat_mul_v3/op_host/op_tiling/matmul_v3_tiling.cpp
@@ -71,6 +71,7 @@ static ge::graphStatus Mc2TilingPrepareForMatmulV3(gert::TilingParseContext *con
   compileInfoPtr->supportL12BtBf16 = (dataMoveL12Bt.find("bf16") != std::string::npos);
   compileInfoPtr->aicNum = ascendcPlatform.GetCoreNumAic();
   compileInfoPtr->socVersion = ascendcPlatform.GetSocVersion();
+  compileInfoPtr->npuArch = ascendcPlatform.GetCurNpuArch();
   compileInfoPtr->btSize = compileInfoPtr->supportL0c2out ? 1024UL : 0UL;                       // 1024 is btSize
   compileInfoPtr->btSize = compileInfoPtr->supportL12BtBf16 ? 4096UL : compileInfoPtr->btSize;  // 4096 is btSize
   ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, compileInfoPtr->ubSize);
