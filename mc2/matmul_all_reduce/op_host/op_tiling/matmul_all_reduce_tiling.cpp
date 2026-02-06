f995b9a4ee5f73477f3f5265d7f3c2caf7c0e82a
inspirations <wangke201@huawei.com>
2026-02-07 02:13:08 +0800

    test allreduce

--- mc2/matmul_all_reduce/op_host/op_tiling/matmul_all_reduce_tiling.cpp
+++ mc2/matmul_all_reduce/op_host/op_tiling/matmul_all_reduce_tiling.cpp
@@ -29,12 +29,6 @@ ge::graphStatus TilingParseForMatmulAllReduce(gert::TilingParseContext* context)
 
 ge::graphStatus MatmulAllReduceTilingFunc(gert::TilingContext* context)
 {
-    auto platformInfo = context->GetPlatformInfo();
-    platform_ascendc::PlatformAscendC ascendcPlatform(platformInfo);
-    NpuArch npuArch = ascendcPlatform.GetCurNpuArch();
-    if (npuArch == NpuArch::DAV_3510) {
-        return TilingRegistryArch::GetInstance().DoTilingImpl(context);
-    }
     return TilingRegistryNew::GetInstance().DoTilingImpl(context);
 }
 
