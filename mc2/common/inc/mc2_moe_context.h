47898e6b2806a50aceee40daf556120c6c039394
zzg_code <zengzhiguo1@huawei.com>
2026-02-06 16:34:48 +0800

    1

--- mc2/common/inc/mc2_moe_context.h
+++ mc2/common/inc/mc2_moe_context.h
@@ -4,12 +4,15 @@
 namespace Mc2Kernel {
 constexpr uint32_t HCCL_HOST_KFC_MAX_RANK_NUM = 1024; //AIV
 }
+namespace Mc2Context {
+    struct Mc2MoeContext {
+        uint32_t rankId; // 当前卡rankId
+        uint32_t rankDim; // 总卡数
+        uint64_t winsize;
+        uint64_t kfcContextPtr; // kfccontext 指针
+        uint64_t windowsIn[Mc2Kernel::HCCL_HOST_KFC_MAX_RANK_NUM]; // ccu不使用, MTE 数据区
+    };
 
-struct Mc2MoeContext {
-    uint32_t rankId; // 当前卡rankId
-    uint32_t rankDim; // 总卡数
-    uint64_t winsize;
-    uint64_t kfcContextPtr; // kfccontext 指针
-    uint64_t windowsIn[Mc2Kernel::HCCL_HOST_KFC_MAX_RANK_NUM]; // ccu不使用, MTE 数据区
 }
+
 #endif
