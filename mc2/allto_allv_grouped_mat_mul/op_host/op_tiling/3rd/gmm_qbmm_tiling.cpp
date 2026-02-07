a3a23acfc90d60ae4731dc3151feb9a18a361890
王天乐 <wangtianle3@huawei.com>
2026-02-07 14:19:37 +0800

    update gmm 3rd party files

--- mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/3rd/gmm_qbmm_tiling.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/3rd/gmm_qbmm_tiling.cpp
@@ -516,11 +516,11 @@ bool GroupedQbmmTiling::CheckActiveModeDtype(const gert::StorageShape *xScaleSto
     return true;
 }
 
-bool GroupedQbmmTiling::CheckActiveMode(const gert::Shape &wScaleShape, const gert::StorageShape *xScaleStorageShape) const
+bool GroupedQbmmTiling::CheckActiveMode(const gert::Shape &wScaleShape, const gert::StorageShape *xScaleStorageShape)
 {
-    OP_CHECK_IF(inputParams_.actType == static_cast<int8_t>(GMMActType::GMM_ACT_TYPE_GELU_ERR_FUNC),
+    OP_CHECK_IF(inputParams_.actType == GMMActType::GMM_ACT_TYPE_GELU_ERR_FUNC,
                 OP_LOGE(context_->GetNodeName(), "Activation function does not support GELU_ERR_FUNC now."), return false);
-    OP_CHECK_IF(inputParams_.actType > static_cast<int8_t>(GMMActType::GMM_ACT_TYPE_SILU) || inputParams_.actType < static_cast<int8_t>(GMMActType::GMM_ACT_TYPE_NONE),
+    OP_CHECK_IF(inputParams_.actType > GMMActType::GMM_ACT_TYPE_SILU || inputParams_.actType < GMMActType::GMM_ACT_TYPE_NONE,
                 OP_LOGE(context_->GetNodeName(), "Activation function only supports RELU/GELU_TANH/FASTGELU/SILU."),
                 return false);
     OP_CHECK_IF(!CheckActiveModeDtype(xScaleStorageShape), OP_LOGE(context_->GetNodeName(), "CheckActiveModeDtype failed."), return false);
@@ -536,18 +536,18 @@ bool GroupedQbmmTiling::CheckActiveMode(const gert::Shape &wScaleShape, const ge
                     "the shape of perTokenScale should be (%d,), "
                     "actual is (%d,).", inputParams_.mSize, static_cast<uint64_t>(xScaleShape[0])), return false);
     }
-
-    if (!(wScaleDims == 2 && wScaleShape[wScaleDims - 1] == 1 && inputParams_.nSize == 1)) { // scale为2维且shape为(g,1)时，不做拦截
-        OP_CHECK_IF(wScaleDims != 2, OP_LOGE(context_->GetNodeName(), // 在启用激活函数情景下，Scale应该为2维
-                    "When the activation function is enabled, the dim of Scale should be 2, "
-                    "actual is %d.", wScaleDims), return false);
-        OP_CHECK_IF(static_cast<uint64_t>(wScaleShape[0]) != inputParams_.groupNum ||
-                    static_cast<uint64_t>(wScaleShape[1]) != inputParams_.nSize,
-                    OP_LOGE(context_->GetNodeName(),
-                    "When the activation function is enabled, the shape of Scale should be (%d, %d), "
-                    "actual is (%d, %d).", inputParams_.groupNum, inputParams_.nSize,
-                    static_cast<uint64_t>(wScaleShape[0]), static_cast<uint64_t>(wScaleShape[1])),
-                    return false);
+    OP_CHECK_IF(wScaleDims != 2, OP_LOGE(context_->GetNodeName(), // 在启用激活函数情景下，Scale应该为2维
+                "When the activation function is enabled, the dim of Scale should be 2, "
+                "actual is %d.", wScaleDims), return false);
+    OP_CHECK_IF(static_cast<uint64_t>(wScaleShape[0]) != inputParams_.groupNum ||
+                static_cast<uint64_t>(wScaleShape[1]) != inputParams_.nSize,
+                OP_LOGE(context_->GetNodeName(),
+                "When the activation function is enabled, the shape of Scale should be (%d, %d), "
+                "actual is (%d, %d).", inputParams_.groupNum, inputParams_.nSize,
+                static_cast<uint64_t>(wScaleShape[0]), static_cast<uint64_t>(wScaleShape[1])),
+                return false);
+    if (inputParams_.nSize == 1) {
+        inputParams_.bQuantMode = optiling::QuantMode::PERCHANNEL_MODE;
     }
     return true;
 }
@@ -588,10 +588,10 @@ bool GroupedQbmmTiling::AnalyzeInputs()
         OP_CHECK_IF(!CheckShapeForWeightNz(weightNzStorageShape), OP_LOGE(context_->GetNodeName(), "CheckShapeForWeightNz failed."),
                     return false);
     }
-    if (inputParams_.actType != static_cast<int8_t>(GMMActType::GMM_ACT_TYPE_NONE)) {
+    if (inputParams_.actType != GMMActType::GMM_ACT_TYPE_NONE) {
         OP_CHECK_IF(!CheckActiveMode(wScaleShape, xScaleStorageShape), OP_LOGE(context_->GetNodeName(), "CheckActiveMode failed."),
                     return false);
-    }
+    }   
     if (inputParams_.aDtype == ge::DT_FLOAT4_E2M1 || inputParams_.aDtype == ge::DT_FLOAT4_E1M2) {
         OP_CHECK_IF(!CheckFp4Shape(), OP_LOGE(inputParams_.opName, "CheckFp4Shape failed."), return false);
         if (inputParams_.hasBias) {
