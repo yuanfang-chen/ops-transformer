4ea62c2c0711799395ebed08caa867329d7432ae
w00951525 <wangfuhao7@huawei.com>
2026-02-07 14:46:22 +0800

    atamm算子以A5代码适配A3非量化新特性

---
+++ mc2/allto_all_matmul/op_host/op_tiling/arch32/allto_all_fp_matmul_tiling_910_93.h
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file allto_all_fp_matmul_tiling_base.h
+ * \brief
+ */
+
+#ifndef ALLTO_ALL_FP_MATMUL_TILING_BASE_H
+#define ALLTO_ALL_FP_MATMUL_TILING_BASE_H
+
+#include <string>
+#include "securec.h"
+#include "mc2_matmul_tiling_cfg.h"
+#include "tiling/new_mc2_tiling_utils.h"
+#include "mc2/3rd/mat_mul_v3/op_host/op_tiling/matmul_v3_base_tiling.h"
+#include "../allto_all_matmul_tiling_base.h"
+#include "../../../op_kernel/arch32/allto_all_matmul_tiling_data_910_93.h"
+#include "../../../op_kernel/arch32/allto_all_matmul_tiling_key_910_93.h"
+#include "mc2/matmul_allto_all/op_host/op_tiling/common/matmul_allto_all_util_tiling.h"
+
+namespace MC2Tiling {
+
+using namespace optiling;
+using namespace mc2_matmul_v3_advanced;
+
+class AllToAllFpMatmulTilingBaseA3 : public AllToAllMatmulTilingBase {
+    friend class AllToAllFpMatmulHelper;
+
+public:
+    explicit AllToAllFpMatmulTilingBaseA3(gert::TilingContext *context);
+    ~AllToAllFpMatmulTilingBaseA3() override = default;
+
+protected:
+    bool IsCapable() override;
+    ge::graphStatus DoOpTiling() override;
+    ge::graphStatus PostTiling() override;
+    uint64_t GetTilingKey() const override;
+
+    ge::graphStatus CheckA3NonQuantTensorDataType(const gert::TilingContext *context, const char *opName);
+    ge::graphStatus CheckOpInputInfo();
+    ge::graphStatus InitTilingContextParameters();
+    ge::graphStatus DoMMTiling();
+    ge::graphStatus DoMatmulV3Tiling(Mc2MatmulHelper::Mc2MatmulTilingCfg &tilingCfg, Mc2MMRegisterCfg &registerCfg,
+                                     Mc2MatMulV3TilingData &tilingData);
+    ge::graphStatus SetHcclTiling();
+    void SetTilingInfo(AlltoAllMatmulTilingInfoA3 &tilingInfo) const;
+
+private:
+    void PrintAlltoAllMatmulTilingData(AlltoAllMatmulTilingDataA3 &AlltoAllMatmulTilingDataA3);
+    void PrintAlltoAllMatmulTilingInfo(const std::string &opName, AlltoAllMatmulTilingInfoA3 &tilingInfo);
+    void PrintMMV3TilingData(const std::string &opName, Mc2MatMulV3TilingData &tiling);
+
+    AlltoAllMatmulTilingDataA3 localTilingData_;
+    std::string socVersionStr_;
+};
+
+class AllToAllFpMatmulHelper : public mc2_matmul_v3::Mc2MatmulV3BaseTiling
+{
+public:
+    AllToAllFpMatmulHelper(AllToAllFpMatmulTilingBaseA3& alltoAllMatmulTilingA3, Mc2MatmulV3TilingData& data);
+
+    ge::graphStatus PostTiling() override;
+
+private:
+    AllToAllFpMatmulTilingBaseA3& tilingProcesser_;
+};
+
+} // namespace MC2Tiling
+#endif
