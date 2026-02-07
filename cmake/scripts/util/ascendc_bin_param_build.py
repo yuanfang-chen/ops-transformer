5696cede3f8ece2aa49a06f8b1421b09eab4d00b
lidongsheng <lidongsheng43@huawei.com>
2026-02-07 14:19:05 +0800

    增加编译耗时统计脚本

--- cmake/scripts/util/ascendc_bin_param_build.py
+++ cmake/scripts/util/ascendc_bin_param_build.py
@@ -384,6 +384,7 @@ grep -q \"None of the given tiling keys are in the supported list\"; then\n"
 
         build_cmd_var = "#!/bin/bash\n"
         build_cmd_var += f'echo "[{self.soc}] Generating {bin_file} ..."\n'
+        build_cmd_var += f"start_time=$(date +%s.%N)\n"
         plog_level = os.environ.get("ASCEND_GLOBAL_LOG_LEVEL")
         plog_stdout = os.environ.get("ASCEND_SLOG_PRINT_TO_STDOUT")
         if plog_level is None:
@@ -415,6 +416,18 @@ grep -q \"None of the given tiling keys are in the supported list\"; then\n"
     
         check_result = self._generate_check_result(enable_tiling_keys, bin_file)
         build_cmd_var += check_result
+        build_cmd_var += f'end_time=$(date +%s.%N)\n'
+        build_cmd_var += f'duration=$(awk "BEGIN {{ print $end_time - $start_time }}")\n'
+        build_bin_path = os.path.dirname(self.out_path)
+        build_cmd_var += f'kernel_path=$(find "{build_bin_path}" -type f -name "{bin_file}.o" -print | head -n 1)\n'
+        build_cmd_var += f'kernel_size=$(stat -c%s "$kernel_path")\n'
+        build_cmd_var += (
+            f'echo "Build [{self.soc}] op_name [{self.op_file}] '
+            f'index [{str(index)}] bin_file [{bin_file}] '
+            f'duration [$duration] size [$kernel_size] '
+            f'start_time [$start_time] end_time [$end_time]" '
+            f'>> {self.op_file}-{str(index)}.txt\n'
+        )
         build_cmd_var += f'echo "[{self.soc}] Generating {bin_file} Done"\n'
 
         with os.fdopen(os.open(compile_file, const_var.WFLAGS, const_var.WMODES), 'w') as fd:
