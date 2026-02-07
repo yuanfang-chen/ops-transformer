2b8a6c19080976e050f8d4889fc2535c66a62950
qq_57796320 <zhushuqin1@huawei.com>
2026-02-07 11:33:45 +0800

    alltoallvGMM

--- mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul.cpp
@@ -12,7 +12,7 @@
  * \file allto_allv_grouped_mat_mul.cpp
  * \brief
  */
-#include "basic_api/kernel_basic_intf.h"
+#include "kernel_operator.h"
 #include "allto_allv_grouped_mat_mul_coarse_grained.h"
 #include "allto_allv_grouped_mat_mul_tiling_key.h"
 
