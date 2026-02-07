6507b9476069ce40eba917e8cb0d97d437fd10d9
qq_57796320 <zhushuqin1@huawei.com>
2026-02-07 11:33:45 +0800

    alltoallvGMM

--- mc2/allto_allv_grouped_mat_mul/op_host/allto_allv_grouped_mat_mul_def.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_host/allto_allv_grouped_mat_mul_def.cpp
@@ -1,4 +1,4 @@
-/**
+/* *
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
@@ -6,73 +6,288 @@
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
- */
+  */
 
-/*!
+/* !
  * \file allto_allv_grouped_mat_mul.cpp
  * \brief
  */
 #include "register/op_def_registry.h"
 
-namespace ops
-{
-class AlltoAllvGroupedMatMul : public OpDef
-{
+namespace ops {
+class AlltoAllvGroupedMatMul : public OpDef {
 public:
-    explicit AlltoAllvGroupedMatMul(const char* name) : OpDef(name)
+    explicit AlltoAllvGroupedMatMul(const char *name) : OpDef(name)
     {
         this->Input("gmm_x")
             .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .AutoContiguous();
-        this->Input("gmm_weight")
-            .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .AutoContiguous();
-        this->Input("send_counts_tensor")
-            .ParamType(OPTIONAL)
-            .DataType({ge::DT_INT32, ge::DT_INT64, ge::DT_INT32, ge::DT_INT64})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .AutoContiguous();
-        this->Input("recv_counts_tensor")
-            .ParamType(OPTIONAL)
-            .DataType({ge::DT_INT32, ge::DT_INT64, ge::DT_INT32, ge::DT_INT64})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .AutoContiguous();
-        this->Input("mm_x")
-            .ParamType(OPTIONAL)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .AutoContiguous();
-        this->Input("mm_weight")
-            .ParamType(OPTIONAL)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .DataType({
+                ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_HIFLOAT8, ge::DT_HIFLOAT8
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
             .AutoContiguous();
 
+        this->Input("gmm_weight")
+            .ParamType(REQUIRED)
+            .DataType({
+                ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_HIFLOAT8, ge::DT_HIFLOAT8})
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("send_counts_tensor")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_INT32, ge::DT_INT64, ge::DT_INT32, ge::DT_INT64,
+                ge::DT_INT32, ge::DT_INT64
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("recv_counts_tensor")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_INT32, ge::DT_INT64, ge::DT_INT32, ge::DT_INT64,
+                ge::DT_INT32, ge::DT_INT64
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("mm_x")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_HIFLOAT8, ge::DT_HIFLOAT8
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("mm_weight")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_HIFLOAT8, ge::DT_HIFLOAT8
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("gmm_x_scale")
+            .ParamType(REQUIRED)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("gmm_weight_scale")
+            .ParamType(REQUIRED)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("gmm_x_offset")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+        
+        this->Input("gmm_weight_offset")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+        
+        this->Input("mm_x_scale")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("mm_weight_scale")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+        
+        this->Input("mm_x_offset")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
+        this->Input("mm_weight_offset")
+            .ParamType(OPTIONAL)
+            .DataType({
+                ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT, ge::DT_FLOAT,
+                ge::DT_FLOAT, ge::DT_FLOAT,
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+        
         this->Output("gmm_y")
             .ParamType(REQUIRED)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            .DataType({
+                ge::DT_FLOAT16,ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_FLOAT16, ge::DT_BF16
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
         this->Output("mm_y")
             .ParamType(OPTIONAL)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            .DataType({
+                ge::DT_FLOAT16,ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_FLOAT16, ge::DT_BF16
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .AutoContiguous();
+
         this->Output("permute_out")
             .ParamType(OPTIONAL)
-            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16})
-            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
-            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+            .DataType({
+                ge::DT_FLOAT16, ge::DT_FLOAT16, ge::DT_BF16, ge::DT_BF16,
+                ge::DT_HIFLOAT8, ge::DT_HIFLOAT8
+            })
+            .Format({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            })
+            .UnknownShapeFormat({
+                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
+                ge::FORMAT_ND, ge::FORMAT_ND
+            });
 
         this->Attr("group").AttrType(REQUIRED).String();
         this->Attr("ep_world_size").AttrType(REQUIRED).Int();
@@ -81,6 +296,13 @@ public:
         this->Attr("trans_gmm_weight").AttrType(OPTIONAL).Bool(false);
         this->Attr("trans_mm_weight").AttrType(OPTIONAL).Bool(false);
         this->Attr("permute_out_flag").AttrType(OPTIONAL).Bool(false);
+        this->Attr("gmm_x_quant_mode").AttrType(OPTIONAL).Int();
+        this->Attr("gmm_weight_quant_mode").AttrType(OPTIONAL).Int();
+        this->Attr("mm_x_quant_mode").AttrType(OPTIONAL).Int();
+        this->Attr("mm_weight_quant_mode").AttrType(OPTIONAL).Int();
+        this->Attr("group_size").AttrType(OPTIONAL).Int();
+        this->Attr("y_dtype").AttrType(OPTIONAL).Int(static_cast<int64_t>(ge::DT_UNDEFINED));
+        this->Attr("mm_dtype").AttrType(OPTIONAL).Int(static_cast<int64_t>(ge::DT_UNDEFINED));
 
         OpAICoreConfig aicore_config;
         aicore_config.DynamicCompileStaticFlag(true)
@@ -90,7 +312,7 @@ public:
             .NeedCheckSupportFlag(false)
             .PrecisionReduceFlag(true)
             .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
-            .ExtendCfgInfo("jitCompile.flag", "static_false")  // 动态shape,复用二进制,后续图支持后修改
+            .ExtendCfgInfo("jitCompile.flag", "static_false")
             .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
         this->AICore().AddConfig("ascend910_93", aicore_config);
         this->AICore().AddConfig("ascend950", aicore_config);
@@ -99,4 +321,4 @@ public:
 };
 
 OP_ADD(AlltoAllvGroupedMatMul);
-}  // namespace ops
+} // namespace ops
--- mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul.cpp
@@ -22,45 +22,45 @@
 
 using namespace AscendC;
 
-#define INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL()                                                                 \
-    do {                                                                                                          \
-        op.Init(                                                                                                  \
-            gmmxGM, gmmweightGM, sendCountsTensorOptionalGM, recvCountsTensorOptionalGM, mmxOptionalGM,           \
-            mmweightOptionalGM, gmmyGM, mmyOptionalGM, permuteOutOptionalGM, workspaceGM, contextGM, &tilingData, \
-            hcclInitTiling, alltoAllvCcTiling, &pipe);                                                            \
-        op.Process();                                                                                             \
+#define INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL()                                                                      \
+    do {                                                                                                               \
+        op.Init(gmmxGM, gmmweightGM, sendCountsTensorOptionalGM, recvCountsTensorOptionalGM, mmxOptionalGM,            \
+                mmweightOptionalGM, gmmyGM, mmyOptionalGM, permuteOutOptionalGM, workspaceGM, contextGM, &tilingData,  \
+                hcclInitTiling, alltoAllvCcTiling, &pipe);                                                             \
+        op.Process();                                                                                                  \
     } while (0)
 
-template <
-    int D_T_MM, bool TILINGKEY_MM, bool TILINGKEY_GMM_WEIGHT_TRANSPOSE, 
-    bool TILINGKEY_MM_WEIGHT_TRANSPOSE>
-__global__ __aicore__ void allto_allv_grouped_mat_mul(
-    GM_ADDR gmmxGM, GM_ADDR gmmweightGM, GM_ADDR sendCountsTensorOptionalGM, GM_ADDR recvCountsTensorOptionalGM,
-    GM_ADDR mmxOptionalGM, GM_ADDR mmweightOptionalGM, GM_ADDR gmmyGM, GM_ADDR mmyOptionalGM,
-    GM_ADDR permuteOutOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM)
+template <int D_T_MM, bool TILINGKEY_MM, bool TILINGKEY_GMM_WEIGHT_TRANSPOSE, bool TILINGKEY_MM_WEIGHT_TRANSPOSE>
+__global__ __aicore__ void
+allto_allv_grouped_mat_mul(GM_ADDR gmmxGM, GM_ADDR gmmweightGM, GM_ADDR sendCountsTensorOptionalGM,
+                           GM_ADDR recvCountsTensorOptionalGM, GM_ADDR mmxOptionalGM, GM_ADDR mmweightOptionalGM,
+                           GM_ADDR gmmxScaleGM, GM_ADDR gmmWeightScaleGM, GM_ADDR gmmxOffsetGM,
+                           GM_ADDR gmmWeightOffsetGM, GM_ADDR mmxScaleGM, GM_ADDR mmWeightScaleGM,
+                           GM_ADDR mmxOptionalOffsetGM, GM_ADDR mmWeightOptionalOffsetGM, GM_ADDR gmmyGM,
+                           GM_ADDR mmyOptionalGM, GM_ADDR permuteOutOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM)
 {
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
     REGISTER_TILING_DEFAULT(AlltoAllvGmmTilingData);
-    auto tiling = (__gm__ AlltoAllvGmmTilingData*)tilingGM;
-    __gm__ void* hcclInitTiling = (__gm__ void*)(&(tiling->hcclInitTiling));
-    __gm__ void* alltoAllvCcTiling = (__gm__ void*)(&(tiling->alltoAllvCcTiling));
+    auto tiling = (__gm__ AlltoAllvGmmTilingData *)tilingGM;
+    __gm__ void *hcclInitTiling = (__gm__ void *)(&(tiling->hcclInitTiling));
+    __gm__ void *alltoAllvCcTiling = (__gm__ void *)(&(tiling->alltoAllvCcTiling));
     GET_TILING_DATA(tilingData, tilingGM);
 
     TPipe pipe;
     GM_ADDR contextGM = GetHcclContext<HCCL_GROUP_ID_0>();
 
-if (D_T_MM == ADD_TPL_BP16) {
-    AlltoAllvGmmCoarseGrained<bfloat16_t, TILINGKEY_MM, TILINGKEY_GMM_WEIGHT_TRANSPOSE,
-                                TILINGKEY_MM_WEIGHT_TRANSPOSE> op;
-    INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL();
-    return;
-}
+    if (D_T_MM == ADD_TPL_BP16) {
+        AlltoAllvGmmCoarseGrained<bfloat16_t, TILINGKEY_MM, TILINGKEY_GMM_WEIGHT_TRANSPOSE,
+                                  TILINGKEY_MM_WEIGHT_TRANSPOSE>
+            op;
+        INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL();
+        return;
+    }
 
-if (D_T_MM == ADD_TPL_FP16) {
-    AlltoAllvGmmCoarseGrained<half, TILINGKEY_MM, TILINGKEY_GMM_WEIGHT_TRANSPOSE,
-                                TILINGKEY_MM_WEIGHT_TRANSPOSE> op;
-    INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL();
-    return;
-}
+    if (D_T_MM == ADD_TPL_FP16) {
+        AlltoAllvGmmCoarseGrained<half, TILINGKEY_MM, TILINGKEY_GMM_WEIGHT_TRANSPOSE, TILINGKEY_MM_WEIGHT_TRANSPOSE> op;
+        INVOKE_ALLTOALLV_GROUPED_MATMUL_OP_IMPL();
+        return;
+    }
 }
