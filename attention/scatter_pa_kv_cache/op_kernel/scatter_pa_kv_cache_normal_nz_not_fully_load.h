37b5143defa2f2fff98e6edfee70cc9b1298e87c
wy519 <wangyang519@huawei.com>
2026-02-08 16:04:37 +0800

    修改scatterPaKvCache的tiling校验以支持cacheMode为pa_nz时cache的5维输入/输出

--- attention/scatter_pa_kv_cache/op_kernel/scatter_pa_kv_cache_normal_nz_not_fully_load.h
+++ attention/scatter_pa_kv_cache/op_kernel/scatter_pa_kv_cache_normal_nz_not_fully_load.h
@@ -28,7 +28,7 @@ public:
     __aicore__ inline ScatterPaKvCacheNormalNzNotFullyLoad(TPipe *pipe,
                                                            const ScatterPaKvCacheTilingData *__restrict tiling)
         : pipe_(pipe), tilingData_(tiling){};
-    __aicore__ inline void Init(GM_ADDR key, GM_ADDR key_cache_in, GM_ADDR slot_mapping,
+    __aicore__ inline void Init(GM_ADDR key, GM_ADDR slot_mapping, GM_ADDR value,
                                 GM_ADDR key_cache_out, GM_ADDR value_cache_out);
     __aicore__ inline void Process();
 
@@ -64,8 +64,7 @@ private:
 
 template <typename T1, typename T2, typename IndexDtype>
 __aicore__ inline void ScatterPaKvCacheNormalNzNotFullyLoad<T1, T2, IndexDtype>::Init(
-    GM_ADDR key, GM_ADDR key_cache_in, GM_ADDR slot_mapping, GM_ADDR seq_lens,
-    GM_ADDR key_cache_out, GM_ADDR value_cache_out)
+    GM_ADDR key, GM_ADDR slot_mapping, GM_ADDR value, GM_ADDR key_cache_out, GM_ADDR value_cache_out)
 {
     blockIdx_ = GetBlockIdx();
     blockSize_ = tilingData_->blockSize;
