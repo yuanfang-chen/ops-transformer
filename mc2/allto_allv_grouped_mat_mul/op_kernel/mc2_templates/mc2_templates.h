c3e5354280ca04bd35edd431d3d475cdf88bf015
libohao6 <libohao3@huawei.com>
2026-02-09 15:06:39 +0800

    去掉alltoallvgmm不依赖的部分

--- mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/mc2_templates.h
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/mc2_templates.h
@@ -15,12 +15,9 @@
 #ifndef MC2_TEMPLATES_H
 #define MC2_TEMPLATES_H
 
-#include "scheduler/gmm_a2av_scheduler.h"
 #include "scheduler/a2av_gmm_scheduler.h"
 #include "communication/hccl_a2av_op.h"
-#include "compute/gmm_expert_op.h"
 #include "compute/quant_grouped_matmul.h"
-#include "compute/gmm_compute_op.h"
 #include "common/a2av_common_tiling.h"
 #include "../../3rd/grouped_matmul/op_kernel/arch35/quant_adaptive_sliding_window_templates/gqmm_cube_on_the_fly.h"
 
