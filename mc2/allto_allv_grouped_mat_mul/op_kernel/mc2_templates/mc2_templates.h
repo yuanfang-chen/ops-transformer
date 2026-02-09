550932474497a011cec70c9f564adade731d9abe
libohao6 <libohao3@huawei.com>
2026-02-09 10:06:35 +0800

    kernel

---
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/mc2_templates.h
@@ -0,0 +1,27 @@
+/**
+* Copyright (c) 2025 Huawei Technologies Co., Ltd.
+* This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+* CANN Open Software License Agreement Version 2.0 (the "License").
+* Please refer to the License for details. You may not use this file except in compliance with the License.
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+* See LICENSE in the root of the software repository for the full text of the License.
+*/
+
+/*!
+* \file mc2_templates.h
+* \brief
+*/
+#ifndef MC2_TEMPLATES_H
+#define MC2_TEMPLATES_H
+
+#include "scheduler/gmm_a2av_scheduler.h"
+#include "scheduler/a2av_gmm_scheduler.h"
+#include "communication/hccl_a2av_op.h"
+#include "compute/gmm_expert_op.h"
+#include "compute/quant_grouped_matmul.h"
+#include "compute/gmm_compute_op.h"
+#include "common/a2av_common_tiling.h"
+#include "../../3rd/grouped_matmul/op_kernel/arch35/quant_adaptive_sliding_window_templates/gqmm_cube_on_the_fly.h"
+
+#endif // MC2_TEMPLATES_H
