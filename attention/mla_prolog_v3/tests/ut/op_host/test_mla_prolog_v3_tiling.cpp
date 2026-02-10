29a370943036ec813f1c85606380010a390edf1d
wangss21 <wangxiaoshuang21@huawei.com>
2026-02-10 20:25:11 +0800

    mlaprolog支持qc_qr_scale/kc_scale
    Co-authored-by: wangss21<wangxiaoshuang21@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1560 merge master into master
    
    mlaprolog支持qc_qr_scale/kc_scale
    
    Created-by: wangss21
    Commit-by: wangss21
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1560

--- attention/mla_prolog_v3/tests/ut/op_host/test_mla_prolog_v3_tiling.cpp
+++ attention/mla_prolog_v3/tests/ut/op_host/test_mla_prolog_v3_tiling.cpp
@@ -4443,3 +4443,58 @@ TEST_F(MlaPrologV3, MlaPrologV3_tiling_test80) {
     int64_t expectTilingKey = 1836577;
     ExecuteTestCase(tilingContextPara, ge::GRAPH_FAILED, expectTilingKey);
 }
+
+// 非量化场景正常case：qc_qr_scale = 0.4, kc_scale = 0.5
+TEST_F(MlaPrologV3, MlaPrologV3_tiling_test80) {
+    optiling::MlaPrologCompileInfo compileInfo = {48};
+    gert::TilingContextPara tilingContextPara("MlaPrologV3",
+    {
+        {{{1, 129, 7168}, {1, 129, 7168}}, ge::DT_BF16, ge::FORMAT_ND}, // token_x
+        {{{7168, 1536}, {7168, 1536}}, ge::DT_BF16, ge::FORMAT_FRACTAL_NZ}, // weight_dq
+        {{{1536, 8 * (128 + 64)}, {1536, 8 * (128 + 64)}}, ge::DT_BF16, ge::FORMAT_FRACTAL_NZ}, // weight_uq_qr
+        {{{8, 128, 512}, {8, 128, 512}}, ge::DT_BF16, ge::FORMAT_ND}, // weight_uk
+        {{{7168, 512 + 64}, {7168, 512 + 64}}, ge::DT_BF16, ge::FORMAT_FRACTAL_NZ}, // weight_dkv_kr
+        {{{1536}, {1536}}, ge::DT_BF16, ge::FORMAT_ND}, // rmsnorm_gamma_cq
+        {{{512}, {512}}, ge::DT_BF16, ge::FORMAT_ND}, // rmsnorm_gamma_ckv
+        {{{1, 129, 64}, {1, 129, 64}}, ge::DT_BF16, ge::FORMAT_ND}, // rope_sin
+        {{{1, 129, 64}, {1, 129, 64}}, ge::DT_BF16, ge::FORMAT_ND}, // rope_cos
+        {{{2, 128, 1, 512}, {2, 128, 1, 512}}, ge::DT_BF16, ge::FORMAT_ND}, // kv_cache
+        {{{2, 128, 1, 64}, {2, 128, 1, 64}}, ge::DT_BF16, ge::FORMAT_ND}, // kr_cache
+        {{{1, 129}, {1, 129}}, ge::DT_INT64, ge::FORMAT_ND}, // cache_index
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // dequant_scale_x
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // dequant_scale_w_dq
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // dequant_scale_w_uq_qr
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // dequant_scale_w_dkv_kr
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // quant_scale_ckv
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // quant_scale_ckr
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // smooth_scales_cq
+        {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND}, // actual_seq_len
+        {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND}, // k_nope_clip_alpha
+    },
+    {
+        {{{1, 129, 8, 512}, {1, 129, 8, 512}}, ge::DT_BF16, ge::FORMAT_ND}, // query
+        {{{1, 129, 8, 64}, {1, 129, 8, 64}}, ge::DT_BF16, ge::FORMAT_ND}, // query_rope
+        {{{2, 128, 1, 512}, {2, 128, 1, 512}}, ge::DT_BF16, ge::FORMAT_ND}, // kv_cache
+        {{{2, 128, 1, 64}, {2, 128, 1, 64}}, ge::DT_BF16, ge::FORMAT_ND}, // kr_cache
+        {{{0}, {0}}, ge::DT_FLOAT, ge::FORMAT_ND}, // dequant_scale_q_nope
+        {{{0}, {0}}, ge::DT_BF16, ge::FORMAT_ND}, // query_norm
+        {{{0}, {0}}, ge::DT_FLOAT, ge::FORMAT_ND} // dequant_scale_q_norm
+    },
+    {
+        {"rmsnorm_epsilon_cq", Ops::Transformer::AnyValue::CreateFrom<float>(1e-05f)},
+        {"rmsnorm_epsilon_ckv", Ops::Transformer::AnyValue::CreateFrom<float>(1e-05f)},
+        {"cache_mode", Ops::Transformer::AnyValue::CreateFrom<std::string>("PA_NZ")},
+        {"query_norm_flag", Ops::Transformer::AnyValue::CreateFrom<bool>(false)},
+        {"weight_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        {"kv_cache_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        {"query_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        {"ckvkr_repo_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        {"quant_scale_repo_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        {"tile_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(128)}, // 128 : set value of tile size
+        {"qc_qr_scale", Ops::Transformer::AnyValue::CreateFrom<float>(0.4f)},
+        {"kc_scale", Ops::Transformer::AnyValue::CreateFrom<float>(0.5f)},
+    },
+    &compileInfo,"Ascend950", MlaPrologV3_tiling_A3SocInfo, 4096);
+    int64_t expectTilingKey = 1835026;
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey);
+}
