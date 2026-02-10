8d489abfd7cb934859998fa1e5aa958f7f11131c
Yuanfeng_HW <yuanfeng24@huawei.com>
2026-02-10 17:14:04 +0800

    3rd support

--- mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/common/a2av_common_tiling.h
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/common/a2av_common_tiling.h
@@ -16,10 +16,10 @@
 #ifndef A2AV_COMMON_H
 #define A2AV_COMMON_H
 
-#if __has_include("../../../3rd/grouped_matmul/op_tiling/grouped_matmul_tiling_data_apt.h")
-#include "../../../3rd/grouped_matmul/op_tiling/grouped_matmul_tiling_data_apt.h"
+#if __has_include("../../../3rd/grouped_matmul/op_kernel/grouped_matmul_tiling_data_apt.h")
+#include "../../../3rd/grouped_matmul/op_kernel/grouped_matmul_tiling_data_apt.h"
 #else
-#include "../../../../3rd/grouped_matmul/op_tiling/grouped_matmul_tiling_data_apt.h"
+#include "../../../../3rd/grouped_matmul/op_kernel/grouped_matmul_tiling_data_apt.h"
 #endif
 
 namespace MC2KernelTemplate {
