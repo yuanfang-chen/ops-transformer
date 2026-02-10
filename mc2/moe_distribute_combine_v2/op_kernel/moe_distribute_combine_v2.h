ca587dda12145f87d436e44aa78ca7fb1852c4c5
jiangxiuhan1 <jiangxiuhan@huawei.com>
2026-02-10 21:20:26 +0800

    fix

--- mc2/moe_distribute_combine_v2/op_kernel/moe_distribute_combine_v2.h
+++ mc2/moe_distribute_combine_v2/op_kernel/moe_distribute_combine_v2.h
@@ -251,6 +251,7 @@ private:
     TBuf<> gammaBuf_;
     TBuf<TPosition::VECCALC> reduceFp32Buf_;
     TBuf<> xActMaskTBuf_;
+    TBuf<> rmsNormYBuf_;
     TBuf<> xActMaskCastTBuf_;
     TBuf<> tokenTargetTBuf_;
     TBuf<> validBsIndexTBuf_;
