33d75a6fa03f96d41b86680b6f8e259890fb888a
Yuanfeng_HW <yuanfeng24@huawei.com>
2026-02-10 17:06:55 +0800

    3rd move to mc2/3rd

--- mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/compute/quant_grouped_matmul.h
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/mc2_templates/compute/quant_grouped_matmul.h
@@ -17,8 +17,7 @@
 #define MC2_QUANT_GROUPED_MATMUL_H
 
 #include "kernel_operator.h"
-#include "../../3rd/gqmm_cube_on_the_fly.h"
-
+#include "../../../3rd/grouped_matmul/op_kernel/gqmm_cube_on_the_fly.h"
 using namespace AscendC;
 
 namespace MC2KernelTemplate {
