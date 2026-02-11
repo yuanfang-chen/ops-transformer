f74ac610b3df5f0a3575ccc8b9e9a7c6f2353bea
qwqinyang <qinyang24@hisilicon.com>
2026-02-11 09:50:28 +0800

    clean sc

--- attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling.cpp
+++ attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling.cpp
@@ -185,6 +185,9 @@ constexpr int64_t S1_VEC2_MULTIPLIER_2_HOST_TILING = 2;
 
 constexpr uint32_t BATCH_MODE_SCHEDULE = 1;
 
+constexpr uint32_t BATCH_LIMIT_128 = 128;
+constexpr uint32_t BATCH_LIMIT_300 = 300;
+
 inline int32_t ConvertValueToIndexMM(int32_t val, int32_t idxBound)
 {
     return (val > PP_MM[idxBound]) ? idxBound : (val / PP_INDEX - 1);
@@ -4349,7 +4352,7 @@ ge::graphStatus PromptFlashAttentionTiling::RunBigKernelTilingWithParams(Context
         OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
         "batch size(%u) should not be larger than 128 when input layout is SH!", b),
         return ge::GRAPH_FAILED);
-    size_t batchLimit = (curShortSocName == platform_ascendc::SocVersion::ASCEND310P && inputLayout == InputLayout::BSH) ? 300 : 128;
+    size_t batchLimit = (curShortSocName == platform_ascendc::SocVersion::ASCEND310P && inputLayout == InputLayout::BSH) ? BATCH_LIMIT_300 : BATCH_LIMIT_128;
     OP_CHECK_IF((curShortSocName == platform_ascendc::SocVersion::ASCEND310P && b > batchLimit),
         OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
         "ascend310p platform do not support batch size(%u) more than %zu.", b, batchLimit),
