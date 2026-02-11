2b2f898773f662e21929a4a75edbea47c56b046e
Yuanfeng_HW <yuanfeng24@huawei.com>
2026-02-12 01:35:00 +0800

    apt kernel

--- mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul_apt.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_kernel/allto_allv_grouped_mat_mul_apt.cpp
@@ -88,34 +88,15 @@ __global__ __aicore__ void allto_allv_grouped_mat_mul(GM_ADDR gmmxGM, GM_ADDR gm
         }
     #else 
         REGISTER_TILING_DEFAULT(QuantAlltoAllvGroupedMatmulTilingData);
-        
-        using ComputeOpType = QuantGroupedMatmul<
-            QuantAlltoAllvGroupedMatmulTilingData, 
-            GMMQuantTilingData, 
-            DTYPE_GMM_X, 
-            DTYPE_GMM_WEIGHT,
-            float, 
-            DTYPE_GMM_Y, 
-            CubeFormat::ND, 
-            TILINGKEY_GMM_WEIGHT_TRANSPOSE, 
-            TILINGKEY_MM_WEIGHT_TRANSPOSE,
-            false>;  // isLocal=false
-
-        using LocalComputeOpType = QuantGroupedMatmul<
-            QuantAlltoAllvGroupedMatmulTilingData, 
-            GMMQuantTilingData, 
-            DTYPE_GMM_X, 
-            DTYPE_GMM_WEIGHT,
-            float, 
-            DTYPE_GMM_Y, 
-            CubeFormat::ND, 
-            TILINGKEY_GMM_WEIGHT_TRANSPOSE, 
-            TILINGKEY_MM_WEIGHT_TRANSPOSE,
-            true>;  // isLocal=true
-
-        A2avGmmScheduler<HcclA2avOp<DTYPE_GMM_WEIGHT, true>,
-            ComputeOpType, LocalComputeOpType, QuantAlltoAllvGroupedMatmulTilingData, 
-            GMMQuantTilingData, TILING_TYPE, TILINGKEY_MM>
+        using ComputeOpType = QuantGroupedMatmul<QuantAlltoAllvGroupedMatmulTilingData, GMMQuantTilingData, DTYPE_GMM_X,
+            DTYPE_GMM_WEIGHT, float, DTYPE_GMM_Y, CubeFormat::ND, TILINGKEY_GMM_WEIGHT_TRANSPOSE, TILINGKEY_MM_WEIGHT_TRANSPOSE,
+            false>; // isLocal=false
+        using LocalComputeOpType =
+            QuantGroupedMatmul<QuantAlltoAllvGroupedMatmulTilingData, GMMQuantTilingData, DTYPE_GMM_X, DTYPE_GMM_WEIGHT,
+            float, DTYPE_GMM_Y, CubeFormat::ND, TILINGKEY_GMM_WEIGHT_TRANSPOSE, TILINGKEY_MM_WEIGHT_TRANSPOSE,
+            true>; // isLocal=true
+        A2avGmmScheduler<HcclA2avOp<DTYPE_GMM_WEIGHT, true>, ComputeOpType, LocalComputeOpType,
+            QuantAlltoAllvGroupedMatmulTilingData, GMMQuantTilingData, TILING_TYPE, TILINGKEY_MM>
             a2avGmmScheduler;
         GET_NESTED_TILING_DATA_MEMBER_ADDR(QuantAlltoAllvGroupedMatmulTilingData, GMMQuantTilingData, gmmQuantTilingData,
             gmmArray, gmmArrayAddr_, tilingGM);
