832a3a282ef3f15192327af57c25c428ab7b03ab
tooslow <zhangzhizhuo1@huawei.com>
2026-02-11 11:14:33 +0800

    我要出包

--- gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing.cpp
+++ gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing.cpp
@@ -12,12 +12,13 @@
  * \file grouped_matmul_finalize_routing.cpp
  * \brief
  */
-#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 220
-#include "grouped_matmul_finalize_routing.h"
-#include "grouped_matmul_finalize_routing_antiquant_a8w4_msd_pre.h"
-#include "grouped_matmul_finalize_routing_antiquant_a8w4_msd.h"
-#include "grouped_matmul_finalize_routing_antiquant_a8w4_msd_l1_opt.h"
-#endif
+
+// #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 220
+// #include "grouped_matmul_finalize_routing.h"
+// #include "grouped_matmul_finalize_routing_antiquant_a8w4_msd_pre.h"
+// #include "grouped_matmul_finalize_routing_antiquant_a8w4_msd.h"
+// #include "grouped_matmul_finalize_routing_antiquant_a8w4_msd_l1_opt.h"
+// #endif
 
 
 #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
@@ -29,6 +30,7 @@
 #include "kernel_operator.h"
 #endif
 #include "lib/matmul_intf.h"
+#include "arch35/grouped_matmul_finalize_routing_tiling_key.h"
 #if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
     #include "arch35/grouped_matmul_finalize_routing.h"
 #elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
