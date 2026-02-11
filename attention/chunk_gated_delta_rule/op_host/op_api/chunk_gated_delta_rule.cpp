a7fe993741392e4bc803f26589b49625033a87f2
LAIM321 <laiming2@huawei.com>
2026-02-11 14:32:11 +0800

    新增Chunk Gated Delta Rule的单算子调用、图模式调用、opapi ut和ophost ut功能

---
+++ attention/chunk_gated_delta_rule/op_host/op_api/chunk_gated_delta_rule.cpp
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file chunk_gated_delta_rule.cpp
+ * \brief
+ */
+
+#include "chunk_gated_delta_rule.h"
+#include "aclnn_kernels/common/op_error_check.h"
+#include "opdev/make_op_executor.h"
+#include "opdev/op_def.h"
+#include "opdev/op_dfx.h"
+#include "opdev/op_executor.h"
+#include "opdev/op_log.h"
+#include "level0/fill.h"
+
+using namespace op;
+
+namespace l0op {
+    OP_TYPE_REGISTER(ChunkGatedDeltaRule);
+    
+    const std::array<const aclTensor *, 2> ChunkGatedDeltaRule(
+        const aclTensor *query,
+        const aclTensor *key, 
+        const aclTensor *value,
+        const aclTensor *beta,
+        const aclTensor *initialState,
+        const aclTensor *actualSeqLengths,
+        const aclTensor *gOptional,
+        float scaleValue,
+        aclOpExecutor *executor) 
+    {
+        L0_DFX(ChunkGatedDeltaRule, query, key, value, beta, initialState, actualSeqLengths, gOptional, scaleValue);
+
+        // 校验函数：校验gOptional 是否为空，为空时按全 0 赋值
+        const aclTensor *gOptionalTensor = gOptional;
+        if (gOptionalTensor == nullptr) {
+            auto valueShape = value->GetViewShape();
+            const int64_t tDim = valueShape.GetDim(0);
+            const int64_t nvDim = valueShape.GetDim(1);
+            const int64_t gShape[2] = {tDim, nvDim};
+            const aclTensor *dims = executor->ConvertToTensor(gShape, 2, DataType::DT_INT64);
+            aclIntArray *shapeArray = executor->AllocIntArray(gShape, 2);
+
+            const aclScalar *zeroScalar = executor->AllocScalar(0);
+            const aclTensor *zeroTensor = executor->ConvertToTensor(zeroScalar, DataType::DT_FLOAT);
+            gOptionalTensor = l0op::Fill(dims, zeroTensor, shapeArray, executor);
+
+            if (gOptionalTensor == nullptr) {
+                OP_LOGE(ACLNN_ERR_INNER_NULLPTR,
+                    "gOptional is nullptr, but ChunkGatedDeltaRule create zero tensor gOptional failed.");
+                return {nullptr, nullptr};
+            }
+        }
+
+        DataType outType = value->GetDataType();
+        Format format = Format::FORMAT_ND;
+        auto out = executor->AllocTensor(outType, format, format);
+        auto finalState = executor->AllocTensor(outType, format, format);
+
+        auto ret = INFER_SHAPE(ChunkGatedDeltaRule, 
+            OP_INPUT(query, key, value, beta, initialState, actualSeqLengths, gOptionalTensor),
+            OP_OUTPUT(out, finalState), 
+            OP_ATTR(scaleValue));
+        if (ret != ACLNN_SUCCESS) {
+            OP_LOGE(ACLNN_ERR_PARAM_INVALID, "ChunkGatedDeltaRule InferShape failed.");
+            return {nullptr, nullptr};
+        }
+ 
+        ret = ADD_TO_LAUNCHER_LIST_AICORE(ChunkGatedDeltaRule,
+            OP_INPUT(query, key, value, beta, initialState, actualSeqLengths, gOptionalTensor),
+            OP_OUTPUT(out, finalState), 
+            OP_ATTR(scaleValue));
+        if (ret != ACLNN_SUCCESS) {
+            OP_LOGE(ACLNN_ERR_PARAM_INVALID, "ChunkGatedDeltaRule ADD_TO_LAUNCHER_LIST_AICORE failed.");
+            return {nullptr, nullptr};
+        }
+
+        return {out, finalState};
+    }
+} // namespace l0op
