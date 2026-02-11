422e2d19f572f483948cafcf9b2251fdc59ff0bd
wuyewei <wuyewei@huawei.com>
2026-02-11 16:01:52 +0800

    v1
    v2-修改入参和配置文件
    
    v3-修正infershape和tiling和kernel结构
    
    v4-参考A3实现进行具体修改和补全
    
    人工调整编译相关
    
    v5-UT修改
    
    编译修正
    
    使用新tiling方式
    
    CodeCheck修改
    
    aclnn格式调整
    
    v6-根据方案修改
    
    编译问题调整
    
    v7-精度问题修正
    
    Revert "v7-精度问题修正"
    
    This reverts commit 65c86af159b5e067e2dff779f3e322abe1986c07.
    
    Revert "编译问题调整"
    
    This reverts commit 986abd3b40c05a2a084c190fa0f8627cb20096db.
    
    Revert "v6-根据方案修改"
    
    This reverts commit 18eb1821c12c6be371f6dde78bfc63cd23c8f2c2.
    
    Revert "aclnn格式调整"
    
    This reverts commit f1e9e823583915de22c2c244915daead7a08675e.
    
    add proto
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify unknownshape
    
    modify cmake
    
    modify infershape
    
    modify infershape&UT
    
    add proto

--- moe/moe_init_routing_v2/op_graph/moe_init_routing_v2_proto.h
+++ moe/moe_init_routing_v2/op_graph/moe_init_routing_v2_proto.h
@@ -19,60 +19,24 @@
 
 namespace ge {
 /**
-* @brief compute init routing for moe input.
+* @brief Fuse the main branch feature h_out with the residual branch feature x 
+* using the gating mechanism h_post and the doubly stochastic matrix  h_res 
+* to enable information flow.
 * @par Inputs:
-* @li x: A 2D Tensor. Represents the input token. shape is (num_rows, H), num_rows is the number of tokens, H is the
-*        length of each token. Type is:BFloat16, Float16 or Float32. Format support ND.
-* @li expert_idx: A 2D Tensor. Represents the id of top k experts for each tokens. shape is (num_row, K),
-*                 it can be (num_row, ) in Ascend 950PR/Ascend 950DT AI Processor.
-*                 When drop_pad_mode is 1, or drop_pad_mode is 0 but expert_tokens_count_or_cumsum_flag is not 0,
-*                 The range of value is [0, expert_num). Other scenario the value of expert_idx cannot be less than
-*                 0. Type is:Int32 or Int64, Int64 only support in Ascend 950PR/Ascend 950DT AI Processor.
-*                 Format support ND.
+* @li x: A Tensor. Type is:BFloat16 or Float16.
+* @li h_res: A Tensor. Type is:Float32.
+* @li h_out: A Tensor. Type is:BFloat16 or Float16.
+* @li h_post: A Tensor. Type is:Float32.
 * @par Outputs:
-* @li expanded_x: A 2D or 3D Tensor. Type is:BFloat16, Float16 or Float32.
-*                 The data type must be the same as that of x. Format support ND.
-*                 dropless scenario:
-*                 the first dim is val=min(num_rows * K, active_num), so shape is (val, H)
-*                 dropPad scenario:
-*                 shape is (expert_num, expert_capacity, H)
-* @li expanded_row_idx: A 1D Tensor. Type is:Int32. Shape is (num_rows * K,). Format support ND.
-* @li expert_tokens_count_or_cumsum: A 1D Tensor. Represents the number of tokens processed by each expert and the
-* cumulative value. The value is controlled by expert_tokens_count_or_cumsum_flag to output. Type is:Int32. shape
-* is (expert_num,). Format support ND.
-* @li expert_tokens_before_capacity: A 1D Tensor. Represents the number of tokens processed by each expert before
-* drop. The value is controlled by expert_tokens_before_capacity_flag to output. Type is:Int32. shape is
-* (expert_num,). Format support ND.
-* @par Attributes:
-* @li active_num: Optional parameter. Type is:Int32. Identify activate scenario. The value 0 indicates a non-active
-*                 scenario, and a value greater than 0 indicates an active scenario. In the active scenario, the size
-*                 of axis 0 of grad_expanded_x must be equal to the value of active_num. Default: 0.
-* @li expert_capacity: Optional parameter. Type is:Int32. The max tokens count of every expert. Default: 0.
-* @li expert_num: Optional parameter. Type is:Int32. Default: 0.
-* @li drop_pad_mode: Optional parameter. Type is:Int32. The value is 0(dropless) or 1(dropPad). Default: 0.
-* @li expert_tokens_count_or_cumsum_flag: Optional parameter. Type is:Int32. The value is 0 (no token count),
-*                                         1(compute token count) or 2(compute token cumsum), which in dropless
-*                                         scenario. Default: 0.
-* @li expert_tokens_before_capacity_flag: Optional parameter. Type is:Bool. The value is true (no tokens count) or
-*                                         1(compute token count), which in dropPad scenario. Default: false.
+* @li y: A Tensor. Type is:BFloat16 or Float16.
 */
-REG_OP(MoeInitRoutingV2)
-    .INPUT(x, "T1")
-    .INPUT(expert_idx, "T3")
-    .OUTPUT(expanded_x, "T1")
-    .OUTPUT(expanded_row_idx, "T2")
-    .OUTPUT(expert_tokens_count_or_cumsum, "T2")
-    .OUTPUT(expert_tokens_before_capacity, "T2")
-    .DATATYPE(T1, TensorType({DT_FLOAT, DT_FLOAT16, DT_BF16}))
-    .DATATYPE(T2, TensorType({DT_INT32}))
-    .DATATYPE(T3, TensorType({DT_INT32, DT_INT64}))
-    .ATTR(active_num, Int, 0)
-    .ATTR(expert_capacity, Int, 0)
-    .ATTR(expert_num, Int, 0)
-    .ATTR(drop_pad_mode, Int, 0)
-    .ATTR(expert_tokens_count_or_cumsum_flag, Int, 0)
-    .ATTR(expert_tokens_before_capacity_flag, Bool, false)
-    .OP_END_FACTORY_REG(MoeInitRoutingV2)
+REG_OP(MhcPost)
+    .INPUT(x, TensorType({DT_FLOAT16, DT_BF16}))
+    .INPUT(h_res, TensorType({DT_FLOAT}))
+    .INPUT(h_out, TensorType({DT_FLOAT16, DT_BF16}))
+    .OUTPUT(h_post, TensorType({DT_FLOAT}))
+    .OUTPUT(y, TensorType({DT_FLOAT16, DT_BF16}))
+    .OP_END_FACTORY_REG(MhcPost)
 
 } // namespace ge
 
