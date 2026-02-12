25c59a18251f1eb34c9e50ada5ff1b88a8000213
Davon14272 <liuwenda4@huawei.com>
2026-02-12 17:32:36 +0800

    离线编译修改文档
    Co-authored-by: davon<liuwenda4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1792 merge Outdocs into master
    
    离线编译修改文档
    
    Created-by: Davon14272
    Commit-by: davon
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    本次PR主要解决了项目文档的准确性与完整性，以及离线编译场景下依赖获取的易用性问题。具体包括：修正了文档中关于“算子调用”流程的模糊描述和错误的内部链接；重构了算子调用指南，补充了离线编译指导与静态库使用示例，使指引更结构化、清晰。同时，新增了一个自动化脚本，用于在无网络环境下下载关键的第三方编译依赖库，以提升项目的部署效率。
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    关联issue 469
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    更新了README.md、docs/zh/context/build_offline.md、docs/zh/context/build_offline.md、docs/zh/develop/aicore_develop_guide.md和docs/zh/invocation/quick_op_invocation.md
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [x] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1792

---
+++ cann_3rd_lib_path_download.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+import urllib.request
+import os
+
+
+def down_files_native(url_list):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # 创建子目录（例如：downloads）
+    download_dir = os.path.join(current_dir, "cann_3rd_lib_path_download")
+    os.makedirs(download_dir, exist_ok=True)  # 如果目录不存在则创建
+    
+    for url in url_list:
+
+        file_name = url.split('/')[-1]
+        
+        if not file_name:
+            file_name = "downloaded_file"
+        
+        # 将文件保存到新建的目录下
+        file_path = os.path.join(download_dir, file_name)
+        
+        urllib.request.urlretrieve(url, file_path)
+
+if __name__ == "__main__":
+    my_urls = [
+        "https://gitcode.com/cann-src-third-party/googletest/releases/download/v1.14.0/googletest-1.14.0.tar.gz",
+        "https://gitcode.com/cann-src-third-party/json/releases/download/v3.11.3/include.zip",
+        ("https://gitcode.com/cann-src-third-party/makeself/releases/download/"
+        "release-2.5.0-patch1.0/makeself-release-2.5.0-patch1.tar.gz"),
+        "https://gitcode.com/cann-src-third-party/pybind11/releases/download/v2.13.6/pybind11-2.13.6.tar.gz",
+        "https://gitcode.com/cann-src-third-party/eigen/releases/download/5.0.0/eigen-5.0.0.tar.gz",
+        "https://gitcode.com/cann-src-third-party/protobuf/releases/download/v25.1/protobuf-25.1.tar.gz",
+        ("https://gitcode.com/cann-src-third-party/abseil-cpp/releases/download/"
+        "20230802.1/abseil-cpp-20230802.1.tar.gz")
+    ]
+    
+    down_files_native(my_urls)
--- docs/README.md
+++ docs/README.md
@@ -33,7 +33,7 @@
 | [算子列表](zh/op_list.md)                        | 介绍项目包含的所有算子清单。                                 |
 | [aclnn列表](zh/op_api_list.md)                   | 介绍项目包含的所有算子API，通过该API可直调算子。             |
 | [环境部署](zh/context/quick_install.md)          | 介绍基础环境搭建过程，包括不同场景下软件包和第三方依赖的获取和安装。 |
-| [算子调用](zh/invocation/quick_op_invocation.md) | 介绍如何调用项目算子，包括不同场景下编译和执行算子包、UT等。 |
+| [算子调用](zh/invocation/quick_op_invocation.md) | 介绍如何编译源码并执行算子，包括不同场景下的算子包编译、算子样例执行、UT执行等。 |
 | [算子开发](zh/develop/aicore_develop_guide.md)   | 介绍如何基于本项目工程开发新算子，包括算子原型定义、Tiling实现、Kernel实现等。 |
 | [算子调用方式](zh/invocation/op_invocation.md)   | 介绍多种算子调用方式和调用流程，例如aclnn调用、图模式调用等。 |
 | [算子调试调优](zh/debug/op_debug_prof.md)        | 介绍常见的算子调试、调优方法。                               |
