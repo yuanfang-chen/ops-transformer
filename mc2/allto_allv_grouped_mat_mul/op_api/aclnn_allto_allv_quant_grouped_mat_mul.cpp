58a156832a4f71b5db5142ba062adb9b8600a48a
qq_57796320 <zhushuqin1@huawei.com>
2026-02-12 20:54:19 +0800

    code-check

--- mc2/allto_allv_grouped_mat_mul/op_api/aclnn_allto_allv_quant_grouped_mat_mul.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_api/aclnn_allto_allv_quant_grouped_mat_mul.cpp
@@ -726,8 +726,7 @@ extern "C" aclnnStatus aclnnAlltoAllvQuantGroupedMatMulGetWorkspaceSize(
     }
 
     // 处理非连续Tensor，目前支持转置的mmWeightOptional涉及该处理
-    if (CheckMmWeightValid(mmWeightOptional)) // 先检查mmWeightOptional是否合法，避免非法操作
-    {
+    if (CheckMmWeightValid(mmWeightOptional)) {
         bool notContiguous = IsTransposeLastTwoDims(mmWeightOptional); // notContiguous标识mmWeightOptional是否是非连续的
         auto transMmWeightOptional = mmWeightOptional;
         if (notContiguous && transMmWeight) { // 当非连续和转置同时生效时，判断为错误用法，直接报错
