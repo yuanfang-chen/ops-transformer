6c629707fc5527a6d44ce452c33ca91176f00b1d
fishing_artist <liuhuansheng1@h-partners.com>
2026-02-12 15:46:47 +0800

    add pipeall

--- attention/scatter_pa_kv_cache/op_host/scatter_pa_kv_cache_tiling_arch35.cpp
+++ attention/scatter_pa_kv_cache/op_host/scatter_pa_kv_cache_tiling_arch35.cpp
@@ -290,12 +290,13 @@ ge::graphStatus ScatterPaKvCacheTiling::TemplateRope()
         std::max(alignKHead, alignVHead) * floatFactor * DIM1 +    // reduce Buf for inputKeyLocal or inputValueLocal
         std::max(alignKHead, alignVHead) * floatFactor * DIM1 +    // divide Buf
         std::max(alignKHead, alignVHead) * floatFactor * DIM1;     // cast Buf
-    // if (ubThreshold <= maxHandleNumPerLoop) {
-    //     // tail dim can fully load
-    //     isFullyLoad_ = FULLY_LOAD;
-    //     OP_LOGD(context_, "tail dim can fully load.");
-    //     return ge::GRAPH_SUCCESS;
-    // }
+    if (ubThreshold <= maxHandleNumPerLoop) {
+        // tail dim can fully load
+        isFullyLoad_ = FULLY_LOAD;
+        OP_LOGD(context_, "tail dim can fully load.1");
+        OP_LOGI(context_, "tail dim can fully load.2");
+        return ge::GRAPH_SUCCESS;
+    }
     // can not fully load
     OP_LOGD(context_, "tail dim can not fully load.1");
     OP_LOGI(context_, "tail dim can not fully load.2");
