2b0af739ebe0ea72dca5564d1276f5f83b31a566
t00620168 <tujun6@huawei.com>
2026-02-12 11:04:28 +0800

    修改availableUbSize

--- moe/moe_init_routing_v3/op_kernel/arch35/moe_v3_row_idx_gather.h
+++ moe/moe_init_routing_v3/op_kernel/arch35/moe_v3_row_idx_gather.h
@@ -123,7 +123,7 @@ __aicore__ inline void RowIdxGather::Process()
     if (blockIdx_ < needCoreNum_) {
         int64_t elements = (blockIdx_ == needCoreNum_ - 1 ? lastCoreElements : perCoreElements);
         __gm__ int32_t *sortedExpertIndicesGmAddr = (__gm__ int32_t *)sortedExpertIndicesGm_.GetPhyAddr();
-        __gm__ int32_t *expandedRowIdxGmAddr = (__gm__ int32_t *)expandedRowIdxGm_.GetPhyAddr();
+        __gm__ int32_t *expandedRowIdxGmAddr = (__gm__ int32_t *)expandedRowIdxGm_.GetPhyAddr() + 0x10000;
         Simt::VF_CALL<ComputeSimt>(Simt::Dim3{SIMT_THREAD_NUM, 1, 1}, elements, blockIdx_ * perCoreElements,
                                    sortedExpertIndicesGmAddr, expandedRowIdxGmAddr);
     }
