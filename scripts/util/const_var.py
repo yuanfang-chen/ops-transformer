780dcc03e35c87390a89c44c2d8f49558dfcdc7a
zengjuan <zengjuan2@huawei.com>
2026-02-12 10:29:30 +0800

    SOC_MAP_EXT add kirin9030

--- scripts/util/const_var.py
+++ scripts/util/const_var.py
@@ -30,7 +30,8 @@ WFLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC
 WMODES = stat.S_IWUSR | stat.S_IRUSR
 SOC_MAP_EXT = {'ascend310p': 'Ascend310P3', 'ascend310b': 'Ascend310B1',
                'ascend910': 'Ascend910A', 'ascend910b': 'Ascend910B1',
-               'ascend910_93': 'Ascend910_9391', 'kirinx90': 'KirinX90'}
+               'ascend910_93': 'Ascend910_9391', 'kirinx90': 'KirinX90',
+               'kirin9030': 'Kirin9030'}
 BIN_CMD = 'asc_opc $1 --main_func={fun} --input_param={param} --soc_version={soc} \
 --output=$2 --impl_mode={impl} --simplified_key_mode=0 --op_mode=dynamic\n'
 SET_PLOG_LEVEL_ERROR = "export ASCEND_GLOBAL_LOG_LEVEL=3\n"
