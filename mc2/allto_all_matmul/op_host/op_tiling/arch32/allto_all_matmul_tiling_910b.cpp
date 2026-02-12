90152bcb408878481d2e358c58f792e2591cf51d
lyt_claire <luyitong1@huawei.com>
2026-02-12 17:16:49 +0800

    fix 8ranks perf

--- mc2/allto_all_matmul/op_host/op_tiling/arch32/allto_all_matmul_tiling_910b.cpp
+++ mc2/allto_all_matmul/op_host/op_tiling/arch32/allto_all_matmul_tiling_910b.cpp
@@ -956,6 +956,10 @@ ge::graphStatus AlltoAllMatmulTiling910b::DoMmCommTiling(CoCTiling &cocTilingDat
         AlltoAllMatmulNPU910BTwoRankA16W8Tiling(cocTilingData, info);
         return ge::GRAPH_SUCCESS;
     }
+    if (info.rankSize == 8 && quantType == TILINGKEY_TPL_A16W8) {
+        AlltoAllMatmulNPU910BEightRankA16W8Tiling(cocTilingData, info);
+        return ge::GRAPH_SUCCESS;
+    }
     // basic、A4W4策略
     if (info.rankSize == 2) {  // 若2卡
         DoTwoRankTiling(cocTilingData, info);
