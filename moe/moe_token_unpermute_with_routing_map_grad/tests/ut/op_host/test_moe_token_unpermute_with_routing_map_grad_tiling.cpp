b9b84bcd98b18242c224c9a8e339e0070dd086da
陈琳鑫 <chenlinxin4@huawei.com>
2026-02-12 16:32:28 +0800

    add moe_token_unpermute_with_routing_map_grad A5

--- moe/moe_token_unpermute_with_routing_map_grad/tests/ut/op_host/test_moe_token_unpermute_with_routing_map_grad_tiling.cpp
+++ moe/moe_token_unpermute_with_routing_map_grad/tests/ut/op_host/test_moe_token_unpermute_with_routing_map_grad_tiling.cpp
@@ -38,7 +38,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_not_none_bf16
     optiling::MoeTokenUnpermuteWithRoutingMapGradCompileInfo compileInfo = {};
     std::string socVersion = "Ascend910B";
     uint64_t coreNum = 40;
-    uint64_t ubSize = 196608;
+    uint64_t ubSize = 196352;
     gert::TilingContextPara tilingContextPara("MoeTokenUnpermuteWithRoutingMapGrad",
                                               {
                                                   {{{30, 64}, {30, 64}}, ge::DT_BF16, ge::FORMAT_ND},
@@ -56,7 +56,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_not_none_bf16
                                               &compileInfo,
                                               socVersion, coreNum, ubSize);
     int64_t expectTilingKey = 1;
-    string expectTilingData = "30 1 0 64 64 30 30 10 1 0 1 0 64 1 0 64 0 1 16 64 196608 ";
+    string expectTilingData = "30 1 0 64 64 30 30 10 1 0 1 0 64 1 0 64 0 1 16 64 196352 ";
     std::vector<size_t> expectWorkspaces = {16 * 1024 * 1024};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
@@ -65,7 +65,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_not_none_bf16
     optiling::MoeTokenUnpermuteWithRoutingMapGradCompileInfo compileInfo = {};
     std::string socVersion = "Ascend910B";
     uint64_t coreNum = 40;
-    uint64_t ubSize = 196608;
+    uint64_t ubSize = 196352;
     gert::TilingContextPara tilingContextPara("MoeTokenUnpermuteWithRoutingMapGrad",
                                               {
                                                   {{{30, 64}, {30, 64}}, ge::DT_BF16, ge::FORMAT_ND},
@@ -83,7 +83,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_not_none_bf16
                                               &compileInfo,
                                               socVersion, coreNum, ubSize);
     int64_t expectTilingKey = 11;
-    string expectTilingData = "30 0 0 64 64 30 30 10 0 0 1 0 64 1 8 64 1 1 0 0 196608 ";
+    string expectTilingData = "30 0 0 64 64 30 30 10 0 0 1 0 64 1 8 64 1 1 0 0 196352 ";
     std::vector<size_t> expectWorkspaces = {16 * 1024 * 1024};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
@@ -92,7 +92,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_none_bf16_til
     optiling::MoeTokenUnpermuteWithRoutingMapGradCompileInfo compileInfo = {};
     std::string socVersion = "Ascend910B";
     uint64_t coreNum = 40;
-    uint64_t ubSize = 196608;
+    uint64_t ubSize = 196352;
     gert::TilingContextPara tilingContextPara("MoeTokenUnpermuteWithRoutingMapGrad",
                                               {
                                                   {{{30, 64}, {30, 64}}, ge::DT_BF16, ge::FORMAT_ND},
@@ -108,7 +108,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tiling_prob_none_bf16_til
                                               &compileInfo,
                                               socVersion, coreNum, ubSize);
     int64_t expectTilingKey = 0;
-    string expectTilingData = "30 1 0 0 64 30 30 10 0 0 1 0 64 1 0 64 1 1 0 0 196608 ";
+    string expectTilingData = "30 1 0 0 64 30 30 10 0 0 1 0 64 1 0 64 1 1 0 0 196352 ";
     std::vector<size_t> expectWorkspaces = {16 * 1024 * 1024};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
@@ -117,7 +117,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tilingkey_101) {
     optiling::MoeTokenUnpermuteWithRoutingMapGradCompileInfo compileInfo = {};
     std::string socVersion = "Ascend910B";
     uint64_t coreNum = 40;
-    uint64_t ubSize = 196608;
+    uint64_t ubSize = 196352;
     gert::TilingContextPara tilingContextPara("MoeTokenUnpermuteWithRoutingMapGrad",
                                               {
                                                   {{{30, 8}, {30, 8}}, ge::DT_BF16, ge::FORMAT_ND},
@@ -135,7 +135,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tilingkey_101) {
                                               &compileInfo,
                                               socVersion, coreNum, ubSize);
     int64_t expectTilingKey = 101;
-    string expectTilingData = "30 1 0 8 8 30 30 10 1 0 1 0 16 1 0 8 0 1 16 32 196608 ";
+    string expectTilingData = "30 1 0 8 8 30 30 10 1 0 1 0 16 1 0 8 0 1 16 32 196352 ";
     std::vector<size_t> expectWorkspaces = {16 * 1024 * 1024};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
@@ -144,7 +144,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tilingkey_111) {
     optiling::MoeTokenUnpermuteWithRoutingMapGradCompileInfo compileInfo = {};
     std::string socVersion = "Ascend910B";
     uint64_t coreNum = 40;
-    uint64_t ubSize = 196608;
+    uint64_t ubSize = 196352;
     gert::TilingContextPara tilingContextPara("MoeTokenUnpermuteWithRoutingMapGrad",
                                               {
                                                   {{{30, 8}, {30, 8}}, ge::DT_BF16, ge::FORMAT_ND},
@@ -162,7 +162,7 @@ TEST_F(MoeTokenUnpermuteWithRoutingMapGradTiling, test_tilingkey_111) {
                                               &compileInfo,
                                               socVersion, coreNum, ubSize);
     int64_t expectTilingKey = 111;
-    string expectTilingData = "30 0 3 8 8 30 30 10 0 0 1 0 16 1 8 8 1 1 0 0 196608 ";
+    string expectTilingData = "30 0 3 8 8 30 30 10 0 0 1 0 16 1 8 8 1 1 0 0 196352 ";
     std::vector<size_t> expectWorkspaces = {16 * 1024 * 1024};
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
 }
