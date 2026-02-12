b1a3da61d285032b3946099d2b3d189b9cc45308
qq_57796320 <zhushuqin1@huawei.com>
2026-02-12 21:47:00 +0800

    quant-delete

--- mc2/allto_allv_grouped_mat_mul/op_api/aclnn_allto_allv_quant_grouped_mat_mul.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_api/aclnn_allto_allv_quant_grouped_mat_mul.cpp
@@ -274,8 +274,7 @@ static bool CheckEmptyTensor(const aclTensor *gmmX, const aclTensor *gmmWeight, 
 }
 
 static bool CheckQuantValid(int64_t gmmXQuantMode, int64_t gmmWeightQuantMode, const aclTensor *gmmXScale,
-                            const aclTensor *gmmWeightScale, int64_t mmXQuantMode, int64_t mmWeightQuantMode,
-                            const aclTensor *mmXScaleOptional, const aclTensor *mmWeightScaleOptional) {
+                            const aclTensor *gmmWeightScale) {
     if (static_cast<QuantModeType>(gmmXQuantMode) == QuantModeType::NO_QUANT) {
         if ((gmmXScale != nullptr)) {
             OP_LOGE(ACLNN_ERR_PARAM_INVALID, "gmmXScale should be empty.");
@@ -609,9 +608,7 @@ static aclnnStatus CheckParams(const aclTensor *gmmX, const aclTensor *gmmWeight
                                mmXScaleOptional, mmWeightScaleOptional, gmmY, mmYOptional, permuteOutOptional),
               ACLNN_ERR_PARAM_INVALID);
     // 8.检查Quant
-    CHECK_RET(CheckQuantValid(gmmXQuantMode, gmmWeightQuantMode, gmmXScale, gmmWeightScale, mmXQuantMode,
-                              mmWeightQuantMode, mmXScaleOptional, mmWeightScaleOptional),
-              ACLNN_ERR_PARAM_INVALID);
+    CHECK_RET(CheckQuantValid(gmmXQuantMode, gmmWeightQuantMode, gmmXScale, gmmWeightScale), ACLNN_ERR_PARAM_INVALID);
     // 9.检查shape
     CHECK_RET(CheckGmmShape(gmmX, gmmWeight, gmmXScale, gmmWeightScale, gmmY, epWorldSize), ACLNN_ERR_PARAM_INVALID);
 
