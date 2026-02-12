686f8bd81c59da288f5d8ea46a06c6662e2e2557
wuyewei <wuyewei@huawei.com>
2026-02-12 20:43:29 +0800

    使用新tiling方式

--- mhc/mhc_post/op_host/mhc_post_tiling.h
+++ mhc/mhc_post/op_host/mhc_post_tiling.h
@@ -18,28 +18,10 @@
 #include <cstdint>
 #include <register/tilingdata_base.h>
 #include <tiling/tiling_api.h>
+#include "../op_kernel/arch35/mhc_post_tiling_data.h"
+#include "../op_kernel/arch35/mhc_post_tiling_key.h"
 
 namespace optiling {
-
-BEGIN_TILING_DATA_DEF(MhcPostTilingData)
-TILING_DATA_FIELD_DEF(uint32_t, totalItems);
-TILING_DATA_FIELD_DEF(uint32_t, itemsPerCore);
-TILING_DATA_FIELD_DEF(uint32_t, remainderItems);
-TILING_DATA_FIELD_DEF(uint32_t, usedCores);
-TILING_DATA_FIELD_DEF(uint32_t, S);
-TILING_DATA_FIELD_DEF(uint32_t, n);
-TILING_DATA_FIELD_DEF(uint32_t, D);
-TILING_DATA_FIELD_DEF(uint32_t, tileD);
-TILING_DATA_FIELD_DEF(uint32_t, nTilesD);
-TILING_DATA_FIELD_DEF(uint32_t, alignedD);
-TILING_DATA_FIELD_DEF(uint32_t, lastTileD);
-TILING_DATA_FIELD_DEF(uint32_t, alignedN);      // n aligned to 8 for float32 vector ops
-TILING_DATA_FIELD_DEF(uint32_t, alignedNN);     // n*n aligned to 8 for float32 vector ops
-TILING_DATA_FIELD_DEF(uint32_t, isNAligned);    // n == 8 (32 bytes for DataCopy)
-TILING_DATA_FIELD_DEF(uint32_t, isNNAligned);   // (n*n) % 8 == 0
-TILING_DATA_FIELD_DEF(uint32_t, isDAligned);    // D % 16 == 0 && nTilesD == 1
-END_TILING_DATA_DEF;
-REGISTER_TILING_DATA_CLASS(MhcPost, MhcPostTilingData)
 
 struct MhcPostCompileInfo {
     uint32_t aicNum;
