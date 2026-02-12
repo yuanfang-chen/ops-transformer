c031c93dab3e3a5f6915eaea2aefd0c42f2c9b8b
zzg_code <zengzhiguo1@huawei.com>
2026-02-12 09:49:45 +0800

    1

--- mc2/moe_distribute_dispatch_v2/op_kernel/newlog.h
+++ mc2/moe_distribute_dispatch_v2/op_kernel/newlog.h
@@ -31,10 +31,10 @@ public:
 
     template<typename T>
     __aicore__ inline void print_value(const T& val);
-    __aicore__ inline void LogInfo(const uint32_t line, const __gm__ char *msg, const GM_ADDR addr);
-    __aicore__ inline void LogInfo(const uint32_t line, const __gm__ char *msg);
+    __aicore__ inline void LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg, const GM_ADDR addr);
+    __aicore__ inline void LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg);
     template<typename T>
-    __aicore__ inline void LogInfo(const uint32_t line, const __gm__ char *msg,   const T num);
+    __aicore__ inline void LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg,   const T num);
 
     template<typename T>
     __aicore__ inline uint32_t GetElementWidth(T val);
@@ -57,10 +57,10 @@ public:
     __aicore__ inline void PrintValue(const GlobalTensor<T>& tensor, const uint32_t (&shape)[N]);
     
     template <typename T,size_t N>
-    __aicore__ inline void LogInfo(uint32_t line, const __gm__ char *msg, LocalTensor<T>& tensor,const uint32_t (&shape)[N]); //按指定shap打
+    __aicore__ inline void LogInfo(const __gm__ char * func, uint32_t line, const __gm__ char *msg, LocalTensor<T>& tensor,const uint32_t (&shape)[N]); //按指定shap打
 
     template <typename T,size_t N>
-    __aicore__ inline void LogInfo(uint32_t line, const __gm__ char *msg, GlobalTensor<T>& tensor,const uint32_t (&shape)[N]); //按指定shap打
+    __aicore__ inline void LogInfo(const __gm__ char * func, uint32_t line, const __gm__ char *msg, GlobalTensor<T>& tensor,const uint32_t (&shape)[N]); //按指定shap打
 
 private:
     int32_t rankId_ = -1;
@@ -86,17 +86,17 @@ __aicore__ inline void Log::Init(TPipe * tpipe,int32_t rankId) {
     rankId_ =rankId;
 }
 
-__aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg, const GM_ADDR addr){
+__aicore__ inline void Log::LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg, const GM_ADDR addr){
     const __gm__ void * addrtemp = static_cast<const __gm__ void*>(addr);
     printf("[rankId: %d][aivId: %d][line: %d]", rankId_, aivId_, line);
     printf("%s: %p\n", msg, addrtemp);
 }
-__aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg){
+__aicore__ inline void Log::LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg){
     printf("[rankId: %d][aivId: %d][line: %d]%s\n", rankId_, aivId_, line, msg);
 }
 
 template<typename T>
-__aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg,   const T num){
+__aicore__ inline void Log::LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg,   const T num){
     printf("[rankId: %d][aivId: %d][line: %d]%s: ", rankId_, aivId_, line, msg);
     print_value(num);
     printf("\n");
@@ -244,7 +244,7 @@ __aicore__ inline void Log::PrintValue(const LocalTensor<T>& tensor, const uint3
 
 
 template <typename T,size_t N>
-__aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg, LocalTensor<T>& tensor,const uint32_t (&shape)[N]) {//按指定shap打
+__aicore__ inline void Log::LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg, LocalTensor<T>& tensor,const uint32_t (&shape)[N]) {//按指定shap打
     printf("[rankId: %d][aivId: %d][line: %d]", rankId_, aivId_, line);
     printf("%s\n", msg);
     if(tensor.GetSize() == 0){
@@ -367,7 +367,7 @@ __aicore__  inline void Log::PrintValue(const GlobalTensor<T>& tensor, const uin
 }
 
 template <typename T,size_t N>
-__aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg, GlobalTensor<T>& tensor,const uint32_t (&shape)[N]) {//按指定shap打
+__aicore__ inline void Log::LogInfo(const __gm__ char * func, const uint32_t line, const __gm__ char *msg, GlobalTensor<T>& tensor,const uint32_t (&shape)[N]) {//按指定shap打
     printf("[rankId: %d][aivId: %d][line: %d]", rankId_, aivId_, line);
     printf("%s\n", msg);
     bool cast = !is_decimal_integer_v<T> && !is_unsigned_integer_v<T> && !is_fp_point_v<T>;
@@ -391,7 +391,7 @@ __aicore__ inline void Log::LogInfo(const uint32_t line, const __gm__ char *msg,
 //在最开始定义LOG_INIT(...)，
 //#define DEBUG 1
 //#if DEBUG
-//#define LOG_INFO(...) this->logger.LogInfo(__LINE__,__VA_ARGS__)
+//#define LOG_INFO(...) this->logger.LogInfo(__FUNCTION__,__LINE__,__VA_ARGS__)
 //#define LOG_INIT(...) this->logger.Init(__VA_ARGS__)
 //#else
 //#define LOG_INFO(...) 
