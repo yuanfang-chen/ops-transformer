e4dcdea579d7325bbe3b4f5004292a5c591f7dde
lilongwei_hw <lilongwei11@huawei.com>
2026-02-13 09:20:39 +0800

    enable innerPrec0

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -1383,8 +1383,10 @@ static bool IsUsingFAI(gert::TilingContext &context, const string inputLayoutStr
     bool isRopeSplitMla = (qRope != nullptr) && (kRope != nullptr);
     bool sparseModeSupported = (sparseMode == 0) || (sparseMode == 3) || (sparseMode == 4);
     bool isMha = (kvHeadNum == 0) || (headNum == kvHeadNum);
+    // bool mhaConditions = isMha && (tempAttnMaskShape == nullptr) &&
+    //     (qDataType == ge::DT_FLOAT16) && (innerPrecise == 1) && !isPageAttention;
     bool mhaConditions = isMha && (tempAttnMaskShape == nullptr) &&
-        (qDataType == ge::DT_FLOAT16) && (innerPrecise == 1) && !isPageAttention;
+        (qDataType == ge::DT_FLOAT16) && !isPageAttention;
     bool nonMhaConditions = !isMha && (innerPrecise == 0);
 
     bool usingFAI = false;
