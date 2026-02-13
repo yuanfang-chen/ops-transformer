b57591e069bab73888d48cc9e8a50186fa859830
tangkaidi <tangkaidi1@huawei.com>
2026-02-13 16:53:05 +0800

    try

--- attention/rain_fusion_attention/op_kernel/kernel_common.hpp
+++ attention/rain_fusion_attention/op_kernel/kernel_common.hpp
@@ -79,7 +79,8 @@ namespace RfaKenelCommon {
     constexpr uint32_t UNIT_BLOCK_STACK_NUM = 4;
 
     constexpr uint32_t Q_TILE_CEIL = 128;
-    constexpr uint32_t MAX_KV_STACK_LEN = 512;
+    constexpr uint32_t MAX_KV_STACK_LEN = 1024;
+    // constexpr uint32_t MAX_KV_STACK_LEN = 512;
 
     template <typename T>
     __aicore__ inline T AlignUp(T a, T b)
