0eeff5a9e6e8fc4da6b3da7c5cee9e4936110e12
tangkaidi <tangkaidi1@huawei.com>
2026-02-13 17:59:05 +0800

    try

--- attention/rain_fusion_attention/op_kernel/rain_fusion_attention_kernel.h
+++ attention/rain_fusion_attention/op_kernel/rain_fusion_attention_kernel.h
@@ -372,6 +372,8 @@ namespace RainFusion {
                 uint32_t kvSLoopNumTotal = (noSkipKvS + pagedBlockSize - 1) / pagedBlockSize; // CeilDiv
 
                 uint32_t blockStackNum = MAX_KV_STACK_LEN / pagedBlockSize;
+                AscendC::printf("tkd pagedBlockSize: %u\n", pagedBlockSize);
+                AscendC::printf("tkd blockStackNum: %u\n", blockStackNum);
                 uint32_t stackSeqTile;
                 uint32_t stackSeqTilePad = blockStackNum * pagedBlockSize;
                 uint32_t preKVNum = PRE_LAUNCH * blockStackNum;
@@ -402,9 +404,12 @@ namespace RainFusion {
                     // Stage 1: QK matmul (computed on CUBE core)
                     if (kvSIdx < kvSLoopNumTotal) {
                         stackSeqTile = noSkipKvS - kvSIdx * pagedBlockSize;
+                        AscendC::printf("tkd stackSeqTile: %u\n", stackSeqTile);
                         if (stackSeqTile >= pagedBlockSize * blockStackNum) {
+                            AscendC::printf("tkd if (stackSeqTile >= pagedBlockSize * blockStackNum)\n");
                             stackSeqTile = pagedBlockSize * blockStackNum;
                         }
+                        AscendC::printf("tkd final stackSeqTile: %u\n", stackSeqTile);
                         uint32_t curStackTileMod = stackSeqCount % (PRE_LAUNCH + 1);
                         uint64_t gmOffsetS = coreIdx * WORKSPACE_BLOCK_SIZE_DB * (PRE_LAUNCH + 1) +
                             curStackTileMod * WORKSPACE_BLOCK_SIZE_DB;
