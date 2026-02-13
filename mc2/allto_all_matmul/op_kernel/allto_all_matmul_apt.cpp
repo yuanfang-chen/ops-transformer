c7ab9586ec76a5025c93a8677f47cdaf937c6722
wuziyu <wuziyu11@huawei.com>
2026-02-13 16:26:56 +0800

    add

--- mc2/allto_all_matmul/op_kernel/allto_all_matmul_apt.cpp
+++ mc2/allto_all_matmul/op_kernel/allto_all_matmul_apt.cpp
@@ -91,14 +91,17 @@ __global__ __aicore__ void allto_all_matmul(GM_ADDR x1, GM_ADDR x2, GM_ADDR bias
         using DtypeBias = float;
         ALLTO_ALL_MATMUL_APT_FP_IMPL(tilingData, pipe);
     }
+
 #else
     REGISTER_TILING_DEFAULT(AlltoAllQuantMatmulTilingData);
     GET_TILING_DATA_WITH_STRUCT(AlltoAllQuantMatmulTilingData, tilingData, tilingGM);
-
-    if constexpr (QUANTMODE == KC_QUANT_FP8E5M2_MODE) {
-        ALLTO_ALL_KC_QUANT_MATMUL_IMPL(tilingData, pipe, float8_e5m2_t);
-    } else if constexpr (QUANTMODE == KC_QUANT_FP8E4M3_MODE) {
-        ALLTO_ALL_KC_QUANT_MATMUL_IMPL(tilingData, pipe, float8_e4m3_t);
-    }
+    #if !(((ORIG_DTYPE_X1 == DT_FLOAT8_E4M3FN) || (ORIG_DTYPE_X1 == DT_FLOAT8_E5M2)) && \
+        ((ORIG_DTYPE_X2 == DT_FLOAT8_E4M3FN) || (ORIG_DTYPE_X2 == DT_FLOAT8_E5M2)))
+        if constexpr (QUANTMODE == KC_QUANT_FP8E5M2_MODE) {
+            ALLTO_ALL_KC_QUANT_MATMUL_IMPL(tilingData, pipe, float8_e5m2_t);
+        } else if constexpr (QUANTMODE == KC_QUANT_FP8E4M3_MODE) {
+            ALLTO_ALL_KC_QUANT_MATMUL_IMPL(tilingData, pipe, float8_e4m3_t);
+        }
+    #endif
 #endif
 }
