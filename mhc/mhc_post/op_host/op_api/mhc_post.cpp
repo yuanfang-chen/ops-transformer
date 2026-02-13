8a8e2056d7475e525d567e193c57ec400d7a51ba
wuyewei <wuyewei@huawei.com>
2026-02-13 19:12:06 +0800

    aclnn格式调整

--- mhc/mhc_post/op_host/op_api/mhc_post.cpp
+++ mhc/mhc_post/op_host/op_api/mhc_post.cpp
@@ -42,7 +42,11 @@ const aclTensor* MhcPostAICore(
         MhcPost,
         OP_INPUT(x, hRes, hOut, hPost),
         OP_OUTPUT(output));
-    (void)retAicore;
+
+    if (retAicore != ACLNN_SUCCESS) {
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "MhcPost launch kernel failed.");
+        return nullptr;
+    }
     return output;
 }
 
