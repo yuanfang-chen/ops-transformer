3f309dd784d512b43c89888c24f7fd268f1832da
l30066185 <leiqingji@h-partners.com>
2026-02-14 15:05:18 +0800

    feature: infer FA, Ascend950, deprecate api

--- attention/incre_flash_attention/op_host/op_api/aclnn_incre_flash_attention.h
+++ attention/incre_flash_attention/op_host/op_api/aclnn_incre_flash_attention.h
@@ -13,6 +13,7 @@
 #warning "aclnn_incre_flash_attention.h is scheduled to be deprecated in December 2026, and will be replaced by the aclnn_incre_flash_attention_v4.h. We apologize for any inconvenience caused and appreciate your timely migration to the new interface. "
 
 #include "aclnn/aclnn_base.h"
+#include <tiling/platform/platform_ascendc.h>
 
 #ifdef __cplusplus
 extern "C" {
