51fe9c820887e219a03a7d11aad1e9f13b58c921
z1017i <zengzhe4@huawei.com>
2026-02-14 11:24:14 +0800

    Bugfix ARNDQ

--- mc2/add_rms_norm_dynamic_quant_all_gather_qbmm/op_host/op_tiling/add_rms_norm_dynamic_quant_all_gather_qbmm_tiling.cpp
+++ mc2/add_rms_norm_dynamic_quant_all_gather_qbmm/op_host/op_tiling/add_rms_norm_dynamic_quant_all_gather_qbmm_tiling.cpp
@@ -156,16 +156,9 @@ ge::graphStatus CheckAttrs(const gert::TilingContext *context)
     // transpose校验
     const bool *transposeX2Ptr = attrs->GetAttrPointer<bool>(TRANSPOSE_X2_INDEX);
     OP_TILING_CHECK(transposeX2Ptr == nullptr, OP_LOGE(nodeName, "transposeX2Ptr is nullptr."), return ge::GRAPH_FAILED);
-    OP_TILING_CHECK(*transposeX2Ptr,
-        OP_LOGE(nodeName, "transposeX2 should be false."), return ge::GRAPH_FAILED);
     // 输出type校验
     const int64_t *outputTypePtr = attrs->GetAttrPointer<int64_t>(DTYPE_INDEX);
     OP_TILING_CHECK(outputTypePtr == nullptr, OP_LOGE(nodeName, "outputTypePtr is nullptr."), return ge::GRAPH_FAILED);
-    ge::DataType outputType = static_cast<ge::DataType>(*outputTypePtr);
-    OP_TILING_CHECK(outputType != ge::DT_INT32,
-                    OP_LOGE(nodeName, "outPutType should be int, but actual value is %s.",
-                            Ops::Base::ToString(outputType).c_str()),
-                    return ge::GRAPH_FAILED);
     // residual_norm_mode校验
     const float *residualNormModePtr = attrs->GetAttrPointer<float>(RESIDUAL_NORM_MODE_INDEX);
     OP_TILING_CHECK(residualNormModePtr == nullptr, OP_LOGE(nodeName, "residualNormModePtr is nullptr."), return ge::GRAPH_FAILED);
@@ -357,14 +350,14 @@ static ge::graphStatus AddRmsNormDynamicQuantAllGatherQbmmTilingFunc(gert::Tilin
     OP_TILING_CHECK(CheckAttrs(context) != ge::GRAPH_SUCCESS,
         OP_LOGE(nodeName, "CheckAttrs failed."), return ge::GRAPH_FAILED);
     // 检查输入输出tensor的维度
-    OP_TILING_CHECK(CheckInputOutputTensorDim(context) != true,
+    OP_TILING_CHECK(CheckInputOutputTensorDim(context) != ge::GRAPH_SUCCESS,
         OP_LOGE(nodeName, "CheckInputOutputTensorDim failed."), return ge::GRAPH_FAILED);
     // 检查输入输出tensor的数据类型
-    OP_TILING_CHECK(CheckTensorDataType(context) != true,
+    OP_TILING_CHECK(CheckTensorDataType(context) != ge::GRAPH_SUCCESS,
         OP_LOGE(nodeName, "CheckTensorDataType failed."), return ge::GRAPH_FAILED);
 
     // 检查输入输出tensor的格式
-    OP_TILING_CHECK(CheckTensorFormat(context) != true,
+    OP_TILING_CHECK(CheckTensorFormat(context) != ge::GRAPH_SUCCESS,
         OP_LOGE(nodeName, "CheckTensorFormat failed."), return ge::GRAPH_FAILED);
     
     // addrmsnormdynamicquantallgatherqbmm 部分tiling切分
