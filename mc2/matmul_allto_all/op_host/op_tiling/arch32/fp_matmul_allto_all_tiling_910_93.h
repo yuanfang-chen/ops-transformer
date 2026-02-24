e9e90b64e7d6d249348f24bf0ef4900dbc836d30
qzzzy1 <qiziyu2@huawei.com>
2026-02-24 15:23:50 +0800

    fix

--- mc2/matmul_allto_all/op_host/op_tiling/arch32/fp_matmul_allto_all_tiling_910_93.h
+++ mc2/matmul_allto_all/op_host/op_tiling/arch32/fp_matmul_allto_all_tiling_910_93.h
@@ -22,7 +22,7 @@
 #include "tiling/new_mc2_tiling_utils.h"
 #include "mc2/3rd/mat_mul_v3/op_host/op_tiling/matmul_v3_base_tiling.h"
 #include "../matmul_allto_all_tiling_base.h"
-#include "../common/matmul_allto_all_util_tiling.h"
+#include "mc2/matmul_allto_all/op_host/op_tiling/common/matmul_allto_all_util_tiling.h"
 #include "../../../op_kernel/arch32/matmul_allto_all_tiling_data_910_93.h"
 #include "../../../op_kernel/arch32/matmul_allto_all_tiling_key_910_93.h"
 
@@ -60,7 +60,7 @@ private:
 
     Mc2MatMulV3Args mmV3Args_;
     Mc2MatmulV3CompileInfo compileInfo_;
-    
+
     void PrintMatmulAlltoAllTilingData(MatmulAlltoAllTilingDataA3 &outTilingData);
     void PrintMMV3TilingData(const std::string &opName, Mc2MatMulV3TilingData &tiling);
     void PrintMatmulAlltoAllTilingInfo(const std::string &opName, MatmulAlltoAllTilingInfoA3 &tilingInfo);
