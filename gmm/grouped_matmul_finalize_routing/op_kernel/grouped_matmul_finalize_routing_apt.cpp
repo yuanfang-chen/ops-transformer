e73cd21dc62e8e1a25a7d1f91e14b30863b340ea
jayshu <shuhaojie@huawei.com>
2026-02-25 16:22:19 +0800

    Rectification Review Comments

---
+++ gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing_apt.cpp
@@ -0,0 +1,45 @@
+#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
+#include "kernel_utils.h"
+#include "kernel_tiling/kernel_tiling.h"
+#include "kernel_basic_intf.h"
+#include "lib/matmul_intf.h"
+#include "arch35/grouped_matmul_finalize_routing_tiling_key.h"
+#if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
+    #include "arch35/grouped_matmul_finalize_routing.h"
+#elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
+    #include "arch35/grouped_matmul_finalize_routing_pertoken_dequant.h"
+#endif
+
+template <int ATRANS, int BTRANS>
+__global__ __aicore__ void
+grouped_matmul_finalize_routing(GM_ADDR x, GM_ADDR w, GM_ADDR scale, GM_ADDR bias, GM_ADDR pertoken_scale,
+                                GM_ADDR group_list, GM_ADDR share_input, GM_ADDR logit, GM_ADDR row_index,
+                                GM_ADDR offset, GM_ADDR y, GM_ADDR workspaceGM, GM_ADDR tilingGM)
+{
+    TPipe pipe;
+    KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
+    #if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
+    if constexpr (ATRANS == 0 && BTRANS == 0) { // transX = false, transW = false
+        grouped_matmul_finalize_routing<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::RowMajor>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    if constexpr (ATRANS == 0 && BTRANS == 1) { // transX = false, transW = true
+        grouped_matmul_finalize_routing<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::ColumnMajor>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    #elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
+    if constexpr (ATRANS == 0 && BTRANS == 0) { // transX = false, transW = false
+        grouped_matmul_finalize_routing_pertoken_dequant<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::Nz>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    if constexpr (ATRANS == 0 && BTRANS == 1) { // transX = false, transW = true
+        grouped_matmul_finalize_routing_pertoken_dequant<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::Zn>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    #endif
+}
+#endif
