d940f97bd8b393cd5f27fee82ee056a4f5a4f1ef
fazhenyao <fazhenyao@h-partners.com>
2026-02-25 16:32:27 +0800

    迁移inplace_index_add、masked_scatter，添加moe_token_permute_with_routing_map_grad支持a5

---
+++ moe/3rd/moe_inplace_index_add/op_host/moe_inplace_index_add_tiling_arch35.cpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/* !
+ * \file moe_inplace_index_add_tiling_arch35.cpp
+ * \brief
+ */
+
+#include "tiling_base/tiling_templates_registry.h"
+#include "moe_inplace_index_add_tiling_arch35.h"
+
+namespace optiling
+{
+ge::graphStatus MoeInplaceIndexAddTilingForAscendC(gert::TilingContext* context)
+{
+    return Ops::Transformer::OpTiling::TilingRegistry::GetInstance().DoTilingImpl(context);
+}
+
+int64_t CeilAlign(int64_t u_value, int64_t d_value) {
+  int64_t res_value = 0;
+  if (d_value == 0) {
+    return u_value;
+  }
+  res_value = (u_value + d_value - 1) / d_value * d_value;
+
+  return res_value;
+}
+
+}  // namespace optiling
