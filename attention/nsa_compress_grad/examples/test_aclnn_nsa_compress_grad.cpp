9b3beeb6cff881df04da28a2ae0277168b9302c2
caihualilili <liangfuzhan@h-partners.com>
2026-02-25 18:07:42 +0800

    fa/fag等算子资料修改

--- attention/nsa_compress_grad/examples/test_aclnn_nsa_compress_grad.cpp
+++ attention/nsa_compress_grad/examples/test_aclnn_nsa_compress_grad.cpp
@@ -209,7 +209,7 @@ int main() {
     }
     // 调用aclnnGeGluBackward第二段接口
     ret = aclnnNsaCompressGrad(workspaceAddr, workspaceSize, executor, stream);
-    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGeGluGradV2 failed. ERROR: %d\n", ret); return ret);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGeGluBackward failed. ERROR: %d\n", ret); return ret);
 
     // 4. （固定写法）同步等待任务执行结束
     ret = aclrtSynchronizeStream(stream);
