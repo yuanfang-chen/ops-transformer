616de09c605d48cd708eb419745832158a89b105
warming <wangming176@huawei.com>
2026-02-25 19:17:59 +0800

    modify unknownshape

--- mhc/mhc_post/op_host/mhc_post_infershape.cpp
+++ mhc/mhc_post/op_host/mhc_post_infershape.cpp
@@ -77,7 +77,7 @@ static ge::graphStatus InferShapeForMhcPost(gert::InferShapeContext* context)
     OP_CHECK_NULL_WITH_CONTEXT(context, hOutShape);
     const gert::Shape* hPostShape = context->GetInputShape(INDEX_HPOST);
     OP_CHECK_NULL_WITH_CONTEXT(context, hPostShape);
-    gert::Shape* yShape = context->GetOutputShape(INDEX_Y);
+    auto yShape = context->GetOutputShape(INDEX_Y);
     OP_CHECK_NULL_WITH_CONTEXT(context, yShape);
 
     if (ops::IsUnknownRank(xShape)) {
