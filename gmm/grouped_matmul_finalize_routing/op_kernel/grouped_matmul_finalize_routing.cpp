14f0836dd8605907e8bb46d116573fa9adb4cf28
tooslow <zhangzhizhuo1@huawei.com>
2026-02-25 09:26:16 +0800

    先注释掉编包

--- gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing.cpp
+++ gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing.cpp
@@ -121,69 +121,69 @@ using namespace GroupedMatmulFinalizeRouting;
         op.Process();                                                                                                  \
     } while (0)
 
-#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 220
-extern "C" __global__ __aicore__ void grouped_matmul_finalize_routing(GM_ADDR x, GM_ADDR w, GM_ADDR scale, GM_ADDR bias,
-                                                                      GM_ADDR pertoken_scale, GM_ADDR group_list,
-                                                                      GM_ADDR share_input, GM_ADDR logit,
-                                                                      GM_ADDR row_index, GM_ADDR offset, GM_ADDR y,
-                                                                      GM_ADDR workspaceGM, GM_ADDR tilingGM)
-{
-    GET_TILING_DATA(tilingData, tilingGM);
-    __gm__ uint8_t *user = GetUserWorkspace(workspaceGM);
-    MMInitParams initParams{x,      w,     bias,      group_list,  scale, pertoken_scale,
-                            offset, logit, row_index, share_input, y,     workspaceGM};
-    KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
-    if (TILING_KEY_IS(10000000000000000001UL)) {
-        GMMFR_A8W8_IMPL(int64_t, float, false, false);
-    } else if (TILING_KEY_IS(10000000000000000011UL)) {
-        GMMFR_A8W8_IMPL(int32_t, float, false, false);
-    } else if (TILING_KEY_IS(10000000000000000101UL)) {
-        GMMFR_A8W8_IMPL(int64_t, bfloat16_t, false, false);
-    } else if (TILING_KEY_IS(10000000000000000111UL)) {
-        GMMFR_A8W8_IMPL(int32_t, bfloat16_t, false, false);
-    } else if (TILING_KEY_IS(11000000000000000011UL)) {
-        GMMFR_A8W4_IMPL(false, false);
-    } else if (TILING_KEY_IS(11000000000000000111UL)) {
-        GMMFR_A8W4_L1_OPT_IMPL(false, false);
-    } else if (TILING_KEY_IS(10000000000000001001UL)) {
-        GMMFR_A8W8_IMPL(int64_t, float, true, false);
-    } else if (TILING_KEY_IS(10000000000000001011UL)) {
-        GMMFR_A8W8_IMPL(int32_t, float, true, false);
-    } else if (TILING_KEY_IS(10000000000000001101UL)) {
-        GMMFR_A8W8_IMPL(int64_t, bfloat16_t, true, false);
-    } else if (TILING_KEY_IS(10000000000000001111UL)) {
-        GMMFR_A8W8_IMPL(int32_t, bfloat16_t, true, false);
-    } else if (TILING_KEY_IS(11000000000000001011UL)) {
-        GMMFR_A8W4_IMPL(true, false);
-    } else if (TILING_KEY_IS(11000000000000001111UL)) {
-        GMMFR_A8W4_L1_OPT_IMPL(true, false);
-    } else if (TILING_KEY_IS(10000000000000010001UL)) {
-        GMMFR_A8W8_IMPL(int64_t, float, false, true);
-    } else if (TILING_KEY_IS(10000000000000010011UL)) {
-        GMMFR_A8W8_IMPL(int32_t, float, false, true);
-    } else if (TILING_KEY_IS(10000000000000010101UL)) {
-        GMMFR_A8W8_IMPL(int64_t, bfloat16_t, false, true);
-    } else if (TILING_KEY_IS(10000000000000010111UL)) {
-        GMMFR_A8W8_IMPL(int32_t, bfloat16_t, false, true);
-    } else if (TILING_KEY_IS(11000000000000010011UL)) {
-        GMMFR_A8W4_IMPL(false, true);
-    } else if (TILING_KEY_IS(11000000000000010111UL)) {
-        GMMFR_A8W4_L1_OPT_IMPL(false, true);
-    } else if (TILING_KEY_IS(10000000000000011001UL)) {
-        GMMFR_A8W8_IMPL(int64_t, float, true, true);
-    } else if (TILING_KEY_IS(10000000000000011011UL)) {
-        GMMFR_A8W8_IMPL(int32_t, float, true, true);
-    } else if (TILING_KEY_IS(10000000000000011101UL)) {
-        GMMFR_A8W8_IMPL(int64_t, bfloat16_t, true, true);
-    } else if (TILING_KEY_IS(10000000000000011111UL)) {
-        GMMFR_A8W8_IMPL(int32_t, bfloat16_t, true, true);
-    } else if (TILING_KEY_IS(11000000000000011011UL)) {
-        GMMFR_A8W4_IMPL(true, true);
-    } else if (TILING_KEY_IS(11000000000000011111UL)) {
-        GMMFR_A8W4_L1_OPT_IMPL(true, true);
-    }
-}
-#endif
+// #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 220
+// extern "C" __global__ __aicore__ void grouped_matmul_finalize_routing(GM_ADDR x, GM_ADDR w, GM_ADDR scale, GM_ADDR bias,
+//                                                                       GM_ADDR pertoken_scale, GM_ADDR group_list,
+//                                                                       GM_ADDR share_input, GM_ADDR logit,
+//                                                                       GM_ADDR row_index, GM_ADDR offset, GM_ADDR y,
+//                                                                       GM_ADDR workspaceGM, GM_ADDR tilingGM)
+// {
+//     GET_TILING_DATA(tilingData, tilingGM);
+//     __gm__ uint8_t *user = GetUserWorkspace(workspaceGM);
+//     MMInitParams initParams{x,      w,     bias,      group_list,  scale, pertoken_scale,
+//                             offset, logit, row_index, share_input, y,     workspaceGM};
+//     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
+//     if (TILING_KEY_IS(10000000000000000001UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, float, false, false);
+//     } else if (TILING_KEY_IS(10000000000000000011UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, float, false, false);
+//     } else if (TILING_KEY_IS(10000000000000000101UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, bfloat16_t, false, false);
+//     } else if (TILING_KEY_IS(10000000000000000111UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, bfloat16_t, false, false);
+//     } else if (TILING_KEY_IS(11000000000000000011UL)) {
+//         GMMFR_A8W4_IMPL(false, false);
+//     } else if (TILING_KEY_IS(11000000000000000111UL)) {
+//         GMMFR_A8W4_L1_OPT_IMPL(false, false);
+//     } else if (TILING_KEY_IS(10000000000000001001UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, float, true, false);
+//     } else if (TILING_KEY_IS(10000000000000001011UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, float, true, false);
+//     } else if (TILING_KEY_IS(10000000000000001101UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, bfloat16_t, true, false);
+//     } else if (TILING_KEY_IS(10000000000000001111UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, bfloat16_t, true, false);
+//     } else if (TILING_KEY_IS(11000000000000001011UL)) {
+//         GMMFR_A8W4_IMPL(true, false);
+//     } else if (TILING_KEY_IS(11000000000000001111UL)) {
+//         GMMFR_A8W4_L1_OPT_IMPL(true, false);
+//     } else if (TILING_KEY_IS(10000000000000010001UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, float, false, true);
+//     } else if (TILING_KEY_IS(10000000000000010011UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, float, false, true);
+//     } else if (TILING_KEY_IS(10000000000000010101UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, bfloat16_t, false, true);
+//     } else if (TILING_KEY_IS(10000000000000010111UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, bfloat16_t, false, true);
+//     } else if (TILING_KEY_IS(11000000000000010011UL)) {
+//         GMMFR_A8W4_IMPL(false, true);
+//     } else if (TILING_KEY_IS(11000000000000010111UL)) {
+//         GMMFR_A8W4_L1_OPT_IMPL(false, true);
+//     } else if (TILING_KEY_IS(10000000000000011001UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, float, true, true);
+//     } else if (TILING_KEY_IS(10000000000000011011UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, float, true, true);
+//     } else if (TILING_KEY_IS(10000000000000011101UL)) {
+//         GMMFR_A8W8_IMPL(int64_t, bfloat16_t, true, true);
+//     } else if (TILING_KEY_IS(10000000000000011111UL)) {
+//         GMMFR_A8W8_IMPL(int32_t, bfloat16_t, true, true);
+//     } else if (TILING_KEY_IS(11000000000000011011UL)) {
+//         GMMFR_A8W4_IMPL(true, true);
+//     } else if (TILING_KEY_IS(11000000000000011111UL)) {
+//         GMMFR_A8W4_L1_OPT_IMPL(true, true);
+//     }
+// }
+// #endif
 
 #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
 template <int ATRANS, int BTRANS>
