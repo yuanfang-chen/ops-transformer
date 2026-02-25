460b385f46f2265e3b216c9c0ae7f27353d65266
wangxun21 <wangxun21@huawei.com>
2026-02-25 11:26:30 +0800

    patial rope

--- posembedding/apply_rotary_pos_emb/op_host/apply_rotary_pos_emb_tiling_base_arch35.cpp
+++ posembedding/apply_rotary_pos_emb/op_host/apply_rotary_pos_emb_tiling_base_arch35.cpp
@@ -181,8 +181,8 @@ ge::graphStatus ApplyRotaryPosEmbRegbaseTilingBaseClass::CheckShapeRelation()
                 OP_LOGE(context_, "S of query, key, cos, sin should be same, actual %ld %ld %ld %ld.",
                         qShape_.GetDim(sIdx), kShape_.GetDim(sIdx), cosShape_.GetDim(sIdx), cosShape_.GetDim(sIdx)),
                 return ge::GRAPH_FAILED);
-    OP_CHECK_IF(!(cosShape_.GetDim(dIdx) == qShape_.GetDim(dIdx) && kShape_.GetDim(dIdx) == qShape_.GetDim(dIdx)),
-                OP_LOGE(context_, "D of query, key, cos, sin should be same, actual %ld %ld %ld %ld.",
+    OP_CHECK_IF(!(cosShape_.GetDim(dIdx) <= qShape_.GetDim(dIdx) && kShape_.GetDim(dIdx) <= qShape_.GetDim(dIdx)),
+                OP_LOGE(context_, "D of query, key should be bigger than cos, sin , actual %ld %ld %ld %ld.",
                         qShape_.GetDim(dIdx), kShape_.GetDim(dIdx), cosShape_.GetDim(dIdx), cosShape_.GetDim(dIdx)),
                 return ge::GRAPH_FAILED);
     OP_CHECK_IF(
@@ -316,6 +316,8 @@ ge::graphStatus ApplyRotaryPosEmbRegbaseTilingBaseClass::GetShapeAttrsInfo()
         kn_ = kShape_.GetDim(DIM_2);
         d_ = qShape_.GetDim(DIM_3);
     }
+    reald_ = cosShape_.GetDim(DIM_3);
+    isPartialRope_ = (reald_ != d_);
     return ge::GRAPH_SUCCESS;
 }
 } // namespace optiling
