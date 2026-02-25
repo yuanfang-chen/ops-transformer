d262d894929e362c4aa2b89203381519232e3bef
warming <wangming176@huawei.com>
2026-02-25 19:11:56 +0800

    modify unknownshape

--- mhc/mhc_post/op_host/mhc_post_infershape.cpp
+++ mhc/mhc_post/op_host/mhc_post_infershape.cpp
@@ -80,14 +80,14 @@ static ge::graphStatus InferShapeForMhcPost(gert::InferShapeContext* context)
     const gert::Shape* yShape = context->GetOutputShape(INDEX_Y);
     OP_CHECK_NULL_WITH_CONTEXT(context, yShape);
 
-    if (ops::IsUnknownRank(*xShape)) {
-        ops::SetUnknownRank(*yShape);
+    if (ops::IsUnknownRank(xShape)) {
+        ops::SetUnknownRank(yShape);
         OP_LOGD(context->GetNodeName(), "MhcPost infershape handles unknown rank.");
         return ge::GRAPH_SUCCESS;
     }
     size_t xDims = xShape->GetDimNum();
-    if (ops::IsUnknownShape(*xShape)) {
-        ops::SetUnknownShape(xDims, *yShape);
+    if (ops::IsUnknownShape(xShape)) {
+        ops::SetUnknownShape(xDims, yShape);
         OP_LOGD(context->GetNodeName(), "MhcPost infershape handles unknown shape.");
         return ge::GRAPH_SUCCESS;
     }
