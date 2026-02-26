dd44072577c7dd20973c5f95ad6d51a403ae4feb
jiangyu741 <jiangyu100@huawei.com>
2026-02-26 11:53:38 +0800

    update: 更新文件 fused_infer_attention_score_tiling.cpp
    Signed-off-by: jiangyu741 <jiangyu100@huawei.com>

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -1410,8 +1410,8 @@ static bool IsUsingFAI(gert::TilingContext &context, const string inputLayoutStr
     bool isRopeSplitMla = (qRope != nullptr) && (kRope != nullptr);
     bool sparseModeSupported = (sparseMode == 0) || (sparseMode == 3) || (sparseMode == 4);
     bool isMha = (kvHeadNum == 0) || (headNum == kvHeadNum);
-    bool mhaConditions = isMha && !((qDataType == ge::DT_BF16) && (innerPrecise == 1)) && 	 
-         (tempAttnMaskShape == nullptr);
+    bool mhaConditions = isMha && !((qDataType == ge::DT_BF16) && (innerPrecise == 1)) && 
+        (tempAttnMaskShape == nullptr);
     bool nonMhaConditions = !isMha && (innerPrecise == 0);
 
     bool usingFAI = false;
