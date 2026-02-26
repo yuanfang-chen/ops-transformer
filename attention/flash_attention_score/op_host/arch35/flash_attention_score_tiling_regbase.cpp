eb03aa802d007b4746cc26cce2b133f8815c682d
hsqnb <heshouqi@h-partners.com>
2026-02-26 11:10:26 +0800

    魔鬼数字问题修改

--- attention/flash_attention_score/op_host/arch35/flash_attention_score_tiling_regbase.cpp
+++ attention/flash_attention_score/op_host/arch35/flash_attention_score_tiling_regbase.cpp
@@ -1590,7 +1590,7 @@ ge::graphStatus FlashAttentionScoreTilingRegbase::DoLibApiTiling()
 
 void FlashAttentionScoreTilingRegbase::CalcDVBasicBlock() {
     dVBasicBlock = AlignUp(dSizeV, D_TEMPLATE_SPLIT_SIZE);
-    if (dTemplateType == DTemplateType::ALIGNED_192 && (hasRope || dVBasicBlock == 128)) {
+    if (dTemplateType == DTemplateType::ALIGNED_192 && (hasRope || dVBasicBlock == DTemplateType::ALIGNED_128)) {
         dVTemplateType = DTemplateType::ALIGNED_128;
     } else {
         dVTemplateType = dTemplateType;
