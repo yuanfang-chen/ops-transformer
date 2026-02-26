1faed050668ae9510b6500de21855bc190c3078b
王天乐 <wangtianle3@huawei.com>
2026-02-26 22:35:35 +0800

    update gmm 950 compile

--- mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv.cpp
+++ mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv.cpp
@@ -18,7 +18,7 @@
 #include "kernel_operator.h"
 #endif
 #include "grouped_mat_mul_allto_allv.h"
-#include "arch35/grouped_mat_mul_allto_allv_tiling_key.h"
+#include "grouped_mat_mul_allto_allv_tiling_key.h"
 
 using namespace AscendC;
 
@@ -53,15 +53,15 @@ struct GMMATAVType { // Grouped_Mat_Mul_All_To_Allv_Type
 
 template <
     bool TILINGKEY_COMPUTE_MATMUL, bool TILINGKEY_GROUPED_MATMUL_TRANS,
-    bool TILINGKEY_MATMUL_TRANS, uint8_t TILINGKEY_GMM_QUANT_MODE, uint8_t TILINGKEY_SHARED_MM_QUANT_MODE>
+    bool TILINGKEY_MATMUL_TRANS>
 __global__ __aicore__ void grouped_mat_mul_allto_allv(
     GM_ADDR gmmxGM, GM_ADDR gmmweightGM, GM_ADDR sendCountsTensorOptionalGM, GM_ADDR recvCountsTensorOptionalGM,
     GM_ADDR mmxOptionalGM, GM_ADDR mmweightOptionalGM, GM_ADDR gmmxScaleGM, GM_ADDR gmmWeightScaleGM,
     GM_ADDR gmmXOffsetOptinalGM, GM_ADDR gmmWeightOffsetOptinalGM, GM_ADDR mmxScaleGM, GM_ADDR mmWeightScaleGM,
     GM_ADDR mmXOffsetOptinalGM, GM_ADDR mmWeightOffsetOptinalGM, GM_ADDR commQuantScaleGM,
-    GM_ADDR gmmyGM, GM_ADDR mmyOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM)
+    GM_ADDR yGM, GM_ADDR mmyOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM)
+
 {
-    GM_ADDR yGM = gmmyGM;
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
     if (workspaceGM == nullptr) {
         return;
@@ -70,7 +70,7 @@ __global__ __aicore__ void grouped_mat_mul_allto_allv(
     if (userWorkspace == nullptr) {
         return;
     }
-    REGISTER_TILING_DEFAULT(GroupedMatMulAlltoAllvTilingData); 
+    REGISTER_TILING_DEFAULT(GroupedMatMulAlltoAllvTilingData);
     auto tiling = (__gm__ GroupedMatMulAlltoAllvTilingData*)tilingGM;
     __gm__ void* hcclInitTiling = (__gm__ void*)(&(tiling->hcclInitTiling));
     __gm__ void* alltoAllvCcTiling = (__gm__ void*)(&(tiling->alltoAllvCcTiling));
