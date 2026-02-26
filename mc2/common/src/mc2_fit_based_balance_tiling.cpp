0a4dfa6c941693aa355b65dfa6a655f50f70526c
chuguowei <chuguowei@huawei.com>
2026-02-26 23:01:15 +0800

    [mc2] allgather adjust x1 size

--- mc2/common/src/mc2_fit_based_balance_tiling.cpp
+++ mc2/common/src/mc2_fit_based_balance_tiling.cpp
@@ -27,6 +27,9 @@ CutResult Mc2FitBasedBalanceTiling::GetTiling()
         SetLongTileLen();
         AdjustLongShortTileLen();
     }
+
+    CheckHCCLLimit();
+
     OPS_LOG_D("Mc2FitBasedBalanceTiling", "Input shape {M, N, K} = {%lu, %lu, %lu}, Final cut: shortTileAtBack %d,"
         " longTileLen %lu, numLongTile %lu, shortTileLen %lu, numShortTile %lu",
         mmInfo_.mValue, mmInfo_.nValue, mmInfo_.kValue, tilingM_.cutRes.shortTileAtBack, tilingM_.cutRes.longTileLen,
