81e5cf75d3ed8a8774fa685a9afbf8515479507e
tooslow <zhangzhizhuo1@huawei.com>
2026-02-26 10:57:05 +0800

    调整infershape

--- gmm/grouped_matmul_finalize_routing/op_host/grouped_matmul_finalize_routing_infershape.cpp
+++ gmm/grouped_matmul_finalize_routing/op_host/grouped_matmul_finalize_routing_infershape.cpp
@@ -129,7 +129,10 @@ static ge::graphStatus SetXAndWShapesForMX(const InferShapeContext *context,cons
 {
     params.m = params.shape_x1->GetDim(xIndex);
     params.k = params.shape_x1->GetDim(wIndex);
-    params.n = params.weightTrans ? params.shape_x2->GetDim(DIM_ONE) : params.shape_x2->GetDim(DIM_TWO);
+
+    auto shape_scale = context->GetOptionalInputShape(scaleOptionIndex);
+
+    params.n = params.weightTrans ? shape_scale->GetDim(DIM_ONE) : shape_scale->GetDim(DIM_TWO);
     params.e = params.shape_x2->GetDim(xIndex);
     return ge::GRAPH_SUCCESS;
 }
@@ -248,14 +251,16 @@ static ge::graphStatus SetupOutputForMX(InferShapeContext *context, const int& b
     
     shape_out->SetDimNum(twoDimNum);
     const int *output_bs = attrs->GetAttrPointer<int>(outputBSAttrIndex);
+    
     if (output_bs != nullptr) {
         shape_out->SetDim(0, *output_bs);
     }
     auto x2_dim = xAndWParams.shape_x2->GetDimNum();
-    OP_LOGI(op_name, "wtrans is %ld", xAndWParams.weightTrans);
-    shape_out->SetDim(DIM_ONE, xAndWParams.weightTrans ? 
-                              xAndWParams.shape_x2->GetDim(DIM_ONE) : 
-                              xAndWParams.shape_x2->GetDim(DIM_TWO)); // 如果非转置，n为最后一维，如果转置，n为倒数第二维。
+    auto shape_scale = context->GetOptionalInputShape(scaleOptionIndex);
+
+    shape_out->SetDim(DIM_ONE, xAndWParams.weightTrans ?
+                             shape_scale->GetDim(DIM_ONE) :
+                             shape_scale->GetDim(DIM_TWO)); // 如果非转置，n为最后一维，如果转置，n为倒数第二维。
     OP_LOGI(op_name, "shape out is %ld, %ld", shape_out->GetDim(DIM_ZERO), shape_out->GetDim(DIM_ONE));
     return ge::GRAPH_SUCCESS;
 }
@@ -289,12 +294,13 @@ static ge::graphStatus InferShapeGroupedMatmulFinalizeRouting(InferShapeContext 
     
     const bool *transposeWeightPtr = attrs->GetBool(weightTransIndex);
     bool transposeWeight = (transposeWeightPtr != nullptr ? *transposeWeightPtr : false);
+    
     CheckXandWParams xAndWParams{shape_x1, shape_x2, 0, 0, 0, 0, transposeWeight};
-    // OP_LOGI(op_name, "wtrans after is %d", xAndWParams.weightTrans);
+    
     // MX量化模式涉及图模式交付不走校验逻辑
     auto shape_scale = context->GetOptionalInputShape(scaleOptionIndex);
     OP_CHECK_IF(shape_scale == nullptr, OPS_REPORT_CUBE_INNER_ERR(op_name, "scale is not given."), return ge::GRAPH_FAILED);
-    if (shape_scale->GetDimNum() == threeDimNum) {
+    if (shape_scale->GetDimNum() == fourDimNum) {
         OP_CHECK_IF(SetXAndWShapesForMX(context, op_name, xAndWParams) != ge::GRAPH_SUCCESS, return ge::GRAPH_FAILED, );
         OP_CHECK_IF(SetupOutputForMX(context, bsdp, op_name, xAndWParams) != ge::GRAPH_SUCCESS,
                     return ge::GRAPH_FAILED, );
@@ -312,9 +318,6 @@ static ge::graphStatus InferShapeGroupedMatmulFinalizeRouting(InferShapeContext 
                     return ge::GRAPH_FAILED, );
 
         OP_CHECK_IF(ValidateRowIndex(context, op_name, xAndWParams) != ge::GRAPH_SUCCESS, return ge::GRAPH_FAILED, );
-
-        OP_CHECK_IF(SetupOutputAndCheckAttrs(context, bsdp, op_name, xAndWParams) != ge::GRAPH_SUCCESS,
-                    return ge::GRAPH_FAILED, );
 
         OP_CHECK_IF(ValidateOffsetShape(context, op_name, xAndWParams) != ge::GRAPH_SUCCESS, return ge::GRAPH_FAILED, );
 
@@ -416,9 +419,7 @@ static bool IsSupportMX(gert::InferDataTypeContext *context){
     return false;
 }
 static ge::graphStatus InferDataTypeGroupedMatmulFinalizeRouting(gert::InferDataTypeContext *context)
-{   
-    context->SetOutputDataType(0, ge::DT_FLOAT);
-    return ge::GRAPH_SUCCESS;
+{
     bool supportDataTypeMX = IsSupportMX(context);
     
     bool supportDataTypeW8A8 = context->GetInputDataType(xIndex) == ge::DT_INT8 && 
