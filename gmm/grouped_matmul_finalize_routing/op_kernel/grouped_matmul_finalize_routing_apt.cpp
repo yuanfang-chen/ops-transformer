f9b57b4aefbae120c0874683a0e8d03a3e27503e
tooslow <zhangzhizhuo1@huawei.com>
2026-02-26 12:10:22 +0800

    重新同步下代码

--- gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing_apt.cpp
+++ gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing_apt.cpp
@@ -1,7 +1,25 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file grouped_matmul_finalize_routing_apt.cpp
+ * \brief
+ */
 #if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
 #include "kernel_utils.h"
 #include "kernel_tiling/kernel_tiling.h"
+#if ASC_DEVKIT_MAJOR >= 9
 #include "kernel_basic_intf.h"
+#else
+#include "kernel_operator.h"
+#endif
 #include "lib/matmul_intf.h"
 #include "arch35/grouped_matmul_finalize_routing_tiling_key.h"
 #if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
@@ -9,10 +27,7 @@
 #elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
     #include "arch35/grouped_matmul_finalize_routing_pertoken_dequant.h"
 #endif
-#endif
 
-
-#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
 template <int ATRANS, int BTRANS>
 __global__ __aicore__ void
 grouped_matmul_finalize_routing(GM_ADDR x, GM_ADDR w, GM_ADDR scale, GM_ADDR bias, GM_ADDR pertoken_scale,
