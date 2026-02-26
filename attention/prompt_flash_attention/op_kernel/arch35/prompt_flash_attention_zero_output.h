931928138d3c17d04ae431cc958ecf322f710cd9
changgl0404 <changguangliang1@h-partners.com>
2026-02-26 16:33:16 +0800

    编译头文件包含路径bugfix
    Signed-off-by: changgl0404 <changguangliang1@h-partners.com>

--- attention/prompt_flash_attention/op_kernel/arch35/prompt_flash_attention_zero_output.h
+++ attention/prompt_flash_attention/op_kernel/arch35/prompt_flash_attention_zero_output.h
@@ -18,7 +18,11 @@
 #include "kernel_tiling/kernel_tiling.h"
 #include "kernel_operator.h"
 #include "lib/matmul_intf.h"
+#if __has_include("../../../common/op_kernel/arch35/flash_attention_score_tiling_regbase.h")
 #include "../../../common/op_kernel/arch35/flash_attention_score_tiling_regbase.h"
+#else
+#include "../../common/arch35/flash_attention_score_tiling_regbase.h"
+#endif
 
 template<typename T>
 class PromptFlashAttentionZeroOutPut {
