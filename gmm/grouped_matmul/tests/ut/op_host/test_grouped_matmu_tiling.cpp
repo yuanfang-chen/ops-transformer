afba24382d90b55a0b6e9787bf7bd72fb9d630c7
liyinghao10 <liyinghao10@h-partners.com>
2026-02-26 16:52:55 +0800

    Fixed tiling UT

--- gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
+++ gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
@@ -4996,7 +4996,7 @@ TEST_F(GroupedMatmulTiling, test_tiling_a8w8o32_weightnz_notrans)
                                                     {{{M, K}, {M, K}}, ge::DT_INT8, ge::FORMAT_ND},              //x
                                                     {{{E, K, N}, {E, N/32, K/16, 16, 32}}, ge::DT_INT8, ge::FORMAT_FRACTAL_NZ},   //weight
                                                     {{{E, N}, {E, N}}, ge::DT_INT32, ge::FORMAT_ND},                //bias
-                                                    {{{E, N}, {E, N}}, ge::DT_FLOAT, ge::FORMAT_ND},                //scale
+                                                    {{{}, {}}, ge::DT_UINT64, ge::FORMAT_ND},                //scale
                                                     {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND},                        //offset
                                                     {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},                        //antiquantScale
                                                     {{{}, {}}, ge::DT_FLOAT16, ge::FORMAT_ND},                        //antiquantOffset
