af26c144f67d944385109ca3078f7674406e1888
tongzy1242 <tongzeyun@huawei.com>
2026-02-27 21:08:11 +0800

    fix: 去除上帝头文件

--- mc2/moe_distribute_combine_teardown/op_kernel/arch32/moe_distribute_combine_teardown.h
+++ mc2/moe_distribute_combine_teardown/op_kernel/arch32/moe_distribute_combine_teardown.h
@@ -15,7 +15,7 @@
 #ifndef MOE_DISTRIBUTE_COMBINE_TEARDOWN_H
 #define MOE_DISTRIBUTE_COMBINE_TEARDOWN_H
 
-#include "kernel_operator.h"
+#include "basic_api/kernel_basic_intf.h"
 #include "kernel_tiling/kernel_tiling.h"
 #include "../moe_distribute_dispatch/moe_distribute_base.h"
 #include "moe_distribute_combine_teardown_tiling.h"
