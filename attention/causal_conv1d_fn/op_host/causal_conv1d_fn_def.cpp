ea757c891790f143f2f9127a9b12021b814d663f
jzj007 <jiangzhijie9@huawei.com>
2026-02-27 15:35:11 +0800

    update def

--- attention/causal_conv1d_fn/op_host/causal_conv1d_fn_def.cpp
+++ attention/causal_conv1d_fn/op_host/causal_conv1d_fn_def.cpp
@@ -15,52 +15,67 @@
 #include "register/op_def_registry.h"
 
 namespace ops {
-    class CausalConv1dFn : public OpDef {
-    public:
-        explicit CausalConv1dFn(const char* name) : OpDef(name)
-        {
-            this->Input("x")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_FLOAT16, ge::DT_BF16})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Input("weight")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_FLOAT16, ge::DT_BF16})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Input("cacheStates")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_FLOAT16, ge::DT_BF16})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Input("cacheIndices")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_INT64, ge::DT_INT64})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Input("seqStartIndex")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_INT64, ge::DT_INT64})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Input("hasInitialState")
-                .ParamType(OPTIONAL)
-                .DataType({ge::DT_BOOL, ge::DT_BOOL})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Output("y")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_FLOAT16, ge::DT_BF16})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->Output("cacheStates")
-                .ParamType(REQUIRED)
-                .DataType({ge::DT_FLOAT16, ge::DT_BF16})
-                .Format({ge::FORMAT_ND, ge::FORMAT_ND})
-                .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
-            this->AICore().AddConfig("ascend950");
-        }
-    };
+class CausalConv1dFn : public OpDef {
+public:
+    explicit CausalConv1dFn(const char *name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("weight")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("bias")
+            .ParamType(OPTIONAL)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("convStates")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("queryStartLoc")
+            .ParamType(REQUIRED)
+            .DataTypeList({ge::DT_INT32})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("cacheIndices")
+            .ParamType(REQUIRED)
+            .DataTypeList({ge::DT_INT32})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("hasInitialState")
+            .ParamType((REQUIRED))
+            .DataTypeList({ge::DT_BOOL})
+            .FormatList({ge::FORMAT_ND})
+        this->Input("numAcceptedTokens")
+            .ParamType(OPTIONAL)
+            .DataTypeList({ge::DT_INT32})
+            .FormatList({ge::FORMAT_ND})
+
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+        this->Output("convStates")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+            .FormatList({ge::FORMAT_ND})
+
+        this->Attr("activationMode").AttrType(OPTIONAL).String("None");
+        this->Attr("padSlotId").AttrType(OPTIONAL).Int(-1);
+        this->Attr("residualConnMode").AttrType(OPTIONAL).Int(0);
+        this->Attr("runMode").AttrType(OPTIONAL).Int(0);
+
+        OpAICoreConfig config_950;
+        config_950.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(false)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false)
+            .PrecisionReduceFlag(true)
+            .ExtendCfgInfo("opFile.value", "causal_conv1d_apt");
+        this->AICore().AddConfig("ascend950", config_950);
+    }
+};
 OP_ADD(CausalConv1dFn);
 } // namespace ops
