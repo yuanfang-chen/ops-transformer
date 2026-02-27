c49488fa038c93be6235b47e48eb8d1eb8be1709
sky-zyh <zhangyuhan45@huawei.com>
2026-02-27 12:09:56 +0800

    检视意见fix ： 1

--- mc2/grouped_mat_mul_allto_allv/op_kernel/arch35/grouped_mat_mul_allto_allv_tiling_key.h
+++ mc2/grouped_mat_mul_allto_allv/op_kernel/arch35/grouped_mat_mul_allto_allv_tiling_key.h
@@ -1,12 +1,12 @@
-/**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+/* *
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+  */
 
 /*!
  * \file grouped_mat_mul_allto_allv_tiling_key.h
@@ -20,16 +20,9 @@
 // 量化模式宏定义
 #define QUANT_MODE_NONE 0 // 无量化
 #define QUANT_MODE_TT 1   // TT
-#define QUANT_MODE_2 2
-#define QUANT_MODE_3 3
-#define QUANT_MODE_4 4
-#define QUANT_MODE_5 5
-#define QUANT_MODE_6 6
 
 // 通信量化模式宏定义
 #define COMM_QUANT_MODE_NONE 0 // 不量化
-#define COMM_QUANT_MODE_INT8 1 // INT8 量化
-#define COMM_QUANT_MODE_INT4 2 // INT4 量化
 
 /**
  * TilingKey 定义
@@ -54,13 +47,11 @@ ASCENDC_TPL_ARGS_DECL(GroupedMatMulAlltoAllv,
 
                     // GMM 量化模式：0=NONE, 1=TT
                     ASCENDC_TPL_UINT_DECL(TILINGKEY_GMM_QUANT_MODE, ASCENDC_TPL_4_BW, ASCENDC_TPL_UI_LIST,
-                                            QUANT_MODE_NONE, QUANT_MODE_TT, QUANT_MODE_2, QUANT_MODE_3, QUANT_MODE_4,
-                                            QUANT_MODE_5, QUANT_MODE_6),
+                                            QUANT_MODE_NONE, QUANT_MODE_TT),
 
                     // 共享专家 MM 量化模式: 0=NONE, 1=TT
                     ASCENDC_TPL_UINT_DECL(TILINGKEY_SHARED_MM_QUANT_MODE, ASCENDC_TPL_4_BW, ASCENDC_TPL_UI_LIST,
-                                            QUANT_MODE_NONE, QUANT_MODE_TT, QUANT_MODE_2, QUANT_MODE_3, QUANT_MODE_4,
-                                            QUANT_MODE_5, QUANT_MODE_6));
+                                            QUANT_MODE_NONE, QUANT_MODE_TT));
 
 /**
  * TilingKey 选择器定义
