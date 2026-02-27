80a894b675ec3980c33a578fbb0ae3a0892158f6
吴剑飞 <wujianfei15@huawei.com>
2026-02-27 15:34:02 +0800

    check_compile_version

--- scripts/package/common/py/utils/pkg_utils.py
+++ scripts/package/common/py/utils/pkg_utils.py
@@ -95,6 +95,16 @@ class CompressError(PackageError):
         super().__init__(package_name)
         self.package_name = package_name
 
+class InstallScriptNotInPackageInfo(PackageError):
+    """package_info中没有配置install_script。"""
+
+
+class InstallScriptFormatError(PackageError):
+    """install_script配置格式错误。"""
+
+
+class VersionInfoNotExist(PackageError):
+    """version.info文件不存在。"""
 
 def flatten(list_of_lists):
     """Flatten one level of nesting"""
