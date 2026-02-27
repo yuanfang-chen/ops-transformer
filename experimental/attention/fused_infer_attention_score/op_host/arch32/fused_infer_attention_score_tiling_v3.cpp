dd1dcffc0f41f5fa3a627e162113aa7083eae660
huang-chuhong <huangchuhong1@h-partners.com>
2026-02-27 21:46:39 +0800

    ttt

--- experimental/attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
+++ experimental/attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
@@ -74,7 +74,7 @@ REGISTER_TILING_DATA_CLASS(FusedInferAttentionScore_103000000000422220, FusedInf
 REGISTER_TILING_DATA_CLASS(FusedInferAttentionScore_103000000010422221, FusedInferAttentionScoreTilingData)
 REGISTER_TILING_DATA_CLASS(FusedInferAttentionScore_103000000000522220, FusedInferAttentionScoreTilingData)
 REGISTER_TILING_DATA_CLASS(FusedInferAttentionScore_103000000010522221, FusedInferAttentionScoreTilingData)
-
+111111
 FIA_EXTERN_C ge::graphStatus TilingFusedInferAttentionScoreV3(gert::TilingContext *context)
 {
     FiaTilingInfo fiaInfo;
