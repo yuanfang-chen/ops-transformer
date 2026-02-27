486961b500a534ce7b00d0a59b86d2044dc74ba1
tangkaidi <tangkaidi1@huawei.com>
2026-02-27 10:46:54 +0800

    try

--- attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax_low_prec.hpp
+++ attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax_low_prec.hpp
@@ -169,7 +169,7 @@ public:
         uint8_t blockNumPerRow = numElemsAligned / BLOCK_SIZE; // half低精度场景，每行共有1024/16=64个datablock。
         uint8_t dataBlockStride = 1;
 
-        // 1024个元素，以128为单位分治求和。1024->512->256->128
+        // 1024个元素，以128为单位分治求和。1024->512->256->128。
         for (uint32_t columnStrideIndex = 2; columnStrideIndex <= loopCount; columnStrideIndex *= 2) {
             ReduceSumByPair(srcUb, numRowsRound, loopCount, columnStrideIndex, dataBlockStride, blockNumPerRow);
             AscendC::PipeBarrier<PIPE_V>();
@@ -317,7 +317,7 @@ public:
         uint8_t blockNumPerRow = numElemsAligned / BLOCK_SIZE; // half低精度场景，每行共有1024/16=64个datablock。
         uint8_t dataBlockStride = 1;
 
-        // 1024个元素，以128为单位分治求最大值。1024->512->256->128
+        // 1024个元素，以128为单位分治求最大值。1024->512->256->128。
         uint32_t columnStrideIndex = 2;
         // 后续Rowsum计算还会使用到srcUb，因此第一轮分治使用lsUbTensor作为目的操作数，srcUb作为源操作数。
         ReduceMaxByPair(lsUbTensor, srcUb, numRowsRound, loopCount, columnStrideIndex, dataBlockStride, blockNumPerRow);
@@ -327,6 +327,39 @@ public:
             ReduceMaxByPair(lsUbTensor, lsUbTensor, numRowsRound, loopCount, columnStrideIndex, dataBlockStride, blockNumPerRow);
             AscendC::PipeBarrier<PIPE_V>();
         }
+
+        //每行128个元素分别规约求最大值。
+        AscendC::WholeReduceMax<half, false>(
+            rowmaxUb,
+            lsUbTensor,
+            AscendC::MASK_PLACEHOLDER, // (uint64_t)0
+            numRowsRound,
+            dataBlockStride,
+            dataBlockStride,
+            blockNumPerRow,
+            AscendC::ReduceOrder::ORDER_ONLY_VALUE);
+        AscendC::PipeBarrier<PIPE_V>();
+    }
+
+    __aicore__ inline
+    void RowmaxSPECTILE512(const AscendC::LocalTensor<half> &srcUb, const AscendC::LocalTensor<half> &rowmaxUb,
+                            const AscendC::LocalTensor<half> &tvUbTensor, uint32_t numRowsRound, uint32_t numElems,
+                            uint32_t numElemsAligned)
+    {
+        // Vector计算单元每个迭代最多处理256Byte数据，因此half低精度场景，每次迭代最多处理256/2=128个元素。
+        uint32_t loopCount = numElemsAligned / HALF_VECTOR_SIZE; // half低精度场景，每行需要512/128=4次循环处理。
+        // 每个datablock长度32Byte，因此half低精度场景，每个datablock内有32/2=16个元素。
+        uint8_t blockNumPerRow = numElemsAligned / BLOCK_SIZE; // half低精度场景，每行共有512/16=32个datablock。
+        uint8_t dataBlockStride = 1;
+
+        // 512个元素，以128为单位分治求最大值。512->256->128。
+        uint32_t columnStrideIndex = 2;
+        // 后续Rowsum计算还会使用到srcUb，因此第一轮分治使用lsUbTensor作为目的操作数，srcUb作为源操作数。
+        ReduceMaxByPair(lsUbTensor, srcUb, numRowsRound, loopCount, columnStrideIndex, dataBlockStride, blockNumPerRow);
+        AscendC::PipeBarrier<PIPE_V>();
+        columnStrideIndex *= 2;
+        ReduceMaxByPair(lsUbTensor, lsUbTensor, numRowsRound, loopCount, columnStrideIndex, dataBlockStride, blockNumPerRow);
+        AscendC::PipeBarrier<PIPE_V>();
 
         //每行128个元素分别规约求最大值。
         AscendC::WholeReduceMax<half, false>(
@@ -547,7 +580,15 @@ public:
                 rowNumCurLoopRound,
                 columnNum,
                 columnNumRound);
-        } else {      
+        } else if (columnNum == 512U) {
+            RowmaxSPECTILE512(
+                computeUbTensor,
+                lmUbTensor[rowOffset],
+                tvUbTensor,
+                rowNumCurLoopRound,
+                columnNum,
+                columnNumRound);
+        } else {
             RowmaxTAILTILE(
                 computeUbTensor,
                 lmUbTensor[rowOffset],
@@ -555,14 +596,7 @@ public:
                 rowNumCurLoopRound,
                 columnNum,
                 columnNumRound);
-        }    
-        // RowmaxTAILTILE(
-        //     computeUbTensor,
-        //     lmUbTensor[rowOffset],
-        //     tvUbTensor,
-        //     rowNumCurLoopRound,
-        //     columnNum,
-        //     columnNumRound);
+        }
     }
 
     __aicore__ inline
