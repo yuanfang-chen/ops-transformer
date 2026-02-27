12e860d9ea44d368742f05d946e5c7836a82580a
hblnb <huangbolin3@huawei.com>
2026-02-27 16:45:14 +0800

    ut编译隔离不同芯片

---
+++ cmake/scripts/get_soc_version.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+"""
+获取修改文件应触发的soc版本.
+"""
+
+import argparse
+import logging
+from pathlib import Path
+from typing import List, Dict, Any, Optional
+import sys
+import yaml
+from parse_changed_file import Module
+
+
+class GetSocModule(Module):
+    def __init__(self, name):
+        self.name: str = name
+        self.src_files: List[Path] = []
+        self.src_exclude_files: List[Path] = []
+        self.options: List[str] = []
+        self.test_excludes: List[str] = []
+
+    def get_test_options(self, f: Path) -> List[str]:
+
+        def check_option_arch(path_obj: Path, option: List[str]):
+            if not option:
+                return True
+            if option[0] == "ascend310p":
+                ascend310p_exclued_folders = ["arch32", "arch35",
+                    "ascend910b", "ascend910_93", "ascend950"]
+                return any(folder in path_obj.parts for folder in ascend310p_exclued_folders)
+            if option[0] == "ascend910b":
+                ascend910b_exclued_folders = ["arch20", "arch31", "arch35",
+                    "ascend310p", "ascend310p"]
+                return any(folder in path_obj.parts for folder in ascend910b_exclued_folders)
+            if option[0] == "ascend950":
+                ascend950_exclued_folders = ["arch20", "arch31", "arch32",
+                    "ascend910b", "ascend910_93", "ascend310p"]
+                return any(folder in path_obj.parts for folder in ascend950_exclued_folders)
+            return False
+
+        def is_excluded(e_f: Path, option: str):
+            if check_option_arch(e_f, option):
+                return True
+            for e in self.src_exclude_files:
+                try:
+                    e_f.relative_to(e)
+                    return True
+                except ValueError:
+                    continue
+            return False
+
+        related_options: List[str] = []
+        for s in self.src_files:
+            if is_excluded(e_f=f, option=self.options):
+                continue
+            try:
+                if f.relative_to(s):
+                    # 当同一个修改文件需要触发多个 Options 时, 需要把这些 Options 全部添加
+                    related_options.extend(self.options)
+            except ValueError:
+                continue
+        # 关联 Options 去重
+        related_options = list(set(related_options))
+        return related_options
+
+
+class Parser:
+    """
+    规则文件、修改文件列表文件解析.
+    """
+
+    _Modules: List[GetSocModule] = []         # 保存规则文件(tests/test_config.yaml)内设置的模块列表
+    _ChangedPaths: List[Path] = []      # 修改文件列表文件(changed_file)内设置的修改文件列表
+    _UTExcludes: List[str] = []
+    _ExamplesExcludes: List[str] = []
+
+    @staticmethod
+    def main() -> str:
+        # 参数注册
+        ps = argparse.ArgumentParser(description="Parse changed files", epilog="Best Regards!")
+        ps.add_argument("-c", "--classify", required=True, nargs=1, type=Path, help="tests/test_config.yaml")
+        ps.add_argument("-f", "--file", required=True, nargs=1, type=Path, help="changed files desc file.")
+        # 子命令行
+        sub_ps = ps.add_subparsers(help="Sub-Command")
+        p_soc = sub_ps.add_parser('get_related_soc', help="Get related soc.")
+        p_soc.set_defaults(func=Parser.get_related_soc)
+        # 处理
+        args = ps.parse_args()
+        logging.debug(args)
+        if not Parser.parse_classify_file(file=Path(args.classify[0])):
+            return ""
+        if not Parser.parse_changed_file(file=Path(args.file[0])):
+            return ""
+        Parser.print_details()
+        rst = args.func()
+        return rst
+
+    @classmethod
+    def get_related_soc(cls):
+        def soc_test_list_append(soc, soc_test_option_lst):
+            if soc not in soc_test_option_lst:
+                soc_test_option_lst.append(soc)
+        soc_test_option_lst: List[str] = []
+        for p in cls._ChangedPaths:
+            for m in cls._Modules:
+                new_options = m.get_test_options(f=p)
+                for soc in new_options:
+                    soc_test_list_append(soc, soc_test_option_lst)
+        if len(soc_test_option_lst) == 0:
+            logging.info("lst is empty, trigger soc ascend910b.")
+            return "ascend910b"
+        soc_test_ut_str: str = ""
+        for soc in soc_test_option_lst:
+            if soc not in cls._UTExcludes:
+                soc_test_ut_str += f"{soc},"
+        soc_test_ut_str = f"{soc_test_ut_str}"
+        soc_test_ut_str = soc_test_ut_str[:-1]
+        logging.info(f"Trigger UTs of soc: {soc_test_ut_str}")
+        return soc_test_ut_str
+
+    @classmethod
+    def get_ops_test_option_lst(cls) -> List[str]:
+        def ops_test_list_append(opt, ops_test_option_lst):
+            if opt not in ops_test_option_lst:
+                ops_test_option_lst.append(opt)
+        ops_test_option_lst: List[str] = []
+        for p in cls._ChangedPaths:
+            for m in cls._Modules:
+                new_options = m.get_test_example_ops_test_options(f=p)
+                for opt in new_options:
+                    ops_test_list_append(opt, ops_test_option_lst)
+        return ops_test_option_lst
+
+    @classmethod
+    def _parse_classify_item(cls, name: str, desc: Optional[Dict[str, Any]] = None) -> bool:
+        if desc is None:
+            logging.error("[%s]'s desc is None.", name)
+            return False
+        if desc.get('module', False):
+            mod = GetSocModule(name=name)
+            rst = mod.update_classify_cfg(desc=desc)
+            if rst:
+                cls._Modules.append(mod)
+                short_name = name.split('/')[-1]
+                if 'examples' in mod.test_excludes:
+                    cls._ExamplesExcludes.append(short_name)
+                if 'ut' in mod.test_excludes:
+                    cls._UTExcludes.append(short_name)
+            return rst
+        for k, sub_desc in desc.items():
+            if not cls._parse_classify_item(name=name + '/' + k, desc=sub_desc):
+                return False
+        return True
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, format='[%(asctime)s][%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+    result = Parser.main()
+    logging.info(result)
