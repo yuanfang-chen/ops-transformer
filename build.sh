61081ea3295386ea2b6268b075fcf55e33c80df0
huang-chuhong <huangchuhong1@h-partners.com>
2026-02-27 21:12:56 +0800

    ttt

--- build.sh
+++ build.sh
@@ -856,6 +856,29 @@ set_ut_mode() {
   fi
 }
 
+parse_changed_files() {
+ 	if [[ -z "$PR_CHANGED_FILES" ]]; then
+ 	    return
+ 	fi
+
+ 	if [[ "$PR_CHANGED_FILES" != /* ]]; then
+ 	    PR_CHANGED_FILES=$PWD/$PR_CHANGED_FILES
+ 	fi
+
+ 	echo "changed files is" $PR_CHANGED_FILES
+ 	echo $dotted_line
+ 	cat $PR_CHANGED_FILES
+ 	ops_names=$(python3 scripts/ci/parse_changed_ops.py $PR_CHANGED_FILES "$ENABLE_EXPERIMENTAL")
+ 	if [[ -z $ops_names ]]; then
+ 	    if [[ "$ENABLE_EXPERIMENTAL" == "TRUE" ]]; then
+ 	        ops_names='fused_infer_attention_score'
+ 	    else
+ 	        ops_names='fused_infer_attention_score'
+ 	    fi
+ 	    echo "NO ops changed found,set op $ops_names as default."
+ 	fi
+}
+
 set_example_opt() {
   if [[ -n $1 && $1 != -* ]]; then
     EXAMPLE_NAME=$1
@@ -1006,9 +1029,13 @@ while [[ $# -gt 0 ]]; do
         ;;
     --PR_PKG)
         PR_CHANGED_FILES="$2"
-        ops_names=$(python3 "$CURRENT_DIR"/cmake/scripts/parse_changed_files.py -c "$CURRENT_DIR"/tests/test_config.yaml -f "$PR_CHANGED_FILES" get_related_examples)
-        echo "Operators that need custom package compilation:$ops_names"
-        if [ -z "${ops_names}" ];then
+        if [[ "$ENABLE_EXPERIMENTAL" == "TRUE" ]]; then
+ 	        parse_changed_files
+ 	    else
+ 	        ops_names=$(python3 "$CURRENT_DIR"/cmake/scripts/parse_changed_files.py -c "$CURRENT_DIR"/tests/test_config.yaml -f "$PR_CHANGED_FILES" get_related_examples)
+ 	    fi
+ 	    echo "Operators that need custom package compilation:$ops_names"
+ 	    if [ -z "${ops_names}" ]; then
             log "Info: No custom packages to build for this PR."
             # ops_names="incre_flash_attention"
             exit 200
