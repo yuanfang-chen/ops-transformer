ead9cbdd34997c04dc4e8ed7f8c7c51ec44cb622
jayshu <shuhaojie@huawei.com>
2026-02-28 10:53:43 +0800

    add pertoken mode kernel files of op GMMFR
    Co-authored-by: jayshu<shuhaojie@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1459 merge jayshu_gmmfr_pertoken into master
    
    add pertoken mode kernel files of op GMMFR
    
    Created-by: jayshu
    Commit-by: jayshu
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    新需求，功能新增：add pertoken mode kernel files of op GMMFR
    新增文件：
    gmm/common/cgmct/epilogue/block_epilogue_dequant_finalize_routing.h
    gmm/common/cgmct/kernel/kernel_gmm_finalize_routing_pertoken_dequant.h
    gmm/grouped_matmul_finalize_routing/op_kernel/arch35/grouped_matmul_finalize_routing_pertoken_dequant.h
    修改文件：
    gmm/common/cgmct/block/block_scheduler_gmm_aswt_with_tail_split.h
    gmm/common/cgmct/utils/coord_utils.h
    gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing.cpp
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/894
    
    ## 测试
    算子泛化及二级冒烟
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1459

---
+++ gmm/grouped_matmul_finalize_routing/op_kernel/grouped_matmul_finalize_routing_apt.cpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file grouped_matmul_finalize_routing_apt.cpp
+ * \brief
+ */
+#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 310
+#include "kernel_utils.h"
+#include "kernel_tiling/kernel_tiling.h"
+#if ASC_DEVKIT_MAJOR >= 9
+#include "kernel_basic_intf.h"
+#else
+#include "kernel_operator.h"
+#endif
+#include "lib/matmul_intf.h"
+#include "arch35/grouped_matmul_finalize_routing_tiling_key.h"
+#if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
+    #include "arch35/grouped_matmul_finalize_routing.h"
+#elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
+    #include "arch35/grouped_matmul_finalize_routing_pertoken_dequant.h"
+#endif
+
+template <int ATRANS, int BTRANS>
+__global__ __aicore__ void
+grouped_matmul_finalize_routing(GM_ADDR x, GM_ADDR w, GM_ADDR scale, GM_ADDR bias, GM_ADDR pertoken_scale,
+                                GM_ADDR group_list, GM_ADDR share_input, GM_ADDR logit, GM_ADDR row_index,
+                                GM_ADDR offset, GM_ADDR y, GM_ADDR workspaceGM, GM_ADDR tilingGM)
+{
+    TPipe pipe;
+    KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
+    #if ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT8_E8M0
+    if constexpr (ATRANS == 0 && BTRANS == 0) { // transX = false, transW = false
+        grouped_matmul_finalize_routing<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::RowMajor>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    if constexpr (ATRANS == 0 && BTRANS == 1) { // transX = false, transW = true
+        grouped_matmul_finalize_routing<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::ColumnMajor>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    #elif ORIG_DTYPE_PERTOKEN_SCALE == DT_FLOAT
+    if constexpr (ATRANS == 0 && BTRANS == 0) { // transX = false, transW = false
+        grouped_matmul_finalize_routing_pertoken_dequant<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::Nz>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    if constexpr (ATRANS == 0 && BTRANS == 1) { // transX = false, transW = true
+        grouped_matmul_finalize_routing_pertoken_dequant<Cgmct::Gemm::layout::RowMajor, Cgmct::Gemm::layout::Zn>(
+            x, w, scale, bias, pertoken_scale, group_list, share_input, logit, row_index, offset, y, workspaceGM,
+            tilingGM);
+    }
+    #endif
+}
+#endif
