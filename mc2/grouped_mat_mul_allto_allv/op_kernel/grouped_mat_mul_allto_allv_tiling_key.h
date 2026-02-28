b213d7b0664fdb5e4e530d0d4adf663a0f42dc74
huangshuai <huangshuai59@huawei.com>
2026-02-28 16:27:44 +0800

    review修改

--- mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv_tiling_key.h
+++ mc2/grouped_mat_mul_allto_allv/op_kernel/grouped_mat_mul_allto_allv_tiling_key.h
@@ -20,16 +20,9 @@
 // 量化模式宏定义
 #define QUANT_MODE_NONE 0 // 无量化
 #define QUANT_MODE_TT 1   // TT
-#define QUANT_MODE_2 2
-#define QUANT_MODE_3 3
-#define QUANT_MODE_4 4
-#define QUANT_MODE_5 5
-#define QUANT_MODE_6 6
 
 // 通信量化模式宏定义
 #define COMM_QUANT_MODE_NONE 0 // 不量化
-#define COMM_QUANT_MODE_INT8 1 // INT8 量化
-#define COMM_QUANT_MODE_INT4 2 // INT4 量化
 
 /**
  * TilingKey 定义
@@ -54,13 +47,11 @@ ASCENDC_TPL_ARGS_DECL(GroupedMatMulAlltoAllv,
 
                     // GMM 量化模式：0=NONE, 1=TT
                     ASCENDC_TPL_UINT_DECL(TILINGKEY_GMM_QUANT_MODE, ASCENDC_TPL_4_BW, ASCENDC_TPL_UI_LIST,
-                                            QUANT_MODE_NONE, QUANT_MODE_TT, QUANT_MODE_2, QUANT_MODE_3, QUANT_MODE_4,
-                                            QUANT_MODE_5, QUANT_MODE_6),
+                                            QUANT_MODE_NONE, QUANT_MODE_TT),
 
                     // 共享专家 MM 量化模式: 0=NONE, 1=TT
                     ASCENDC_TPL_UINT_DECL(TILINGKEY_SHARED_MM_QUANT_MODE, ASCENDC_TPL_4_BW, ASCENDC_TPL_UI_LIST,
-                                            QUANT_MODE_NONE, QUANT_MODE_TT, QUANT_MODE_2, QUANT_MODE_3, QUANT_MODE_4,
-                                            QUANT_MODE_5, QUANT_MODE_6));
+                                            QUANT_MODE_NONE, QUANT_MODE_TT));
 
 /**
  * TilingKey 选择器定义
