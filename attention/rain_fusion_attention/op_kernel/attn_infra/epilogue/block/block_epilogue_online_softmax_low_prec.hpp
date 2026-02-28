a7adacfe1920831e417a34c6957dcb4b942859b8
tangkaidi <tangkaidi1@huawei.com>
2026-02-28 16:41:13 +0800

    try

--- attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax_low_prec.hpp
+++ attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax_low_prec.hpp
@@ -722,15 +722,33 @@ public:
         uint32_t rowOffset)
     {
         // *** ll = rowsum(ls32)
-        if (columnNum == 1024U) {
-            RowsumSPECTILE1024(
-                computeUbTensor,
-                llUbTensor[rowOffset],
-                tvUbTensor,
-                rowNumCurLoopRound,
-                columnNum,
-                columnNumRound);
-        } else if (columnNum == 512U) {
+        // if (columnNum == 1024U) {
+        //     RowsumSPECTILE1024(
+        //         computeUbTensor,
+        //         llUbTensor[rowOffset],
+        //         tvUbTensor,
+        //         rowNumCurLoopRound,
+        //         columnNum,
+        //         columnNumRound);
+        // } else if (columnNum == 512U) {
+        //     RowsumSPECTILE512(
+        //         computeUbTensor,
+        //         llUbTensor[rowOffset],
+        //         tvUbTensor,
+        //         rowNumCurLoopRound,
+        //         columnNum,
+        //         columnNumRound);
+        // } else {
+        //     RowsumTAILTILE(
+        //         computeUbTensor,
+        //         llUbTensor[rowOffset],
+        //         tvUbTensor,
+        //         rowNumCurLoopRound,
+        //         columnNum,
+        //         columnNumRound);
+        // }
+
+        if (columnNum == 512U) {
             RowsumSPECTILE512(
                 computeUbTensor,
                 llUbTensor[rowOffset],
