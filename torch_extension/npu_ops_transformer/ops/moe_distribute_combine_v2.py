6fed847ccf1638a14520b060857eb921bb0724ac
朱雪玲 <zhuxueling4@huawei.com>
2026-02-28 14:56:44 +0800

    :wq!add_update_context

--- torch_extension/npu_ops_transformer/ops/moe_distribute_combine_v2.py
+++ torch_extension/npu_ops_transformer/ops/moe_distribute_combine_v2.py
@@ -52,11 +52,9 @@ class MoeDistributeCombineV2OpBuilder(OpBuilder):
             dim_tuple = (expert_ids.size(0), expand_x.size(1))
             return expand_x.new_empty(dim_tuple)
 
-
 # Instantiate the builder
 moe_distribute_combine_v2_op_builder = MoeDistributeCombineV2OpBuilder()
 op_module = moe_distribute_combine_v2_op_builder.load()  # Compiles/loads the .so file
-
 
 @impl(AS_LIBRARY, moe_distribute_combine_v2_op_builder.name, "PrivateUse1")
 def npu_moe_distribute_combine_v2(expand_x, expert_ids, assist_info_for_combine, ep_send_counts, 
