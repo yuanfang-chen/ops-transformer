91c0e37077dd0a62876b0f927473f7f6c1d85073
chenjiale56 <chenjiale56@huawei.com>
2026-02-28 18:24:49 +0800

    新增MoeDistributeDispatchTeardown算子host侧实现
    Co-authored-by: 陈佳乐<chenjiale56@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1605 merge dispatch_teardown into master
    
    新增MoeDistributeDispatchTeardown算子host侧实现
    
    Created-by: chenjiale56
    Commit-by: 陈佳乐
    Merged-by: cann-robot
    Description: ## 描述
    新增DispatchTeardown算子host侧实现
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    关联Issue [#892](https://gitcode.com/cann/ops-transformer/issues/892) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    新增README.md文件及aclnnDispatchTeardown.md文件
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1605

---
+++ mc2/moe_distribute_dispatch_teardown/op_api/aclnn_moe_distribute_dispatch_teardown.cpp
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "aclnn_moe_distribute_dispatch_teardown.h"
+#include <algorithm>
+#include "op_mc2.h"
+#include "common/op_host/op_api/matmul_util.h"
+#include "op_mc2_def.h"
+#include "aclnn_kernels/common/op_error_check.h"
+#include "opdev/op_log.h"
+#include "opdev/common_types.h"
+
+using namespace op;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static constexpr int32_t DISPATCH_DYNAMIC_QUANT_MODE = 2;
+enum NnopbaseHcclServerType {
+    NNOPBASE_HCCL_SERVER_TYPE_AICPU = 0,
+    NNOPBASE_HCCL_SERVER_TYPE_MTE,
+    NNOPBASE_HCCL_SERVER_TYPE_END
+};
+
+extern aclnnStatus aclnnInnerMoeDistributeDispatchTeardownGetWorkspaceSize(
+    const aclTensor* x, const aclTensor* y, const aclTensor* expertIds, const aclTensor* commCmdInfo,
+    const char* groupEp, int64_t epWorldSize, int64_t epRankId,
+    int64_t moeExpertNum, int64_t expertShardType, int64_t sharedExpertNum, int64_t sharedExpertRankNum,
+    int64_t quantMode, int64_t globalBs, int64_t expertTokenNumsType, int64_t commType, char* commAlg,
+    aclTensor* expandXOut, aclTensor* dynamicScalesOut, aclTensor* assistInfoForCombineOut,
+    aclTensor* expertTokenNumsOut, uint64_t* workspaceSize, aclOpExecutor** executor);
+extern aclnnStatus aclnnInnerMoeDistributeDispatchTeardown(void* workspace, uint64_t workspaceSize,
+                                                           aclOpExecutor* executor, aclrtStream stream);
+extern "C" void __attribute__((weak)) NnopbaseSetHcclServerType(void* executor, NnopbaseHcclServerType sType);
+
+// check nullptr
+static bool CheckNotNull(
+    const aclTensor* x, const aclTensor* y, const aclTensor* expertIds, const aclTensor* commCmdInfo,
+    const char* groupEp, aclTensor* expandXOut, aclTensor* dynamicScalesOut, aclTensor* assistInfoForCombineOut,
+    aclTensor* expertTokenNumsOut)
+{
+    OP_CHECK_NULL(x, return false);
+    OP_CHECK_NULL(y, return false);
+    OP_CHECK_NULL(expertIds, return false);
+    OP_CHECK_NULL(commCmdInfo, return false);
+    OP_CHECK_NULL(expandXOut, return false);
+    OP_CHECK_NULL(dynamicScalesOut, return false);
+    OP_CHECK_NULL(assistInfoForCombineOut, return false);
+    OP_CHECK_NULL(expertTokenNumsOut, return false);
+    if ((groupEp == nullptr) || (strnlen(groupEp, HCCL_GROUP_NAME_MAX) == 0)) {
+        OP_LOGE(ACLNN_ERR_PARAM_NULLPTR, "group groupEp name is Empty");
+        return false;
+    }
+    return true;
+}
+
+// check invalid parameters
+static aclnnStatus CheckParams(
+    const aclTensor* x, const aclTensor* y, const aclTensor* expertIds, const aclTensor* commCmdInfo, 
+    const char* groupEp, int64_t expertShardType, int64_t sharedExpertRankNum, int64_t moeExpertNum, 
+    int64_t quantMode, int64_t globalBs,int64_t commType, char* commAlg, aclTensor* expandXOut, 
+    aclTensor* dynamicScalesOut, aclTensor* assistInfoForCombineOut, aclTensor* expertTokenNumsOut)
+{
+    CHECK_RET(CheckNotNull(x, y, expertIds, commCmdInfo, groupEp, expandXOut, dynamicScalesOut,
+                           assistInfoForCombineOut, expertTokenNumsOut),
+              ACLNN_ERR_PARAM_NULLPTR);
+    if (quantMode == DISPATCH_DYNAMIC_QUANT_MODE) {
+        CHECK_RET(dynamicScalesOut != nullptr, ACLNN_ERR_PARAM_NULLPTR);
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "dynamicScalesOut can't be null while quantMode = 2");
+    }
+    if (strnlen(groupEp, HCCL_GROUP_NAME_MAX) >= HCCL_GROUP_NAME_MAX) {
+        OP_LOGE(ACLNN_ERR_PARAM_NULLPTR, "Required groupEp name exceeds %zu", HCCL_GROUP_NAME_MAX);
+        return ACLNN_ERR_PARAM_NULLPTR;
+    }
+    return ACLNN_SUCCESS;
+}
+
+aclnnStatus aclnnMoeDistributeDispatchTeardownGetWorkspaceSize(
+    const aclTensor* x, const aclTensor* y, const aclTensor* expertIds, const aclTensor* commCmdInfo,
+    const char* groupEp, int64_t epWorldSize, int64_t epRankId,
+    int64_t moeExpertNum, int64_t expertShardType, int64_t sharedExpertNum, int64_t sharedExpertRankNum,
+    int64_t quantMode, int64_t globalBs, int64_t expertTokenNumsType, int64_t commType, char* commAlg,
+    aclTensor* expandXOut, aclTensor* dynamicScalesOut, aclTensor* assistInfoForCombineOut,
+    aclTensor* expertTokenNumsOut, uint64_t* workspaceSize, aclOpExecutor** executor)
+{
+    OP_LOGD("aclnnMoeDistributeDispatchTeardownGetWorkspaceSize start");
+    if (GetCurrentPlatformInfo().GetCurNpuArch() != NpuArch::DAV_3510) {
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Unsupported npuArch"); // 暂不支持A3
+        return ACLNN_ERR_PARAM_INVALID;
+    }
+    auto ret_param = CheckParams(
+        x, y, expertIds, commCmdInfo, groupEp, expertShardType, sharedExpertRankNum, 
+        moeExpertNum, quantMode, globalBs, commType, commAlg, expandXOut, 
+        dynamicScalesOut, assistInfoForCombineOut, expertTokenNumsOut);
+    CHECK_RET(ret_param == ACLNN_SUCCESS, ret_param);
+
+    aclnnStatus ret = aclnnInnerMoeDistributeDispatchTeardownGetWorkspaceSize(
+        x, y, expertIds, commCmdInfo, groupEp, epWorldSize, epRankId, moeExpertNum,
+        expertShardType, sharedExpertNum, sharedExpertRankNum, quantMode, globalBs, expertTokenNumsType, commType,
+        commAlg, expandXOut, dynamicScalesOut, assistInfoForCombineOut, expertTokenNumsOut, workspaceSize, executor);
+    return ret;
+}
+
+aclnnStatus aclnnMoeDistributeDispatchTeardown(void* workspace, uint64_t workspaceSize, aclOpExecutor* executor,
+                                               aclrtStream stream)
+{
+    if (NnopbaseSetHcclServerType) {
+        NnopbaseSetHcclServerType(executor, NNOPBASE_HCCL_SERVER_TYPE_MTE);
+    }
+    aclnnStatus ret = aclnnInnerMoeDistributeDispatchTeardown(workspace, workspaceSize, executor, stream);
+    return ret;
+}
+#ifdef __cplusplus
+}
+#endif
