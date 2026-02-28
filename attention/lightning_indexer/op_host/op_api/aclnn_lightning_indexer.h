82d1d3d229d73ccf4e813e45b3ae2a441d51c24d
zzzyh22 <zhangyaohui22@huawei.com>
2026-02-28 10:08:52 +0800

    LI算子aclnnInner检视意见修改

--- attention/lightning_indexer/op_host/op_api/aclnn_lightning_indexer.h
+++ attention/lightning_indexer/op_host/op_api/aclnn_lightning_indexer.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -24,33 +24,34 @@ extern "C" {
  */
 __attribute__((visibility("default")))
 aclnnStatus aclnnLightningIndexerGetWorkspaceSize(
-const aclTensor *query,
-const aclTensor *key,
-const aclTensor *weights,
-const aclTensor *actualSeqLengthsQueryOptional,
-const aclTensor *actualSeqLengthsKeyOptional,
-const aclTensor *blockTableOptional,
-char *layoutQueryOptional,
-char *layoutKeyOptional,
-int64_t sparseCount,
-int64_t sparseMode,
-int64_t preTokens,
-int64_t nextTokens,
-bool returnValues,
-const aclTensor *sparseIndicesOut,
-const aclTensor *sparseValuesOut,
-uint64_t *workspaceSize,
-aclOpExecutor **executor);
+    const aclTensor *query,
+    const aclTensor *key,
+    const aclTensor *weights,
+    const aclTensor *actualSeqLengthsQueryOptional,
+    const aclTensor *actualSeqLengthsKeyOptional,
+    const aclTensor *blockTableOptional,
+    char *layoutQueryOptional,
+    char *layoutKeyOptional,
+    int64_t sparseCount,
+    int64_t sparseMode,
+    int64_t preTokens,
+    int64_t nextTokens,
+    bool returnValues,
+    const aclTensor *sparseIndicesOut,
+    const aclTensor *sparseValuesOut,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor);
 
 /**
  * @brief The second interface of aclnnLightningIndexer is used to perform calculations.
  */
 __attribute__((visibility("default")))
 aclnnStatus aclnnLightningIndexer(
-void *workspace,
-uint64_t workspaceSize,
-aclOpExecutor *executor,
-aclrtStream stream);
+    void *workspace,
+    uint64_t workspaceSize,
+    aclOpExecutor *executor,
+    aclrtStream stream);
+
 #ifdef __cplusplus
 }
 #endif
