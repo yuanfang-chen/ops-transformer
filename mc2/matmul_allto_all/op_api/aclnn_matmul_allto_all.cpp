dabe2febcc60983a8c3d54879450990ee8574129
qzzzy1 <qiziyu2@huawei.com>
2026-03-01 09:41:33 +0800

    fix

--- mc2/matmul_allto_all/op_api/aclnn_matmul_allto_all.cpp
+++ mc2/matmul_allto_all/op_api/aclnn_matmul_allto_all.cpp
@@ -207,10 +207,8 @@ static aclnnStatus CheckAndHandleParams(const aclTensor *x1, const aclTensor *x2
     // bias的数据类型限制在950和910B上有所区别，这里根据芯片版本做区分
     if (GetCurrentPlatformInfo().GetCurNpuArch() == NpuArch::DAV_3510) {
         CHECK_RET(CheckAllDtypesValid(x1, x2, biasOptional, output), ACLNN_ERR_PARAM_INVALID);
-    } else if{
-        GetCurrentPlatformInfo().GetSocVersion()== SocVersion::ASCEND910_93;
-    } else if (GetCurrentPlatformInfo().GetSocVersion()== SocVersion::ASCEND910_93) {
-        GetCurrentPlatformInfo().GetSocVersion()== SocVersion::ASCEND910_93
+    } else {
+        CHECK_RET(CheckAllDtypesValid910B(x1, x2, biasOptional, output), ACLNN_ERR_PARAM_INVALID);
     }
     // 5. 检查输入的数据格式是否为ND
     CHECK_RET(CheckFormat(x1, x2, biasOptional, output), ACLNN_ERR_PARAM_INVALID);
