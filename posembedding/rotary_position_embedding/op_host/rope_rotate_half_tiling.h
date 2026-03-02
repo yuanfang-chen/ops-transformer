1173fa445eb21a3f5c4467c34f95fc18196e8c5a
yu-xinjie62 <yuxinjie1@huawei.com>
2026-03-02 14:49:39 +0800

    rope half mode supports 310p.
    Co-authored-by: yu-xinjie62<yuxinjie1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1396 merge feature/rope_310p into master
    
    rope half mode supports 310p.
    
    Created-by: yu-xinjie62
    Commit-by: yu-xinjie62
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    rope算子在mode = half模式下，310p下支持对齐场景运算。
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/949
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    针对算子泛化全场景，设计5000个泛化case，已全部验证通过。
    原冒烟case也均通过：
    ![image.png](https://raw.gitcode.com/user-images/assets/7673863/0cb75b93-adf5-4561-bcc1-3aa1cf45a7fb/image.png 'image.png')
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!1396

--- posembedding/rotary_position_embedding/op_host/rope_rotate_half_tiling.h
+++ posembedding/rotary_position_embedding/op_host/rope_rotate_half_tiling.h
@@ -34,6 +34,7 @@ protected:
     bool IsCapable() override
     {
         if ((socVersion_ == platform_ascendc::SocVersion::ASCEND910B ||
+             socVersion_ == platform_ascendc::SocVersion::ASCEND310P ||
              socVersion_ == platform_ascendc::SocVersion::KIRINX90 ||
              socVersion_ == platform_ascendc::SocVersion::KIRIN9030) && inputMode_ != MODE_ROTATE_INTERLEAVED) {
             return true;
