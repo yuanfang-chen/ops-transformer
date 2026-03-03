077967210db04d7e9024c775336a58b57daf5199
h00808442 <hanxiaobin3@huawei.com>
2026-03-03 11:49:13 +0800

    add dump tensor

--- attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_rescale_o.hpp
+++ attention/rain_fusion_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_rescale_o.hpp
@@ -321,6 +321,7 @@ public:
                     AscendC::SetFlag<AscendC::HardEvent::V_MTE3>(EVENT_ID4);
                     AscendC::WaitFlag<AscendC::HardEvent::V_MTE3>(EVENT_ID4);
                     if (qNThisSubBlock == 0U) { // 不切头
+                        AscendC::DumpTensor(tvUbTensor, 0, totalRowNum);
                         AscendC::DataCopyPad(
                             gLse, tvUbTensor, // 源是vec，一共totalRowNum个数据块，每次取sizeof(float)长度，因为每个datablock是重复的8个数，目的排布是T（BS）N，拷贝到每个S上，头尾间隔是head-1
                             AscendC::DataCopyExtParams(totalRowNum, sizeof(float), 0, (qHeads - 1) * sizeof(float), 0)); // todo 要区分BNSD？BNSD目的就是相邻的 可以按qHeads = 1
@@ -331,6 +332,7 @@ public:
                                 tvUbTensor[qNIdx * qSBlockSize * FLOAT_BLOCK_SIZE],
                                 AscendC::DataCopyExtParams(
                                     qSBlockSize, sizeof(float), 0, (qHeads - 1) * sizeof(float), 0));
+                                AscendC::DumpTensor(tvUbTensor[qNIdx * qSBlockSize * FLOAT_BLOCK_SIZE], 1, 10);
                         }
                     }
                     // AscendC::SetFlag<AscendC::HardEvent::MTE3_V>(EVENT_ID4);
