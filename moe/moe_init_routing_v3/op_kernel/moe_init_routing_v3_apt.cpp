c9b60c76ebba8dc80ee6ac6a9f95deffd6686ec1
youngcao119 <caoyang119@huawei.com>
2026-03-03 14:49:48 +0800

    Add the mxfp4 quant feature for the MoeInitRoutingV3 Op.

--- moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
+++ moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
@@ -24,6 +24,7 @@
 #include "arch35/moe_v3_gather_hif8_pertensor_quant.h"
 #include "arch35/moe_v3_gather_hif8_pertoken_quant.h"
 #include "arch35/moe_v3_gather_hif8_quant.h"
+#include "arch35/moe_v3_gather_mxfp4_quant.h"
 
 /*
  * 非量化
@@ -72,6 +73,14 @@
 #define MOE_INIT_ROUTING_V3_SORTONECORE_HIF8_PERTOKEN_QUANT_SCATTER 1091000   // 单核排序、HIF8 PENTEOKEN量化、SCATTER索引
 #define MOE_INIT_ROUTING_V3_SORTMULTICORE_HIF8_PERTOKEN_QUANT_GATHER 1190000  // 多核排序、HIF8 PENTEOKEN量化、GATHER索引
 #define MOE_INIT_ROUTING_V3_SORTMULTICORE_HIF8_PERTOKEN_QUANT_SCATTER 1191000 // 多核排序、HIF8 PENTEOKEN量化、SCATTER索引
+
+/*
+ * MXFP4量化
+ */
+#define MOE_INIT_ROUTING_V3_SORTONECORE_MXFP4QUANT_GATHER 9010000    // 单核排序、MXFP4量化、GATHER索引
+#define MOE_INIT_ROUTING_V3_SORTONECORE_MXFP4QUANT_SCATTER 9011000   // 单核排序、MXFP4量化、SCATTER索引
+#define MOE_INIT_ROUTING_V3_SORTMULTICORE_MXFP4QUANT_GATHER 9110000  // 多核排序、MXFP4量化、GATHER索引
+#define MOE_INIT_ROUTING_V3_SORTMULTICORE_MXFP4QUANT_SCATTER 9111000 // 多核排序、MXFP4量化、SCATTER索引
 
 using namespace AscendC;
 using namespace MoeInitRoutingV3;
@@ -236,6 +245,19 @@ extern "C" __global__ __aicore__ void moe_init_routing_v3(GM_ADDR x, GM_ADDR exp
             MoeGatherOutHif8PertokenQuant<DTYPE_X> gatherHif8PerTokenQuantOp;
             gatherHif8PerTokenQuantOp.Init(x, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
             gatherHif8PerTokenQuantOp.Process();
+            gatherPipe.Destroy();
+        }
+    } else if (TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTONECORE_MXFP4QUANT_GATHER) ||
+               TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTONECORE_MXFP4QUANT_SCATTER) ||
+               TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_MXFP4QUANT_GATHER) ||
+               TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_MXFP4QUANT_SCATTER)) {
+        // MXFP4量化
+        if constexpr ((IsSameType<DTYPE_X, bfloat16_t>::value || IsSameType<DTYPE_X, half>::value || IsSameType<DTYPE_X, float>::value) && 
+            (IsSameType<DTYPE_EXPANDED_X, fp4x2_e2m1_t>::value)) {
+            TPipe gatherPipe;
+            MoeGatherOutMxfp4Quant<DTYPE_X, DTYPE_EXPANDED_X> gatherMxfp8QuantOp;
+            gatherMxfp4QuantOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
+            gatherMxfp4QuantOp.Process();
             gatherPipe.Destroy();
         }
     }
