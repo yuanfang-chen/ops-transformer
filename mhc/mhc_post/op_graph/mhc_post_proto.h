32afd89f8841cf694018bf06df4bb3477e592e58
wuyewei <wuyewei@huawei.com>
2026-03-03 15:29:23 +0800

    新增MhcPost算子

---
+++ mhc/mhc_post/op_graph/mhc_post_proto.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/**
+* @brief Fuse the main branch feature h_out with the residual branch feature x 
+* using the gating mechanism h_post and the doubly stochastic matrix  h_res 
+* to enable information flow.
+* @par Inputs:
+* @li x: A Tensor. Type is:BFloat16 or Float16.
+* @li h_res: A Tensor. Type is:Float32.
+* @li h_out: A Tensor. Type is:BFloat16 or Float16.
+* @li h_post: A Tensor. Type is:Float32.
+* @par Outputs:
+* @li y: A Tensor. Type is:BFloat16 or Float16.
+*/
+REG_OP(MhcPost)
+    .INPUT(x, TensorType({DT_FLOAT16, DT_BF16}))
+    .INPUT(h_res, TensorType({DT_FLOAT}))
+    .INPUT(h_out, TensorType({DT_FLOAT16, DT_BF16}))
+    .INPUT(h_post, TensorType({DT_FLOAT}))
+    .OUTPUT(y, TensorType({DT_FLOAT16, DT_BF16}))
+    .OP_END_FACTORY_REG(MhcPost)
---
+++ mhc/mhc_post/op_kernel/arch35/mhc_post_tiling_key.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file mhc_post_tiling_key.h
+ * \brief mhc_post tiling key declare
+ */
+
+#ifndef __MHC_POST_TILING_KEY_H__
+#define __MHC_POST_TILING_KEY_H__
+
+#include "ascendc/host_api/tiling/template_argument.h"
+
+ASCENDC_TPL_ARGS_DECL(MhcPost,
+    ASCENDC_TPL_BOOL_DECL(USE_PERMANENT_X, 0, 1)
+);
+
+ASCENDC_TPL_SEL(
+    ASCENDC_TPL_ARGS_SEL(
+        ASCENDC_TPL_KERNEL_TYPE_SEL(ASCENDC_TPL_AIV_ONLY),
+        ASCENDC_TPL_BOOL_SEL(USE_PERMANENT_X, 0, 1)
+    )
+);
+
+#endif
