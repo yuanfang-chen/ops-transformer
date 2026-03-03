09db075ebe68fa107070cb900a3dbe50a5c6a039
hello_simida <wangyi206@huawei.com>
2026-03-03 19:12:22 +0800

    common目录调整-bugfix

--- tests/ut/framework_normal/common/mc2_hcom_topology_mocker.cpp
+++ tests/ut/framework_normal/common/mc2_hcom_topology_mocker.cpp
@@ -48,7 +48,7 @@ void MC2HcomTopologyMocker::Reset()
     mockValue_.clear();
 }
 
-// mock mc2/common/inc/mc2_hcom_topo_info.h ----------------------------------------------------------------------------
+// mock mc2/op_tiling/mc2_hcom_topo_info.h ----------------------------------------------------------------------------
 constexpr static uint64_t DEFAULT_RANK_NUM = 8;
 constexpr static uint64_t DEFAULT_CCL_BUFFER_SIZE = 6000ULL * 1024ULL * 1024ULL;
 
