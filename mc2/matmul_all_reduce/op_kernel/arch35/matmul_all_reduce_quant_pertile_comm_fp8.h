11eb41cb2f8610a3e6efb0e82621633bf83502ae
yangyajie <yangyajie3@huawei.com>
2026-03-03 15:46:12 +0800

    kernel侧完成

--- mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_quant_pertile_comm_fp8.h
+++ mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_quant_pertile_comm_fp8.h
@@ -126,7 +126,7 @@ __aicore__ inline void MatmulAllReduceQuantPertileCommFp8<XType, WType, YType, M
     OOMInit(context);
     hccl_.InitV2(GetHcclContext<0>(), tilingData);
     hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTiling));
-    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTilingCommQuant));
+    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTilingComm));
     tilingData_ = tilingData;
     rankNum_ = tilingData_->param.rankDim;
     tPipe_ = tPipe;
