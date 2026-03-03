1091124ef0d68775c2fe6ab77d51dee85122fc48
h00808442 <hanxiaobin3@huawei.com>
2026-03-03 20:37:33 +0800

    fix

--- attention/rain_fusion_attention/op_host/rain_fusion_attention_infershape.cpp
+++ attention/rain_fusion_attention/op_host/rain_fusion_attention_infershape.cpp
@@ -201,8 +201,21 @@ static ge::graphStatus InferShapeRainFusionAttention(gert::InferShapeContext *co
     return ge::GRAPH_SUCCESS;
 }
 
+ ge::graphStatus InferDataTypeBlockSparseAttention(gert::InferDataTypeContext *context)
+{
+    if (context == nullptr) {
+        return ge::GRAPH_FAILED;
+    }
+    auto dtype = context->GetInputDataType(0);
+    context->SetOutputDataType(0, dtype);
+    context->SetOutputDataType(1, DT_FLOAT);
+
+    return GRAPH_SUCCESS;
+}
+
 IMPL_OP_INFERSHAPE(RainFusionAttention)
     .InferShape(InferShapeRainFusionAttention);
+    .InferDataType(InferDataTypeBlockSparseAttention);
 
 }  // namespace ops
 
