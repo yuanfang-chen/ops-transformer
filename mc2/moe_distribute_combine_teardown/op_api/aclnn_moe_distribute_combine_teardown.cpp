19b90108b16573dbe17654d36cdb8987261e1373
hello_simida <wangyi206@huawei.com>
2026-03-04 11:32:09 +0800

    common目录调整-bugfix

--- mc2/moe_distribute_combine_teardown/op_api/aclnn_moe_distribute_combine_teardown.cpp
+++ mc2/moe_distribute_combine_teardown/op_api/aclnn_moe_distribute_combine_teardown.cpp
@@ -15,7 +15,7 @@
 #include "aclnn_moe_distribute_combine_teardown.h"
 #include <algorithm>
 #include "common/op_tiling/op_mc2.h"
-#include "op_tiling/matmul_util.h"
+#include "common/op_tiling/matmul_util.h"
 #include "common/op_tiling/op_mc2_def.h"
 #include "aclnn_kernels/common/op_error_check.h"
 #include "opdev/op_log.h"
