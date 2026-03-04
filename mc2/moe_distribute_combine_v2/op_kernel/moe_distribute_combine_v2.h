7b49a570cff83a749fa74c57220673d7a305f2f7
hello_simida <wangyi206@huawei.com>
2026-03-04 14:23:28 +0800

    common目录调整-bugfix

--- mc2/moe_distribute_combine_v2/op_kernel/moe_distribute_combine_v2.h
+++ mc2/moe_distribute_combine_v2/op_kernel/moe_distribute_combine_v2.h
@@ -24,7 +24,7 @@
 #include "kernel_tiling/kernel_tiling.h"
 #include "moe_distribute_combine_v2_tiling.h"
 #include "moe_distribute_combine_v2_quant.h"
-#include "../common/inc/kernel/mc2_moe_context.h"
+#include "../common/op_kernel/mc2_moe_context.h"
 #if __has_include("../moe_distribute_dispatch_v2/check_winsize.h")
 #include "../moe_distribute_dispatch_v2/moe_distribute_v2_constant.h"
 #include "../common/op_kernel/moe_distribute_base.h"
