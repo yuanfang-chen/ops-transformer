f9474e28e75d881b3cc1c2cfe5beb779aa8d24a6
PerrySkywalker <wangmingkang1@huawei.com>
2026-03-04 15:11:17 +0800

    fix bug

--- attention/common/op_kernel/arch32/fia_block_vec_flashdecode.h
+++ attention/common/op_kernel/arch32/fia_block_vec_flashdecode.h
@@ -542,7 +542,7 @@ void FiaBlockVecFlashDecode<FIAT>::ReduceFinalRes(LocalTensor<T> &reduceOut,
     LocalTensor<T> tmpRes =
         cntKV == 0 ? reduceOut : mm2Res; // 第一次mul结果直接写入reduceOut，否则在mm2Res原地进行mul，再加到reduceOut
 
-    fa_base_vector::RowMuls(tmpRes, mm2Res, lseLocal[cntKV * dealRowCountAlign], dealRowCount, constInfo.headDimAlign, constInfo.headDim);
+    fa_base_vector::VecRowMulsForBigRowCount(tmpRes, mm2Res, lseLocal[cntKV * dealRowCountAlign], dealRowCount, constInfo.headDimAlign, constInfo.headDim);
 
     if (cntKV != 0) {
         AscendC::PipeBarrier<PIPE_V>();
