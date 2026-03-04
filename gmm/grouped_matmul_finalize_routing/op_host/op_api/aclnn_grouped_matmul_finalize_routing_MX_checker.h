b78e274e3d30f8d76cc76407da043dc9bd56d645
tooslow <zhangzhizhuo1@huawei.com>
2026-03-04 14:41:28 +0800

    加个提交节点

--- gmm/grouped_matmul_finalize_routing/op_host/op_api/aclnn_grouped_matmul_finalize_routing_MX_checker.h
+++ gmm/grouped_matmul_finalize_routing/op_host/op_api/aclnn_grouped_matmul_finalize_routing_MX_checker.h
@@ -77,17 +77,22 @@ public:
             return ACLNN_ERR_PARAM_INVALID;
         }
         // 1. 检查参数是否为空指针
+        OP_LOGI("ZZZtestlog4");
         CHECK_RET(CheckNotNull() == ACLNN_SUCCESS, ACLNN_ERR_PARAM_NULLPTR);
         // 2. 检查输入的数据类型是否在支持的数据类型范围之内
+        OP_LOGI("ZZZtestlog5");
         CHECK_RET(CheckDtypeValid(), ACLNN_ERR_PARAM_INVALID);
         // 3. 校验输入、输出参数维度
+        OP_LOGI("ZZZtestlog6");
         CHECK_RET(CheckInputOutDims() == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
         // 4. 校验输入、输出shape参数
+        OP_LOGI("ZZZtestlog8");
         CHECK_RET(CheckInputOutShape(), ACLNN_ERR_PARAM_INVALID);
         // 5. 校验输入、输出shape参数针对MXFP4
         if (CheckType(gmmParams_.x1->GetDataType(), X_WEIGHT_TYPE_SUPPORT_LIST_FP4)) {
             CHECK_RET(CheckInputOutShapeForMXFP4(), ACLNN_ERR_PARAM_INVALID);
         }
+        OP_LOGI("ZZZtestlog9");
         // 6. 检查数据形状是否支持
         CHECK_RET(CheckFormat(), ACLNN_ERR_PARAM_INVALID);
         return ACLNN_SUCCESS;
@@ -116,7 +121,6 @@ public:
     {
         auto xDimNumber = gmmParams_.x1->GetViewShape().GetDimNum();
         auto wDimNumber = gmmParams_.x2->GetViewShape().GetDimNum();
-        auto xScaleDimNumber = gmmParams_.pertokenScaleOptional->GetViewShape().GetDimNum();
         auto wScaleDimNumber = gmmParams_.scale->GetViewShape().GetDimNum();
         auto grouplistDimNumber = gmmParams_.groupList->GetViewShape().GetDimNum();
         auto logitDimNumber = gmmParams_.logit->GetViewShape().GetDimNum();
@@ -140,6 +144,7 @@ public:
         CHECK_COND(outDimNumber == TWO_DIM, ACLNN_ERR_PARAM_INVALID,
                    "The dim num of out should be equal 1, current dim is %lu.", outDimNumber);
         if (gmmParams_.pertokenScaleOptional != nullptr) {
+            auto xScaleDimNumber = gmmParams_.pertokenScaleOptional->GetViewShape().GetDimNum();
             CHECK_COND(xScaleDimNumber == xscaleExpectDim, ACLNN_ERR_PARAM_INVALID,
                        "The dim num of pertokenscale should be equal %lu, current dim is %lu.", xscaleExpectDim,
                        xScaleDimNumber);
@@ -354,7 +359,8 @@ public:
     bool CheckFormat()
     {
         if (op::IsPrivateFormat(gmmParams_.x1->GetStorageFormat()) ||
-            op::IsPrivateFormat(gmmParams_.pertokenScaleOptional->GetStorageFormat())) {
+            (gmmParams_.gmmParams_.pertokenScaleOptional != nullptr &&
+             op::IsPrivateFormat(gmmParams_.pertokenScaleOptional->GetStorageFormat()))) {
             OP_LOGE(ACLNN_ERR_PARAM_INVALID, "x and pertokenScaleOptional must be ND format, but got: %s, %s.",
                     op::ToString(gmmParams_.x1->GetStorageFormat()).GetString(),
                     op::ToString(gmmParams_.pertokenScaleOptional->GetStorageFormat()).GetString());
