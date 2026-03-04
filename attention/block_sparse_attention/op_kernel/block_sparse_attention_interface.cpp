2deafe2684f4525d8fb811fd65603c2290127b97
clee11 <chenlong238@huawei.com>
2026-03-04 21:58:12 +0800

    support block sparse attention operator
    Co-authored-by: clee11<chenlong238@huawei.com>
    Co-authored-by: h00808442<hanxiaobin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2235 merge master into master
    
    support block sparse attention operator
    
    Created-by: clee11
    Commit-by: clee11;h00808442
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    Transformer增加稀疏Attention算子
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    [#1045](https://gitcode.com/cann/ops-transformer/issues/1045) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!2235

---
+++ attention/block_sparse_attention/op_kernel/block_sparse_attention_interface.cpp
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file block_sparse_attention_interface.cpp
+ * \brief Block Sparse Attention Interface
+ */
+ #include "kernel_operator.h"
+ #include "block_sparse_attention_kernel.h"
+
+ using namespace NpuArch;
+ 
+ namespace BlockSparse {
+    // Block Sparse Attention Infer Interface
+    // This template provides the entry point for block sparse attention kernels
+    // where attention is computed only on selected KV blocks based on selectIdx
+    template <
+       typename InputDtype = half,
+       typename SoftmaxDtype = float,
+       Epilogue::LseMode lseMode = Epilogue::LseMode::NONE,
+       uint32_t QueryLayout = 0,      // 0=TND, 1=BNSD
+       uint32_t KvCacheLayout = 0>    // 0=TND, 1=BNSD
+   __global__ __aicore__ void BlockSparseAttentionInfer(
+        GM_ADDR q,
+        GM_ADDR k,
+        GM_ADDR v,
+        GM_ADDR blockSparseMask,
+        GM_ADDR mask,
+        GM_ADDR blockTables,
+        GM_ADDR o,
+        GM_ADDR actualQseqlen,
+        GM_ADDR actualKvseqlen,
+        GM_ADDR blockShape,
+        GM_ADDR workspace,                // S matrix (QK^T result)
+        GM_ADDR lse,
+        GM_ADDR tiling)
+    {
+        using ArchTag = Arch::AtlasA2;
+        using ElementQ = InputDtype;
+        using LayoutQ = layout::RowMajor;
+        using ElementK = InputDtype;
+        using LayoutK = layout::ColumnMajor;
+        using ElementV = InputDtype;
+        using LayoutV = layout::RowMajor;
+        using ElementS = SoftmaxDtype;
+        using LayoutS = layout::RowMajor;
+        using ElementP = InputDtype;
+        using LayoutP = layout::RowMajor;
+        using ElementO = InputDtype;
+        using LayoutO = layout::RowMajor;
+        using ElementLse = float; 
+        using LayoutLse = layout::RowMajor;
+        using ElementMask = int8_t;
+        using LayoutMask = layout::RowMajor;
+        using ElementOTmp = SoftmaxDtype;
+        using LayoutOTmp = layout::RowMajor;
+        using ElementUpdate = SoftmaxDtype;
+        using LayoutUpdate = layout::RowMajor;
+
+        // Use sparse-specific dispatch policies
+        using L1TileShapeQK = GemmShape<Q_TILE_CEIL, 128, 128>;
+        using L0TileShapeQK = GemmShape<128, 128, 128>;
+        using DispatchPolicyQK = Gemm::MmadAtlasA2SFAIQK<false, false>;
+        using QType = Gemm::GemmType<ElementQ, LayoutQ>;
+        using KType = Gemm::GemmType<ElementK, LayoutK>;
+        using SType = Gemm::GemmType<ElementS, LayoutS>;
+        using BlockMmadQK = Gemm::Block::BlockMmad<DispatchPolicyQK, L1TileShapeQK, L0TileShapeQK,
+                                                QType, KType, SType>;
+
+        using L1TileShapePV = GemmShape<128, 128, 256>;
+        using L0TileShapePV = GemmShape<128, 128, 128>;
+        using DispatchPolicyPV = Gemm::MmadAtlasA2SFAIPV<false, false>;
+        using PType = Gemm::GemmType<ElementP, LayoutP>;
+        using VType = Gemm::GemmType<ElementV, LayoutV>;
+        using OTmpType = Gemm::GemmType<ElementOTmp, LayoutOTmp>;
+        using BlockMmadPV = Gemm::Block::BlockMmad<DispatchPolicyPV, L1TileShapePV, L0TileShapePV,
+                                                PType, VType, OTmpType>;
+
+        // Epilogue policies for sparse attention
+        using DispatchPolicyOnlineSoftmax = Epilogue::EpilogueAtlasA2OnlineSoftmax<lseMode, SoftmaxDtype>;
+        using MaskType = Gemm::GemmType<ElementMask, LayoutMask>;
+        using EpilogueOnlineSoftmax = Epilogue::Block::BlockEpilogue<DispatchPolicyOnlineSoftmax, 
+                                                                    PType, SType, MaskType>;
+        using DispatchPolicyRescaleO = Epilogue::EpilogueAtlasA2RescaleO<lseMode, SoftmaxDtype>;
+        using OType = Gemm::GemmType<ElementO, LayoutO>;
+        using OUpdateType = Gemm::GemmType<ElementUpdate, LayoutUpdate>;
+        using LseType = Gemm::GemmType<ElementLse, LayoutLse>;
+        using EpilogueRescaleO = Epilogue::Block::BlockEpilogue<DispatchPolicyRescaleO, 
+                                                                OType, OTmpType, OUpdateType, LseType>;
+
+        // Kernel instantiation
+        using BlockSparseAttentionKernelType = BlockSparseAttentionKernel<BlockMmadQK, BlockMmadPV, 
+                                                                        EpilogueOnlineSoftmax, 
+                                                                        EpilogueRescaleO, 
+                                                                        false,           // PAGED_CACHE_FLAG
+                                                                        QueryLayout,     // QUERY_LAYOUT
+                                                                        KvCacheLayout>;  // KV_CACHE_LAYOUT
+        BlockSparseAttentionKernelParams params{q, k, v, blockSparseMask, mask, blockTables, actualQseqlen, actualKvseqlen, 
+                                    o, lse, workspace, tiling};
+
+        // Call block sparse attention kernel
+        BlockSparseAttentionKernelType blockSparseAttenInfer;
+        blockSparseAttenInfer(params);
+    }
+}
+ 
+ 
