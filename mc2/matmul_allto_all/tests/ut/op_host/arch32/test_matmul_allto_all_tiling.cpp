0283b7fe7528e6e05d09304727a45704bdaf127f
qzzzy1 <qiziyu2@huawei.com>
2026-03-04 11:02:41 +0800

    fix

--- mc2/matmul_allto_all/tests/ut/op_host/arch32/test_matmul_allto_all_tiling.cpp
+++ mc2/matmul_allto_all/tests/ut/op_host/arch32/test_matmul_allto_all_tiling.cpp
@@ -8,219 +8,64 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-/*!
- * \file test_matmul_allto_all_tiling.cpp
- * \brief tiling ut
- */
-
 #include <iostream>
-#include <fstream>
-#include <thread>
-#include <vector>
 #include <gtest/gtest.h>
-#include "tiling_context_faker.h"
-#include "tiling_case_executor.h"
+#include "../../../../op_kernel/arch32/matmul_allto_all_tiling_data_910_93.h"
+#include "mc2_tiling_case_executor.h"
 
-namespace MatmulAlltoAllA3UT {
+namespace MatmulAlltoAllUT {
 
-using namespace std;
-using namespace ge;
-using namespace gert;
-
-static const std::string OP_NAME = "MatmulAlltoAll";
-
-struct MatmulAlltoAllTestParam {
-    std::string caseName;
-    // input
-    // x1
-    std::initializer_list<int64_t> x1Shape;
-    ge::DataType x1Dtype;
-    ge::Format x1Format;
-
-    // x2
-    std::initializer_list<int64_t> x2Shape;
-    ge::DataType x2Dtype;
-    ge::Format x2Format;
-
-    // bias
-    std::initializer_list<int64_t> biasShape;
-    ge::DataType biasDtype;
-    ge::Format biasFormat;
-
-    // x1_scale
-    std::initializer_list<int64_t> x1ScaleShape;
-    ge::DataType x1ScaleDtype;
-    ge::Format x1ScaleFormat;
-
-    // x2_scale
-    std::initializer_list<int64_t> x2ScaleShape;
-    ge::DataType x2ScaleDtype;
-    ge::Format x2ScaleFormat;
-
-    // comm_scale
-    std::initializer_list<int64_t> commScaleShape;
-    ge::DataType commScaleDtype;
-    ge::Format commScaleFormat;
-
-    // x1_offset
-    std::initializer_list<int64_t> x1OffsetShape;
-    ge::DataType x1OffsetDtype;
-    ge::Format x1OffsetFormat;
-
-    // x2_offset
-    std::initializer_list<int64_t> x2OffsetShape;
-    ge::DataType x2OffsetDtype;
-    ge::Format x2OffsetFormat;
-
-    // output
-    // y
-    std::initializer_list<int64_t> yShape;
-    ge::DataType youtputDtype;
-    ge::Format youtputFormat;
-
-    // attrs
-    std::string groupAttr;
-    int64_t worldSizeAttr;
-    int64_t alltoAllAxesAttr;
-    int64_t yDtypeAttr;
-    int64_t x1QuantModeAttr;
-    int64_t x2QuantModeAttr;
-    int64_t commQuantModeAttr;
-    int64_t commQuantDtypeAttr;
-    bool transposex1Attr;
-    bool transposex2Attr;
-    int64_t groupSizeAttr;
-    // soc version
-    std::string socVersion;
-    // expert result
-    ge::graphStatus status;
-    uint64_t expectTilingKey;
-    std::string expectTilingData;
-    std::vector<size_t> expectWorkspaces;
-    uint64_t mc2TilingDataReservedLen;
-};
-
-// ut/
-// expectWorkspaces = 16 * 1024 * 1024
-// tilingDataReservedLen = 43tilingDatamc2InitTilingmc2CcTiling
-static MatmulAlltoAllTestParam g_testCases[] = {
-    // legal
-    {"matmul_alltoall_case_normal_dtype_float16",
-    {88, 128}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {128, 256}, ge::DT_FLOAT16,ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {}, ge::DT_FLOAT16, ge::FORMAT_ND,
-    {176, 128},ge::DT_FLOAT16, ge::FORMAT_ND,
-    "group", 2, 0, 0, 0, 0, 0, 0, false, false, 0,
-    "2201",
-    ge::GRAPH_SUCCESS,
-    0UL, "", {16867328}, 0},
-
-
-};
-
-// setup & teardown
-class MatmulAlltoAllA3TilingTest : public testing::TestWithParam<MatmulAlltoAllTestParam> {
+class MatmulAlltoAllA3TilingTest : public testing::Test {
 protected:
     static void SetUpTestCase()
     {
-        std::cout << "MatmulAlltoAllA3TilingTest SetUp." << std::endl;
+        std::cout << "MatmulAlltoAllA3TilingTest SetUp" << std::endl;
     }
 
     static void TearDownTestCase()
     {
-        std::cout << "MatmulAlltoAllA3TilingTest TearDown." << std::endl;
+        std::cout << "MatmulAlltoAllA3TilingTest TearDown" << std::endl;
     }
 };
 
-struct MatmulAlltoAllCompileInfo {
-} compileInfo;
-
-// ut
-static void TestOneParamCase(const MatmulAlltoAllTestParam &param)
+TEST_F(MatmulAlltoAllA3TilingTest, Float16Test1)
 {
-    std::cout << "[TEST_CASE] " << param.caseName << std::endl;
-    //
-    //  Shape  tensor  shape  gert::StorageShape
-    gert::StorageShape x1Shape = {param.x1Shape, param.x1Shape};
-    gert::StorageShape x2Shape = {param.x2Shape, param.x2Shape};
-    gert::StorageShape biasShape = {param.biasShape, param.biasShape};
-    gert::StorageShape x1ScaleShape = {param.x1ScaleShape, param.x1ScaleShape};
-    gert::StorageShape x2ScaleShape = {param.x2ScaleShape, param.x2ScaleShape};
-    gert::StorageShape commScaleShape = {param.commScaleShape, param.commScaleShape};
-    gert::StorageShape x1OffsetShape = {param.x1OffsetShape, param.x1OffsetShape};
-    gert::StorageShape x2OffsetShape = {param.x2OffsetShape, param.x2OffsetShape};
+    struct MatmulAlltoAllCompileInfo {} compileInfo;
+    uint64_t coreNum = 20;
+    void* pCompileInfo = &compileInfo;
 
-    gert::StorageShape yShape = {param.yShape, param.yShape};
-
-    //  input tensor
-    std::vector<gert::TilingContextPara::TensorDescription> inputTensorDesc_(
-        {{x1Shape, param.x1Dtype, param.x1Format},
-         {x2Shape, param.x2Dtype, param.x2Format},
-         {biasShape, param.biasDtype, param.biasFormat},
-         {x1ScaleShape, param.x1ScaleDtype, param.x1ScaleFormat},
-         {x2ScaleShape, param.x2ScaleDtype, param.x2ScaleFormat},
-         {commScaleShape, param.commScaleDtype, param.commScaleFormat},
-         {x1OffsetShape, param.x1OffsetDtype, param.x1OffsetFormat},
-         {x2OffsetShape, param.x2OffsetDtype, param.x2OffsetFormat}});
-
-    //  output tensor
-    std::vector<gert::TilingContextPara::TensorDescription> outputTensorDesc_(
-        {{yShape, param.youtputDtype, param.youtputFormat}});
-
-    //  attributes
-    std::vector<gert::TilingContextPara::OpAttr> attrs_(
-        {{"group", Ops::Transformer::AnyValue::CreateFrom<std::string>(param.groupAttr)},
-         {"world_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.worldSizeAttr)},
-         {"all2all_axes",
-          Ops::Transformer::AnyValue::CreateFrom<int64_t>(static_cast<int64_t>(param.alltoAllAxesAttr))},
-         {"y_dtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(static_cast<int64_t>(param.yDtypeAttr))},
-         {"x1_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.x1QuantModeAttr)},
-         {"x2_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.x2QuantModeAttr)},
-         {"comm_quant_mode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.commQuantModeAttr)},
-         {"comm_quant_dtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.commQuantDtypeAttr)},
-         {"transpose_x1", Ops::Transformer::AnyValue::CreateFrom<bool>(param.transposex1Attr)},
-         {"transpose_x2", Ops::Transformer::AnyValue::CreateFrom<bool>(param.transposex2Attr)},
-         {"group_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(param.groupSizeAttr)}});
-    //
-    gert::TilingContextPara tilingContextPara(OP_NAME, inputTensorDesc_, outputTensorDesc_, attrs_, &compileInfo,
-                                              param.socVersion);
-    ExecuteTestCase(tilingContextPara, param.status, param.expectTilingKey, param.expectTilingData,
-                        param.expectWorkspaces, param.mc2TilingDataReservedLen);                                          
+    gert::TilingContextPara tilingContextPara("MatmulAlltoAll",
+        {
+            {{{88, 128}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{{128, 256}},  ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_FLOAT16, ge::FORMAT_ND},
+        },
+        {
+            {{{176, 128}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+        },
+        {
+            {"group", Ops::Transformer::AnyValue::CreateFrom<std::string>("group")},
+            {"worldsize", Ops::Transformer::AnyValue::CreateFrom<int64_t>(2)},
+            {"alltoAllAxes", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"yDtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"x1QuantMode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"x2QuantMode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"commQuantMode", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"x1QuantDtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"commQuantDtype", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+            {"transposex1", Ops::Transformer::AnyValue::CreateFrom<bool>(false)},
+            {"transposex2", Ops::Transformer::AnyValue::CreateFrom<bool>(false)},
+            {"groupSize", Ops::Transformer::AnyValue::CreateFrom<int64_t>(0)},
+        },
+        pCompileInfo, "Ascend910_93", coreNum);
+    Mc2Hcom::MockValues hcomTopologyMockValues{{"rankNum", 2}};
+    uint64_t expectTilingKey = 0UL;
+    Mc2ExecuteTestCase(tilingContextPara, hcomTopologyMockValues, ge::GRAPH_SUCCESS, expectTilingKey);
 }
 
-static void ThreadFunction(const MatmulAlltoAllTestParam *testCases, size_t caseNum, size_t threadIdx, size_t threadNum)
-{
-    for (size_t idx = threadIdx; idx < caseNum; idx += threadNum) {
-        TestOneParamCase(testCases[idx]);
-    }
-}
-
-static void TestExecMultiThread(const MatmulAlltoAllTestParam *testCases, size_t testCaseNum, size_t threadNum)
-{
-    std::thread threads[threadNum];
-    for (size_t idx = 0; idx < threadNum; ++idx) {
-        threads[idx] = std::thread(ThreadFunction, testCases, testCaseNum, idx, threadNum);
-    }
-    for (size_t idx = 0; idx < threadNum; ++idx) {
-        threads[idx].join();
-    }
-}
-
-TEST_P(MatmulAlltoAllA3TilingTest, GeneralCasesTest)
-{
-    TestOneParamCase(GetParam());
-}
-
-TEST_F(MatmulAlltoAllA3TilingTest, GeneralCasesMultiThread)
-{
-    TestExecMultiThread(g_testCases, sizeof(g_testCases) / sizeof(MatmulAlltoAllTestParam), 1);
-}
-
-INSTANTIATE_TEST_CASE_P(MatmulAlltoAllTilingUT, MatmulAlltoAllA3TilingTest, testing::ValuesIn(g_testCases));
-
-} // namespace
+} // MatmulAlltoAllUT
