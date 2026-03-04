0f2353268a9f23253f6d4e8687c4d919a76728f0
SuperYuan <zhangziyuan11@huawei.com>
2026-03-04 17:07:13 +0800

    重构new_mc2_mm目录

--- mc2/common/inc/kernel/mc2_mat_mul_asw_block.h
+++ mc2/common/inc/kernel/mc2_mat_mul_asw_block.h
@@ -1,23 +1,23 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /* !
  * \file mc2_mat_mul_asw_block.h
  * \brief
  */
 
-#ifndef NEW_MC2_MAT_MUL_ASW_BLOCK_H
-#define NEW_MC2_MAT_MUL_ASW_BLOCK_H
+#ifndef MC2_MAT_MUL_ASW_BLOCK_H
+#define MC2_MAT_MUL_ASW_BLOCK_H
 
 #include "../../../3rd/mat_mul_v3/op_kernel/arch35/mat_mul_asw_block.h"
-#include "../../inc/kernel/mc2_tiling_struct.h"
+#include "mc2_tiling_struct.h"
 
 namespace MC2MatmulV3
 {
--- mc2/grouped_mat_mul_all_reduce/op_host/op_tiling/grouped_mat_mul_all_reduce_tiling.h
+++ mc2/grouped_mat_mul_all_reduce/op_host/op_tiling/grouped_mat_mul_all_reduce_tiling.h
@@ -58,6 +58,58 @@ TILING_DATA_FIELD_DEF_STRUCT(GMMAllReduceCoreTiling, aicoreTiling);
 END_TILING_DATA_DEF;
 REGISTER_TILING_DATA_CLASS(GroupedMatMulAllReduce, GMMAllReduceTilingData)
 
+BEGIN_TILING_DATA_DEF(Mc2Msg)                        // 同aicpu_hccl_def KFCTilingData
+    TILING_DATA_FIELD_DEF(uint32_t, preparePosition);  // 任务准备位置：0表示在host完成所有通信任务的准备，1表示在kernel侧完成
+    TILING_DATA_FIELD_DEF(uint64_t, sendOff);        // 发送数据地址偏移，count * dataTypeSize
+    TILING_DATA_FIELD_DEF(uint64_t, recvOff);        // 接收数据地址偏移, count * dataTypeSize
+    TILING_DATA_FIELD_DEF(uint64_t, tailSendOff);    // 尾块发送数据地址偏移，count * dataTypeSize
+    TILING_DATA_FIELD_DEF(uint64_t, tailRecvOff);    // 尾块接收数据地址偏移, count * dataTypeSize
+    TILING_DATA_FIELD_DEF(uint64_t, sendCnt);        // 整块发送数据个数
+    TILING_DATA_FIELD_DEF(uint64_t, recvCnt);        // 尾块接收数据个数
+    TILING_DATA_FIELD_DEF(uint64_t, tailSendCnt);    // 尾块发送数据个数
+    TILING_DATA_FIELD_DEF(uint64_t, tailRecvCnt);    // 尾块接收数据个数
+    TILING_DATA_FIELD_DEF(uint64_t, totalCnt);       // 总数据个数
+    TILING_DATA_FIELD_DEF(uint32_t, turnNum);        // 总轮次
+    TILING_DATA_FIELD_DEF(uint32_t, tailNum);        // 尾块的轮次
+    TILING_DATA_FIELD_DEF(uint32_t, stride);         // 跳写间隔
+    TILING_DATA_FIELD_DEF(uint32_t, workspaceOff);   // 使用workspace作为recvbuf时的workspace偏移
+    TILING_DATA_FIELD_DEF(uint32_t, notifyOff);      // device notify write/read value偏移
+
+    TILING_DATA_FIELD_DEF(uint16_t, notifyBeginCnt); // notify write value的使用个数
+    TILING_DATA_FIELD_DEF(uint16_t, notifyEndCnt);   // notify write value的使用个数
+    TILING_DATA_FIELD_DEF(uint8_t, useBufferType);    // recvBuf类型
+    TILING_DATA_FIELD_DEF(uint8_t, funID);           // function ID
+    TILING_DATA_FIELD_DEF(uint8_t, dataType);        // hccl 数据类型
+    TILING_DATA_FIELD_DEF(uint8_t, groupNum);        // groupNum
+
+    TILING_DATA_FIELD_DEF(uint8_t, reuseMode);       // 不复用填turnNum，内存优化选择复用的内存块个数
+    TILING_DATA_FIELD_DEF(uint8_t, commType);        // 通信类型
+    TILING_DATA_FIELD_DEF(uint8_t, reduceOp);        // reduce op type
+    TILING_DATA_FIELD_DEF(uint8_t, commOrder);       // 通信顺序，0表示通信在前，1表示通信在后
+    TILING_DATA_FIELD_DEF(uint8_t, waitPolicy);      // 等待任务启动的阻塞策略，2、首轮等待，1、每轮等待。
+                                                     // KFC根据此标记在主流任务前面加wait，AIC需要按策略发对应record才能触发执行
+    TILING_DATA_FIELD_DEF(uint8_t, rspPolicy);       // 任务执行结束时的响应策略， 2、最后通知一次，
+                                                     // 1、每轮通知一次。KFC根据此标记在主流任务后面加record
+    TILING_DATA_FIELD_DEF(uint8_t, exitPolicy);      // 退出策略，0，一次通信任务下发完成直接退出；1. 通信任务执行完成退出；2.
+                                                     // 等待AIC通知退出(可以多次执行任务)。
+    TILING_DATA_FIELD_DEF(uint8_t, commAlg);         // 用于指定具体通信算法。
+                                                     // 0：default, 1：fullmesh, 2：doublering, 3：switchwing
+    TILING_DATA_FIELD_DEF(uint8_t, taskType);        // 从参数获取通信任务，直接下发。AIC自己发Record激活
+    TILING_DATA_FIELD_DEF(uint8_t, debugMode);       // 调测模式
+                                                     // 1:单独执行CUBE
+                                                     // 2:单独执行Vector
+                                                     // 4:单独执行AICPU KFC算子
+                                                     // 8:KFC等待通信结束
+                                                     // 16:KFC统计各阶段耗时
+    TILING_DATA_FIELD_DEF(uint8_t, stepSize);        // 用于指定通算频率步长
+    TILING_DATA_FIELD_DEF(uint8_t, sendArgIndex);    // 发送数据参数索引，对应算子原型的参数顺序
+    TILING_DATA_FIELD_DEF(uint8_t, recvArgIndex);    // 接收数据参数索引，对应算子原型的参数顺序
+    TILING_DATA_FIELD_DEF(uint8_t, commOutArgIndex); // 通信输出参数索引，对应算子原型的参数顺序
+    TILING_DATA_FIELD_DEF(uint8_t, hasCommOut);      // 是否有通信输出
+    TILING_DATA_FIELD_DEF(uint8_t, reserve);
+    TILING_DATA_FIELD_DEF(uint32_t, reserve2);
+END_TILING_DATA_DEF;
+REGISTER_TILING_DATA_CLASS(Mc2MsgOp, Mc2Msg)
 } // namespace optiling
 
 #endif // AIR_CXX_RUNTIME_V2_OP_IMPL_GROUPED_MATMUL_ALL_REDUCE_H
