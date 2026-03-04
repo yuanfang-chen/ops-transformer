cf9c09f799765cc9696176d8dacc9ba2d438eb89
m30040597 <mgl1984@163.com>
2026-03-04 15:17:11 +0800

    202603041517 comparecompile

--- gmm/grouped_matmul/op_kernel/gmm_infra/detail/alignment.hpp
+++ gmm/grouped_matmul/op_kernel/gmm_infra/detail/alignment.hpp
@@ -26,7 +26,7 @@ template <class T, class U>
 CATLASS_HOST_DEVICE
 constexpr auto RoundUp(T const &val, U const &align)
 {
-	static_assert(align != static_cast<U>(0), "align must not be 0");
+	static_assert(static_cast<U>(align) != 0, "align must not be 0");
     return (val + align - 1) / align * align;
 }
 
@@ -42,7 +42,7 @@ template <class T, class U>
 CATLASS_HOST_DEVICE
 constexpr auto RoundDown(T const &val, U const &align)
 {
-	static_assert(align != static_cast<U>(0), "align must not be 0");
+	static_assert(static_cast<U>(align) != 0, "align must not be 0");
     return val / align * align;
 }
 
@@ -58,7 +58,7 @@ template <class T, class U>
 CATLASS_HOST_DEVICE
 constexpr auto CeilDiv(T const &dividend, U const &divisor)
 {
-	static_assert(divisor != static_cast<U>(0), "divisor must not be 0");
+	static_assert(static_cast<U>(divisor) != 0, "divisor must not be 0");
     return (dividend + divisor - 1) / divisor;
 }
 
