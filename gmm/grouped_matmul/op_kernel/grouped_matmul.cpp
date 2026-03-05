88f68a1c0a7976a9334cb096b2480c79ccc22585
m30040597 <mgl1984@163.com>
2026-03-05 17:16:27 +0800

    202603051716

--- gmm/grouped_matmul/op_kernel/grouped_matmul.cpp
+++ gmm/grouped_matmul/op_kernel/grouped_matmul.cpp
@@ -34,6 +34,7 @@
 #include "a16w4_msd/grouped_matmul_weight_quant_a16w4_msd_controller.h"
 #ifndef __CCE_KT_TEST__
 #include "grouped_matmul_fixaxismove_interface.cpp"
+#include "grouped_matmul_a4w4_interface.cpp"
 #endif
 #endif
 
@@ -484,7 +485,26 @@ __global__ __aicore__ void grouped_matmul(GM_ADDR x, GM_ADDR weight, GM_ADDR bia
     // QUANT_A4W4
     if constexpr (D_T_A == GMM_TPL_INT4 && D_T_B == GMM_TPL_INT4) {
         if constexpr (TRANS_B == 0){
-            GMM_A4W4_IMP(GMMA4W4Compute, false, false, matmulCFG, xType, weightType, yType);
+            GET_TILING_DATA_MEMBER(GMMTilingData, gmmBaseParams, gmmBaseParams_, tiling);
+            if (gmmBaseParams_.isA4W4Optimize) {
+                tPipe.Destroy();
+                AscendC::SetMMLayoutTransform(true);
+                using XDType = AscendC::int4b_t;
+                using WeightDType = AscendC::int4b_t;
+                using CDType = half;
+                using ScaleDType = uint64_t;
+                using GrouplistDType = int64_t;
+                using PerTokenScaleDType = float;
+                using YDType = bfloat16_t;
+#ifndef __CCE_KT_TEST__
+                CatlassA4W4::grouped_matmul_a4w4_catlass<XDType, WeightDType, CDType, ScaleDType, GrouplistDType, PerTokenScaleDType, YDType>(
+                    gmmBaseParams_.m, gmmBaseParams_.k, gmmBaseParams_.n, gmmBaseParams_.groupNum, gmmBaseParams_.quantGroupNum,
+                    x, weight, scale, groupList, perTokenScale, y, user1, gmmBaseParams_.coreNum);
+#endif
+                AscendC::SetMMLayoutTransform(false);
+            } else {
+                GMM_A4W4_IMP(GMMA4W4Compute, false, false, matmulCFG, xType, weightType, yType);
+            }
         }else{
             GMM_A4W4_IMP(GMMA4W4Compute, false, true, matmulCFG, xType, weightType, yType);
         }
