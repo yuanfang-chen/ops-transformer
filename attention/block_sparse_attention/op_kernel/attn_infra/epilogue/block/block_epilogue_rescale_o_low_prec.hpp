c1eb15c834d9d92dfabc3868a8c950939c6798b9
h00808442 <hanxiaobin3@huawei.com>
2026-03-05 18:59:55 +0800

    add low prec dump tensor

--- attention/block_sparse_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_rescale_o_low_prec.hpp
+++ attention/block_sparse_attention/op_kernel/attn_infra/epilogue/block/block_epilogue_rescale_o_low_prec.hpp
@@ -316,10 +316,12 @@ public:
                     AscendC::WaitFlag<AscendC::HardEvent::V_MTE3>(EVENT_ID4);
                     
                     if (qNThisSubBlock == 0U) {
+                        AscendC::DumpTensor(tvUbTensor, 0, totalRowNum);
                         AscendC::DataCopyPad(
                             gLse, tvUbTensor32,
                             AscendC::DataCopyExtParams(
                                 totalRowNum, sizeof(float), 0, (qHeads - 1) * sizeof(float), 0));
+                        AscendC::DumpTensor(gLse, 333, totalRowNum);
                     } else {
                         for (uint32_t qNIdx = 0; qNIdx < qNThisSubBlock; qNIdx++) {
                             AscendC::DataCopyPad(
