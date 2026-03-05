19080eb76f26f2a8e60f52b6342e417f741b9e1d
j60100428 <jingsong5@h-partners.com>
2026-03-05 21:30:47 +0800

    fix: clean code

--- attention/prompt_flash_attention/op_kernel/arch35/prompt_flash_attention_entry_regbase.h
+++ attention/prompt_flash_attention/op_kernel/arch35/prompt_flash_attention_entry_regbase.h
@@ -34,8 +34,8 @@ using namespace regbaseutil;
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-#define REGBASE_COPY_TILING_DATA_ASCEND950_KVSAME_BASEAPI(tiling)                                                    \
-    GET_TILING_DATA_WITH_STRUCT(FlashAttentionScoreSimplifiedTilingData, tilingDataIn, tiling);                           \
+#define REGBASE_COPY_TILING_DATA_ASCEND950_KVSAME_BASEAPI(tiling)                                           \
+    GET_TILING_DATA_WITH_STRUCT(FlashAttentionScoreSimplifiedTilingData, tilingDataIn, tiling);         \
     const FlashAttentionScoreSimplifiedTilingData *__restrict tilingData = &tilingDataIn
 
 #define INVOKE_FA_OP_IMPL_ASCEND950_KVSAME_BASEAPI(templateClass, ...)                                               \
