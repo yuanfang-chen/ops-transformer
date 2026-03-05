ae5c2f3f31afadd2dc2c3c86467c076a10d22ae2
gaozheng <gaozheng16@huawei.com>
2026-03-05 11:39:42 +0800

    ops-transfomer clean code 330

--- gmm/grouped_matmul_finalize_routing/examples/arch35/test_aclnn_grouped_matmul_finalize_routing_weightnz.cpp
+++ gmm/grouped_matmul_finalize_routing/examples/arch35/test_aclnn_grouped_matmul_finalize_routing_weightnz.cpp
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -296,7 +305,6 @@ int main()
     aclrtFree(logitDeviceAddr);
     aclrtFree(rowIndexDeviceAddr);
     aclrtFree(outDeviceAddr);
-    //aclDestroyIntArray(tuningConfig);
 
     if (workspaceSize > 0) {
         aclrtFree(workspaceAddr);
