5adc4ee7262089039daa2bc94fcae4c64e9682a7
w00951525 <wangfuhao7@huawei.com>
2026-03-06 16:18:05 +0800

    performance test

--- mc2/matmul_allto_all/op_host/op_tiling/common/allto_all_formulaic_tiling.cpp
+++ mc2/matmul_allto_all/op_host/op_tiling/common/allto_all_formulaic_tiling.cpp
@@ -55,8 +55,15 @@ void AlltoAllMM::EstimateKernelTime()
  */
 void AlltoAllMM::SetCommTimeFactor()
 {
-    // A5上的时间因子AlltoAll暂时定义为2
-    commPerf_.ChangeCommTimeFactorByDivision(TWO); // 2x time of factor
+    if (socVersion_ == SocVersion::SOC910_93) {
+        OP_LOAD("AlltoAllMatmul, Current socVersion is SOC910_93.");
+        // A3上的时间因子AlltoAll暂时定义为4
+        commPerf_.ChangeCommTimeFactorByDivision(FOUR); // 4x time of factor
+    } else {
+        OP_LOAD("AlltoAllMatmul, Current socVersion is SOC950.");
+        // A5上的时间因子AlltoAll暂时定义为2
+        commPerf_.ChangeCommTimeFactorByDivision(TWO); // 2x time of factor
+    }
 }
 
 /**
