f046493b16201c3a896d63986be02478779783c9
Jiaqin <gaojiaqin@hisilicon.com>
2026-03-06 15:43:54 +0800

    Fix D/C v3 graph failure
    Co-authored-by: GJQ<gaojiaqin@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2338 merge moe_context into master
    
    Fix D/C v3 graph failure
    
    Created-by: Jiaqin
    Commit-by: GJQ
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
     在线编译失败，找不到头文件，为头文件路径与run包展开后存在差异导致
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!2338

--- mc2/moe_distribute_dispatch_v3/op_kernel/moe_distribute_dispatch_v3.cpp
+++ mc2/moe_distribute_dispatch_v3/op_kernel/moe_distribute_dispatch_v3.cpp
@@ -14,10 +14,10 @@
  */
 #include "basic_api/kernel_basic_intf.h"
 
-#if __has_include("../../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling.h")
-#include "../../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2.h"
-#include "../../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling.h"
-#include "../../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling_key.h"
+#if __has_include("../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling.h")
+#include "../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2.h"
+#include "../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling.h"
+#include "../moe_distribute_dispatch_v2/moe_distribute_dispatch_v2_tiling_key.h"
 #else
 #include "../../moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.h"
 #include "../../moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2_tiling.h"
