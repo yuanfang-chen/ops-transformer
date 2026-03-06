23ce3f0e6065c9ee28a15f31409fa1fd9d8ed65e
liujiawei-21 <liujiawei56@huawei.com>
2026-03-06 15:32:03 +0800

    bug fix

--- attention/common/op_kernel/arch35/infer_flash_attention_kvcache.h
+++ attention/common/op_kernel/arch35/infer_flash_attention_kvcache.h
@@ -34,8 +34,8 @@ __aicore__ inline void InitQueryLeftPaddingSize(RunParamStr<isInfer>& runParam, 
         runParam.queryLeftPaddingSize = 0;
     } else {
         int64_t qLeftPaddingSize = 0;
-        if (constInfo.isGqa) {
-            qLeftPaddingSize = constInfo.s1Size - actualS1Size - constInfo.queryRightPaddingSize;
+        if constexpr (hasRope && (dTemplateType == DTemplateType::Aligned576)) {
+            qLeftPaddingSize = constInfo.s1Size - actualS1Size / constInfo.gSize - constInfo.queryRightPaddingSize;
         } else {
             qLeftPaddingSize = constInfo.s1Size - actualS1Size - constInfo.queryRightPaddingSize;
         }
