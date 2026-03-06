96fd144e98a744001075dd5fca4b470c5306173a
hello_simida <wangyi206@huawei.com>
2026-03-06 10:45:05 +0800

    Revert "调整common目录适配-npu case fix"

--- experimental/mc2/moe_distribute_combine_shmem/op_host/op_api/aclnn_moe_distribute_combine_shmem.cpp
+++ experimental/mc2/moe_distribute_combine_shmem/op_host/op_api/aclnn_moe_distribute_combine_shmem.cpp
@@ -11,8 +11,8 @@
 
 #include "aclnn_kernels/common/op_error_check.h"
 #include "aclnn_moe_distribute_combine_shmem.h"
-#include "common/op_tiling/op_mc2.h"
-#include "common/op_tiling/op_mc2_def.h"
+#include "op_mc2.h"
+#include "op_mc2_def.h"
 #include "opdev/common_types.h"
 #include "opdev/op_log.h"
 #include "opdev/platform.h"
