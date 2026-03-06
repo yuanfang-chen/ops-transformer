9d94bbb4cec71610e8fbe0fd7e72435e3bf1cfef
OblivionZHU <zhujun116@huawei.com>
2026-03-06 15:17:59 +0800

    UT of dispatchV3 and combineV3

---
+++ mc2/moe_distribute_combine_v3/tests/ut/op_kernel/moe_distribute_combine_v3_tiling_def.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+ 
+#ifndef MOE_DISTRIBUTE_COMBINE_V3_TILING_DEF_H
+#define MOE_DISTRIBUTE_COMBINE_V3_TILING_DEF_H
+
+#include "kernel_tiling/kernel_tiling.h"
+#include "../../../../moe_distribute_combine_v2/op_kernel/moe_distribute_combine_v2_tiling.h"
+
+#define GET_TILING_DATA(tilingData, tilingArg)                                                        \
+    MoeDistributeCombineV2TilingData tilingData;                                                       \
+    memcpy(&tilingData, tilingArg, sizeof(MoeDistributeCombineV2TilingData))
+
+#define GET_TILING_DATA_WITH_STRUCT(TilingDataStru, tilingData, tilingArg)       \
+    TilingDataStru tilingData;                                                 \
+    memcpy(&tilingData, tilingArg, sizeof(TilingDataStru))
+
+#endif
