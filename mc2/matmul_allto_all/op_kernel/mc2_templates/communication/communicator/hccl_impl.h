51c2c5d5a029535b3597f8488704b8454ecd1af5
qq_43844249 <fanglin17@huawei.com>
2026-03-06 16:14:19 +0800

    fix bug:big shape sync fail.

--- mc2/matmul_allto_all/op_kernel/mc2_templates/communication/communicator/hccl_impl.h
+++ mc2/matmul_allto_all/op_kernel/mc2_templates/communication/communicator/hccl_impl.h
@@ -55,7 +55,7 @@ private:
     uint32_t endIndex_ = 0;
     bool notifyFlag_ = false;
     Communicationtype communicationType_ = COMMUNICATION_WAIT_ONE;
-    static constexpr uint8_t MAX_HCCL_HANDLE_ = 16;
+    static constexpr uint8_t MAX_HCCL_HANDLE_ = 63;
     AscendC::HcclHandle hTasks_[MAX_HCCL_HANDLE_]; //hccl只支持最多16个任务并行
     bool taskSuccess_[MAX_HCCL_HANDLE_];
 };
