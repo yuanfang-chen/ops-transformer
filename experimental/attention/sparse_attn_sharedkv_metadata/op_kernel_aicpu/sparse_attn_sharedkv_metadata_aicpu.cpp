b0846a1d607f9ddc7b908a4519617e6d2879b30b
qq_32807861 <handongchen2@huawei.com>
2026-03-06 07:15:57 +0000

    sas metadata适配N1=128

--- experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.cpp
+++ experimental/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.cpp
@@ -97,8 +97,8 @@ bool SparseAttnSharedkvMetadataCpuKernel::CheckSingleParam()
         return false;
     }
     // num_heads_q 校验
-    if (queryHeadNum_ != 64) {
-        KERNEL_LOG_ERROR("num_heads_q should only be 64, but got %d", queryHeadNum_);
+    if (queryHeadNum_ > 128) {
+        KERNEL_LOG_ERROR("num_heads_q should not be greater than 128, but got %d", queryHeadNum_);
         return false;
     }
     // num_heads_kv 校验
@@ -243,6 +243,9 @@ bool SparseAttnSharedkvMetadataCpuKernel::ParamsInit()
     attentionMode_ = 1;
     isS1G_ = (layoutQuery_ == "BSND" || layoutQuery_ == "BSH" || layoutQuery_ == "TND");
     groupSize_ = queryHeadNum_ / kvHeadNum_;
+    if (queryHeadNum_ == 128) {
+        isN128 = true;
+    }
     if (hasCmpKv_) {
         if (cmpTopK_ > 0) {
             isSCFA = true;
@@ -260,7 +263,13 @@ bool SparseAttnSharedkvMetadataCpuKernel::ParamsInit()
         mBaseSize_ = groupSize_ * s1BlockLen;
         s2BaseSize_ = 512U;
     } else if (validSocVersion == ValidSocVersion::ASCEND950){
-        mBaseSize_ = 64U;
+        if (isN128) {
+            mBaseSize_ = groupSize_;
+            aicCoreNum_ /= 2;
+            aivCoreNum_ /= 2;
+    	} else {
+            mBaseSize_ = 64U;
+        }
         s2BaseSize_ = 128U;
     }
     return true;
@@ -967,22 +976,53 @@ bool SparseAttnSharedkvMetadataCpuKernel::BalanceSchedule(SplitResult &splitRes)
 bool SparseAttnSharedkvMetadataCpuKernel::GenMetaData(SplitResult &splitRes) {
     optiling::detail::SasMetaData* metaDataPtr = (optiling::detail::SasMetaData*)metaData_->GetData();
     // FA Metadata Generate
-    for (size_t i = 0; i < aicCoreNum_; ++i) {
-        if (i >= splitRes.usedCoreNum) {
-            metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
-            continue;
+    if (isN128) {
+        for (size_t i = 0; i < aicCoreNum_; i++) {
+            if (i >= splitRes.usedCoreNum) {
+                metaDataPtr->faMetadata[2 * i][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
+                metaDataPtr->faMetadata[2 * i + 1][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
+                continue;
+            }
+            metaDataPtr->faMetadata[2 * i][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
+            metaDataPtr->faMetadata[2 * i + 1][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
+            // FA START
+            metaDataPtr->faMetadata[2 * i][FA_BN2_START_INDEX] = i == 0 ? 0 : splitRes.bN2End[i-1];
+            metaDataPtr->faMetadata[2 * i][FA_M_START_INDEX] = i == 0 ? 0 : splitRes.gS1End[i-1];
+            metaDataPtr->faMetadata[2 * i][FA_S2_START_INDEX] = i == 0 ? 0 : splitRes.s2End[i-1];
+
+            metaDataPtr->faMetadata[2 * i + 1][FA_BN2_START_INDEX] = i == 0 ? 0 : splitRes.bN2End[i-1];
+            metaDataPtr->faMetadata[2 * i + 1][FA_M_START_INDEX] = i == 0 ? 0 : splitRes.gS1End[i-1];
+            metaDataPtr->faMetadata[2 * i + 1][FA_S2_START_INDEX] = i == 0 ? 0 : splitRes.s2End[i-1];
+            // FA END
+            metaDataPtr->faMetadata[2 * i][FA_BN2_END_INDEX] = splitRes.bN2End[i];
+            metaDataPtr->faMetadata[2 * i][FA_M_END_INDEX] = splitRes.gS1End[i];
+            metaDataPtr->faMetadata[2 * i][FA_S2_END_INDEX] = splitRes.s2End[i];
+
+            metaDataPtr->faMetadata[2 * i + 1][FA_BN2_END_INDEX] = splitRes.bN2End[i];
+            metaDataPtr->faMetadata[2 * i + 1][FA_M_END_INDEX] = splitRes.gS1End[i];
+            metaDataPtr->faMetadata[2 * i + 1][FA_S2_END_INDEX] = splitRes.s2End[i];
+            //
+            metaDataPtr->faMetadata[2 * i][FA_FIRST_FD_DATA_WORKSPACE_IDX_INDEX] = splitRes.firstFdDataWorkspaceIdx[i];
+            metaDataPtr->faMetadata[2 * i + 1][FA_FIRST_FD_DATA_WORKSPACE_IDX_INDEX] = splitRes.firstFdDataWorkspaceIdx[i];
         }
-        metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
-        // FA START
-        metaDataPtr->faMetadata[i][FA_BN2_START_INDEX] = i == 0 ? 0 : splitRes.bN2End[i-1];
-        metaDataPtr->faMetadata[i][FA_M_START_INDEX] = i == 0 ? 0 : splitRes.gS1End[i-1];
-        metaDataPtr->faMetadata[i][FA_S2_START_INDEX] = i == 0 ? 0 : splitRes.s2End[i-1];
-        // FA END
-        metaDataPtr->faMetadata[i][FA_BN2_END_INDEX] = splitRes.bN2End[i];
-        metaDataPtr->faMetadata[i][FA_M_END_INDEX] = splitRes.gS1End[i];
-        metaDataPtr->faMetadata[i][FA_S2_END_INDEX] = splitRes.s2End[i];
-        // 
-        metaDataPtr->faMetadata[i][FA_FIRST_FD_DATA_WORKSPACE_IDX_INDEX] = splitRes.firstFdDataWorkspaceIdx[i];
+    } else {
+        for (size_t i = 0; i < aicCoreNum_; ++i) {
+            if (i >= splitRes.usedCoreNum) {
+                metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
+                continue;
+            }
+            metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
+            // FA START
+            metaDataPtr->faMetadata[i][FA_BN2_START_INDEX] = i == 0 ? 0 : splitRes.bN2End[i-1];
+            metaDataPtr->faMetadata[i][FA_M_START_INDEX] = i == 0 ? 0 : splitRes.gS1End[i-1];
+            metaDataPtr->faMetadata[i][FA_S2_START_INDEX] = i == 0 ? 0 : splitRes.s2End[i-1];
+            // FA END
+            metaDataPtr->faMetadata[i][FA_BN2_END_INDEX] = splitRes.bN2End[i];
+            metaDataPtr->faMetadata[i][FA_M_END_INDEX] = splitRes.gS1End[i];
+            metaDataPtr->faMetadata[i][FA_S2_END_INDEX] = splitRes.s2End[i];
+            //
+            metaDataPtr->faMetadata[i][FA_FIRST_FD_DATA_WORKSPACE_IDX_INDEX] = splitRes.firstFdDataWorkspaceIdx[i];
+        }
     }
 
     // FD Metadata Generate
