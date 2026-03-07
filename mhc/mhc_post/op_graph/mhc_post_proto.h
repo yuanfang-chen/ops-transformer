cd0ee53b4f57dc0cc7236d55f510888f20bec343
warming <wangming176@huawei.com>
2026-03-07 14:39:13 +0800

    modify proto.h

--- mhc/mhc_post/op_graph/mhc_post_proto.h
+++ mhc/mhc_post/op_graph/mhc_post_proto.h
@@ -14,23 +14,28 @@
  * \file mhc_post_proto.h
  * \brief
  */
-#ifndef OPS_OP_PROTO_INC_MOECOMPUTEEXPERT_H_
-#define OPS_OP_PROTO_INC_MOECOMPUTEEXPERT_H_
+#ifndef OPS_OP_PROTO_INC_MHCPOST_H_
+#define OPS_OP_PROTO_INC_MHCPOST_H_
 
 #include "graph/operator_reg.h"
 
 namespace ge {
 /**
-* @brief Fuse the main branch feature h_out with the residual branch feature x 
-* using the gating mechanism h_post and the doubly stochastic matrix  h_res 
+* @brief Fuse the main branch feature h_out with the residual branch feature x
+* using the gating mechanism h_post and the doubly stochastic matrix  h_res
 * to enable information flow.
 * @par Inputs:
-* @li x: A Tensor. Type is:BFloat16 or Float16.
-* @li h_res: A Tensor. Type is:Float32.
-* @li h_out: A Tensor. Type is:BFloat16 or Float16.
-* @li h_post: A Tensor. Type is:Float32.
+* @li x: A Tensor. Represents input data in the mHC layer. Type is:BFloat16 or Float16. \n
+* Dataformat:ND. Supports 3D (Shape:[T, n, D]) or 4D (Shape:[B, S, n, D]) tensors.
+* @li h_res: A Tensor. Represents the h_res transformation matrix. Type is:Float32. \n 
+* Dataformat:ND. Supports 3D (Shape:[T, n, n]) or 4D (Shape:[B, S, n, n]) tensors.
+* @li h_out: A Tensor. Represents output of the Atten/MLP layer. Type is:BFloat16 or Float16. \n
+* Dataformat:ND. Supports 2D (Shape:[T, D]) or 3D (Shape:[B, S, D]) tensors.
+* @li h_post: A Tensor. Represents the h_res transformation matrix. Type is:Float32. \n
+* Dataformat:ND. Supports 2D (Shape:[T, n]) or 3D (Shape:[B, S, n]) tensors.
 * @par Outputs:
-* @li y: A Tensor. Type is:BFloat16 or Float16.
+* y: A Tensor. Type is:BFloat16 or Float16. Dataformat:ND. \n
+* Supports 3D (Shape:[T, n, D]) or 4D (Shape:[B, S, n, D]) tensors. 
 */
 REG_OP(MhcPost)
     .INPUT(x, TensorType({DT_FLOAT16, DT_BF16}))
