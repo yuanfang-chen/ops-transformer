81096512fea1d414b39d202fb16c24b0964d2077
tangjn <tangjianing@huawei.com>
2026-03-08 15:15:04 +0800

    codecheck fix

--- mc2/moe_distribute_dispatch_teardown/op_kernel/arch35/moe_distribute_dispatch_teardown_arch35.h
+++ mc2/moe_distribute_dispatch_teardown/op_kernel/arch35/moe_distribute_dispatch_teardown_arch35.h
@@ -18,7 +18,11 @@
 
 #include "adv_api/reduce/sum.h"
 #include "kernel_tiling/kernel_tiling.h"
-#include "../moe_distribute_base.h"
+#if __has_include("../../moe_distribute_combine_setup/moe_distribute_base.h")
+#include "../../moe_distribute_combine_setup/moe_distribute_base.h"
+#else
+#include "../../moe_distribute_combine_setup/op_kernel/moe_distribute_base.h"
+#endif
 #include "../moe_distribute_dispatch_teardown_tiling.h"
 
 namespace MoeDistributeDispatchTeardownImpl {
