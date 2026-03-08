f0c5cbe65689ef28f798d053aa8586314c7e88fd
我是石头人大王 <luohao148@huawei.com>
2026-03-08 15:07:03 +0800

    QLI pangu

--- attention/quant_lightning_indexer/op_host/quant_lightning_indexer_tiling.h
+++ attention/quant_lightning_indexer/op_host/quant_lightning_indexer_tiling.h
@@ -75,7 +75,9 @@ constexpr uint32_t DIM_NUM_FOUR = 4;
 constexpr uint32_t HEAD_DIM_LIMIT = 128;
 constexpr uint32_t SPARSE_LIMIT = 2048;
 constexpr uint32_t G_SIZE_LIMIT = 64;
-constexpr uint32_t G_SIZE_LIMIT_950 = 24; // ascend950只支持 G = 24/64
+constexpr uint32_t G_SIZE_LIMIT_950 = 24;
+constexpr uint32_t G_SIZE_LIMIT_32_950 = 32; 
+constexpr uint32_t G_SIZE_LIMIT_16_950 = 16; // ascend950只支持 G = 16/24/32/64
 constexpr uint32_t BLOCK_SIZE_LIMIT = 1024;
 constexpr uint32_t BLOCK_SIZE_FACTOR = 16;
 constexpr uint32_t SPARSE_MODE_LOWER = 3;
