93895b7849ea49b40e43fda78742406ef4fc298f
我是石头人大王 <luohao148@huawei.com>
2026-03-08 15:07:03 +0800

    QLI pangu

--- attention/quant_lightning_indexer/op_kernel/quant_lightning_indexer.cpp
+++ attention/quant_lightning_indexer/op_kernel/quant_lightning_indexer.cpp
@@ -15,7 +15,6 @@
 
 #include "kernel_operator.h"
 #include "lib/matmul_intf.h"
-#include "quant_lightning_indexer_template_tiling_key.h"
 
 #if (__CCE_AICORE__ == 310)
     #include "arch35/quant_lightning_indexer_kernel.h"
@@ -23,9 +22,9 @@
 #else
     #include "quant_lightning_indexer_kernel.h"
 #endif
-
+#include "quant_lightning_indexer_template_tiling_key.h"
 using namespace QLIKernel;
-
+using namespace optiling::detail;
 #define INVOKE_LI_NO_KFC_OP_IMPL(templateClass, ...)                                                         \
     do {                                                                                                     \
         templateClass<QLIType<__VA_ARGS__>> op;                                                              \
@@ -48,13 +47,13 @@ __global__ __aicore__ void quant_lightning_indexer(__gm__ uint8_t *query, __gm__
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
 
 #if (__CCE_AICORE__ == 310)
-    if (ORIG_DTYPE_QUERY == DT_FLOAT8_E4M3FN) {
-        INVOKE_LI_NO_KFC_OP_IMPL(QLIPreload, fp8_e4m3fn_t, fp8_e4m3fn_t, int32_t,
+    // if (ORIG_DTYPE_QUERY == DT_FLOAT8_E4M3FN) {
+        INVOKE_LI_NO_KFC_OP_IMPL(QLIPreload, fp8_e4m3fn_t, fp8_e4m3fn_t, float, uint16_t, int32_t,
                                 PAGE_ATTENTION, LI_LAYOUT(Q_LAYOUT_T), LI_LAYOUT(K_LAYOUT_T));
-    } else {
-        INVOKE_LI_NO_KFC_OP_IMPL(QLIPreload, hifloat8, hifloat8, int32_t,
-                                PAGE_ATTENTION, LI_LAYOUT(Q_LAYOUT_T), LI_LAYOUT(K_LAYOUT_T));  
-    }
+    // } else {
+    //     INVOKE_LI_NO_KFC_OP_IMPL(QLIPreload, hifloat8, hifloat8, int32_t,
+    //                             PAGE_ATTENTION, LI_LAYOUT(Q_LAYOUT_T), LI_LAYOUT(K_LAYOUT_T));  
+    // }
 #else
     INVOKE_LI_NO_KFC_OP_IMPL(QLIPreload, int8_t, int8_t, int32_t,
                                 PAGE_ATTENTION, LI_LAYOUT(Q_LAYOUT_T), LI_LAYOUT(K_LAYOUT_T));
