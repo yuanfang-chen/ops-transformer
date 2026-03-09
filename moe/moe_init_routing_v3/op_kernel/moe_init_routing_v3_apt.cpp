bcbcd84d1bab2f3936d453ec9d11fb4ba96d5fa0
zoujiamin123 <zoujiamin1@huawei.com>
2026-03-09 16:09:13 +0800

    修复moeinitroutingv3 quantmode=2 if条件判断错误问题

--- moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
+++ moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
@@ -196,7 +196,7 @@ extern "C" __global__ __aicore__ void moe_init_routing_v3(GM_ADDR x, GM_ADDR exp
                TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_MXFP8QUANT_SCATTER)) {
         // MXFP8量化。由于MXFP8模板用到的指令不支持DTYPE_X为int8_t等类型，因此需要constexpr-if来规避编译
         if constexpr ((IsSameType<DTYPE_X, bfloat16_t>::value || IsSameType<DTYPE_X, half>::value) && 
-            (IsSameType<DTYPE_EXPANDED_X, fp8_e4m3fn_t>::value || IsSameType<DTYPE_X, fp8_e5m2_t>::value)) {
+            (IsSameType<DTYPE_EXPANDED_X, fp8_e4m3fn_t>::value || IsSameType<DTYPE_EXPANDED_X, fp8_e5m2_t>::value)) {
             TPipe gatherPipe;
             MoeGatherOutMxfp8Quant<DTYPE_X, DTYPE_EXPANDED_X> gatherMxfp8QuantOp;
             gatherMxfp8QuantOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
