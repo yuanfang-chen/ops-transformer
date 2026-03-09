891086f33ca90022de7170cb4913cdb429630d5f
hello_simida <wangyi206@huawei.com>
2026-03-09 20:26:38 +0800

    调整common目录适配:#include路径适配

--- mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_tiling_base.h
+++ mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_tiling_base.h
@@ -17,7 +17,7 @@
 #include "register/tilingdata_base.h"
 #include "tiling/tiling_api.h"
 #include "tiling_base/tiling_base.h"
-#include "tiling/mc2_tiling_struct.h"
+#include "op_tiling/mc2_tiling_struct.h"
 #include "op_tiling/matmul_formulaic_tiling.h"
 #include "mat_mul_v3/op_host/op_tiling/matmul_v3_tiling.h"
 #include "op_tiling/mc2_tiling_utils.h"
