2959e627e3e860d3980fb02b060df6001aa5bfd2
hello_simida <wangyi206@huawei.com>
2026-03-09 20:05:43 +0800

    调整common目录适配:#include路径适配

--- mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_no_quant_tiling.cpp
+++ mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_no_quant_tiling.cpp
@@ -22,7 +22,7 @@
 #include "context_util.h"
 #include "allto_allv_grouped_mat_mul_no_quant_tiling.h"
 #include "tiling/matmul_formulaic_tiling.h"
-#include "tiling/hccl_formulaic_tiling.h"
+#include "op_tiling/hccl_formulaic_tiling.h"
 #include "mc2_hcom_topo_info.h"
 #include "mc2_log.h"
 #include "graph/utils/type_utils.h"
