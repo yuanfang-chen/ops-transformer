08092ad1d4ce568229d5e5e58e73af51b9526a41
hello_simida <wangyi206@huawei.com>
2026-03-09 19:48:08 +0800

    调整common目录适配-cmake脚本适配

--- cmake/custom_build.cmake
+++ cmake/custom_build.cmake
@@ -146,7 +146,8 @@ if (BUILD_OPEN_PROJECT)
         $<$<TARGET_EXISTS:opbase_tiling_objs>:$<TARGET_OBJECTS:opbase_tiling_objs>>
     )
     target_include_directories(cust_opmaster PRIVATE
-            ${CMAKE_CURRENT_SOURCE_DIR}/mc2/common/inc
+            ${CMAKE_CURRENT_SOURCE_DIR}/mc2/common/op_tiling
+            ${CMAKE_CURRENT_SOURCE_DIR}/mc2/common/op_kernel
             $<$<BOOL:${BUILD_OPEN_PROJECT}>:$<BUILD_INTERFACE:${ASCEND_CANN_PACKAGE_PATH}/include/experiment>>
     )
     target_compile_options(cust_opmaster PRIVATE
@@ -922,7 +923,7 @@ install(DIRECTORY ${OPS_ADV_UTILS_KERNEL_INC}/
 install(DIRECTORY ${OPS_ADV_DIR}/gmm/common/cgmct
         DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common
 )
-install(DIRECTORY ${OPS_ADV_DIR}/mc2/common/inc/kernel
+install(DIRECTORY ${OPS_ADV_DIR}/mc2/common/op_kernel
         DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common/inc
 )
 
