063ec8b6400cdea73bb9d18473cf8cec6b4c5f8f
Hu1L1 <chenshujian5@huawei.com>
2026-03-09 16:24:32 +0800

    [GMM]部分头文件命名错误，不应该出现aclnn前缀

--- gmm/grouped_matmul_swiglu_quant_v2/op_host/op_api/gmm_dsq_base.h
+++ gmm/grouped_matmul_swiglu_quant_v2/op_host/op_api/gmm_dsq_base.h
@@ -8,8 +8,8 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#ifndef OP_HOST_OP_API_ACLNN_GMM_DSQ_BASE_H
-#define OP_HOST_OP_API_ACLNN_GMM_DSQ_BASE_H
+#ifndef OP_HOST_OP_API_GMM_DSQ_BASE_H
+#define OP_HOST_OP_API_GMM_DSQ_BASE_H
 
 #include "grouped_matmul_swiglu_quant_utils.h"
 
