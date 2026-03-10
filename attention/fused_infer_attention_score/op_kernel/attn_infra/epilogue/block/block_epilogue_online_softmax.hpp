426a0b9c78fe9c365a56187a473b1a200943b9ab
z00911896 <zhanghu61@huawei.com>
2026-03-10 16:53:25 +0800

    softmax cleancode

--- attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax.hpp
+++ attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax.hpp
@@ -1164,12 +1164,9 @@ public:
                     uint32_t kvNBlockSize,
                     uint64_t gmOffsetSBase,
                     uint64_t gmOffsetPBase,
-                    // uint32_t stackSeqTile,
-                    // uint32_t stackSeqTilePad,
                     Arch::CrossCoreFlag qkReady,
                     Arch::CrossCoreFlag softmaxReady)
     {
-        // Arch::CrossCoreWaitFlag(qkReady);
         uint32_t rowNum = actualBlockShape.m();
         uint32_t columnNum = actualBlockShape.n();
         uint32_t columnNumRound = NpuArch::Detail::Alignment::RoundUp(columnNum, BLOCK_SIZE);
@@ -1178,8 +1175,6 @@ public:
         uint32_t subBlockIdx = AscendC::GetSubBlockIdx();
         uint32_t subBlockNum = AscendC::GetSubBlockNum();
 
-        // 直接切分KVhead
-        // AscendC::printf("123123 split kvN than QN");
         uint32_t kvNSplitSubBlock = kvNBlockSize / subBlockNum;
         uint32_t kvNThisSubBlock = (kvNBlockSize == 1U) ? 0
             : (subBlockIdx == 1U) ? (kvNBlockSize - kvNSplitSubBlock) : kvNSplitSubBlock;
@@ -1188,7 +1183,6 @@ public:
         uint32_t qNThisSubBlock = (qNBlockSize == 1U) ? 0
             : (subBlockIdx == 1U) ? (qNBlockSize - qNSplitSubBlock) : qNSplitSubBlock;
 
-        // 先切分kvn 不能切分的话就切分qn
         uint32_t rowSplitSubBlock = (kvNBlockSize == 1) ?
             (qNSplitSubBlock * kvNBlockSize * qSBlockSize) : (qSBlockSize * qNBlockSize * kvNSplitSubBlock);
         uint32_t rowActualThisSubBlock = (subBlockIdx == 1) ? (rowNum - rowSplitSubBlock) : rowSplitSubBlock;
@@ -1198,9 +1192,6 @@ public:
         rowNumTile = AscendC::Std::min(rowNumTile, FLOAT_VECTOR_SIZE);
         uint32_t rowLoopNum = NpuArch::Detail::Alignment::CeilDiv(rowActualThisSubBlock, rowNumTile);
         uint32_t preLoad = 1;
-
-        // AscendC::printf("如何切分》》 kvNSplitSubBlock %d, qNSplitSubBlock %d, rowSplitSubBlock %d rowActualThisSubBlock %d\n", kvNSplitSubBlock, qNSplitSubBlock, rowSplitSubBlock, rowActualThisSubBlock);
-        // AscendC::printf("如何切分》》 rowNumTile %d, rowNumTile %d, rowLoopNum %d\n", rowNumTile, rowNumTile, rowLoopNum);
 
         for (uint32_t rowLoopIdx = 0; rowLoopIdx < rowLoopNum + preLoad; rowLoopIdx++) {
             // AscendC::printf(" d45a6s4da56sd4a \n");
@@ -1213,13 +1204,9 @@ public:
 
                 int64_t offsetInput = layoutInput.GetOffset(MatrixCoord(rowOffsetIoGm, 0));
                 auto gInputCurLoop = gInputBase[offsetInput];
-                // QK结果按行均匀排布
 
                 AscendC::WaitFlag<AscendC::HardEvent::V_MTE2>(pingpongFlag);
 
-                // AscendC::printf("如何切分》》 columnNumRound %d, columnNumPad %d\n", columnNumRound, columnNumPad);
-                // 这是逐行搬运进去的,通过设置DataCopyParams,重复搬移rowNumCurLoop次
-                // 实际搬移的长度是columnNumRound，是对齐到UB数据操作单位的大小，会跳过padding的部分数据
                 CopySGmToUb(
                     gInputCurLoop, (pingpongFlag * MAX_UB_S_ELEM_NUM), rowNumCurLoop, columnNumRound, columnNumPad);
                 AscendC::SetFlag<AscendC::HardEvent::MTE2_V>(pingpongFlag);
