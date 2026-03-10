c1fb83c997b52112ad06b70993a54235299334b0
wuyewei <wuyewei@huawei.com>
2026-03-10 16:05:18 +0800

    inf/nan场景

--- mhc/mhc_post/op_host/mhc_post_tiling.cpp
+++ mhc/mhc_post/op_host/mhc_post_tiling.cpp
@@ -421,8 +421,8 @@ void MhcPostTilingBase::ComputeTiling()
 
     // Calculate bytes per tileD element
     // TQue bf16: 3 * 2 bytes (hOut:1, x:1, output:1)
-    // TBuf f32:  3 * 4 bytes (hOutF32:1, xF32:1, outF32:1)
-    uint32_t bytesPerTileD = 3 * (DOUBLE_BUFFER_DEPTH * SIZE_OF_16BIT + SINGLE_BUFFER_DEPTH * SIZE_OF_32BIT);
+    // TBuf f32:  5 * 4 bytes (hOutF32:1, hCombF32:1, hMulF32:1, xF32:1, outF32:1)
+    uint32_t bytesPerTileD = 3 * DOUBLE_BUFFER_DEPTH * SIZE_OF_16BIT + 4 * SIZE_OF_32BIT;
     uint32_t maxTileD = UB_SIZE / bytesPerTileD;
     dOuter_ = 1;
     dInner_ = d_;
@@ -446,7 +446,7 @@ void MhcPostTilingBase::ComputeTiling()
     usedCoreNum_ = Ops::Base::CeilDiv(totalCount, normalCoreProcessNum_);
     tailCoreProcessNum_ = totalCount - (usedCoreNum_ - 1) * normalCoreProcessNum_;
 
-    uint64_t fullyBytesPerTileD = (n_ + 2) * (DOUBLE_BUFFER_DEPTH * SIZE_OF_16BIT + SIZE_OF_32BIT);
+    uint64_t fullyBytesPerTileD = (n_ + 2) * DOUBLE_BUFFER_DEPTH * SIZE_OF_16BIT + (n_ + 4) * SIZE_OF_32BIT;
     if (fullyBytesPerTileD * dInner_ <= UB_SIZE) {
         usePermanentX_ = 1;
     }
