6cee0c9e0a423bd47147d3ab900afc36f18b5156
qq_23260455 <liujiali27@h-partners.com>
2026-03-10 15:49:15 +0800

    fix

--- attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax.hpp
+++ attention/fused_infer_attention_score/op_kernel/attn_infra/epilogue/block/block_epilogue_online_softmax.hpp
@@ -1030,7 +1030,7 @@ public:
     void operator()(AscendC::GlobalTensor<ElementOutput> gOutput, AscendC::GlobalTensor<ElementInput> gInput, AscendC::GlobalTensor<ElementSink> gSink,
         const LayoutOutput &layoutOutput, const LayoutInput &layoutInput, GemmCoord actualBlockShape,
         uint32_t isFirstStackTile, uint32_t isLastNoMaskStackTile, uint32_t qSBlockSize, uint32_t qNBlockSize, 
-        uint32_t curStackTileMod, bool isSplitKV = false bool startsWithMaskTile = false, bool startsWithMaskThenNomaskFlag = false)
+        uint32_t curStackTileMod, bool isSplitKV = false, bool startsWithMaskTile = false, bool startsWithMaskThenNomaskFlag = false)
     {
         uint32_t rowNum = actualBlockShape.m();
         uint32_t columnNum = actualBlockShape.n();
