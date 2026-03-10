7543399893b902f55d4e21d9d6b5412708d3e583
wkyan <yanwenkai@huawei.com>
2026-03-10 14:23:57 +0800

    fix apt

--- attention/causal_conv1d/op_kernel/causal_conv1d_apt.cpp
+++ attention/causal_conv1d/op_kernel/causal_conv1d_apt.cpp
@@ -14,9 +14,13 @@
  */
 
 #include "./arch35/causal_conv1d_update.h"
+#include "./arch35/causal_conv1d_fn.h"
 
-
+#define TILING_KEY_FN_BF16 10000
+#define TILING_KEY_FN_FP16 10001
 #define TILING_KEY_UPDATE_FP16 20000
+
+using namespace CausalConv1dFnNs;
 extern "C" __global__ __aicore__ void causal_conv1d(
     GM_ADDR x,                    // 输入0: x
     GM_ADDR weight,               // 输入1: weight
@@ -32,17 +36,36 @@ extern "C" __global__ __aicore__ void causal_conv1d(
     GM_ADDR tiling)               // tiling
 {
     REGISTER_TILING_DEFAULT(CausalConv1dUpdateTilingData);
-    GET_TILING_DATA_WITH_STRUCT(CausalConv1dUpdateTilingData, tilingData, tiling);
     TPipe pipe;
     // Check if FP16 or BF16
     // Assuming FP16 for now (can be extended to support BF16)
     PRINTF("START UPDATE");
     if (TILING_KEY_IS(TILING_KEY_UPDATE_FP16)) {
+        // REGISTER_TILING_DEFAULT(CausalConv1dUpdateTilingData);
+        GET_TILING_DATA_WITH_STRUCT(CausalConv1dUpdateTilingData, tilingData, tiling);
         CausalConv1dUpdateKernel<half> op(&pipe);
         PRINTF("START Init");
         op.Init(x, weight, convStates, queryStartLoc, cacheIndices, numAcceptedToken, y, &tilingData);
         PRINTF("START Process");
         op.Process();
         PRINTF("END Process");
+    }
+
+    if (TILING_KEY_IS(TILING_KEY_FN_BF16)) {
+        // REGISTER_TILING_DEFAULT(CausalConv1dFnTilingData);
+        GET_TILING_DATA_WITH_STRUCT(CausalConv1dFnTilingData, tilingData, tiling);
+        CausalConv1dFn<bfloat16_t> op;
+        PRINTF("START InitA");
+        op.Init(x, weight, convStates, queryStartLoc, cacheIndices,
+                initialStateMode, y, workspace, &tilingData);
+    }
+
+    if (TILING_KEY_IS(TILING_KEY_FN_FP16))  {
+        // REGISTER_TILING_DEFAULT(CausalConv1dFnTilingData);
+        GET_TILING_DATA_WITH_STRUCT(CausalConv1dFnTilingData, tilingData, tiling);
+        CausalConv1dFn<half> op;
+        PRINTF("START InitB");
+        op.Init(x, weight, convStates, queryStartLoc, cacheIndices,
+                initialStateMode, y, workspace, &tilingData);
     }
 }
