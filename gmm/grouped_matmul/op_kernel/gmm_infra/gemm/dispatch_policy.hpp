8360ce133bde13330c5100d68af05b4ebcb593df
m30040597 <mgl1984@163.com>
2026-03-10 10:43:42 +0800

    202603101049 fix codesegments and blank lines

--- gmm/grouped_matmul/op_kernel/gmm_infra/gemm/dispatch_policy.hpp
+++ gmm/grouped_matmul/op_kernel/gmm_infra/gemm/dispatch_policy.hpp
@@ -155,7 +155,6 @@ struct MmadAtlasA2FAIQK : public MmadAtlasA2{
     static constexpr uint32_t STAGES = 2;
     static constexpr bool PAGED_CACHE_FLAG = PAGED_CACHE_FLAG_;
     static constexpr bool ENABLE_UNIT_FLAG = ENABLE_UNIT_FLAG_;
-
 };
 
 template <bool PAGED_CACHE_FLAG_ = false, bool ENABLE_UNIT_FLAG_ = false>
