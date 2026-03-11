cc6d489de8d84a884792527eb117e9680039c89a
libohao6 <libohao3@huawei.com>
2026-03-11 17:50:04 +0800

    新增AlltoAllvQuantGroupedMatmul算子

--- cmake/func.cmake
+++ cmake/func.cmake
@@ -400,7 +400,7 @@ function(add_ops_src_copy)
         "grouped_mat_mul_all_reduce;"
         "batch_mat_mul_reduce_scatter_allto_all;"
         "allto_allv_grouped_mat_mul;"
-        "allto_allv_grouped_mat_mul_v2;"
+        "allto_allv_quant_grouped_mat_mul;"
         "allto_all_all_gather_batch_mat_mul;"
         "distribute_barrier;"
         "moe_distribute_combine_add_rms_norm;"
