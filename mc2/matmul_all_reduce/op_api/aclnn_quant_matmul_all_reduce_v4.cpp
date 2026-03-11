d06e247658aa723b69a3a4dc6c4cef60a3a825a6
chuguowei <chuguowei@huawei.com>
2026-03-11 14:52:57 +0800

    [mc2]matmul_all_reduce clean code

--- mc2/matmul_all_reduce/op_api/aclnn_quant_matmul_all_reduce_v4.cpp
+++ mc2/matmul_all_reduce/op_api/aclnn_quant_matmul_all_reduce_v4.cpp
@@ -13,21 +13,8 @@
  * \brief
  */
 #include "aclnn_quant_matmul_all_reduce_v4.h"
-#include "securec.h"
 
-#include "acl/acl.h"
-#include "op_mc2.h"
-#include "op_mc2_def.h"
-#include "aclnn_kernels/common/op_error_check.h"
-#include "opdev/common_types.h"
-#include "opdev/op_dfx.h"
-#include "opdev/op_executor.h"
-#include "opdev/make_op_executor.h"
-#include "opdev/op_log.h"
-#include "opdev/platform.h"
 #include "mc2_aclnn_util.h"
-#include "matmul_all_reduce_util.h"
-#include "aclnn_kernels/contiguous.h"
 #include "matmul_all_reduce_util.h"
 
 using namespace op;
@@ -35,13 +22,6 @@ using namespace op;
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-enum class NnopbaseHcclServerType : uint32_t {
-    NNOPBASE_HCCL_SERVER_TYPE_AICPU = 0,
-    NNOPBASE_HCCL_SERVER_TYPE_MTE,
-    NNOPBASE_HCCL_SERVER_TYPE_CCU,
-    NNOPBASE_HCCL_SERVER_TYPE_END
-};
 
 extern aclnnStatus aclnnInnerMatmulAllReduceGetWorkspaceSize(
     const aclTensor* x1, const aclTensor* x2, const aclTensor* bias, const aclTensor* x3,
--- mc2/matmul_all_reduce/op_api/aclnn_weight_quant_matmul_all_reduce.cpp
+++ mc2/matmul_all_reduce/op_api/aclnn_weight_quant_matmul_all_reduce.cpp
@@ -9,34 +9,15 @@
  */
 
 #include "aclnn_weight_quant_matmul_all_reduce.h"
-#include "securec.h"
 
-#include "acl/acl.h"
-#include "op_mc2.h"
-#include "op_mc2_def.h"
-#include "aclnn_kernels/common/op_error_check.h"
-#include "opdev/common_types.h"
-#include "opdev/op_dfx.h"
-#include "opdev/op_executor.h"
-#include "opdev/make_op_executor.h"
-#include "opdev/op_log.h"
-#include "opdev/platform.h"
 #include "opdev/tensor_view_utils.h"
 #include "matmul_all_reduce_util.h"
-#include "aclnn_kernels/contiguous.h"
 
 using namespace op;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-enum class NnopbaseHcclServerType : uint32_t {
-    NNOPBASE_HCCL_SERVER_TYPE_AICPU = 0,
-    NNOPBASE_HCCL_SERVER_TYPE_MTE,
-    NNOPBASE_HCCL_SERVER_TYPE_CCU,
-    NNOPBASE_HCCL_SERVER_TYPE_END
-};
 
 static constexpr int64_t ANTIQUANT_GROUP_SIZE_MIN_VALUE = 32;
 
