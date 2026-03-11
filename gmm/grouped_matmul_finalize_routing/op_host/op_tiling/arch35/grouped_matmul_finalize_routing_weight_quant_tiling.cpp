7ddd443a7379081bf67c3d7128b8a354f4845e7f
shirui17@huawei.com <shirui17@huawei.com>
2026-03-11 17:52:44 +0800

    fix: add missing include for REGISTER_OPS_TILING_TEMPLATE macro

--- gmm/grouped_matmul_finalize_routing/op_host/op_tiling/arch35/grouped_matmul_finalize_routing_weight_quant_tiling.cpp
+++ gmm/grouped_matmul_finalize_routing/op_host/op_tiling/arch35/grouped_matmul_finalize_routing_weight_quant_tiling.cpp
@@ -14,6 +14,7 @@
  */
 #include "grouped_matmul_finalize_routing_weight_quant_tiling.h"
 #include "../../../op_kernel/arch35/weight_quant_basic_block/grouped_matmul_finalize_routing_weight_quant_tiling_key.h"
+#include "tiling_base/tiling_templates_registry.h"
 using namespace Ops::Transformer::OpTiling;
 using namespace optiling::GroupedMatmulFinalizeRoutingArch35WeightQuantTiling;
 using namespace GMMFinalizeRoutingArch35Tiling;
