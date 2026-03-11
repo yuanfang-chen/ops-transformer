f6cbc52f3ca0a445b49339c493548b942fb60557
wkyan <yanwenkai@huawei.com>
2026-03-11 20:36:41 +0800

    fix bug

--- attention/causal_conv1d/op_host/causal_conv1d_fn_tiling_arch35.cpp
+++ attention/causal_conv1d/op_host/causal_conv1d_fn_tiling_arch35.cpp
@@ -1,4 +1,4 @@
-0/**
+/**
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
