275f677e08577c0a3818e8e7241940673de4c20c
liuyang_806 <liuyang930@hisilicon.com>
2026-03-11 09:58:55 +0800

    fix the format bugs

--- gmm/grouped_matmul/tests/ut/op_host/test_grouped_weight_quant_arch35_tiling.cpp
+++ gmm/grouped_matmul/tests/ut/op_host/test_grouped_weight_quant_arch35_tiling.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 
 
 /*!
- * \file test_grouped_weight_quant_batch_matmul_tiling.cpp
+ * \file test_grouped_weight_quant_arch35_tiling.cpp
  * \brief Unit tests for GroupedWeightQuantBatchMatmulTiling
  */
 
