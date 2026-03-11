9f7cb4b66d2c2dc233b444d19530f18c850174e0
lyt_claire <luyitong1@huawei.com>
2026-03-11 16:41:32 +0800

    检视意见修改

--- examples/mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
+++ examples/mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
@@ -86,7 +86,7 @@ int CreateAclTensor(const std::vector<T> &hostData, const std::vector<int64_t> &
     return 0;
 }
 
-int CompareVector(std::vector<op::fp16_t> &vec1, std::vector<op::fp16_t> &vec2)
+int CompareVector(std::vector<op::fp16_t> &vec1, const std::vector<op::fp16_t> &vec2)
 {
     const float tolerance = 0.001f; // 千分之一
     for (size_t i = 0; i < vec1.size(); ++i) {
@@ -115,7 +115,7 @@ struct Args {
     aclrtStream stream;
   };
 
-int LaunchOneThreadAllGatherAdd(Args &args, TestData &testData)
+int LaunchOneThreadAllGatherAdd(Args &args, const TestData &testData)
 {
     int ret = aclrtSetDevice(args.rankId);
     CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtSetDevice failed. ret = %d \n", ret); return ret);
@@ -343,14 +343,14 @@ int main(int argc, char *argv[])
                                       std::ref(args[rankId]), std::ref(testData)));
     }
 
-    int final_ret = 0;
+    int finalRet = 0;
     for (auto& future : futures) {
         int ret = future.get();
         if (ret != 0) {
-            final_ret = ret;
+            finalRet = ret;
         }
     }
 
     aclFinalize();
-    return final_ret;
+    return finalRet;
 }
