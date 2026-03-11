de77a6354d14aa699c4837ad4c1d7bf83b57f5d3
Hu1L1 <chenshujian5@huawei.com>
2026-03-11 11:44:22 +0800

    [GMM]部分头文件命名错误，不应该出现aclnn前缀
    Co-authored-by: Hu1L1<chenshujian5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2444 merge headname into master
    
    [GMM]部分头文件命名错误，不应该出现aclnn前缀
    
    Created-by: Hu1L1
    Commit-by: Hu1L1
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    [GMM]部分头文件(不涉及对外接口的)命名错误，不应该出现aclnn前缀
    修改方法：去除aclnn_前缀
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-transformer/issues/1120
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    UT测试、冒烟测试
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!2444

--- gmm/quant_grouped_matmul_inplace_add/op_host/op_api/aclnn_quant_grouped_matmul_inplace_add.cpp
+++ gmm/quant_grouped_matmul_inplace_add/op_host/op_api/aclnn_quant_grouped_matmul_inplace_add.cpp
@@ -29,10 +29,10 @@
 #include "opdev/make_op_executor.h"
 
 #include "../../../grouped_matmul/op_host/op_api/grouped_matmul_util.h"
-#include "../../../grouped_matmul/op_host/op_api/aclnn_grouped_matmul_950_checker.h"
-#include "aclnn_quant_grouped_matmul_inplace_add_util.h"
+#include "../../../grouped_matmul/op_host/op_api/grouped_matmul_950_checker.h"
+#include "quant_grouped_matmul_inplace_add_util.h"
 #include "quant_grouped_matmul_inplace_add.h"
-#include "aclnn_quant_grouped_matmul_inplace_add_950_checker.h"
+#include "quant_grouped_matmul_inplace_add_950_checker.h"
 
 using namespace op;
 
