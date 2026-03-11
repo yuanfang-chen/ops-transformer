290751e45f95c6076987b39f56c98389dde52023
huangshuai <huangshuai59@huawei.com>
2026-03-11 15:07:41 +0800

    新增quantalltoallv算子

--- mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
+++ mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
@@ -25,8 +25,8 @@
 #include "mc2_matmul_tiling_cfg.h"
 #include "tiling/new_mc2_tiling_utils.h"
 #include "../quant_grouped_mat_mul_allto_allv_tiling_base.h"
-#include "../../../op_kernel/arch35/quant_grouped_mat_mul_allto_allv_v2_tiling.h"
-#include "../../../op_kernel/grouped_mat_mul_allto_allv_v2_tiling_key.h"
+#include "../../../op_kernel/arch35/quant_grouped_mat_mul_allto_allv_tiling.h"
+#include "../../../op_kernel/quant_grouped_mat_mul_allto_allv_tiling_key.h"
 #include "register/tilingdata_base.h"
 
 namespace optiling {
