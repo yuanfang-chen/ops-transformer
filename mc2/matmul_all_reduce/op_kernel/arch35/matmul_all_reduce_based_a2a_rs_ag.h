a8040d4eccb818fc65f80080fdb55f6760816c20
yangyajie <yangyajie3@huawei.com>
2026-03-11 10:00:40 +0800

    解冲突，合并验证铭浩代码

--- mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_based_a2a_rs_ag.h
+++ mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_based_a2a_rs_ag.h
@@ -17,7 +17,6 @@
 
 #include "matmul_all_reduce_base.h"
 namespace MatmulAllReduceImpl {
-using namespace AiVReduceSumImpl;
 using namespace GmUbGmCopyImpl;
 constexpr uint32_t A2A_VSUM_AG_MAX_HANDLE_ID_NUM = 16;
 template <typename XType, typename YType, Mc2CoreType CoreType>
