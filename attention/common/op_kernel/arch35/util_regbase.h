c95cca9196ae309796bf8cccd654186ce07089c4
zhangh2417 <zhanghang172@huawei.com>
2026-03-12 20:18:43 +0800

    分核代码和fd相关代码添加

--- attention/common/op_kernel/arch35/util_regbase.h
+++ attention/common/op_kernel/arch35/util_regbase.h
@@ -24,6 +24,7 @@ using AscendC::QuePosition;
 namespace regbaseutil {
 constexpr uint16_t regBytes = 256;
 constexpr int64_t MAX_PRE_NEXT_TOKENS = 0x7FFFFFFF;
+constexpr uint32_t  FP32_BLOCK_ELEMENT_NUM = BYTE_BLOCK /sizeof(float) ; //FD新增
 enum class VselrIndexEnum {GT_64_AND_LTE_128_INDEX = 0, GT_0_AND_LTE_64_INDEX = 1, DN_INDEX = 2, NZ_INDEX = 3};
 enum class DTemplateType {
     Aligned16 = 16,
@@ -293,7 +294,17 @@ struct RunInfo<false> {
     float scaleValue; \
     int64_t matmulMSize;     /* 在matmul运算中，左矩阵的M轴大小需要区分GS1合轴与不合轴的情况 */ \
     bool learnableSinkFlag = false; /* attentionsink */ \
-    float pScale
+    float pScale; \
+    /*BUFFER的字节数*/  \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_32B = 32; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_64B = 64; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_256B = 256; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_512B = 512; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_1K = 1024; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_2K = 2048; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_4K = 4096; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_8K = 8192; \
+    static constexpr uint32_t BUFFER_SIZE_BYTE_16K = 16384;
 
 
 #define ROPE_INFO \
@@ -349,6 +360,7 @@ struct RunInfo<false> {
     /* 左padding */ \
     bool isQHasLeftPadding; \
     bool isKVHasLeftPadding; \
+    int64_t queryLeftPaddingSize; /* FD新增 */ \
     int64_t queryRightPaddingSize; \
     int64_t kvRightPaddingSize; \
     /* FD */ \
@@ -516,6 +528,17 @@ struct CVSharedParams<true, true> {
     // prefix
     bool isActualSharedPrefixLenNull;
     int64_t kvPrefixSize;
+};
+struct FDparams {
+ 	     uint32_t *fdBN2Idx;
+ 	     uint32_t *fdMIdx;
+ 	     uint32_t *fdS2SplitNum;
+ 	     uint32_t *fdBalanceMSplitNum;
+ 	     uint32_t *fdBalanceMTailSize;
+ 	     uint32_t *fdBalanceEndIdx1;
+ 	     uint32_t *fdBalanceEndIdx2;
+ 	     uint32_t fdUsedVecNum;
+ 	     uint32_t fdBalanceMBaseSize;
 };
 }
 
