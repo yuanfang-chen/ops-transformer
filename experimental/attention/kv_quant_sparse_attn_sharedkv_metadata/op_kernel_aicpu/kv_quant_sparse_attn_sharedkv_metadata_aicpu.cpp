a31f8a674b6f4e3e6ffe5295bdf84dad59472502
qq_32807861 <handongchen2@huawei.com>
2026-03-12 11:10:39 +0800

    N128模板增加未启用核的maxS2BaseNum输出

--- experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_kernel_aicpu/kv_quant_sparse_attn_sharedkv_metadata_aicpu.cpp
+++ experimental/attention/kv_quant_sparse_attn_sharedkv_metadata/op_kernel_aicpu/kv_quant_sparse_attn_sharedkv_metadata_aicpu.cpp
@@ -1109,6 +1109,8 @@ bool KvQuantSparseAttnSharedkvMetadataCpuKernel::GenMetaData(SplitResult &splitR
             if (i >= splitRes.usedCoreNum) {
                 metaDataPtr->faMetadata[2 * i][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
                 metaDataPtr->faMetadata[2 * i + 1][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
+                metaDataPtr->faMetadata[2 * i][FA_S2_MAX_NUM] = splitRes.maxS2GBaseNum; // 单核M基本块最大数量
+                metaDataPtr->faMetadata[2 * i + 1][FA_S2_MAX_NUM] = splitRes.maxS2GBaseNum; // 单核M基本块最大数量
                 continue;
             }
             metaDataPtr->faMetadata[2 * i][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
@@ -1140,6 +1142,7 @@ bool KvQuantSparseAttnSharedkvMetadataCpuKernel::GenMetaData(SplitResult &splitR
         for (size_t i = 0; i < aicCoreNum_; ++i) {
             if (i >= splitRes.usedCoreNum) {
                 metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 0; // AIC disenable
+                metaDataPtr->faMetadata[i][FA_S2_MAX_NUM] = splitRes.maxS2GBaseNum; // 单核M基本块最大数量
                 continue;
             }
             metaDataPtr->faMetadata[i][FA_CORE_ENABLE_INDEX] = 1; // AIC enable
