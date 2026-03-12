2bd1a950235ce88b75b5a20e0c9c342f8f750e1b
m30040597 <mgl1984@163.com>
2026-03-12 09:53:08 +0800

    202603120953 debug ut_case

--- gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
+++ gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
@@ -3089,6 +3089,7 @@ TEST_F(GroupedMatmulTiling, test_tiling_a4w4ofp16_optimize)
     size_t M = 256;
     size_t K = 7168;
     size_t N = 4096;
+    size_t Q = 256;
     size_t E = 4;
     optiling::GMMCompileInfo compileInfo = {
         24,//aicNum
@@ -3107,7 +3108,7 @@ TEST_F(GroupedMatmulTiling, test_tiling_a4w4ofp16_optimize)
                                                     {{{M, K}, {M, K}}, ge::DT_INT4, ge::FORMAT_ND},              //x
                                                     {{{E, K, N}, {E, K, N}}, ge::DT_INT4, ge::FORMAT_FRACTAL_NZ},        //weight
                                                     {{{}, {}}, ge::DT_INT32, ge::FORMAT_ND},                //bias
-                                                    {{{E, N}, {E, N}}, ge::DT_FLOAT, ge::FORMAT_ND},                //scale
+                                                    {{{E, K/Q, N}, {E, K/Q, N}}, ge::DT_FLOAT, ge::FORMAT_ND},                //scale
                                                     {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND},                        //offset
                                                     {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND},                        //antiquantScale
                                                     {{{}, {}}, ge::DT_FLOAT, ge::FORMAT_ND},                        //antiquantOffset
