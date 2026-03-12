c8670e2e995f941006abc163268b0b3ea81f4518
eddie-han-dong <donghan12@huawei.com>
2026-03-12 17:04:10 +0800

    GQA 8bit support static quant of PMatrix

--- attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling_v2.cpp
+++ attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling_v2.cpp
@@ -1018,9 +1018,9 @@ bool PromptFlashAttentionTilingV2::CheckPerblockQuantParams(const ContextParamsF
         OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
             "dequantScaleQuery, keyAntiquantScale or valueAntiquantScale is nullptr in per-block quant scenario."),
         return false);
-    OP_CHECK_IF((deqScale1Shape != nullptr) || (quantScale1Shape != nullptr) || (deqScale2Shape != nullptr),
+    OP_CHECK_IF((deqScale1Shape != nullptr) || (deqScale2Shape != nullptr),
         OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
-            "deqScale1, quantScale1 or deqScale2 is not supported in per-block quant scenario."),
+            "deqScale1 or deqScale2 is not supported in per-block quant scenario."),
         return false);
     OP_CHECK_IF(antiquantScaleShape != nullptr,
         OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
