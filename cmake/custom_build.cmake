94f8c96fa35b16c8d990c92fba3897bdd8afcbca
hello_simida <wangyi206@huawei.com>
2026-03-12 11:04:04 +0800

    调整common目录适配:适配cmake脚本,修复找不到文件的bug

--- cmake/custom_build.cmake
+++ cmake/custom_build.cmake
@@ -924,7 +924,7 @@ install(DIRECTORY ${OPS_ADV_DIR}/gmm/common/cgmct
         DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common
 )
 install(DIRECTORY ${OPS_ADV_DIR}/mc2/common/op_kernel
-        DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common/op_kernel
+        DESTINATION ${IMPL_INSTALL_DIR}/ascendc/common
 )
 
 install(DIRECTORY ${OPS_ADV_DIR}/mc2/3rd/
