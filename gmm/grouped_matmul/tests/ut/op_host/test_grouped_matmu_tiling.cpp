d9dd1dbdf0dc7d4d4a2604975d93f3ae76084d6e
m30040597 <mgl1984@163.com>
2026-03-12 11:22:26 +0800

    202603121122 debug ut_case

--- gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
+++ gmm/grouped_matmul/tests/ut/op_host/test_grouped_matmu_tiling.cpp
@@ -3084,7 +3084,7 @@ TEST_F(GroupedMatmulTiling, test_tiling_a4w4obf16_trans_dynamic_tiling_1aic2aiv)
 }
 
 // QUANT_A4W4优化
-TEST_F(GroupedMatmulTiling, test_tiling_a4w4ofp16_fixed_axis)
+TEST_F(GroupedMatmulTiling, test_tiling_a4w4ofp16_optimize)
 {
     size_t M = 256;
     size_t K = 2048;
@@ -3144,7 +3144,7 @@ TEST_F(GroupedMatmulTiling, test_tiling_a4w4ofp16_fixed_axis)
     //     "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ";
     string expectTilingData =
         "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ";
-    std::vector<size_t> expectWorkspaces = {29360128}; // workspace
+    std::vector<size_t> expectWorkspaces = {24117248}; // workspace
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces,230);
 }
 
