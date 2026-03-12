9c1aa9039ccb6b17dbb5d4df65002f58033d7870
sky-zyh <zhangyuhan45@huawei.com>
2026-03-12 15:43:56 +0800

    no tilingkey sink

--- attention/common/op_kernel/arch35/vf/vf_basic_block_unaligned128_update.h
+++ attention/common/op_kernel/arch35/vf/vf_basic_block_unaligned128_update.h
@@ -31,7 +31,8 @@ __simd_vf__ void ProcessVec1UpdateGeneralImpl128VF(
     __ubuf__ uint32_t * maskUb, __ubuf__ uint32_t * maskUbUnroll, __ubuf__ uint32_t * dropMaskUb, 
     const uint32_t nPadding, const uint32_t blockStride, const uint32_t repeatStride, const uint32_t oriTailN, const uint32_t tailN, 
     const float dScale, uint32_t pltOriTailN, uint32_t pltTailN, float divValue, uint32_t pltN, const uint16_t m, const uint32_t pseStride, 
-    const float slopes, const float posShift, const T scale, const float dScaleQK, const T minValue, const float deSCaleKValue = 1.0f)
+    const float slopes, const float posShift, const T scale, const float dScaleQK, const T minValue, const float deSCaleKValue = 1.0f,
+    const bool hasSink = false, const float sinkValue = 0)
 {
     RegTensor<float> vreg_min;
     RegTensor<float> vreg_sel;
@@ -188,6 +189,11 @@ __simd_vf__ void ProcessVec1UpdateGeneralImpl128VF(
             Reduce<MicroAPI::ReduceType::MAX, float, float, MicroAPI::MaskMergeMode::ZEROING>(
                 vreg_input_max, vreg_max_tmp, preg_all);
         }
+        if (unlikely(hasSink)) {
+            RegTensor<float> vreg_sink_input;
+            Duplicate(vreg_sink_input, sinkValue);
+            Max(vreg_input_max, vreg_input_max, vreg_sink_input, preg_all);
+        }
         StoreUnAlign<float, MicroAPI::PostLiteral::POST_MODE_UPDATE>(
             ((__ubuf__ T *&)tmpMaxUb), vreg_input_max, ureg_max, 1);
     }
@@ -335,7 +341,8 @@ __aicore__ inline void ProcessVec1UpdateGeneralImpl128(
     const LocalTensor<T>& inMaxTensor, const LocalTensor<uint8_t>& maskTensor, const LocalTensor<pseShiftType>& pseTensor,
     const LocalTensor<uint8_t>& dropTensor, const LocalTensor<uint8_t>& sharedTmpBuffer, const LocalTensor<T>& pScaleTensor, const uint16_t m,
     const uint32_t originN, const uint32_t pseStride, const float slopes, const float posShift, const T scale, const float dScaleQK,
-    const T minValue, float keepProb, const LocalTensor<T>& queryScaleUb = LocalTensor<T>(), const float deSCaleKValue = 1.0f)
+    const T minValue, float keepProb, const LocalTensor<T>& queryScaleUb = LocalTensor<T>(), const float deSCaleKValue = 1.0f, const bool hasSink = false,
+    const float sinkValue = 0)
 {
     const uint32_t nPadding = (s2BaseSize + blockBytesU8 - 1) / blockBytesU8 * blockBytesU8;
     // 写的时候固定用65或者33的stride去写，因为正向目前使能settail之后mm2的s1方向必须算满128或者64行
@@ -372,9 +379,9 @@ __aicore__ inline void ProcessVec1UpdateGeneralImpl128(
     __ubuf__ uint32_t * dropMaskUb = (__ubuf__ uint32_t *)dropTensor.GetPhyAddr();
 
     ProcessVec1UpdateGeneralImpl128VF<T, T2, pseShiftType, s1BaseSize, s2BaseSize, hasAtten, pseMode, hasDrop, isMlaSgd, isMlaFullQuant>(
-        expUb, x_expUb, pseUb, maxUb, srcUb, expMaxUb, inMaxUb, tmpExpSumUb, tmpMaxUb, tmpMaxUb2, qScaleUb, pScaleUb, indexesUb, 
-        maskUb, maskUbUnroll, dropMaskUb, nPadding, blockStride, repeatStride, oriTailN, tailN, dScale, pltOriTailN, pltTailN, 
-        divValue, pltN, m, pseStride, slopes, posShift, scale, dScaleQK, minValue, deSCaleKValue);
+        expUb, x_expUb, pseUb, maxUb, srcUb, expMaxUb, inMaxUb, tmpExpSumUb, tmpMaxUb, tmpMaxUb2, qScaleUb, pScaleUb, indexesUb,
+        maskUb, maskUbUnroll, dropMaskUb, nPadding, blockStride, repeatStride, oriTailN, tailN, dScale, pltOriTailN, pltTailN,
+        divValue, pltN, m, pseStride, slopes, posShift, scale, dScaleQK, minValue, deSCaleKValue, hasSink, sinkValue);
 }
 } // namespace
 
