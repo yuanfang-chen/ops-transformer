a86e37fb790fe7f3f57e565f566d5b934a861510
Konstantin Berestizshevsky <konstantin.berestizshevsky@huawei.com>
2026-03-12 08:47:14 +0000

    CI code style corrections round 5 - licensing legacy files

--- experimental/attention/prompt_flash_attention/op_host/op_api/aclnn_prompt_flash_attention_v4.cpp
+++ experimental/attention/prompt_flash_attention/op_host/op_api/aclnn_prompt_flash_attention_v4.cpp
@@ -1,6 +1,5 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
