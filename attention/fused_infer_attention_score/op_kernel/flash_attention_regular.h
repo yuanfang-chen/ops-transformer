cf0622155506c54a2ae1d941b25d4523202e21c9
qq_23260455 <liujiali27@h-partners.com>
2026-03-13 10:21:45 +0800

    feat:kvnz format

--- attention/fused_infer_attention_score/op_kernel/flash_attention_regular.h
+++ attention/fused_infer_attention_score/op_kernel/flash_attention_regular.h
@@ -512,6 +512,12 @@ namespace SplitFuse {
             uint64_t gmOffsetO = oBOffset +
                 static_cast<uint64_t>(qSBlockIdx * curQSBlockTile) * strideO +
                 static_cast<uint64_t>(qNStartIdx * embedV);
+            if constexpr (PAGED_CACHE_FLAG && std::is_same_v<LayoutK, layout::nZ>) {
+                gmOffsetK = kBOffset + kvNIdx * embed * pagedBlockSize;
+            }
+            if constexpr (PAGED_CACHE_FLAG && std::is_same_v<LayoutV, layout::zN>) {
+                gmOffsetV = vBOffset + kvNIdx * embedV * pagedBlockSize;
+            }
             uint64_t gmOffsetLse = lseBOffset +
                 static_cast<uint64_t>(lseTokenOffset + qNStartIdx);
             uint64_t gmOffsetSink = qNStartIdx;
@@ -616,8 +622,22 @@ namespace SplitFuse {
 #endif
 #ifdef __DAV_C220_CUBE__
                 LayoutQ layoutQTemp(rowNum, embed);
+                uint32_t kRow = strideK;
+                uint32_t kCol = stackSeqTile;
+                uint32_t vRow = stackSeqTile;
+                uint32_t vCol = strideV;
                 LayoutK layoutKTemp(strideK, stackSeqTile);
                 LayoutV layoutVTemp(stackSeqTile, strideV);
+                if constexpr (PAGED_CACHE_FLAG && std::is_same_v<LayoutK, layout::nZ>) {
+                    kRow = blockStackNum * strideK;
+                    kCol = pagedBlockSize;
+                    layoutKTemp = LayoutK::template MakeLayout<ElementK>(kRow, kCol);
+                }
+                if constexpr (PAGED_CACHE_FLAG && std::is_same_v<LayoutV, layout::zN>) {
+                    vRow = pagedBlockSize;
+                    vCol = blockStackNum * strideV;
+                    layoutVTemp = LayoutV::template MakeLayout<ElementV>(vRow, vCol);
+                }
                 blockMmadQK.resetBlockStart();
                 blockMmadPV.resetBlockStart();
                 blockMmadQK.loadQGM(gQ[gmOffsetQ], layoutQTemp, rowNum, qNBlockSize, qHeads);
