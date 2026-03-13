f6409513eb14fb50873698fccc1aa75ccb0e9dbe
qq_23260455 <liujiali27@h-partners.com>
2026-03-13 17:00:57 +0800

    fix:kvshape value

--- attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
+++ attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
@@ -1050,8 +1050,8 @@ bool CheckSpecConditions(const gert::TilingContext *context)
                 specConditionFlag = true;
             }
         } else if (kvDimNum == 4U && tempK->GetStorageShape().GetDim(DIM_3) == 16 && tempV->GetStorageShape().GetDim(DIM_3) == 16) {
-            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) / kvHeadNum;
-            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) / kvHeadNum;
+            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
+            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
             int64_t blockSize = tempK->GetStorageShape().GetDim(DIM_2);
             bool isFAIDSize = (tempQD <= 256 && tempKD <= 256 && tempVD <= 256) &&
                     (tempQD == tempKD && tempQD == tempVD);
--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -1429,8 +1429,8 @@ static bool IsUsingFAI(gert::TilingContext &context, const string inputLayoutStr
                 usingFAI = true;
             }
         } else if (kvDimNum == 4U && tempK->GetStorageShape().GetDim(DIM_3) == 16 && tempV->GetStorageShape().GetDim(DIM_3) == 16) {
-            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) / kvHeadNum;
-            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) / kvHeadNum;
+            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
+            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
             int64_t blockSize = tempK->GetStorageShape().GetDim(DIM_2);
             bool isFAIDSize = (tempD <= 256U && tempKD <= 256 && tempVD <= 256) &&
                     (tempD == tempKD && tempD == tempVD);
