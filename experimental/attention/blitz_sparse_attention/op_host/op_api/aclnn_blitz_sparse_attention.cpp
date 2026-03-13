628c864597e9b0d3c4fbb289f6ca1c878d183a23
Konstantin Berestizshevsky <konstantin.berestizshevsky@huawei.com>
2026-03-13 14:49:53 +0000

    start blitz_sparse_attention, remove prompt_flash_attenion

---
+++ experimental/attention/blitz_sparse_attention/op_host/op_api/aclnn_blitz_sparse_attention.cpp
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#include "aclnn_blitz_sparse_attention.h"
+#include "aclnn_blitz_sparse_attention_inner.h"
+#include "aclnn/aclnn_base.h"
+#include "aclnn_kernels/contiguous.h"
+#include "aclnn_kernels/pad.h"
+#include "aclnn_kernels/reshape.h"
+#include "aclnn_kernels/transpose.h"
+#include "aclnn_kernels/slice.h"
+#include "opdev/common_types.h"
+#include "opdev/fast_vector.h"
+#include "opdev/op_executor.h"
+
+using namespace op;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+namespace {
+aclnnStatus aclnnBlitzSparseAttentionGetWorkspaceSize(
+    const aclTensor *query,
+    const aclTensor *key,
+    const aclTensor *value,
+    const aclTensor *pseShift,
+    const aclTensor *attenMask,
+    const aclIntArray *actualSeqLengths,
+    int64_t numHeads, // q_n
+    double scaleValue,
+    int64_t preTokens,
+    int64_t nextTokens,
+    char *inputLayout,
+    int64_t numKeyValueHeads,
+    const aclTensor *attentionOut,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor) {   
+        (void) pseShift;    
+        const aclIntArray *actualSeqLengthsKv = nullptr;
+        int64_t sparseMode = 0;
+        int64_t innerPrecise = 1;
+        const aclTensor *deqScale1 = nullptr;
+        const aclTensor *quantScale1 = nullptr;
+        const aclTensor *deqScale2 = nullptr;
+        const aclTensor *quantScale2 = nullptr;
+        const aclTensor *quantOffset2 = nullptr;
+        return InnerBlitzSparseAttentionV4GetWorkspaceSize(query, key, value, nullptr, attenMask, nullptr,
+                                                              actualSeqLengths, actualSeqLengthsKv,
+                                                              deqScale1, quantScale1, deqScale2,
+                                                              quantScale2, quantOffset2,
+                                                              numHeads, scaleValue, preTokens, nextTokens,
+                                                              inputLayout, numKeyValueHeads, sparseMode,
+                                                              innerPrecise, attentionOut, workspaceSize, executor);
+    }
+
+aclnnStatus aclnnBlitzSparseAttention(
+    void *workspace,
+    uint64_t workspaceSize,
+    aclOpExecutor *executor,
+    const aclrtStream stream) {
+        // perform attention computations.
+        return InnerBlitzSparseAttentionV4(workspace, workspaceSize, executor, stream);
+    }
+
+}
+
+#ifdef __cplusplus
+}
+#endif
