86ff06a6d560595fcb4ffba1a0cf38625ec9257a
yuhao102 <yuhao102@h-partners.com>
2026-03-13 11:53:48 +0800

    datacopy balance2

--- attention/common/op_kernel/arch35/flash_attention_noquant_block_vec_infer.h
+++ attention/common/op_kernel/arch35/flash_attention_noquant_block_vec_infer.h
@@ -118,6 +118,11 @@ private:
     __aicore__ inline void Bmm2FDOut(LocalTensor<T> &vec2ResUb, RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo,
                                      int64_t vec2S1Idx, int64_t vec2CalcSize);
 
+    __aicore__ inline void Bmm2FDOutBalance(LocalTensor<T> &vec2ResUb, RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo,
+                                            int64_t vec2S1Idx, int64_t vec2CalcSize);
+
+    __aicore__ inline void CalcAccumOffset(RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo);
+
     __aicore__ inline void CopyLseIn(ConstInfo<isInfer, hasRope> &constInfo, uint32_t bIdx, uint32_t n2Idx, uint32_t startRow, uint32_t dealRowCount);
 
     __aicore__ inline void CopyFinalResOut(ConstInfo<isInfer, hasRope> &constInfo, uint64_t attenOutOffset, LocalTensor<T> &accumOutLocal, uint32_t startRow,
@@ -127,6 +132,8 @@ private:
                                           uint32_t dealRowCount);
 
     __aicore__ inline void ComputeLogSumExpAndCopyToGm(RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo);
+
+    __aicore__ inline void ComputeLogSumExpAndCopyToGmBalance(RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo);
 
     __aicore__ inline void CopySinkIn(RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo);
 
@@ -215,19 +222,18 @@ __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::InitCubeVecSharedP
     sharedParams.multiCoreInnerOffset = multiCoreParamsRegbase.sparseStartIdx[aicIdx];
     sharedParams.multiCoreInnerLimit = multiCoreParamsRegbase.sparseStartIdx[aicIdx + 1];
 
-    auto &outerSplitParams = this->tilingData->outerSplitParams;
     if (aicIdx == 0) {
         sharedParams.bN2StartIdx = 0;
         sharedParams.gS1StartIdx = 0;
         sharedParams.s2StartIdx = 0;
     } else {
-        sharedParams.bN2StartIdx = outerSplitParams.bN2End[aicIdx - 1];
-        sharedParams.gS1StartIdx = outerSplitParams.mEnd[aicIdx - 1];
-        sharedParams.s2StartIdx = outerSplitParams.s2End[aicIdx - 1];
+        sharedParams.bN2StartIdx = multiCoreParamsRegbase.bN2End[aicIdx - 1];
+        sharedParams.gS1StartIdx = multiCoreParamsRegbase.mEnd[aicIdx - 1];
+        sharedParams.s2StartIdx = multiCoreParamsRegbase.s2End[aicIdx - 1];
     }
-    sharedParams.bN2EndIdx = outerSplitParams.bN2End[aicIdx];
-    sharedParams.gS1EndIdx = outerSplitParams.mEnd[aicIdx];
-    sharedParams.s2EndIdx = outerSplitParams.s2End[aicIdx];
+    sharedParams.bN2EndIdx = multiCoreParamsRegbase.bN2End[aicIdx];
+    sharedParams.gS1EndIdx = multiCoreParamsRegbase.mEnd[aicIdx];
+    sharedParams.s2EndIdx = multiCoreParamsRegbase.s2End[aicIdx];
 
     sharedParams.needInit = this->tilingData->initOutputParams.needInit;
 
@@ -421,9 +427,15 @@ __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::SoftmaxDataCopyOut
     RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo, LocalTensor<float> &sumUb,
     LocalTensor<float> &maxUb)
 {
-    if constexpr (isFd) {
-        ComputeLogSumExpAndCopyToGm(runInfo, constInfo);
-        return;
+    if constexpr (enableSplitCoreBalance) {
+        if (runInfo.isS2SplitCore) {
+            ComputeLogSumExpAndCopyToGmBalance(runInfo, constInfo, sumUb, maxUb);
+        }
+    } else {
+        if constexpr (isFd) {
+            ComputeLogSumExpAndCopyToGm(runInfo, constInfo);
+            return;
+        }
     }
     if (constInfo.learnableSinkFlag) {
         if (constInfo.isGqa) {
@@ -479,10 +491,18 @@ template <typename VEC2_RES_T>
 __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::CopyOutAttentionOut(
     RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo, LocalTensor<VEC2_RES_T> &vec2ResUb, int64_t vec2S1Idx, int64_t vec2CalcSize)
 {
-    if constexpr (isFd) {
-        Bmm2FDOut(vec2ResUb, runInfo, constInfo, vec2S1Idx, vec2CalcSize);
+    if constexpr (enableSplitCoreBalance) {
+        if (runInfo.isS2SplitCore) {
+            Bmm2FDOutBalance(vec2ResUb, runInfo, constInfo, vec2S1Idx, vec2CalcSize);
+        } else {
+            this->Bmm2DataCopyOut(runInfo, constInfo, vec2ResUb, vec2S1Idx, vec2CalcSize);
+        }
     } else {
-        this->Bmm2DataCopyOut(runInfo, constInfo, vec2ResUb, vec2S1Idx, vec2CalcSize);
+        if constexpr (isFd) {
+            Bmm2FDOut(vec2ResUb, runInfo, constInfo, vec2S1Idx, vec2CalcSize);
+        } else {
+            this->Bmm2DataCopyOut(runInfo, constInfo, vec2ResUb, vec2S1Idx, vec2CalcSize);
+        }
     }
 }
 
@@ -766,6 +786,62 @@ __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::CopySinkFDIn(uint3
 }
 
 TEMPLATES_DEF_NO_DEFAULT
+__aicore__ inline void FABlockVecInfer<TEMPLATE_ARGS>::CalcAccumOffset(RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo)
+{
+    const uint32_t *bN2IdxOfFdHead = this->tilingData->fdParams.bN2IdxOfFdHead;
+    const uint32_t *gS1IdxOfFdHead = this->tilingData->fdParams.gS1IdxOfFdHead;
+    const uint32_t *s2SplitNumOfFdHead = this->tilingData->fdParams.s2SplitNumOfFdHead;
+    uint64_t accumTmpOutNum = 0;
+    uint32_t taskId = 0;
+    uint32_t curbN2Idx = runInfo.boIdx * constInfo.n2Size + runInfo.n2oIdx;
+    while (taskId < constInfo.aivIdx && (bN2IdxOfFdHead[taskId] != curbN2Idx || gS1IdxOfFdHead[taskId] != constInfo.gS1Start)) {
+        accumTmpOutNum += s2SplitNumOfFdHead[taskId]; // 计算前面的workspace数
+        taskId++;
+    }
+    runInfo.accumTmpOutNum = accumTmpOutNum;
+
+
+    // runInfo.fATmpResGMPos = constInfo.coreStartKVSplitPos;
+    // constInfo.coreStartKVSplitPos = s2SplitStartIdxOfCore[aiCoreIdx];
+}
+
+TEMPLATES_DEF_NO_DEFAULT
+__aicore__ inline void FABlockVecInfer<TEMPLATE_ARGS>::Bmm2FDOutBalance(LocalTensor<T> &vec2ResUb,
+    RunInfo<isInfer> &runInfo, ConstInfo<isInfer, hasRope> &constInfo, int64_t vec2S1Idx, int64_t vec2CalcSize)
+{
+    LocalTensor<T> attenOut;
+    int64_t dSizeAligned64 = (int64_t)dVTemplateType;
+    if constexpr (BaseClass::splitD){
+        dSizeAligned64 = constInfo.dBasicBlock;
+    }
+    SetFlag<HardEvent::V_MTE3>(this->vToMte3Id[runInfo.taskIdMod2]);
+    WaitFlag<HardEvent::V_MTE3>(this->vToMte3Id[runInfo.taskIdMod2]);
+    attenOut = vec2ResUb;
+    CalcAccumOffset(runInfo, constInfo);
+    PRINTF("Bmm2FDOutBalance accumTmpOutNum: %d\n", runInfo.accumTmpOutNum);
+    uint64_t gmOffset = 0;
+
+    if (isMlaNoQuant) {
+        gmOffset = runInfo.accumTmpOutNum * constInfo.n2Size * constInfo.mBaseSize * constInfo.dSizeV +     // taskoffset
+                   runInfo.fATmpResGMPos * constInfo.n2Size * constInfo.mBaseSize * constInfo.dSizeV +      // 份数offset
+                   (constInfo.subBlockIdx * runInfo.firstHalfS1RealSize +
+                   vec2S1Idx * runInfo.vec2S1BaseSize) * constInfo.dSizeV;                                  // m轴offset
+    } else {
+        gmOffset = runInfo.accumTmpOutNum * constInfo.mBaseSize * constInfo.dSizeV +   // taskoffset
+                   runInfo.fATmpResGMPos * constInfo.mBaseSize * constInfo.dSizeV +    // 份数offset
+                   (constInfo.subBlockIdx * runInfo.firstHalfS1RealSize +
+                   vec2S1Idx * runInfo.vec2S1BaseSize) * constInfo.dSizeV;             // m轴offset
+    }
+    DataCopyExtParams dataCopyParams;
+    dataCopyParams.blockCount = runInfo.vec2S1RealSize;
+    dataCopyParams.blockLen = constInfo.dSizeV * sizeof(T);
+    dataCopyParams.srcStride = (dSizeAligned64 - constInfo.dSizeV) / (FA_BYTE_BLOCK / sizeof(T));
+    dataCopyParams.dstStride = 0;
+
+    DataCopyPad(this->accumOutGm[gmOffset], attenOut, dataCopyParams);
+}
+
+TEMPLATES_DEF_NO_DEFAULT
 __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::Bmm2FDOut(LocalTensor<T> &vec2ResUb,
     RunInfo<isInfer> &runInfo,  ConstInfo<isInfer, hasRope> &constInfo, int64_t vec2S1Idx, int64_t vec2CalcSize)
 {
@@ -891,6 +967,35 @@ __aicore__ inline void FANoQuantBlockVecInfer<TEMPLATE_ARGS>::CopyAccumOutIn(Con
                                      startRow * constInfo.dSizeV;
     DataCopyPad(accumOutLocal, this->accumOutGm[combineAccumOutOffset], copyInParams, copyInPadParams);
     accumOutInputQue.EnQue(accumOutLocal);
+}
+
+__aicore__ inline void
+FABlockVecInfer<TEMPLATE_ARGS>::ComputeLogSumExpAndCopyToGmBalance(RunInfo<isInfer> &runInfo,
+    ConstInfo<isInfer, hasRope> &constInfo)
+{
+    if (unlikely(runInfo.halfS1RealSize == 0)) {
+        return;
+    }
+    int64_t calculateSize = runInfo.halfS1RealSize * fp32BaseSize;
+    // 是否要改成halfMRealSize
+    CalcAccumOffset(runInfo, constInfo);
+    PRINTF("compute log sum exp accumTmpOutNum: %d\n", runInfo.accumTmpOutNum);
+    int64_t gmOffset = 0;
+    
+    if(isMlaNoQuant) {
+        gmOffset =
+            (runInfo.accumTmpOutNum * constInfo.n2Size * constInfo.mBaseSize +      // taskoffset
+             runInfo.fATmpResGMPos * constInfo.n2Size * constInfo.mBaseSize +       // 份数offset
+             constInfo.subBlockIdx * runInfo.firstHalfS1RealSize) * fp32BaseSize;   // m轴offset
+    } else {
+        gmOffset =
+            (runInfo.accumTmpOutNum * constInfo.mBaseSize +                         // taskoffset
+             runInfo.fATmpResGMPos * constInfo.mBaseSize +                          // 份数offset
+             constInfo.subBlockIdx * runInfo.firstHalfS1RealSize) * fp32BaseSize;   // m轴offset
+    }
+    // flashDecodeS2Idx?nBufferStartM?
+    // Copy sum to gm
+    this->BroadCastAndCopyOut(runInfo, softmaxFDSumGm, softmaxFDMaxGm, gmOffset, calculateSize);
 }
 
 TEMPLATES_DEF_NO_DEFAULT
