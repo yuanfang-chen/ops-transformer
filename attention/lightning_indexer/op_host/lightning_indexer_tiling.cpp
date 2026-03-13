1f8974715c197cfaabedf759b6c7bbd4bc47ed78
chenxin_169 <chenxin169@hisilicon.com>
2026-03-13 14:01:35 +0800

    9.0.0

--- attention/lightning_indexer/op_host/lightning_indexer_tiling.cpp
+++ attention/lightning_indexer/op_host/lightning_indexer_tiling.cpp
@@ -347,7 +347,7 @@ ge::graphStatus LIInfoParser::CheckShapeDim()
                OP_LOGE(opName_, "the dim num of weights's shape should be %u, but now is %u", qExpectShapeDim - 1,
                 weightsShapeDim),
                return ge::GRAPH_FAILED);
-    OP_CHECK_IF(opParamInfo_.valuesOut.shape->GetStorageShape().GetShapeSize() != 0 && !(*opParamInfo_.returnValue),
+    OP_CHECK_IF(opParamInfo_.valuesOut.shape->GetStorageShape().GetDimNum() != DIM_NUM_ONE && !(*opParamInfo_.returnValue),
                 OP_LOGE(opName_, "when returnValue is false, valuesOut must be null."),
                 return ge::GRAPH_FAILED);
     return ge::GRAPH_SUCCESS;
