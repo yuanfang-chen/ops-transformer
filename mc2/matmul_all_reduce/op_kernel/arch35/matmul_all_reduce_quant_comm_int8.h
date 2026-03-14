06bf709702ab9a402604d6bdee00f67a8ed1b2c7
yangyajie <yangyajie3@huawei.com>
2026-03-14 15:44:35 +0800

    删除chuguowei的删除文件arch35/matmul_all_reduce_quant_pertile_comm_fp8.h

--- mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_quant_comm_int8.h
+++ mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_quant_comm_int8.h
@@ -103,9 +103,9 @@ __aicore__ inline void MatmulAllReduceQuantCommInt8<XType, WType, YType, MmType,
 {
     __gm__ HcclCombinOpParam* context = (__gm__ HcclCombinOpParam*)(GetHcclContext<0>());
     OOMInit(context);
-    hccl_.InitV2(GetHcclContext<0>(), tilingData);
-    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTiling));
-    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTilingComm));
+    hccl_.InitV2(GetHcclContext<0>(), tilingData); 
+    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTiling));  
+    hccl_.SetCcTilingV2(offsetof(Mc2Tiling::QuantMatmulAllReduceTilingDataA5, mc2CcTilingComm)); 
     tilingData_ = tilingData;
     tPipe_ = tPipe;
     aGM_ = aGM;
