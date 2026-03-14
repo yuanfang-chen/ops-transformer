4118957b9fb12d6d24b6db368986f4da37a6fdbb
jayshu <shuhaojie@huawei.com>
2026-03-14 15:52:29 +0800

    tilingkey modify

--- gmm/grouped_matmul_finalize_routing/op_host/op_tiling/arch35/grouped_matmul_finalize_routing_quant_tiling.h
+++ gmm/grouped_matmul_finalize_routing/op_host/op_tiling/arch35/grouped_matmul_finalize_routing_quant_tiling.h
@@ -118,6 +118,8 @@ private:
     uint64_t rowIndex_ = 0;
     float sharedInputWeight_ = 1.0;
     uint64_t outputBs_ = 0;
+    int8_t scaleType_ = 0;
+    int8_t rowindexType_ = 0;
 };
 } // namespace optiling
 
