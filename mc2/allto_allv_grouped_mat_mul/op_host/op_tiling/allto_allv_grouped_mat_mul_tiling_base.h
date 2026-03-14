cb4c86a6161f80f42c5b4bff187c992b703c0b0c
hello_simida <wangyi206@huawei.com>
2026-03-14 16:10:40 +0800

    新增文件调整common目录适配，编译修复

--- mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_tiling_base.h
+++ mc2/allto_allv_grouped_mat_mul/op_host/op_tiling/allto_allv_grouped_mat_mul_tiling_base.h
@@ -21,7 +21,7 @@
 #include "op_tiling/mc2_tiling_struct.h"
 #include "op_tiling/matmul_formulaic_tiling.h"
 #include "mat_mul_v3/op_host/op_tiling/matmul_v3_tiling.h"
-#include "tiling/mc2_tiling_utils.h"
+#include "op_tiling/mc2_tiling_utils.h"
 
 namespace optiling {
 
