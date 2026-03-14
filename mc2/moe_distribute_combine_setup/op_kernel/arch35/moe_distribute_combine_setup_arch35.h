f7363219a0c6172a3ef2a95b01872cf3821ee1fc
tongzy1242 <tongzeyun@huawei.com>
2026-03-14 16:42:25 +0800

    fix: bs数量过大时，UB空间不足

--- mc2/moe_distribute_combine_setup/op_kernel/arch35/moe_distribute_combine_setup_arch35.h
+++ mc2/moe_distribute_combine_setup/op_kernel/arch35/moe_distribute_combine_setup_arch35.h
@@ -51,6 +51,7 @@ class MoeDistributeCombineSetup {
     constexpr static uint64_t STATE_OFFSET = 512U;        // 状态空间偏移地址
     constexpr static uint32_t STATE_SIZE = 1024U * 1024U; // 1M
     constexpr static uint32_t UB_ALIGN = 32U;             // UB按32字节对齐
+    constexpr static uint64_t REMAIN_UB_SPACE = 227008U;  // UB剩余空间
     constexpr static uint64_t WIN_STATE_OFFSET = 350U * 1024U;
     constexpr static uint64_t STATE_WIN_OFFSET = 950U * 1024U;
     constexpr static uint64_t STATE_SIZE_PER_CORE = 512U;   // 数据和状态的0/1区标识占用空间
@@ -233,7 +234,7 @@ template <TemplateMC2TypeClass>
 __aicore__ inline void MoeDistributeCombineSetup<TemplateMC2TypeFunc>::BuffInit()
 {
     tpipe_->Reset();
-    tpipe_->InitBuffer(assistInfoQueue_, BUFFER_NUM,
+    tpipe_->InitBuffer(assistInfoQueue_, 1,
                        moeSendNum_ * sizeof(int32_t)); // epWorldSize * moeExpertPerRankNum * 4
 
     // 初始化urma相关buf
@@ -245,7 +246,7 @@ __aicore__ inline void MoeDistributeCombineSetup<TemplateMC2TypeFunc>::BuffInit(
     tpipe_->InitBuffer(templateSqeBuf_, WRITE_WITH_NOTIFY_SQE_SIZE);
     tpipe_->InitBuffer(tokenSqeBuf_, WRITE_WITH_NOTIFY_SQE_SIZE * moeDistributeCombineSetupInfo_->moeExpertPerRankNum);
 
-    tpipe_->InitBuffer(expertTokenTmpQueue_, 1, static_cast<uint32_t>(expertPerSizeOnWin_));
+    tpipe_->InitBuffer(expertTokenTmpQueue_, 1, static_cast<uint32_t>(REMAIN_UB_SPACE));
 }
 
 template <TemplateMC2TypeClass>
@@ -429,23 +430,37 @@ MoeDistributeCombineSetup<TemplateMC2TypeFunc>::CurRankComm(const LocalTensor<in
         GM_ADDR srcAddr = expandXGM_ + static_cast<uint64_t>(preCount) * axisHExpandXTypeSize_;
         GM_ADDR dstAddr = GetWinAddrByRankId(moeDistributeCombineSetupInfo_->epRankId, expertIdx) + epDataOffsetOnWin_;
 
-        DataCopyExtParams copyParams{1U, curTokenNum * static_cast<uint32_t>(axisHExpandXTypeSize_), 0U, 0U, 0U};
-        DataCopyPadExtParams<uint8_t> padParams{false, 0U, 0U, 0U};
-
         // expandX在GM上，winIn也属于GM，
         // 因此，数据需要GM -> local -> winIn
         GlobalTensor<uint8_t> selfDataSrcTensor;
+        GlobalTensor<uint8_t> selfDataDstTensor;
         selfDataSrcTensor.SetGlobalBuffer((__gm__ uint8_t *)srcAddr);
+        selfDataDstTensor.SetGlobalBuffer((__gm__ uint8_t *)dstAddr);
         LocalTensor<uint8_t> expertTokenTmpU8 = expertTokenTmpQueue_.AllocTensor<uint8_t>();
 
-        DataCopyPad(expertTokenTmpU8, selfDataSrcTensor, copyParams, padParams);
+        uint64_t sendBytes = static_cast<uint64_t>(curTokenNum) * axisHExpandXTypeSize_;
+
+        DataCopyExtParams copyParams{1U, static_cast<uint32_t>(REMAIN_UB_SPACE), 0U, 0U, 0U};
+        DataCopyPadExtParams<uint8_t> padParams{false, 0U, 0U, 0U};
+
+        uint64_t i = 0;
+        for (; sendBytes > REMAIN_UB_SPACE; sendBytes -= REMAIN_UB_SPACE) {
+            DataCopyPad(expertTokenTmpU8, selfDataSrcTensor[i * REMAIN_UB_SPACE], copyParams, padParams);
+            expertTokenTmpQueue_.EnQue(expertTokenTmpU8);
+            expertTokenTmpU8 = expertTokenTmpQueue_.DeQue<uint8_t>();
+            AscendC::SyncFunc<AscendC::HardEvent::MTE2_MTE3>();
+            DataCopyPad(selfDataDstTensor[i * REMAIN_UB_SPACE], expertTokenTmpU8, copyParams);
+            ++i;
+
+            AscendC::SyncFunc<AscendC::HardEvent::MTE3_MTE2>();
+        }
+
+        copyParams.blockLen = sendBytes;
+        DataCopyPad(expertTokenTmpU8, selfDataSrcTensor[i * REMAIN_UB_SPACE], copyParams, padParams);
         expertTokenTmpQueue_.EnQue(expertTokenTmpU8);
         expertTokenTmpU8 = expertTokenTmpQueue_.DeQue<uint8_t>();
         AscendC::SyncFunc<AscendC::HardEvent::MTE2_MTE3>();
-
-        GlobalTensor<uint8_t> selfDataDstTensor;
-        selfDataDstTensor.SetGlobalBuffer((__gm__ uint8_t *)dstAddr);
-        DataCopyPad(selfDataDstTensor, expertTokenTmpU8, copyParams);
+        DataCopyPad(selfDataDstTensor[i * REMAIN_UB_SPACE], expertTokenTmpU8, copyParams);
 
         expertTokenTmpQueue_.FreeTensor<uint8_t>(expertTokenTmpU8);
     }
