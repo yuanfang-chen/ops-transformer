a6c35f8bbebeed666676280a3f0074e87b975b63
qq_23260455 <liujiali27@h-partners.com>
2026-03-16 16:51:47 +0800

    fix

--- attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
+++ attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_v3.cpp
@@ -1049,10 +1049,10 @@ bool CheckSpecConditions(const gert::TilingContext *context)
             if (isFAIDSize && blockSizeSupported) {
                 specConditionFlag = true;
             }
-        } else if (kvDimNum == 4U && tempK->GetStorageShape().GetDim(DIM_3) == 16 && tempV->GetStorageShape().GetDim(DIM_3) == 16) {
-            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
-            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16 / kvHeadNum;
-            int64_t blockSize = tempK->GetStorageShape().GetDim(DIM_2);
+        } else if (kvDimNum == 5U) {
+            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16;
+            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16;
+            int64_t blockSize = tempK->GetStorageShape().GetDim(DIM_3);
             bool isFAIDSize = (tempQD <= 256 && tempKD <= 256 && tempVD <= 256) &&
                     (tempQD == tempKD && tempQD == tempVD) && (tempQD % BLOCKSIZE_ALIGN_16 == 0);
             bool blockSizeSupported = (blockSize % BLOCKSIZE_ALIGN_16 == 0) &&
