a5aa43fff7552d528f9970262e6441d6b0351f26
wangyinchu <wangyinchu1@huawei.com>
2026-03-16 16:33:31 +0800

    bug fix

--- attention/quant_lightning_indexer/op_kernel/arch35/quant_lightning_indexer_service_vector.h
+++ attention/quant_lightning_indexer/op_kernel/arch35/quant_lightning_indexer_service_vector.h
@@ -386,7 +386,6 @@ __aicore__ inline void QLIVector<QLIT>::ProcessVec1(const QLICommon::RunInfo &in
     uint32_t qScaleStride = 0;
     if constexpr (std::is_same<SCALE_T, float16_t>::value) {
         // cast qScale、kScale: fp16 -> fp32
-        PipeBarrier<Pipe_ALL>();
         AscendC::Cast(qScaleFloatUB_[pingpong * (UB_BANK_STRIDE / sizeof(float))], qScaleUB_[pingpong * (UB_BANK_STRIDE / sizeof(SCALE_T))], RoundMode::CAST_NONE, curAivS1ProcNum * QLICommon::Align((uint64_t)gSize_, (uint64_t)16));
         AscendC::Cast(kScaleFloatUB_[pingpong * s2BaseSize_], kScaleUB_[pingpong * s2BaseSize_], RoundMode::CAST_NONE, s2BaseSize_);
         qScaleBase = qScaleFloatUB_[pingpong * (UB_BANK_STRIDE / sizeof(float))];
