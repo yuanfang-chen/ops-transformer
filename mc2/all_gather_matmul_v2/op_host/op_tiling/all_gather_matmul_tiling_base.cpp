7f4fdf671524eb4cba58e21dd4fd88bb765e9dad
Zhiqi Song <songzhiqi1@huawei.com>
2026-03-16 20:04:31 +0800

    mc2/allgathermmV2: bugfix perblock quant mode offset problem
    Signed-off-by: Zhiqi Song <songzhiqi1@huawei.com>

--- mc2/all_gather_matmul_v2/op_host/op_tiling/all_gather_matmul_tiling_base.cpp
+++ mc2/all_gather_matmul_v2/op_host/op_tiling/all_gather_matmul_tiling_base.cpp
@@ -403,6 +403,22 @@ ge::graphStatus AllGatherMatmulTilingBase::CheckHCCLSize()
     return ge::GRAPH_SUCCESS;
 }
 
+void AllGatherMatmulTilingBase::PostDoSplitMTiling(Mc2Tiling::RCSTiling& rcfCfg, mc2tiling::Mc2QuantMode quantMmMode)
+{
+    auto splitNum = args.mValue / PERBLOCK_SCALE_SIZE;
+    auto tileM = (args.mValue - rcfCfg.tailM * rcfCfg.tailCnt) / rcfCfg.tileCnt;
+
+    if (tileM % PERBLOCK_SCALE_SIZE == 0) {
+        return;
+    } else {
+        tileM = (tileM / PERBLOCK_SCALE_SIZE) * PERBLOCK_SCALE_SIZE;
+    }
+
+    rcfCfg.tailM = args_.mValue - tileM * rcfCfg.tileCnt;
+    tileMValue_ = tileM;
+    tailMValue_ = rcfCfg.tailM;
+}
+
 ge::graphStatus AllGatherMatmulTilingBase::AdjustHCCLLimit(Mc2Tiling::RCSTiling& rcfCfg, mc2tiling::Mc2QuantMode quantMmMode)
 {    
     if (tileMValue_ * args_.kValue * ge::GetSizeByDataType(args_.geAType) * args_.rankDim <= mc2tiling::ALL_GATHER_HCCL_MEM_LIMIT) {
