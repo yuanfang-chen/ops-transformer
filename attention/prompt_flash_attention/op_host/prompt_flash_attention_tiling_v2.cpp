3bcb530a1921b55c79eb4387a97cfd479e36b83e
liujiawei-21 <liujiawei56@huawei.com>
2026-03-16 20:27:30 +0800

    解除tnd和rope限制

--- attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling_v2.cpp
+++ attention/prompt_flash_attention/op_host/prompt_flash_attention_tiling_v2.cpp
@@ -1888,7 +1888,7 @@ bool PromptFlashAttentionTilingV2::CheckRope(ContextParamsForPFATiling& contextK
         return false);
     enableIFA = false;
     enableIFAMask = false;
-    enablePFAMerge = false;
+    // enablePFAMerge = false;
     if (queryShapeInfo.d == QUERY_SHAPE_DIM_D_128_TILING_V2) {
         enablePFARope = true;
     } else {
@@ -2312,9 +2312,9 @@ bool PromptFlashAttentionTilingV2::CheckActSeqLen(ContextParamsForPFATiling& con
                 "Actual_seq_lengths[%u](%ld) must be in range[0, %u]!", i, actSeqTmp, queryShapeInfo.s),
                 return false);
             // query act seq len padding情况下不支持合轴
-            if (actSeqTmp < queryShapeInfo.s) {
-                enablePFAMerge = false;
-            }
+            // if (actSeqTmp < queryShapeInfo.s) {
+                // enablePFAMerge = false;
+            // }
         }
         t1Size = actSeqLen->GetData<int64_t>()[actSeqLengthSize - 1];
     }
