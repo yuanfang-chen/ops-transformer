8cc5184a86a04a445f03e054191694f907019b8d
oreo_hw <wangyuanhe2@huawei.com>
2026-03-16 20:25:13 +0800

    remove LSE fia_tiling_info

--- attention/common/op_host/arch32/fia_tiling_nonquant_mla.cpp
+++ attention/common/op_host/arch32/fia_tiling_nonquant_mla.cpp
@@ -438,7 +438,7 @@ void FiaTilingNonQuantMla::FillTilingBaseParams()
     tilingData_.baseParams.set_outputLayout(static_cast<uint32_t>(fiaInfo_->outputLayout));
     tilingData_.baseParams.set_needInit(fiaInfo_->needInit);
     tilingData_.baseParams.set_usedCoreNum(usedCoreNum_);
-    tilingData_.baseParams.set_softmaxLseFlag(fiaInfo_->softmaxLseFlag ? 1 : 0);
+    
 }
 
 void FiaTilingNonQuantMla::FillTilingPageAttenParams()
