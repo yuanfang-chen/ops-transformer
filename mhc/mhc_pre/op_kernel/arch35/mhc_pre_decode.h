02c17aaa28adb74b2b45e08d07d6771d7e8d9f4b
LiWeiJian16 <liweijian16@huawei.com>
2026-03-17 15:22:09 +0800

    去除额外搬运

--- mhc/mhc_pre/op_kernel/arch35/mhc_pre_decode.h
+++ mhc/mhc_pre/op_kernel/arch35/mhc_pre_decode.h
@@ -611,11 +611,6 @@ __aicore__ inline void MhcPreKernelDecode<T, P>::AIV1Prologue(uint64_t offsetT, 
     }
     HMixProcess(offsetT, lenT);
 
-    // FIX：重新搬进来
-    SetFlag<HardEvent::MTE3_MTE2>(EVENT_ID1);
-    WaitFlag<HardEvent::MTE3_MTE2>(EVENT_ID1);
-
-    HMixCopyIn(HMixOffset, lenT);
     matmulRes_ = xInQueue_.DeQue<P>(); 
     
     __ubuf__ P* matmulPtr = (__ubuf__ P*)matmulRes_.GetPhyAddr();
@@ -935,8 +930,7 @@ __aicore__ inline void MhcPreKernelDecode<T, P>::HMixProcess(uint64_t offsetT, u
     MixOutCopyParams.srcStride = uint32_t(0); // 相邻块的间隔
     MixOutCopyParams.dstStride = uint32_t(0);                                                   // 相邻块的间隔
     DataCopyPad(mmResGm_[HMixOffset], hMixLocal, MixOutCopyParams);
-    // xInQueue_.EnQue(hMixLocal);
-    xInQueue_.FreeTensor(hMixLocal);
+    xInQueue_.EnQue(hMixLocal);
 }
 
 template <class T, class P>
