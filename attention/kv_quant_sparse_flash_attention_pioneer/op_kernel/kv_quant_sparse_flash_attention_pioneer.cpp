081ff76ae7cafb4ce55b52f9c8e017a841a703a1
wangyuanhe1 <wangyuanhe1@huawei.com>
2026-03-17 14:29:10 +0800

    bug

--- attention/kv_quant_sparse_flash_attention_pioneer/op_kernel/kv_quant_sparse_flash_attention_pioneer.cpp
+++ attention/kv_quant_sparse_flash_attention_pioneer/op_kernel/kv_quant_sparse_flash_attention_pioneer.cpp
@@ -30,7 +30,7 @@ using namespace AscendC;
             BaseApi::QSFAVectorServiceDummy<__VA_ARGS__>, BaseApi::QSFAVectorService<__VA_ARGS__>>::type; \
         templateClass<CubeBlockType, VecBlockType> op;                                                    \
         op.Init(query, key, value, sparseIndices, keyScale, valueScale, blocktable,                       \
-            actualSeqLengthsQuery, actualSeqLengthsKV, nullptr, nullptr,                                  \
+            actualSeqLengthsQuery, actualSeqLengthsKV, keySink, valueSink,                                  \
 	    attentionOut, user, nullptr, &tPipe);                                                             \
         op.Process();                                                                                     \
     } while (0)
@@ -45,7 +45,7 @@ using namespace AscendC;
         GET_TILING_DATA_WITH_STRUCT(tilingdataClass, tilingDataIn, tiling);                               \
         const tilingdataClass *__restrict tilingData = &tilingDataIn;                                     \
         op.Init(query, key, value, sparseIndices, keyScale, valueScale, blocktable,                       \
-            actualSeqLengthsQuery, actualSeqLengthsKV, nullptr, nullptr,                                  \
+            actualSeqLengthsQuery, actualSeqLengthsKV, keySink, valueSink,                                  \
 	    attentionOut, user, tilingData, &tPipe);                                                          \
         op.Process();                                                                                     \
     } while (0)
