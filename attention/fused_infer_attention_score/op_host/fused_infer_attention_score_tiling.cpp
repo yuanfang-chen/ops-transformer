b4febfcadce56cbfbb19c5592e57e376c34e7d7a
qq_23260455 <liujiali27@h-partners.com>
2026-03-17 10:45:30 +0800

    fix:kvnz shape

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -1152,8 +1152,8 @@ ge::graphStatus CheckFAISinglePara(const gert::TilingContext *context, bool isPa
             tempVD = (tempV->GetStorageShape().GetDim(DIM_2)) / kvHeadNum;
             cacheBlockSize = tempK->GetStorageShape().GetDim(DIM_1);
         } else if (tempk->GetStorageShape().GetDim() == 5U) {
-            tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16;
-            tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16;
+            tempKD = (tempK->GetStorageShape().GetDim(DIM_2)) * 16;
+            tempVD = (tempV->GetStorageShape().GetDim(DIM_2)) * 16;
             cacheBlockSize = tempK->GetStorageShape().GetDim(DIM_3);
         }
         OP_CHECK_IF(inputBlockSize != cacheBlockSize,
@@ -1457,8 +1457,8 @@ static bool IsUsingFAI(gert::TilingContext &context, const string inputLayoutStr
                 usingFAI = true;
             }
         } else if (kvDimNum == 5U) {
-            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_1)) * 16;
-            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_1)) * 16;
+            int64_t tempKD = (tempK->GetStorageShape().GetDim(DIM_2)) * 16;
+            int64_t tempVD = (tempV->GetStorageShape().GetDim(DIM_2)) * 16;
             int64_t blockSize = tempK->GetStorageShape().GetDim(DIM_3);
             bool isFAIDSize = (tempD <= 256U && tempKD <= 256 && tempVD <= 256) &&
                     (tempD == tempKD && tempD == tempVD) && (blockSize % BLOCK_SIZE_ALIGN_16 == 0);
