024452d79ac6532aa29c7ee77a110a1aed64fa2a
alexeyvaryzgin <varyzgin.alexey@gmail.com>
2026-03-17 13:29:13 +0300

    Singular Value Decomposition (SVD)

---
+++ experimental/svd/ascendc/jacobi/op_kernel/svd_common.h
@@ -0,0 +1,26 @@
+/**
+ * This program is free software, you can redistribute it and/or modify it.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file svd_common.h
+ * \brief
+ */
+
+#ifndef SVD_COMMON_H
+#define SVD_COMMON_H
+
+#include "kernel_operator.h"
+
+// event MTE3 <-> MTE2
+#define EVT_MTE3_MTE2_0 EVENT_ID3
+#define EVT_MTE3_MTE2_1 EVENT_ID4
+#define EVT_MTE3_MTE2_2 EVENT_ID5
+
+#endif //SVD_COMMON_H
