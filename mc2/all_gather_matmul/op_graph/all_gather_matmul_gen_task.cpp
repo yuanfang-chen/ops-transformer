e4e1d9e840759273e0d9b23060138852cb2d153c
hid81635372 <wujianfei15@huawei.com>
2026-03-17 10:07:27 +0800

    delete build_open_project of gen_task

--- mc2/all_gather_matmul/op_graph/all_gather_matmul_gen_task.cpp
+++ mc2/all_gather_matmul/op_graph/all_gather_matmul_gen_task.cpp
@@ -29,7 +29,6 @@
 #endif
 
 namespace ops {
-#ifdef BUILD_OPEN_PROJECT
 static ge::Status AllGatherMatmulCalcOpParam(gert::ExeResGenerationContext *context) {
     return Mc2GenTaskOpsUtils::CommonKFCMc2CalcParamFunc(context, "aicpu kfc server", "kfc_stream");
 }
@@ -40,20 +39,4 @@ static ge::Status AllGatherMatmulGenTask(const gert::ExeResGenerationContext *co
 }
 
 IMPL_OP(AllGatherMatmul).CalcOpParam(AllGatherMatmulCalcOpParam).GenerateTask(AllGatherMatmulGenTask);
-#else // mc2 gen task utils
-static ge::Status AllGatherMatmulGenTaskCallback(const gert::ExeResGenerationContext *context,
-                                                 std::vector<domi::TaskDef> &tasks) {
-    return Mc2GenTaskUtils::Mc2GenTaskCallBack910A2(context, tasks);
-}
-
-static ge::Status AllGatherMatmulCalcOpParam(gert::ExeResGenerationContext *context) {
-    return Mc2GenTaskUtils::CommonKFCMc2CalcParamFunc(context, "aicpu kfc server", "kfc_stream");
-}
-
-static ge::Status AllGatherMatmulGenTask(const gert::ExeResGenerationContext *context,
-                                         std::vector<std::vector<uint8_t>> &tasks) {
-    return Mc2GenTaskUtils::CommonKFCMc2GenTask(context, tasks, AllGatherMatmulGenTaskCallback);
-}
-IMPL_OP_CT(AllGatherMatmul).CalcOpParam(AllGatherMatmulCalcOpParam).GenerateTask(AllGatherMatmulGenTask);
-#endif
 } // namespace ops
