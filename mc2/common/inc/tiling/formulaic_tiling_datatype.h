ac630a1a74134ce350802db69998fe17dbe60ce2
yufang11 <yufang11@huawei.com>
2026-03-17 22:26:23 +0800

    [MC2] support the fit-based balance tiling for MatmulReduceScatter & MatmulAllReduce in 950.

--- mc2/common/inc/tiling/formulaic_tiling_datatype.h
+++ mc2/common/inc/tiling/formulaic_tiling_datatype.h
@@ -52,6 +52,8 @@ enum class KernelType {
     ALL_GATHER,
     REDUCE_SCATTER,
     ALL_TO_ALL,
+    REDUCE_SCATTER_VIA_ALL_TO_ALL,
+    ALL_REDUCE_VIA_ALL_TO_ALL_ALL_GATHER,
 };
 
 enum class MatmulCalcType {
