bf97284fa44e1260977a045df82623dea338d28f
h00808442 <hanxiaobin3@huawei.com>
2026-03-17 10:40:56 +0800

    tmp

--- attention/prompt_flash_attention/op_kernel/arch32/kernel_operator_softmax_compute_nz.h
+++ attention/prompt_flash_attention/op_kernel/arch32/kernel_operator_softmax_compute_nz.h
@@ -25,7 +25,7 @@
 #else
 #include "kernel_operator.h"
 #endif
-#include "kernel_pop_stack_buffer.h"
+#include "kernel_operator.h"
 #include "kernel_tiling/kernel_tiling.h"
 // test
 #pragma begin_pipe(V)
