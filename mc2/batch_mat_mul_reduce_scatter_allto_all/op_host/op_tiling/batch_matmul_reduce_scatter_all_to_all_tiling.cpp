5c1715a11ed8b98ae71329b6a103cd258da034cf
hello_simida <wangyi206@huawei.com>
2026-03-17 14:57:30 +0800

    调整mc2/common目录结构-op_tiling添加op_host

--- mc2/batch_mat_mul_reduce_scatter_allto_all/op_host/op_tiling/batch_matmul_reduce_scatter_all_to_all_tiling.cpp
+++ mc2/batch_mat_mul_reduce_scatter_allto_all/op_host/op_tiling/batch_matmul_reduce_scatter_all_to_all_tiling.cpp
@@ -28,12 +28,12 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-#include "op_tiling/mc2_tiling_common_var.h"
+#include "op_host/op_tiling/mc2_tiling_common_var.h"
 #include "mc2_hcom_topo_info.h"
 #include "mc2_log.h"
 #include "graph/utils/type_utils.h"
 #include "register/op_def_registry.h"
-#include "op_tiling/mc2_tiling_utils.h"
+#include "op_host/op_tiling/mc2_tiling_utils.h"
 #include "batch_matmul_reduce_scatter_all_to_all_formulaic_tiling.h"
 #include "common/utils/op_mc2.h"
 #include "mc2_moe_utils.h"
