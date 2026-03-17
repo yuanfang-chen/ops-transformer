8a196a8bb183542d84d7262ca4362c593492b69e
libohao6 <libohao3@huawei.com>
2026-03-17 22:03:42 +0800

    adapt 3101 to 3510

--- mc2/allto_allv_quant_grouped_mat_mul/op_host/op_tiling/allto_allv_quant_grouped_mat_mul_tiling_base.cpp
+++ mc2/allto_allv_quant_grouped_mat_mul/op_host/op_tiling/allto_allv_quant_grouped_mat_mul_tiling_base.cpp
@@ -164,7 +164,7 @@ ge::graphStatus AlltoAllvGmmTilingBase::CheckEpWorldSizeValue()
     // check epWorldSize in socVersion
     std::vector<int64_t> epWorldSizeValueList;
     std::string epWorldSizeValueStr = "";
-    if (socVersion_ == platform_ascendc::SocVersion::ASCEND950) {
+    if (op::GetCurrentPlatformInfo().GetCurNpuArch() == NpuArch::DAV_3510) {
         epWorldSizeValueList = { 2, 4, 8, 16, 32, 64, 128, 256 }; // epWorldSize value only support 2, 4, 8, 16, 32, 64, 128, 256
     } else {
         epWorldSizeValueList = { 8, 16, 32, 64, 128 }; // epWorldSize value only support 8, 16, 32, 64, 128
