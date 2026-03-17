e2d09eb8b64ca40b8933ede2ee9d44a9f3152898
hello_simida <wangyi206@huawei.com>
2026-03-17 14:57:30 +0800

    调整mc2/common目录结构-op_tiling添加op_host

--- mc2/matmul_allto_all/op_host/op_tiling/arch32/matmul_allto_all_tiling_910b.cpp
+++ mc2/matmul_allto_all/op_host/op_tiling/arch32/matmul_allto_all_tiling_910b.cpp
@@ -17,7 +17,7 @@
 #include "mc2_log.h"
 #include "common/utils/op_mc2.h"
 #include "mc2_hcom_topo_info.h"
-#include "op_tiling/mc2_tiling_utils.h"
+#include "op_host/op_tiling/mc2_tiling_utils.h"
 #include <map>
 #include "matmul_allto_all_tiling_910b.h"
 
