43d6711c8d84ef429f79d2b2ce352fa92fd37602
ZhouChuping <zhouchuping1@huawei.com>
2026-03-17 11:36:22 +0800

    优化moeInitRoutingV3 非量化gatherOut场景

--- moe/moe_init_routing_v3/op_kernel/moe_v3_gather_out.h
+++ moe/moe_init_routing_v3/op_kernel/moe_v3_gather_out.h
@@ -142,7 +142,9 @@ __aicore__ inline void MoeGatherOut<T, EP>::Init(GM_ADDR x, GM_ADDR scale, GM_AD
 
     pipe_->InitBuffer(expandedRowIdxCopyInQueue_, GATHER_OUT_BUFFER_NUM,
                       AlignBytes(curCorePerLoopIndicesElements_, sizeof(int32_t)));
-    pipe_->InitBuffer(xCopyInQueue_, GATHER_OUT_BUFFER_NUM, AlignBytes(perLoopCols_, sizeof(T)));
+    int64_t xCopyInQueueBufferNum = std::max(tilingData->gatherOutComputeParamsOp.xCopyInQueueBufferNum,
+                                                GATHER_OUT_BUFFER_NUM);
+    pipe_->InitBuffer(xCopyInQueue_, xCopyInQueueBufferNum, AlignBytes(perLoopCols_, sizeof(T)));
     pipe_->InitBuffer(scaleCopyInQueue_, GATHER_OUT_BUFFER_NUM, AlignBytes(1, sizeof(float)));
 
     sortedExpertIdxGm_.SetGlobalBuffer((__gm__ int32_t *)workspace + blockIdx_ * perCoreIndicesElements_,
