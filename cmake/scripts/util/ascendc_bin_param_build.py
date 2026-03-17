77ff94e483dc2b8c6f666c1c5b310cc537047b64
hid81635372 <wujianfei15@huawei.com>
2026-03-17 19:34:32 +0800

    multi bisehng_flags input

--- cmake/scripts/util/ascendc_bin_param_build.py
+++ cmake/scripts/util/ascendc_bin_param_build.py
@@ -676,15 +676,35 @@ def parse_args(argv):
     return parser.parse_args(argv)
 
 
+def get_bisheng_flags(input_options):
+    bisheng_flags_option = ['oom', 'dump_cce', 'dump_bin', 'dump_loc', 'ccec_o0', 'ccec_g', 'check_flag', 'sanitizer']
+    bisheng_flags = ""
+    found_flags = set()
+    for elem in args.argv:
+        if ',' not in elem or elem.count(',') == 1 \
+            and not any(elem.startswith(prefix) for prefix in bisheng_flags_option):
+
+            if elem in bisheng_flags_option:
+                found_flags.add(elem)
+        else:
+            parts = elem.split(',')
+            for part in parts:
+                part = part.strip()
+                if part in bisheng_flags_option:
+                    found_flags.add(part)
+
+    if len(found_flags) > 0:
+        bisheng_flags = ",".join(found_flags)
+    return bisheng_flags
+
+
 if __name__ == '__main__':
     args = parse_args(sys.argv)
     if len(args.argv) <= 3:
         raise RuntimeError('arguments must greater than 3')
-    bisheng_flags_option = ['oom', 'dump_cce', 'dump_bin', 'dump_loc', 'ccec_o0', 'ccec_g', 'check_flag_sanitizer']
-    input_bisheng_flags = ""
-    for elem in args.argv:
-        if elem in bisheng_flags_option:
-            input_bisheng_flags = elem
+
+    input_bisheng_flags = get_bisheng_flags(args.argv)
+
     gen_bin_param_file(args.argv[1],
                     args.argv[2],
                     args.argv[3],
