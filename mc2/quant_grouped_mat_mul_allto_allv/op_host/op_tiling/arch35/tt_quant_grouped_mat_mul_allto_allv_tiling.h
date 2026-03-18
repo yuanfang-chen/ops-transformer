9001ebe9ee0916990f220ccb9cf8ef6a5d073864
huangshuai <huangshuai59@huawei.com>
2026-03-18 17:51:12 +0800

    解决代码冲突

--- mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
+++ mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
@@ -15,8 +15,6 @@
 
 #ifndef TT_QUANT_GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H
 #define TT_QUANT_GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H
-#ifndef TT_QUANT_GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H
-#define TT_QUANT_GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H
 
 #pragma once
 #include "securec.h"
@@ -34,9 +32,7 @@
 namespace optiling {
 namespace Mc2GroupedMatmul {
 class TTQuantGroupedMatmulAllToAllvTiling : public QuantGroupedMatmulAllToAllvTilingBase {
-class TTQuantGroupedMatmulAllToAllvTiling : public QuantGroupedMatmulAllToAllvTilingBase {
 public:
-    explicit TTQuantGroupedMatmulAllToAllvTiling(gert::TilingContext *context) : QuantGroupedMatmulAllToAllvTilingBase(context) {};
     explicit TTQuantGroupedMatmulAllToAllvTiling(gert::TilingContext *context) : QuantGroupedMatmulAllToAllvTilingBase(context) {};
     void Reset(gert::TilingContext *context) override
     {
