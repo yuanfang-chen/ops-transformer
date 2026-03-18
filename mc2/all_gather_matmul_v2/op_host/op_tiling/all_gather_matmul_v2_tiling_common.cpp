34a5b0d734c762898d9374b3b224b740549aa7e8
hblnb <huangbolin3@huawei.com>
2026-03-18 10:39:44 +0800

    tiling代码芯片隔离

---
+++ mc2/all_gather_matmul_v2/op_host/op_tiling/all_gather_matmul_v2_tiling_common.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file all_gather_matmul_v2_tiling_common.cpp
+ * \brief
+ */
+
+#include "mc2_log.h"
+#include "tiling_base/tiling_templates_registry.h"
+#include "graph/utils/type_utils.h"
+#include "register/op_def_registry.h"
+#include "platform/platform_infos_def.h"
+#include "all_gather_matmul_v2_tiling_common.h"
+
+using namespace AscendC;
+using namespace ge;
+
+namespace optiling {
+ge::graphStatus AllGatherMatmulTilingV2Func(gert::TilingContext* context);
+struct AllGatherMatmulCompileInfo {
+};
+ge::graphStatus TilingParseForAllGatherMatmulV2(gert::TilingParseContext* context)
+{
+    (void)context;
+    return ge::GRAPH_SUCCESS;
+}
+
+IMPL_OP_OPTILING(AllGatherMatmulV2)
+    .Tiling(AllGatherMatmulTilingV2Func)
+    .TilingParse<AllGatherMatmulCompileInfo>(TilingParseForAllGatherMatmulV2);
+}  // namespace optiling
