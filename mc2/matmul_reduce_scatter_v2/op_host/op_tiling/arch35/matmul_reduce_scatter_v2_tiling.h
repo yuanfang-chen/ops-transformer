5d680c7bc63595d343dba2fc7ab1725549813cc6
hblnb <huangbolin3@huawei.com>
2026-03-18 10:39:44 +0800

    tiling代码芯片隔离

--- mc2/matmul_reduce_scatter_v2/op_host/op_tiling/arch35/matmul_reduce_scatter_v2_tiling.h
+++ mc2/matmul_reduce_scatter_v2/op_host/op_tiling/arch35/matmul_reduce_scatter_v2_tiling.h
@@ -42,6 +42,7 @@ protected:
     ge::graphStatus CheckInput() override;
     ge::graphStatus DoAllMatmulTiling();
     void PrintAllTilingData() const;
+    CutResult GetTilingResult() override;
     ge::graphStatus SetMc2Hcomm();
     ge::graphStatus DoMatmulV3Tiling(Mc2MatmulHelper::Mc2MatmulTilingCfg &tilingCfg, Mc2MMRegisterCfg &registerCfg,
                                      Mc2MatMulV3TilingData &tilingData);
@@ -59,7 +60,6 @@ private:
     Mc2MatMulV3Args mmV3Args_;
     Mc2MatmulV3CompileInfo compileInfo_;
 };
-ge::graphStatus MatmulReduceScatterTilingV2AivModeFunc(gert::TilingContext *context);
 } // namespace optiling
 
 
