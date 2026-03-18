42bb962c167461f13037b3497d869679e4ee0dd1
qq_43844249 <fanglin17@huawei.com>
2026-03-18 10:32:27 +0800

    [WIP]mxfp4 kernel

--- mc2/allto_all_matmul/op_kernel/arch35/allto_all_mx_quant_matmul_arch35.h
+++ mc2/allto_all_matmul/op_kernel/arch35/allto_all_mx_quant_matmul_arch35.h
@@ -29,7 +29,7 @@ __aicore__ inline T1 CeilDiv(T1 a, T2 b)
     return (a + b - 1) / b;
 }
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 class AlltoAllMxQuantMatmulArch35 {
 public:
     __aicore__ inline AlltoAllMxQuantMatmulArch35(SchedulerType *pipeLine) : pipeLine_(pipeLine){};
@@ -54,6 +54,7 @@ private:
     GM_ADDR commOutGM_;
     GM_ADDR transX1ScaleGM1_;
     GM_ADDR transOutGM_;
+    uint64_t rankForComm_;
 private:
     static constexpr uint64_t MXFP_GROUP_SIZE = 64UL;
  	static constexpr uint64_t NUM_TWO = 2UL;
@@ -63,9 +64,9 @@ private:
     __aicore__ inline void ProcessScale();
 };
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 __aicore__ inline void
-AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType>::Init(
+AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType, IsMxFp4>::Init(
     GM_ADDR x1, GM_ADDR x2, GM_ADDR bias, GM_ADDR y, GM_ADDR all2all_out, GM_ADDR x1_scale, GM_ADDR x2_scale,
     GM_ADDR workspaceGM, AlltoAllMatmulTilingDataType *tilingData, TPipe *tPipe)
 {
@@ -83,13 +84,18 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     // MX场景下，通信数据的存储顺序是commX1Scale、commX1、transX1Scale_、transOut
     commX1ScaleGM1_ = workspaceGM;
     uint64_t x1ScaleLen = CeilDiv(mc2Tiling_.rankK, MXFP_GROUP_SIZE) * NUM_TWO *
-                 (uint64_t)mc2Tiling_.rankM * sizeof(AscendC::fp8_e8m0_t);
+                  (uint64_t)mc2Tiling_.rankM * sizeof(AscendC::fp8_e8m0_t);
     x1ScaleLen = CeilDiv(x1ScaleLen, ALIGN_NUM) * ALIGN_NUM; // 对齐
     commOutGM_ = workspaceGM + x1ScaleLen;
     transX1ScaleGM1_ = commOutGM_ + mc2Tiling_.commLen;
     transOutGM_ = all2all_out;
     if (all2all_out == nullptr) {
         transOutGM_ = transX1ScaleGM1_ + x1ScaleLen;
+    }
+    if constexpr (IsMxFp4) {
+        rankForComm_ = (tilingData_->alltoAllQuantMatmulTilingInfo.rankK + 1) / 2 ;
+    } else {
+        rankForComm_ = tilingData_->alltoAllQuantMatmulTilingInfo.rankK;
     }
 
     // 初始化流水线
@@ -98,9 +104,9 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLine_->GetContext(&pipeLineContext_);
 }
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 __aicore__ inline void
-AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType>::Process()
+AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType, IsMxFp4>::Process()
 {
     auto &&mc2Tiling_ = tilingData_->alltoAllQuantMatmulTilingInfo;
 
@@ -121,9 +127,9 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLine_->End();
 }
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 __aicore__ inline void
-AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType>::ProcessScale()
+AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType, IsMxFp4>::ProcessScale()
 {
     auto &&mc2Tiling_ = tilingData_->alltoAllQuantMatmulTilingInfo;
 
@@ -153,13 +159,13 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLine_->ProcessScale();
 }
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 __aicore__ inline void
-AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType>::ProcessTile(uint32_t taskCnt)
+AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType, IsMxFp4>::ProcessTile(uint32_t taskCnt)
 {
     auto &&mc2Tiling_ = tilingData_->alltoAllQuantMatmulTilingInfo;
     // 复用的变量
-    uint64_t tileMMultiRankK = (uint64_t)mc2Tiling_.tileM * (uint64_t)mc2Tiling_.rankK;
+    uint64_t tileMMultiRankK = (uint64_t)mc2Tiling_.tileM * rankForComm_;
 
     // 通信相关地址和偏移
     pipeLineContext_.communicationContext->taskCnt = mc2Tiling_.tileCnt;
@@ -169,7 +175,7 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLineContext_.communicationContext->recvOffset = pipeLineContext_.communicationContext->sendOffset;
     pipeLineContext_.communicationContext->sendCount = tileMMultiRankK;
     pipeLineContext_.communicationContext->strideCount =
-        (uint64_t)mc2Tiling_.rankM * (uint64_t)mc2Tiling_.rankK / (uint64_t)mc2Tiling_.rankDim;
+        (uint64_t)mc2Tiling_.rankM * rankForComm_ / mc2Tiling_.rankDim;
     pipeLineContext_.communicationContext->hcclDataType = mc2Tiling_.hcclDataType;
 
     // 转置相关地址和偏移
@@ -177,15 +183,15 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLineContext_.transposeContext->transposeDstAddr = transOutGM_;
     pipeLineContext_.transposeContext->transposeSrcOffset = tileMMultiRankK * sizeof(DTYPE_X1);
     pipeLineContext_.transposeContext->transposeDstOffset =
-        tileMMultiRankK * (uint64_t)mc2Tiling_.rankDim * sizeof(DTYPE_X1);
+        tileMMultiRankK * mc2Tiling_.rankDim * sizeof(DTYPE_X1);
     pipeLineContext_.transposeContext->nextSrcBlockOffset =
-        (uint64_t)mc2Tiling_.rankM * (uint64_t)mc2Tiling_.rankK / (uint64_t)mc2Tiling_.rankDim;
-    pipeLineContext_.transposeContext->nextDstBlockOffset = (uint64_t)mc2Tiling_.rankK;
+        (uint64_t)mc2Tiling_.rankM * rankForComm_ / mc2Tiling_.rankDim;
+    pipeLineContext_.transposeContext->nextDstBlockOffset = rankForComm_;
     pipeLineContext_.transposeContext->rankCnt = (uint64_t)mc2Tiling_.rankDim;
-    pipeLineContext_.transposeContext->innerAxis = (uint64_t)mc2Tiling_.rankK;
+    pipeLineContext_.transposeContext->innerAxis = rankForComm_;
     pipeLineContext_.transposeContext->transM = (uint64_t)mc2Tiling_.tileM;
-    pipeLineContext_.transposeContext->innerOffsetIn = (uint64_t)mc2Tiling_.rankK;
-    pipeLineContext_.transposeContext->innerOffsetOut = (uint64_t)mc2Tiling_.rankK * mc2Tiling_.rankDim;
+    pipeLineContext_.transposeContext->innerOffsetIn = rankForComm_;
+    pipeLineContext_.transposeContext->innerOffsetOut = rankForComm_ * mc2Tiling_.rankDim;
 
     // quantMatmul相关的地址和偏移
     pipeLineContext_.computationContext->baseData.aGM = transOutGM_;
@@ -207,14 +213,14 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLine_->Process(taskCnt);
 }
 
-template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType>
+template <typename SchedulerType, typename SchedulerContextType, typename AlltoAllMatmulTilingDataType, bool IsMxFp4>
 __aicore__ inline void
-AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType>::ProcessTail(uint32_t taskCnt)
+AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulTilingDataType, IsMxFp4>::ProcessTail(uint32_t taskCnt)
 {
     auto &&mc2Tiling_ = tilingData_->alltoAllQuantMatmulTilingInfo;
-    uint64_t tailMMultiRankK = (uint64_t)mc2Tiling_.tailM * (uint64_t)mc2Tiling_.rankK;
+    uint64_t tailMMultiRankK = (uint64_t)mc2Tiling_.tailM * rankForComm_;
     uint64_t tileCntMultitileMMultiRankK =
-        (uint64_t)mc2Tiling_.tileCnt * (uint64_t)mc2Tiling_.tileM * (uint64_t)mc2Tiling_.rankK;
+        (uint64_t)mc2Tiling_.tileCnt * mc2Tiling_.tileM * rankForComm_;
 
     // 通信相关地址和偏移
     pipeLineContext_.communicationContext->taskCnt = mc2Tiling_.tailCnt;
@@ -224,6 +230,7 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLineContext_.communicationContext->recvOffset = pipeLineContext_.communicationContext->sendOffset;
     pipeLineContext_.communicationContext->sendCount = tailMMultiRankK;
 
+    // 转置相关地址和偏移
     pipeLineContext_.transposeContext->transposeSrcAddr = commOutGM_ + tileCntMultitileMMultiRankK * sizeof(DTYPE_X1);
     pipeLineContext_.transposeContext->transposeDstAddr =
         transOutGM_ + tileCntMultitileMMultiRankK * (uint64_t)mc2Tiling_.rankDim * sizeof(DTYPE_X1);
@@ -231,6 +238,7 @@ AlltoAllMxQuantMatmulArch35<SchedulerType, SchedulerContextType, AlltoAllMatmulT
     pipeLineContext_.transposeContext->transposeDstOffset = tailMMultiRankK * (uint64_t)mc2Tiling_.rankDim * sizeof(DTYPE_X1);
     pipeLineContext_.transposeContext->transM = (uint64_t)mc2Tiling_.tailM;
 
+    // 计算相关地址和偏移
     pipeLineContext_.computationContext->baseData.aGM =
         transOutGM_ + tileCntMultitileMMultiRankK * sizeof(DTYPE_X1) * mc2Tiling_.rankDim;
     pipeLineContext_.computationContext->baseData.bGM = x2_;
