4dd029726e714f17cd12e41796b1d382b4082753
m30040597 <mgl1984@163.com>
2026-03-18 10:34:27 +0800

    add template init

--- gmm/grouped_matmul/op_kernel/grouped_matmul_a4w4_interface.cpp
+++ gmm/grouped_matmul/op_kernel/grouped_matmul_a4w4_interface.cpp
@@ -50,15 +50,19 @@ struct TileCopyGMMPTD : public Catlass::Gemm::Tile::QuantTileCopy<ArchTag, AType
     using CopyGmToL1Scale = typename Base::CopyGmToL1Scale;
     using CopyL1ToFP = typename Base::CopyL1ToFP;
 };
-template <typename WeightDType, typename CDType, typename ScaleDType, typename GrouplistDType,                                                                                                                                                               
-          typename PerTokenScaleDType, typename YDType>
+template <typename XDType = AscendC::int4b_t,
+          typename WeightDType = AscendC::int4b_t,
+          typename CDType = half,
+          typename ScaleDType = uint64_t,
+          typename GrouplistDType = int64_t,
+          typename PerTokenScaleDType = float,
+          typename YDType = bfloat16_t>
 CATLASS_DEVICE void grouped_matmul_a4w4_catlass(uint32_t m, uint32_t k, uint32_t n, uint32_t groupNum, uint64_t quantGroupNum,
                                         GM_ADDR gmA, GM_ADDR gmB, GM_ADDR gmScale, GM_ADDR group_list, 
                                         GM_ADDR per_token_scale, GM_ADDR y, GM_ADDR workspace, uint32_t aicCoreNum) {
     using LayoutA = Catlass::layout::RowMajor;
     using LayoutB = Catlass::layout::zN;
     using LayoutD = Catlass::layout::RowMajor;
-    using XDType = AscendC::int4b_t;
     LayoutA layoutA{m, k};
     LayoutB layoutB = LayoutB::template MakeLayout<WeightDType>(k, n);
     Catlass::layout::VectorLayout layoutScale{n};
