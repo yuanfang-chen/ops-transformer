164fa59da0f81c073ce48d3e008e4a4591150453
tooslow <zhangzhizhuo1@huawei.com>
2026-03-19 10:13:53 +0800

    sc清理

--- gmm/grouped_matmul_finalize_routing/examples/arch35/test_aclnn_grouped_matmul_finalize_routing_weightnz.cpp
+++ gmm/grouped_matmul_finalize_routing/examples/arch35/test_aclnn_grouped_matmul_finalize_routing_weightnz.cpp
@@ -174,7 +174,7 @@ int main()
     std::vector<float> pertokenScaleHostData(GetShapeSize(pertokenScaleShape));
     std::vector<int64_t> groupListHostData(GetShapeSize(groupListShape));
     // 对groupList赋值
-    groupListHostData[0] = 64;
+    groupListHostData[0] = m;
 
     std::vector<uint16_t> sharedInputHostData(GetShapeSize(sharedInputShape));
     std::vector<int64_t> logitHostData(GetShapeSize(logitShape));
