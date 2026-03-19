4159976786cdb8286caaabc3592579088ff4d18d
EugenV <volosenkov.eugeny@huawei.com>
2026-03-19 16:31:03 +0300

    fix headers and tiling files for Jacobi kernel

--- experimental/svd/ascendc/jacobi/op_kernel/jacobi.cpp
+++ experimental/svd/ascendc/jacobi/op_kernel/jacobi.cpp
@@ -1,12 +1,12 @@
 /**
- * This program is free software, you can redistribute it and/or modify it.
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /*!
  * \file jacobi.cpp
@@ -29,12 +29,10 @@ extern "C" __global__ __aicore__ void jacobi(__gm__ uint8_t * a,
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
     if (tilingData.vecInstructionSize == 32) {
         SVD::JacobiBase<float, float, float, float, 32> op(&pipe);
-        // SVD::JacobiBase<float, float, float, float> op(&pipe);
         op.Init(a, s, u, v, workspace, &tilingData);
         op.Process();
     } else if (tilingData.vecInstructionSize == 64) {
         SVD::JacobiBase<float, float, float, float, 64> op(&pipe);
-        // SVD::JacobiBase<float, float, float, float> op(&pipe);
         op.Init(a, s, u, v, user, &tilingData);
         op.Process();
     }
