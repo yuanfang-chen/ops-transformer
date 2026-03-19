948b78aa57629353f8a0e3d7c091ce7a1aa648f4
wigginsli <1747384449@qq.com>
2026-03-19 16:10:37 +0800

    fix test_aclnn

--- mhc/mhc_pre/examples/test_aclnn_mhc_pre.cpp
+++ mhc/mhc_pre/examples/test_aclnn_mhc_pre.cpp
@@ -249,184 +249,121 @@ int CreateAclTensorFloat32Output(
   return 0;
 }
 
+struct Tensors {
+  void* x_addr = nullptr, *phi_addr = nullptr, *alpha_addr = nullptr, *bias_addr = nullptr, *gamma_addr = nullptr;
+  void* hin_addr = nullptr, *h_post_addr = nullptr, *h_res_addr = nullptr, *inv_rms_addr = nullptr;
+  void* h_mix_addr = nullptr, *h_pre_addr = nullptr;
+  aclTensor *x = nullptr, *phi = nullptr, *alpha = nullptr, *bias = nullptr, *gamma = nullptr;
+  aclTensor *hin = nullptr, *h_post = nullptr, *h_res = nullptr, *inv_rms = nullptr;
+  aclTensor *h_mix = nullptr, *h_pre = nullptr;
+};
+
+int CreateInputTensors(const std::vector<int64_t>& x_shape, const std::vector<int64_t>& phi_shape,
+                       const std::vector<int64_t>& alpha_shape, const std::vector<int64_t>& bias_shape,
+                       const std::vector<int64_t>& gamma_shape, Tensors& tensors) {
+  std::vector<float> x_host_data(GetShapeSize(x_shape), 1.0f);
+  std::vector<float> phi_host_data(GetShapeSize(phi_shape), 1.0f);
+  std::vector<float> alpha_host_data(3, 1.0f);
+  std::vector<float> bias_host_data(GetShapeSize(bias_shape), 1.0f);
+  std::vector<float> gamma_host_data(GetShapeSize(gamma_shape), 1.0f);
+
+  int ret = CreateAclTensorFloat16(x_host_data, x_shape, tensors.x_addr, tensors.x);
+  CHECK_RET(ret == 0, LOG_PRINT("Create x_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32(phi_host_data, phi_shape, tensors.phi_addr, tensors.phi);
+  CHECK_RET(ret == 0, LOG_PRINT("Create phi_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32(alpha_host_data, alpha_shape, tensors.alpha_addr, tensors.alpha);
+  CHECK_RET(ret == 0, LOG_PRINT("Create alpha_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32(bias_host_data, bias_shape, tensors.bias_addr, tensors.bias);
+  CHECK_RET(ret == 0, LOG_PRINT("Create bias_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32(gamma_host_data, gamma_shape, tensors.gamma_addr, tensors.gamma);
+  CHECK_RET(ret == 0, LOG_PRINT("Create gamma_tensor failed\n"); return -1);
+  return 0;
+}
+
+int CreateOutputTensors(const std::vector<int64_t>& hin_shape, const std::vector<int64_t>& h_post_shape,
+                        const std::vector<int64_t>& h_res_shape, const std::vector<int64_t>& inv_rms_shape,
+                        const std::vector<int64_t>& h_mix_shape, const std::vector<int64_t>& h_pre_shape,
+                        Tensors& tensors) {
+  int ret = CreateAclTensorFloat16Output(hin_shape, tensors.hin_addr, tensors.hin);
+  CHECK_RET(ret == 0, LOG_PRINT("Create hin_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32Output(h_post_shape, tensors.h_post_addr, tensors.h_post);
+  CHECK_RET(ret == 0, LOG_PRINT("Create h_post_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32Output(h_res_shape, tensors.h_res_addr, tensors.h_res);
+  CHECK_RET(ret == 0, LOG_PRINT("Create h_res_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32Output(inv_rms_shape, tensors.inv_rms_addr, tensors.inv_rms);
+  CHECK_RET(ret == 0, LOG_PRINT("Create inv_rms_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32Output(h_mix_shape, tensors.h_mix_addr, tensors.h_mix);
+  CHECK_RET(ret == 0, LOG_PRINT("Create h_mix_tensor failed\n"); return -1);
+  ret = CreateAclTensorFloat32Output(h_pre_shape, tensors.h_pre_addr, tensors.h_pre);
+  CHECK_RET(ret == 0, LOG_PRINT("Create h_pre_tensor failed\n"); return -1);
+  return 0;
+}
+
+void DestroyTensors(Tensors& tensors) {
+  aclDestroyTensor(tensors.x);
+  aclDestroyTensor(tensors.phi);
+  aclDestroyTensor(tensors.alpha);
+  aclDestroyTensor(tensors.bias);
+  aclDestroyTensor(tensors.gamma);
+  aclDestroyTensor(tensors.hin);
+  aclDestroyTensor(tensors.h_post);
+  aclDestroyTensor(tensors.h_res);
+  aclDestroyTensor(tensors.inv_rms);
+  aclDestroyTensor(tensors.h_mix);
+  aclDestroyTensor(tensors.h_pre);
+}
+
+void FreeDeviceMemory(Tensors& tensors) {
+  aclrtFree(tensors.x_addr);
+  aclrtFree(tensors.phi_addr);
+  aclrtFree(tensors.alpha_addr);
+  aclrtFree(tensors.bias_addr);
+  aclrtFree(tensors.gamma_addr);
+  aclrtFree(tensors.hin_addr);
+  aclrtFree(tensors.h_post_addr);
+  aclrtFree(tensors.h_res_addr);
+  aclrtFree(tensors.inv_rms_addr);
+  aclrtFree(tensors.h_mix_addr);
+  aclrtFree(tensors.h_pre_addr);
+}
+
 int main() {
   int32_t device_id = 0;
   aclrtContext context = nullptr;
   aclrtStream stream = nullptr;
-  
+  Tensors tensors;
+  int B = 1, S = 2048, n = 4, D = 2560;
+  std::vector<int64_t> x_shape = {B * S, n, D}, phi_shape = {n * n + 2 * n, n * D}, alpha_shape = {3}, bias_shape = {n * n + 2 * n}, gamma_shape = {n, D};
+  std::vector<int64_t> hin_shape = {B * S, D}, h_post_shape = {B * S, n}, h_res_shape = {B * S, n, n}, inv_rms_shape = {B * S}, h_mix_shape = {B * S, n * n + 2 * n}, h_pre_shape = {B * S, n};
   int ret = InitAcl(device_id, context, stream);
-  CHECK_RET(ret == 0, 
-            LOG_PRINT("InitAcl failed, error: %d\n", ret); 
-            return -1);
-
-  int B = 1;
-  int S = 2048;
-  int n = 4;
-  int D = 2560;
-
-  std::vector<int64_t> x_shape = {B * S, n, D};
-  int64_t x_size = GetShapeSize(x_shape);
-  std::vector<float> x_host_data(x_size, 1.0f);
-
-  std::vector<int64_t> phi_shape = {n * n + 2 * n, n * D};
-  std::vector<float> phi_host_data(GetShapeSize(phi_shape), 1.0f);
-
-  std::vector<int64_t> alpha_shape = {3};
-  std::vector<float> alpha_host_data(3, 1.0f);
-
-  std::vector<int64_t> bias_shape = {n * n + 2 * n};
-  std::vector<float> bias_host_data(GetShapeSize(bias_shape), 1.0f);
-
-  std::vector<int64_t> gamma_shape = {n, D};
-  std::vector<float> gamma_host_data(GetShapeSize(gamma_shape), 1.0f);
-
-  std::vector<int64_t> output_hin_shape = {B * S, D};
-  std::vector<int64_t> output_h_post_shape = {B * S, n};
-  std::vector<int64_t> output_h_res_shape = {B * S, n, n};
-  std::vector<int64_t> output_inv_rms_shape = {B * S};
-  std::vector<int64_t> output_h_mix_shape = {B * S, n * n + 2 * n};
-  std::vector<int64_t> output_h_pre_shape = {B * S, n};
-
-  void* x_device_addr = nullptr;
-  aclTensor* x_tensor = nullptr;
-  ret = CreateAclTensorFloat16(x_host_data, x_shape, x_device_addr, x_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create x_tensor failed\n"); return -1);
-
-  void* phi_device_addr = nullptr;
-  aclTensor* phi_tensor = nullptr;
-  ret = CreateAclTensorFloat32(phi_host_data, phi_shape, phi_device_addr, phi_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create phi_tensor failed\n"); return -1);
-
-  void* alpha_device_addr = nullptr;
-  aclTensor* alpha_tensor = nullptr;
-  ret = CreateAclTensorFloat32(alpha_host_data, alpha_shape, alpha_device_addr, alpha_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create alpha_tensor failed\n"); return -1);
-
-  void* bias_device_addr = nullptr;
-  aclTensor* bias_tensor = nullptr;
-  ret = CreateAclTensorFloat32(bias_host_data, bias_shape, bias_device_addr, bias_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create bias_tensor failed\n"); return -1);
-
-  void* gamma_device_addr = nullptr;
-  aclTensor* gamma_tensor = nullptr;
-  ret = CreateAclTensorFloat32(gamma_host_data, gamma_shape, gamma_device_addr, gamma_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create gamma_tensor failed\n"); return -1);
-
-  void* output_hin_device_addr = nullptr;
-  aclTensor* output_hin_tensor = nullptr;
-  ret = CreateAclTensorFloat16Output(output_hin_shape, output_hin_device_addr, output_hin_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_hin_tensor failed\n"); return -1);
-
-  void* output_h_post_device_addr = nullptr;
-  aclTensor* output_h_post_tensor = nullptr;
-  ret = CreateAclTensorFloat32Output(output_h_post_shape, output_h_post_device_addr, output_h_post_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_h_post_tensor failed\n"); return -1);
-
-  void* output_h_res_device_addr = nullptr;
-  aclTensor* output_h_res_tensor = nullptr;
-  ret = CreateAclTensorFloat32Output(output_h_res_shape, output_h_res_device_addr, output_h_res_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_h_res_tensor failed\n"); return -1);
-
-  void* output_inv_rms_device_addr = nullptr;
-  aclTensor* output_inv_rms_tensor = nullptr;
-  ret = CreateAclTensorFloat32Output(output_inv_rms_shape, output_inv_rms_device_addr, output_inv_rms_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_inv_rms_tensor failed\n"); return -1);
-
-  void* output_h_mix_device_addr = nullptr;
-  aclTensor* output_h_mix_tensor = nullptr;
-  ret = CreateAclTensorFloat32Output(output_h_mix_shape, output_h_mix_device_addr, output_h_mix_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_h_mix_tensor failed\n"); return -1);
-
-  void* output_h_pre_device_addr = nullptr;
-  aclTensor* output_h_pre_tensor = nullptr;
-  ret = CreateAclTensorFloat32Output(output_h_pre_shape, output_h_pre_device_addr, output_h_pre_tensor);
-  CHECK_RET(ret == 0, LOG_PRINT("Create output_h_pre_tensor failed\n"); return -1);
-
-  double normEps = 1e-6;
-  double hcEps = 1e-6;
-
+  CHECK_RET(ret == 0, LOG_PRINT("InitAcl failed, error: %d\n", ret); return -1);
+  ret = CreateInputTensors(x_shape, phi_shape, alpha_shape, bias_shape, gamma_shape, tensors);
+  CHECK_RET(ret == 0, return -1);
+  ret = CreateOutputTensors(hin_shape, h_post_shape, h_res_shape, inv_rms_shape, h_mix_shape, h_pre_shape, tensors);
+  CHECK_RET(ret == 0, return -1);
   uint64_t workspace_size = 0;
   aclOpExecutor* executor = nullptr;
-  
-  aclnnStatus aclnn_ret = aclnnMhcPreGetWorkspaceSize(
-    x_tensor, phi_tensor, alpha_tensor, bias_tensor, gamma_tensor,
-    normEps, hcEps,
-    output_hin_tensor, output_h_post_tensor, output_h_res_tensor,
-    output_inv_rms_tensor, output_h_mix_tensor, output_h_pre_tensor,
-    &workspace_size, &executor);
-
-  CHECK_RET(aclnn_ret == ACL_SUCCESS, 
-            LOG_PRINT("aclnnMhcPreGetWorkspaceSize failed, error: %d\n", aclnn_ret); 
-            return -1);
-  
+  aclnnStatus aclnn_ret = aclnnMhcPreGetWorkspaceSize(tensors.x, tensors.phi, tensors.alpha, tensors.bias, tensors.gamma, 1e-6, 1e-6, tensors.hin, tensors.h_post, tensors.h_res, tensors.inv_rms, tensors.h_mix, tensors.h_pre, &workspace_size, &executor);
+  CHECK_RET(aclnn_ret == ACL_SUCCESS, LOG_PRINT("aclnnMhcPreGetWorkspaceSize failed, error: %d\n", aclnn_ret); return -1);
   void* workspace_addr = nullptr;
-  if (workspace_size > 0) {
-    ret = aclrtMalloc(&workspace_addr, workspace_size, ACL_MEM_MALLOC_HUGE_FIRST);
-    CHECK_RET(ret == ACL_SUCCESS, 
-              LOG_PRINT("aclrtMalloc workspace failed, error: %d\n", ret); 
-              return -1);
-  }
-
-  aclnn_ret = aclnnMhcPre(
-      workspace_addr,
-      workspace_size,
-      executor,
-      stream
-  );
-  CHECK_RET(aclnn_ret == ACL_SUCCESS, 
-            LOG_PRINT("aclnnMhcPre failed, error: %d\n", aclnn_ret); 
-            return -1);
-
-  ret = aclrtSynchronizeStream(stream);
-  CHECK_RET(ret == ACL_SUCCESS, 
-            LOG_PRINT("aclrtSynchronizeStream failed, error: %d\n", ret); 
-            return -1);
-
-  LOG_PRINT("MhcPre compute success!\n");
-  LOG_PRINT("Output tensor data: \n");
-
-  PrintTensorDataFloat16(output_hin_shape, output_hin_device_addr);
-  PrintTensorDataFloat(output_h_post_shape, output_h_post_device_addr);
-  PrintTensorDataFloat(output_h_res_shape, output_h_res_device_addr);
-  PrintTensorDataFloat(output_inv_rms_shape, output_inv_rms_device_addr);
-  PrintTensorDataFloat(output_h_mix_shape, output_h_mix_device_addr);
-  PrintTensorDataFloat(output_h_pre_shape, output_h_pre_device_addr);
-
-  aclDestroyTensor(x_tensor);
-  aclDestroyTensor(phi_tensor);
-  aclDestroyTensor(alpha_tensor);
-  aclDestroyTensor(bias_tensor);
-  aclDestroyTensor(gamma_tensor);
-
-  aclDestroyTensor(output_hin_tensor);
-  aclDestroyTensor(output_h_post_tensor);
-  aclDestroyTensor(output_h_res_tensor);
-  aclDestroyTensor(output_inv_rms_tensor);
-  aclDestroyTensor(output_h_mix_tensor);
-  aclDestroyTensor(output_h_pre_tensor);
-
-  aclrtFree(x_device_addr);
-  aclrtFree(phi_device_addr);
-  aclrtFree(alpha_device_addr);
-  aclrtFree(bias_device_addr);
-  aclrtFree(gamma_device_addr);
-
-  aclrtFree(output_hin_device_addr);
-  aclrtFree(output_h_post_device_addr);
-  aclrtFree(output_h_res_device_addr);
-  aclrtFree(output_inv_rms_device_addr);
-  aclrtFree(output_h_mix_device_addr);
-  aclrtFree(output_h_pre_device_addr);
-
-  if (workspace_size > 0) {
-    aclrtFree(workspace_addr);
-  }
-
+  if (workspace_size > 0) { ret = aclrtMalloc(&workspace_addr, workspace_size, ACL_MEM_MALLOC_HUGE_FIRST); CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMalloc workspace failed, error: %d\n", ret); return -1); }
+  aclnn_ret = aclnnMhcPre(workspace_addr, workspace_size, executor, stream);
+  CHECK_RET(aclnn_ret == ACL_SUCCESS, LOG_PRINT("aclnnMhcPre failed, error: %d\n", aclnn_ret); return -1);
+  CHECK_RET(aclrtSynchronizeStream(stream) == ACL_SUCCESS, LOG_PRINT("aclrtSynchronizeStream failed\n"); return -1);
+  LOG_PRINT("MhcPre compute success!\nOutput tensor data: \n");
+  PrintTensorDataFloat16(hin_shape, tensors.hin_addr);
+  PrintTensorDataFloat(h_post_shape, tensors.h_post_addr);
+  PrintTensorDataFloat(h_res_shape, tensors.h_res_addr);
+  PrintTensorDataFloat(inv_rms_shape, tensors.inv_rms_addr);
+  PrintTensorDataFloat(h_mix_shape, tensors.h_mix_addr);
+  PrintTensorDataFloat(h_pre_shape, tensors.h_pre_addr);
+  DestroyTensors(tensors);
+  FreeDeviceMemory(tensors);
+  if (workspace_size > 0) aclrtFree(workspace_addr);
   aclrtDestroyStream(stream);
   aclrtDestroyContext(context);
   aclrtResetDevice(device_id);
   aclFinalize();
-
   LOG_PRINT("All resources released successfully!\n");
   return 0;
 }
