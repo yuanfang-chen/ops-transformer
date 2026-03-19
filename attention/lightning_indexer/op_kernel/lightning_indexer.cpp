c369fdad053dc975f3861e588e9c3e5458b06b0c
我是石头人大王 <luohao148@huawei.com>
2026-03-19 19:27:02 +0800

    LI A5

--- attention/lightning_indexer/op_kernel/lightning_indexer.cpp
+++ attention/lightning_indexer/op_kernel/lightning_indexer.cpp
@@ -16,7 +16,13 @@
 #include "kernel_operator.h"
 #include "lib/matmul_intf.h"
 #include "lightning_indexer_template_tiling_key.h"
-#include "lightning_indexer_kernel.h"
+
+#if (__CCE_AICORE__ == 310)
+    #include "arch35/lightning_indexer_kernel.h"
+
+#else
+    #include "lightning_indexer_kernel.h"
+#endif
 
 using namespace LIKernel;
 
@@ -37,12 +43,18 @@ __global__ __aicore__ void lightning_indexer(__gm__ uint8_t *query, __gm__ uint8
                                              __gm__ uint8_t *sparseValues, __gm__ uint8_t *workspace,
                                              __gm__ uint8_t *tiling)
 {
-#if (__CCE_AICORE__ == 310) || (defined __DAV_310R6__) || (__CCE_AICORE__ == 200)
-
-#else
     TPipe tPipe;
     __gm__ uint8_t *user = GetUserWorkspace(workspace);
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_MIX_AIC_1_2);
+#if (__CCE_AICORE__ == 310) || (defined __DAV_310R6__) || (__CCE_AICORE__ == 200)
+    if (ORIG_DTYPE_QUERY == DT_BF16) {
+        INVOKE_LI_NO_KFC_OP_IMPL(LIPreload, bfloat16_t, bfloat16_t, int32_t,
+            PAGE_ATTENTION, LI_LAYOUT(LAYOUT_T), LI_LAYOUT(K_LAYOUT_T), DT_W_FLAG);
+    } else if (ORIG_DTYPE_QUERY == DT_FLOAT16) {
+        INVOKE_LI_NO_KFC_OP_IMPL(LIPreload, half, half, int32_t,
+            PAGE_ATTENTION, LI_LAYOUT(LAYOUT_T), LI_LAYOUT(K_LAYOUT_T), DT_W_FLAG);
+    }
+#else
     if constexpr (DT_Q == LI_TPL_FP16 && DT_K == LI_TPL_FP16 && DT_OUT == LI_TPL_INT32) {
         INVOKE_LI_NO_KFC_OP_IMPL(LIPreload, half, half, int32_t, PAGE_ATTENTION, 
                                  LI_LAYOUT(LAYOUT_T), LI_LAYOUT(K_LAYOUT_T), DT_W_FLAG);
