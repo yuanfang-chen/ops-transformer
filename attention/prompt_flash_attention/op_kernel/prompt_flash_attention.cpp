97cba5d754cf488ee9f7e48a5edf9ade2ebb293a
inspirations <wangke201@huawei.com>
2026-03-19 17:07:19 +0800

    add FIA in arch38

--- attention/prompt_flash_attention/op_kernel/prompt_flash_attention.cpp
+++ attention/prompt_flash_attention/op_kernel/prompt_flash_attention.cpp
@@ -50,7 +50,7 @@ extern "C" __global__ __aicore__ void prompt_flash_attention_FIAS(__gm__ uint8_t
             deq_scale1, quant_scale1, deq_scale2, quant_scale2, quant_offset2, antiquant_scale, antiquant_offset,
             blocktable, queryPaddingSize, kvPaddingSize, key_antiquant_scale, key_antiquant_offset, value_antiquant_scale, 
             value_antiquant_offset, keySharedPrefix, valueSharedPrefix, actualSharedPrefixLen, queryRope, keyRope, dequantScaleQuery, attentionOut,
-            softmaxLse, workspace, tiling);    
+            softmaxLse, workspace, tiling);
     #else
         prompt_flash_attention_FIAS_arch32(query, key, value, pseShift, attenMask, actualSeqLengths,
             actualSeqLengthsKV, deq_scale1, quant_scale1, deq_scale2, quant_scale2, quant_offset2, antiquant_scale, antiquant_offset,
