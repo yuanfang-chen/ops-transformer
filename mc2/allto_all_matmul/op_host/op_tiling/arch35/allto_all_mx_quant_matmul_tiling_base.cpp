2cb4eb6c1e930f6d4c55442a670200e18e235833
wuziyu <wuziyu11@huawei.com>
2026-03-19 15:31:48 +0800

    add

--- mc2/allto_all_matmul/op_host/op_tiling/arch35/allto_all_mx_quant_matmul_tiling_base.cpp
+++ mc2/allto_all_matmul/op_host/op_tiling/arch35/allto_all_mx_quant_matmul_tiling_base.cpp
@@ -325,7 +325,7 @@ ge::graphStatus AllToAllMxQuantMatmulTilingBase::SetMxDataTypeInfo(const gert::T
 
     contextInfo.args_.outputDtypeSize = mc2tiling::GetDataTypeSize(opName, cType);
     // 设置为x1的数据类型
-    contextInfo.args_.inputDtypeSize = MX_SCALE_BLOCK_M;
+    contextInfo.args_.inputDtypeSize = BIT_NUMBER;
     contextInfo.args_.isBias = isBias;
     contextInfo.args_.geCType = cType;
     contextInfo.args_.geBiasType = biasType;
