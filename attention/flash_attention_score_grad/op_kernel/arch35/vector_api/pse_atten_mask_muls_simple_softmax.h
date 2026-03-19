176beecbba86c5a38405bbae8971a1be495e194e
coder_linx <linzhu3@huawei.com>
2026-03-19 10:35:35 +0800

    【FAG】tnd support max sum same as input

--- attention/flash_attention_score_grad/op_kernel/arch35/vector_api/pse_atten_mask_muls_simple_softmax.h
+++ attention/flash_attention_score_grad/op_kernel/arch35/vector_api/pse_atten_mask_muls_simple_softmax.h
@@ -74,7 +74,13 @@ __aicore__ inline void CopyInMaxSum(FagConstInfo &constInfo, FagRunInfo &runInfo
         return;
     }
     int64_t maxSumGmOffset = 0;
-    if (constInfo.commonConstInfo.layoutType == TND) {
+    LocalTensor<T2> maxSumTensor = maxSumInQue.AllocTensor<T2>();
+    if (constInfo.tndMaxSumLayout == MAX_SUM_TND) { // TND + TND
+        maxSumGmOffset =
+            (runInfo.commonRunInfo.queryOffset / constInfo.commonConstInfo.dSize +
+             runInfo.commonRunInfo.firstHalfS1RealSize * GetSubBlockIdx() * constInfo.commonConstInfo.n2G) *
+            MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2);
+    } else if (constInfo.commonConstInfo.layoutType == TND) { // TND + BNS8
         int64_t tndS1PrefixSum =
             (runInfo.commonRunInfo.boIdx == 0 ? 0 :
                                                 ((__gm__ int64_t *)constInfo.seqS1_addr)[runInfo.commonRunInfo.boIdx - 1]);
@@ -88,11 +94,12 @@ __aicore__ inline void CopyInMaxSum(FagConstInfo &constInfo, FagRunInfo &runInfo
         }
         maxSumGmOffset +=
             ((runInfo.commonRunInfo.n2oIdx * constInfo.commonConstInfo.gSize + runInfo.commonRunInfo.goIdx) *
-                actualS1Len +
+                 actualS1Len +
              runInfo.commonRunInfo.s1oIdx * VECTOR_BASEM * CV_CORE_RATIO +
              runInfo.commonRunInfo.firstHalfS1RealSize * GetSubBlockIdx()) *
             MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2);
-    } else {
+
+    } else { // BNS8
         maxSumGmOffset = (((runInfo.commonRunInfo.boIdx * constInfo.n2Size + runInfo.commonRunInfo.n2oIdx) *
                                constInfo.commonConstInfo.gSize +
                            runInfo.commonRunInfo.goIdx) *
@@ -101,11 +108,26 @@ __aicore__ inline void CopyInMaxSum(FagConstInfo &constInfo, FagRunInfo &runInfo
                           runInfo.commonRunInfo.firstHalfS1RealSize * GetSubBlockIdx()) *
                          MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2);
     }
-    LocalTensor<T2> maxSumTensor = maxSumInQue.AllocTensor<T2>();
-    DataCopyPad(maxSumTensor, sumGm[maxSumGmOffset],
-                {1, static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize * MAX_SUM_REDUCE_AXIS_SIZE), 0, 0}, {false, 0, 0, 0});
-    DataCopyPad(maxSumTensor[VECTOR_BASEM * MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2)], maxGm[maxSumGmOffset],
-                {1, static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize * MAX_SUM_REDUCE_AXIS_SIZE), 0, 0}, {false, 0, 0, 0});
+
+    if (constInfo.tndMaxSumLayout == MAX_SUM_TND) {
+        uint32_t srcStride = constInfo.commonConstInfo.n2G * MAX_SUM_REDUCE_AXIS_SIZE - MAX_SUM_REDUCE_AXIS_SIZE;
+        DataCopyPad(maxSumTensor, sumGm[maxSumGmOffset],
+                    {static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize),
+                     static_cast<uint32_t>(MAX_SUM_REDUCE_AXIS_SIZE), static_cast<uint32_t>(srcStride), 0, 0},
+                    {false, 0, 0, 0});
+        DataCopyPad(maxSumTensor[VECTOR_BASEM * MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2)], maxGm[maxSumGmOffset],
+                    {static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize),
+                     static_cast<uint32_t>(MAX_SUM_REDUCE_AXIS_SIZE), static_cast<uint32_t>(srcStride), 0, 0},
+                    {false, 0, 0, 0});
+    } else {
+        DataCopyPad(maxSumTensor, sumGm[maxSumGmOffset],
+                    {1, static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize * MAX_SUM_REDUCE_AXIS_SIZE), 0, 0},
+                    {false, 0, 0, 0});
+        DataCopyPad(maxSumTensor[VECTOR_BASEM * MAX_SUM_REDUCE_AXIS_SIZE / sizeof(T2)], maxGm[maxSumGmOffset],
+                    {1, static_cast<uint16_t>(runInfo.commonRunInfo.halfS1RealSize * MAX_SUM_REDUCE_AXIS_SIZE), 0, 0},
+                    {false, 0, 0, 0});
+    }
+
     maxSumInQue.EnQue(maxSumTensor);
 }
 
