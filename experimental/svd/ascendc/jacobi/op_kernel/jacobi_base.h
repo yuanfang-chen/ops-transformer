ebd25e7805ea414f1e09ecc6b840725af34bebd6
EugenV <volosenkov.eugeny@huawei.com>
2026-03-19 16:31:03 +0300

    fix headers and tiling files for Jacobi kernel

--- experimental/svd/ascendc/jacobi/op_kernel/jacobi_base.h
+++ experimental/svd/ascendc/jacobi/op_kernel/jacobi_base.h
@@ -1,12 +1,12 @@
 /**
- * This program is free software, you can redistribute it and/or modify it.
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /*!
  * \file jacobi_base.h
@@ -16,12 +16,8 @@
 #ifndef JACOBI_BASE_H
 #define JACOBI_BASE_H
 
-  // #define DEBUG_MODE
-// #define RUNTIME_NORM_GENERATION
 #ifdef DEBUG_MODE
-
 #define PRINT_VAL(X)   printf("Variable %s has value %d\n", #X, (int)X)
-// #define PRINT_TENSOR(X, SIZE) PipeBarrier<PIPE_ALL>();PRINT_TENSOR_(X,SIZE);PipeBarrier<PIPE_ALL>();
 #else
 #define PRINT_VAL(X)
 #endif
