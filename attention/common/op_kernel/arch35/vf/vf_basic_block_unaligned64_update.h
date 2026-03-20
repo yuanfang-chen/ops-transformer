c4990c77efe808153484cd05b48030b11889898a
sky-zyh <zhangyuhan45@huawei.com>
2026-03-20 12:53:04 +0800

    flash_attention_score支持sink输入

--- attention/common/op_kernel/arch35/vf/vf_basic_block_unaligned64_update.h
+++ attention/common/op_kernel/arch35/vf/vf_basic_block_unaligned64_update.h
@@ -22,16 +22,16 @@ using namespace regbaseutil;
 
 namespace FaVectorApi {
 template <typename T, typename T2, typename pseShiftType, uint32_t s1BaseSize = 128, uint32_t s2BaseSize = 128,
-    bool hasAtten = 0, PseTypeEnum pseMode = PseTypeEnum::PSE_NONE_TYPE, bool hasDrop = 0, bool isMlaSgd = false, bool isMlaFullQuant = false>
+    bool hasAtten = 0, PseTypeEnum pseMode = PseTypeEnum::PSE_NONE_TYPE, bool hasDrop = 0, bool isMlaSgd = false,
+    bool isMlaFullQuant = false, bool hasSink = false>
 __simd_vf__ void ProcessVec1UpdateImpl64VF(
-    __ubuf__ T2 * expUb, __ubuf__ pseShiftType * pseUb, __ubuf__ T * maxUb, __ubuf__ T * srcUb,
-    __ubuf__ T * expMaxUb, __ubuf__ T * inMaxUb, __ubuf__ T * tmpExpSumUb, __ubuf__ T * tmpMaxUb, 
-    __ubuf__ T * tmpMaxUb2, __ubuf__ T * qScaleUb, __ubuf__ T * pScaleUb, __ubuf__ uint8_t * indexesUb, 
-    __ubuf__ uint32_t * maskUb, __ubuf__ uint32_t * dropMaskUb, 
-    const uint32_t nPadding, const uint32_t blockStride, const uint32_t repeatStride, const float dScale, 
-    uint32_t pltOriginalN, float divValue, uint32_t pltSrcN, uint32_t pltSrcN16, 
-    const uint16_t m, const uint32_t pseStride, const float slopes, const float posShift, const T scale, const float dScaleQK,
-    const T minValue, const float deSCaleKValue = 1.0f)
+    __ubuf__ T2 * expUb, __ubuf__ pseShiftType * pseUb, __ubuf__ T * maxUb, __ubuf__ T * srcUb, __ubuf__ T * expMaxUb,
+    __ubuf__ T * inMaxUb, __ubuf__ T * tmpExpSumUb, __ubuf__ T * tmpMaxUb, __ubuf__ T * tmpMaxUb2,
+    __ubuf__ T * qScaleUb, __ubuf__ T * pScaleUb, __ubuf__ uint8_t * indexesUb, __ubuf__ uint32_t * maskUb,
+    __ubuf__ uint32_t * dropMaskUb, const uint32_t nPadding, const uint32_t blockStride, const uint32_t repeatStride,
+    const float dScale, uint32_t pltOriginalN, float divValue, uint32_t pltSrcN, uint32_t pltSrcN16, const uint16_t m,
+    const uint32_t pseStride, const float slopes, const float posShift, const T scale, const float dScaleQK,
+    const T minValue, const float deSCaleKValue = 1.0f, const float sinkValue = 0.0f)
 {
     RegTensor<float> vreg_min;
     RegTensor<float> vreg_sel;
@@ -50,6 +50,7 @@ __simd_vf__ void ProcessVec1UpdateImpl64VF(
     RegTensor<float> vreg_sel_drop;
     RegTensor<float> vreg_rowmax_p;
     RegTensor<float> vreg_scale_qk;
+    RegTensor<float> vreg_sink_input;
 
     // bfloat16_t
     RegTensor<bfloat16_t> vreg_exp_even_bf16;
@@ -83,6 +84,9 @@ __simd_vf__ void ProcessVec1UpdateImpl64VF(
     MaskReg preg3;
     MaskReg preg4;
 
+    if constexpr (hasSink) {
+        Duplicate(vreg_sink_input, sinkValue);
+    }
     if constexpr (hasAtten == 1) {
         Duplicate(vreg_min, minValue);
         if constexpr (isMlaSgd) {
@@ -156,7 +160,9 @@ __simd_vf__ void ProcessVec1UpdateImpl64VF(
             Reduce<MicroAPI::ReduceType::MAX, float, float, MicroAPI::MaskMergeMode::ZEROING>(
                 vreg_input_max, vreg_input_x, preg_ori_src_n);
         }
-
+        if constexpr (hasSink) {
+            Max(vreg_input_max, vreg_input_max, vreg_sink_input, preg_ori_src_n);
+        }
         StoreUnAlign<float, MicroAPI::PostLiteral::POST_MODE_UPDATE>(
             ((__ubuf__ T *&)tmpMaxUb), vreg_input_max, ureg_max, 1);
     }
@@ -285,14 +291,17 @@ __simd_vf__ void ProcessVec1UpdateImpl64VF(
 }
 // update, originN <= 64
 template <typename T, typename T2, typename pseShiftType, uint32_t s1BaseSize = 128, uint32_t s2BaseSize = 128,
-    bool hasAtten = 0, PseTypeEnum pseMode = PseTypeEnum::PSE_NONE_TYPE, bool hasDrop = 0, bool isMlaSgd = false, bool isMlaFullQuant = false>
+    bool hasAtten = 0, PseTypeEnum pseMode = PseTypeEnum::PSE_NONE_TYPE, bool hasDrop = 0, bool isMlaSgd = false,
+    bool isMlaFullQuant = false, bool hasSink = false>
 __aicore__ inline void ProcessVec1UpdateImpl64(
-    const LocalTensor<T2>& dstTensor, const LocalTensor<uint8_t>& indexesTensor, const LocalTensor<T>& expSumTensor, const LocalTensor<T>& maxTensor,
-    const LocalTensor<T>& srcTensor, const LocalTensor<T>& expMaxTensor, const LocalTensor<T>& inExpSumTensor,
-    const LocalTensor<T>& inMaxTensor, const LocalTensor<uint8_t>& maskTensor, const LocalTensor<pseShiftType>& pseTensor,
-    const LocalTensor<uint8_t>& dropTensor, const LocalTensor<uint8_t>& sharedTmpBuffer, const LocalTensor<T>& pScaleTensor, const uint16_t m,
-    const uint32_t originN, const uint32_t pseStride, const float slopes, const float posShift, const T scale, const float dScaleQK,
-    const T minValue, float keepProb, const LocalTensor<T>& queryScaleUb = LocalTensor<T>(), const float deSCaleKValue = 1.0f)
+    const LocalTensor<T2>& dstTensor, const LocalTensor<uint8_t>& indexesTensor, const LocalTensor<T>& expSumTensor,
+    const LocalTensor<T>& maxTensor, const LocalTensor<T>& srcTensor, const LocalTensor<T>& expMaxTensor,
+    const LocalTensor<T>& inExpSumTensor, const LocalTensor<T>& inMaxTensor, const LocalTensor<uint8_t>& maskTensor,
+    const LocalTensor<pseShiftType>& pseTensor, const LocalTensor<uint8_t>& dropTensor,
+    const LocalTensor<uint8_t>& sharedTmpBuffer, const LocalTensor<T>& pScaleTensor, const uint16_t m,
+    const uint32_t originN, const uint32_t pseStride, const float slopes, const float posShift, const T scale,
+    const float dScaleQK, const T minValue, float keepProb, const LocalTensor<T>& queryScaleUb = LocalTensor<T>(),
+    const float deSCaleKValue = 1.0f, const float sinkValue = 0.0f)
 {
     const uint32_t nPadding = (s2BaseSize + blockBytesU8 - 1) / blockBytesU8 * blockBytesU8;
     // 写的时候固定用65或者33的stride去写，因为正向目前使能settail之后mm2的s1方向必须算满128或者64行
@@ -320,10 +329,11 @@ __aicore__ inline void ProcessVec1UpdateImpl64(
     __ubuf__ uint32_t * maskUb = (__ubuf__ uint32_t*)maskTensor.GetPhyAddr();
     __ubuf__ uint32_t * dropMaskUb = (__ubuf__ uint32_t*)dropTensor.GetPhyAddr();
 
-    ProcessVec1UpdateImpl64VF<T, T2, pseShiftType, s1BaseSize, s2BaseSize, hasAtten, pseMode, hasDrop, isMlaSgd, isMlaFullQuant>(
-        expUb, pseUb, maxUb, srcUb, expMaxUb, inMaxUb, tmpExpSumUb, tmpMaxUb, tmpMaxUb2, qScaleUb, pScaleUb, indexesUb, maskUb, dropMaskUb, 
-        nPadding, blockStride, repeatStride, dScale, pltOriginalN, divValue, pltSrcN, pltSrcN16, m, pseStride, slopes, posShift, 
-        scale, dScaleQK, minValue, deSCaleKValue);
+    ProcessVec1UpdateImpl64VF<T, T2, pseShiftType, s1BaseSize, s2BaseSize, hasAtten, pseMode, hasDrop,
+        isMlaSgd, isMlaFullQuant, hasSink>(
+        expUb, pseUb, maxUb, srcUb, expMaxUb, inMaxUb, tmpExpSumUb, tmpMaxUb, tmpMaxUb2, qScaleUb, pScaleUb,
+        indexesUb, maskUb, dropMaskUb, nPadding, blockStride, repeatStride, dScale, pltOriginalN, divValue, pltSrcN,
+        pltSrcN16, m, pseStride, slopes, posShift, scale, dScaleQK, minValue, deSCaleKValue, sinkValue);
 }
 } // namespace
 
