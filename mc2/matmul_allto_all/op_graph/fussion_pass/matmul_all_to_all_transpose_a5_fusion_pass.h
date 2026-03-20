535b19ea59f6ef5d575e615511182a738c985aa9
liusixia_gitcode <liusixia@h-partners.com>
2026-03-20 17:38:32 +0800

    matmul alltoall fusion pass from canndev to transformer

---
+++ mc2/matmul_allto_all/op_graph/fussion_pass/matmul_all_to_all_transpose_a5_fusion_pass.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software; you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#include "ge/fusion/pass/pattern_fusion_pass.h"
+
+#ifndef NN_MATMUL_ALL_TO_ALL_TRANSPOSE_A5_FUSION_PASS_H
+#define NN_MATMUL_ALL_TO_ALL_TRANSPOSE_A5_FUSION_PASS_H
+
+namespace ge {
+namespace ops {
+class __attribute__((visibility("default"))) MatmulAllToAllTransposeA5FusionPass : public fusion::PatternFusionPass {
+protected:
+    std::vector<fusion::PatternUniqPtr> Patterns() override;
+
+    bool MeetRequirements(const std::unique_ptr<fusion::MatchResult> &match_result) override;
+
+    fusion::GraphUniqPtr Replacement(const std::unique_ptr<fusion::MatchResult> &match_result) override;
+};
+} // namespace ops
+} // namespace ge
+#endif // NN_MATMUL_ALL_TO_ALL_TRANSPOSE_A5_FUSION_PASS_H
