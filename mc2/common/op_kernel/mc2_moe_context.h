1ec0c447dc4e7995f0e271c851f2b91f401342de
zzg_code <zengzhiguo1@huawei.com>
2026-03-20 17:11:31 +0800

    1

--- mc2/common/op_kernel/mc2_moe_context.h
+++ mc2/common/op_kernel/mc2_moe_context.h
@@ -17,14 +17,16 @@
 #define MC2_MOE_CONTEXT_H
 
 namespace MC2MoeContext {
-    
+constexpr uint32_t HCCL_MAX_RANK_SIZE = 1024;
+
 struct Mc2MoeContext {
     uint32_t epRankId;
+    uint32_t tpRankId;
     uint32_t epRankSize;
     uint64_t winSize;
     uint32_t kfcContextAddr; // host kfc方案中，需要传递通信API所需的地址
-    uint64_t epHcclBuffer_[1024];
-};   
+    uint64_t epHcclBuffer_[HCCL_MAX_RANK_SIZE];
+};
 
 }
 
