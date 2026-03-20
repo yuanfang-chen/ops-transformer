6bfe69490d80810a943d44a9cf124ecbe4943809
陈佳乐 <chenjiale56@huawei.com>
2026-03-20 17:50:08 +0800

    fix: v1->v2

--- experimental/mc2/all_gather_matmul_v2/op_host/op_tiling/all_gather_matmul_tiling_base.h
+++ experimental/mc2/all_gather_matmul_v2/op_host/op_tiling/all_gather_matmul_tiling_base.h
@@ -19,9 +19,9 @@
 #include "register/tilingdata_base.h"
 #include "tiling/tiling_api.h"
 #include "tiling_base/tiling_base.h"
-#include "tiling/mc2_tiling_struct.h"
-#include "tiling/matmul_formulaic_tiling.h"
-#include "tiling/mc2_tiling_utils.h"
+#include "op_host/op_tiling/mc2_tiling_struct.h"
+#include "op_host/op_tiling/matmul_formulaic_tiling.h"
+#include "op_host/op_tiling/mc2_tiling_utils.h"
 #include "../../op_kernel/all_gather_matmul_tiling.h"
 
 using Ops::Transformer::OpTiling::TilingBaseClass;
