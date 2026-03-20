b4e832cec91411dfea1286e86f748b49c0874248
yiqiao-wjp <wangjianping48@huawei.com>
2026-03-20 14:47:49 +0800

    fix tilingdata

--- attention/chunk_gated_delta_rule/op_kernel/chunk_gated_delta_rule_tiling_data.h
+++ attention/chunk_gated_delta_rule/op_kernel/chunk_gated_delta_rule_tiling_data.h
@@ -52,6 +52,9 @@ namespace ChunkGatedDeltaRule {
     constexpr uint64_t MTE2_V_EVENT = 2;
     constexpr uint64_t MTE3_MTE2_EVENT = 4;
     constexpr uint64_t FIX_MTE2_EVENT = 6;
+    constexpr uint64_t S_V_EVENT = 3;
+    constexpr uint64_t MTE3_MTE2_EVENT = 4;
+    constexpr uint64_t FIX_MTE2_EVENT = 6;
     
     constexpr uint64_t BUFFER_NUM_ONE = 1;
     constexpr uint64_t BROADCAST_AXIS = 2;
