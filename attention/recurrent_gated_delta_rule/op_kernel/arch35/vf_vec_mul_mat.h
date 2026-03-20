97d56896eea75c2d86a559c8640238a3e418c711
jiang-lirui <jianglirui1@huawei.com>
2026-03-20 22:00:52 +0800

    代码结构调整

--- attention/recurrent_gated_delta_rule/op_kernel/arch35/vf_vec_mul_mat.h
+++ attention/recurrent_gated_delta_rule/op_kernel/arch35/vf_vec_mul_mat.h
@@ -13,14 +13,16 @@
  * \brief
  */
 
-#ifndef VEC_MUL_MAT_INTERFACE_H
-#define VEC_MUL_MAT_INTERFACE_H
+#ifndef VF_VEC_MUL_MAT_H
+#define VF_VEC_MUL_MAT_H
 
 #include "kernel_tensor.h"
-namespace AscendC{
+
+namespace AscendC {
+
 template <typename T>
-__aicore__ inline void MatVecMulVF(const LocalTensor<T>& matrixInUb, const LocalTensor<T>& rowVecInUb,
-                                   const LocalTensor<T>& matrixOutUb, const uint16_t row, const uint32_t col)
+__aicore__ inline void VecMulMatVF(const LocalTensor<T>& matrixOutUb, const LocalTensor<T>& rowVecInUb,
+                                   const LocalTensor<T>& matrixInUb, const uint16_t row, const uint32_t col)
 {
     __ubuf__ float * matrixUb = (__ubuf__ float*)matrixInUb.GetPhyAddr();
     __ubuf__ float * rowVecUb = (__ubuf__ float*)rowVecInUb.GetPhyAddr();
@@ -72,52 +74,6 @@ __aicore__ inline void MatVecMulVF(const LocalTensor<T>& matrixInUb, const Local
     }
 }
 
-template <typename T>
-__aicore__ inline void OuterAddVF(const LocalTensor<T>& outputUb, const LocalTensor<T>& deltaInUb,
-                                  const LocalTensor<T>& kInUb, const LocalTensor<T>& stateInUb, const uint16_t row,
-                                  const uint32_t col)
-{
-    __ubuf__ T * deltaBuf = (__ubuf__ T*)deltaInUb.GetPhyAddr();
-    __ubuf__ T * kInputBuf = (__ubuf__ T*)kInUb.GetPhyAddr();
-    __ubuf__ T * stateInBuf = (__ubuf__ T*)stateInUb.GetPhyAddr();
-    __ubuf__ T * outputBuf = (__ubuf__ T*)outputUb.GetPhyAddr();
-
-    constexpr uint16_t floatRepSize = 64; // 一个寄存器能够存放64个FP32
-    uint16_t dLoops = col / floatRepSize;
-    uint32_t dTail = col % floatRepSize;
-
-    __VEC_SCOPE__
-    {
-        AscendC::MicroAPI::RegTensor<T> vregKInput;
-        AscendC::MicroAPI::RegTensor<T> vregDelta;
-        AscendC::MicroAPI::RegTensor<T> vregStateIn;
-        AscendC::MicroAPI::MaskReg pregAll = AscendC::MicroAPI::CreateMask<T, AscendC::MicroAPI::MaskPattern::ALL>();
-
-        for (uint16_t j = 0; j < dLoops; j++) {
-            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + j * floatRepSize);
-            for (uint16_t i = 0; i < row; i++) {
-                uint16_t loopOffset = i * col + j * floatRepSize;
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
-                AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, pregAll);
-                AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, pregAll);
-            }
-        }
-
-        if (dTail > 0) {
-            AscendC::MicroAPI::MaskReg tailMask = AscendC::MicroAPI::UpdateMask<T>(dTail);
-            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + dLoops * floatRepSize);
-            for (uint16_t i = 0; i < row; i++) {
-                uint16_t loopOffset = i * col + dLoops * floatRepSize;
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
-                AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, tailMask);
-                AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, tailMask);
-            }
-        }
-    }
-}
-
 } // namespace
 
-#endif // VEC_MUL_MAT_INTERFACE_H
+#endif // VF_VEC_MUL_MAT_H
