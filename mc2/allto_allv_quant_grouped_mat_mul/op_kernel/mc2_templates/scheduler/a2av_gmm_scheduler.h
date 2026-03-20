8d2bb86dcea6a42c1f3355573fa0156fd435c748
adamlwang <wangjingquan2@huawei.com>
2026-03-20 11:33:30 +0800

    fix error

--- mc2/allto_allv_quant_grouped_mat_mul/op_kernel/mc2_templates/scheduler/a2av_gmm_scheduler.h
+++ mc2/allto_allv_quant_grouped_mat_mul/op_kernel/mc2_templates/scheduler/a2av_gmm_scheduler.h
@@ -26,37 +26,51 @@
 using namespace AscendC;
 
 namespace MC2KernelTemplate {
-template <typename CommOpType, typename ComputeOpType, typename LocalComputeOpType, typename TilingDataType, typename GmmTilingDataType,
-    typename GmmArrayAddrType, bool IsNeedMM>
+
+__aicore__ inline uint64_t Align(uint64_t a, uint64_t b)
+{
+    if (b == 0) {
+        return 0;
+    }
+    return ((a + b - 1) / b) * b;
+}
+
+template <typename CommOpType, typename ComputeOpType, typename LocalComputeOpType, typename TilingDataType,
+          typename GmmTilingDataType, typename GmmArrayAddrType, bool IsNeedMM>
 class A2avGmmScheduler {
 public:
     __aicore__ inline void Init(GM_ADDR gmmxGM, GM_ADDR gmmweightGM, GM_ADDR mmxOptionalGM, GM_ADDR mmweightOptionalGM,
-        GM_ADDR gmmxScaleGM, GM_ADDR gmmWeightScaleGM, GM_ADDR mmxScaleGM, GM_ADDR mmWeightScaleGM, GM_ADDR gmmyGM,
-        GM_ADDR mmyOptionalGM, GM_ADDR permuteOutOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM,
-        GmmArrayAddrType *gmmArrayAddrIn, GmmArrayAddrType *mmArrayAddrIn, TPipe *tPipe, bool isA2avGmmFlag)
+                                GM_ADDR gmmxScaleGM, GM_ADDR gmmWeightScaleGM, GM_ADDR mmxScaleGM,
+                                GM_ADDR mmWeightScaleGM, GM_ADDR gmmyGM, GM_ADDR mmyOptionalGM,
+                                GM_ADDR permuteOutOptionalGM, GM_ADDR workspaceGM, GM_ADDR tilingGM,
+                                GmmArrayAddrType *gmmArrayAddrIn, GmmArrayAddrType *mmArrayAddrIn, TPipe *tPipe,
+                                bool isA2avGmmFlag)
     {
         GET_TILING_DATA(tilingData, tilingGM);
         tilingData_ = &tilingData;
         e_ = tilingData_->taskTilingInfo.e;
         const void *hcclInitTiling = &(tilingData_->hcclA2avTilingInfo.hcclInitTiling);
-        uint64_t hcclCcTilingOffset = offsetof(TilingDataType,  hcclA2avTilingInfo) +
-                        offsetof(MC2KernelTemplate::HcclA2avTilingInfo, a2avCcTiling);
+        uint64_t hcclCcTilingOffset = offsetof(TilingDataType, hcclA2avTilingInfo) +
+                                      offsetof(MC2KernelTemplate::HcclA2avTilingInfo, a2avCcTiling);
         commOutGm = tilingData_->isPermuteOut ? permuteOutOptionalGM : workspaceGM;
         commOp.Init(hcclInitTiling, hcclCcTilingOffset, &tilingData_->taskTilingInfo, gmmxGM, commOutGm);
 
-        // 增加scale的通信初始化,fp8通信数据类型长度是相同的，因此可以复用（待确认）
-        if IsFp8<DTYPE_GMM_X>() {
-            uint64_t commOutLen = AlignTo512((tilingData_->taskTilingInfo.A) * (tilingData_->taskTilingInfo.H1));
-            gmmxScalecommOutGM = workspaceGM + commOutLen;
-            scaleCommOp.Init(hcclInitTiling, hcclCcTilingOffset, &tilingData_->taskTilingInfo, 
-                        gmmxScaleGM, gmmxScalecommOutGM);
+        // 增加scale的通信初始化,fp8通信数据类型长度是相同的，因此可以复用
+        if (AscendC::IsSameType<DTYPE_GMM_X_SCALE, fp8_e8m0_t>::value) {
+            uint64_t commOutLen =
+                Align((tilingData_->taskTilingInfo.A) * (tilingData_->taskTilingInfo.H1), TENSOR_LIST_SIZE);
+            gmmxScaleCommOutGm = workspaceGM + commOutLen;
+            // commOp中已经初始化了上下文，这里只更新地址
+            scaleCommOp.UpdateBuffer(gmmxScaleGM, gmmxScaleCommOutGm);
         }
         if (IsNeedMM) {
             localComputeOp.Init(mmxOptionalGM, mmweightOptionalGM, mmxScaleGM, mmWeightScaleGM, mmyOptionalGM,
-                workspaceGM, tilingData_, &tilingData_->mmQuantTilingData, mmArrayAddrIn, tPipe, isA2avGmmFlag);
+                                workspaceGM, tilingData_, &tilingData_->mmQuantTilingData, mmArrayAddrIn, tPipe,
+                                isA2avGmmFlag);
         }
-        computeOp.Init(commOutGm, gmmweightGM, gmmxScaleGM, gmmWeightScaleGM, gmmyGM, workspaceGM, tilingData_,
-            &tilingData_->gmmQuantTilingData, gmmArrayAddrIn, tPipe, isA2avGmmFlag);
+        computeScaleGM = (AscendC::IsSameType<DTYPE_GMM_X_SCALE, fp8_e8m0_t>::value) ? gmmxScaleCommOutGm : gmmxScaleGM;
+        computeOp.Init(commOutGm, gmmweightGM, computeScaleGM, gmmWeightScaleGM, gmmyGM, workspaceGM, tilingData_,
+                       &tilingData_->gmmQuantTilingData, gmmArrayAddrIn, tPipe, isA2avGmmFlag);
     }
 
     __aicore__ inline void Process()
@@ -68,14 +82,14 @@ public:
         for (uint32_t expertIdx = 0U; expertIdx < e_; expertIdx++) {
             commOp.Launch(expertIdx, 1);
             // MXFP8量化：同时启动xscale的alltoallv
-            if IsFp8<DTYPE_GMM_X>() {
+            if (AscendC::IsSameType<DTYPE_GMM_X_SCALE, fp8_e8m0_t>::value) {
                 scaleCommOp.LaunchScale(expertIdx, 1);
             }
         }
         for (uint32_t expertIdx = 0U; expertIdx < e_; expertIdx++) {
             commOp.Wait(expertIdx);
             // MXFP8量化：等待xscale的alltoallv完成
-            if IsFp8<DTYPE_GMM_X>() {
+            if (AscendC::IsSameType<DTYPE_GMM_X_SCALE, fp8_e8m0_t>::value) {
                 scaleCommOp.Wait(expertIdx);
             }
             SyncAll<false>();
@@ -88,18 +102,21 @@ protected:
     __aicore__ inline void End()
     {
         commOp.End();
+        scaleCommOp.End();
         computeOp.End();
         localComputeOp.End();
     }
 
 private:
     CommOpType commOp;
-    CommOpType scaleCommOp;  // xscale的通信操作
+    CommOpType scaleCommOp; // xscale的通信操作
     ComputeOpType computeOp;
     LocalComputeOpType localComputeOp;
+    GM_ADDR commOutGm = nullptr;
     GM_ADDR gmmxScaleCommOutGm = nullptr;
+    GM_ADDR computeScaleGM = nullptr;
     const TilingDataType *tilingData_ = nullptr;
     uint32_t e_ = 0U;
 };
-};
+}; // namespace MC2KernelTemplate
 #endif
