551898c52567a710c6e1ccc813525c90d35aecac
h00808442 <hanxiaobin3@huawei.com>
2026-03-20 11:42:11 +0800

    add output init value

--- attention/block_sparse_attention/op_host/block_sparse_attention_def.cpp
+++ attention/block_sparse_attention/op_host/block_sparse_attention_def.cpp
@@ -56,10 +56,12 @@ public:
             .ParamType(REQUIRED)
             .DataType({ge::DT_FLOAT16, ge::DT_BF16})
             .FormatList({ge::FORMAT_ND});
+            .InitValue(0);
         this->Output("softmaxLse")
             .ParamType(OPTIONAL)
             .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
             .FormatList({ge::FORMAT_ND});
+            .InitValue(0);
         
         this->Attr("qInputLayout").AttrType(OPTIONAL).String("TND");
         this->Attr("kvInputLayout").AttrType(OPTIONAL).String("TND");
