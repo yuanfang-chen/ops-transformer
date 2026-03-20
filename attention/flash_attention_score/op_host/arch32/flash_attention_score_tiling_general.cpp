942b40ec32efe805f26f74740dbc3aee7b4707f7
Yizhouw21 <yizhouw21@163.com>
2026-03-20 10:02:35 +0800

    FIX X1 PSE CASE

--- attention/flash_attention_score/op_host/arch32/flash_attention_score_tiling_general.cpp
+++ attention/flash_attention_score/op_host/arch32/flash_attention_score_tiling_general.cpp
@@ -846,7 +846,7 @@ bool FlashAttentionScoreTilingBase::SetPseAlibiParams()
         }
         return true;
     }
-    if (pseShape->GetStorageShape().GetDimNum() < 2) {  // 2 is min dim num of legal pse
+    if (pseShape->GetStorageShape().GetDimNum() < 1) {  // 1 is min dim num of legal pse
         OP_LOGE(context_, "Invalid Pse DimNum(%zu), PseType(%ld).",
                   pseShape->GetStorageShape().GetDimNum(), pseType);
         return false;
@@ -3419,7 +3419,7 @@ protected:
             return true;
         }
         // 2: pre last axiss
-        if (pseShape->GetStorageShape().GetDimNum() < 2) {  // 2 is min dim num of legal pse
+        if (pseShape->GetStorageShape().GetDimNum() < 1) {  // 1 is min dim num of legal pse
             OP_LOGE(context_, "Invalid Pse DimNum(%zu), PseType(%ld).",
                       pseShape->GetStorageShape().GetDimNum(), pseType);
             return false;
@@ -3696,7 +3696,7 @@ protected:
             return true;
         }
         // 2: pre last axiss
-        if (pseShape->GetStorageShape().GetDimNum() < 2) {  // 2 is min dim num of legal pse
+        if (pseShape->GetStorageShape().GetDimNum() < 1) {  // 1 is min dim num of legal pse
             OP_LOGE(context_, "Invalid Pse DimNum(%zu), PseType(%ld).",
                       pseShape->GetStorageShape().GetDimNum(), pseType);
             return false;
@@ -3875,7 +3875,7 @@ protected:
             return true;
         }
         // 2: pre last axiss
-        if (pseShape->GetStorageShape().GetDimNum() < 2) {  // 2 is min dim num of legal pse
+        if (pseShape->GetStorageShape().GetDimNum() < 1) {  // 1 is min dim num of legal pse
             OP_LOGE(context_, "Invalid Pse DimNum(%zu), PseType(%ld).",
                       pseShape->GetStorageShape().GetDimNum(), pseType);
             return false;
