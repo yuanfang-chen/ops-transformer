8b7c2f76e34f0644c9a5f035df7fe740cd7d83fa
fazhenyao123 <fazhenyao@h-partners.com>
2026-03-21 17:11:26 +0800

    同步3rd/moe_inplace_index_add修改

--- moe/3rd/moe_inplace_index_add/op_host/arch35/moe_inplace_index_add_simt_sort_tiling.h
+++ moe/3rd/moe_inplace_index_add/op_host/arch35/moe_inplace_index_add_simt_sort_tiling.h
@@ -36,6 +36,7 @@ TILING_DATA_FIELD_DEF(int64_t, tailUpdatesPreNum);        // 尾循环搬运upda
 TILING_DATA_FIELD_DEF(int64_t, updatesPreLoop);           // 搬1次indices时，要搬多份updates的循环次数
 TILING_DATA_FIELD_DEF(int64_t, usedCoreNum);
 TILING_DATA_FIELD_DEF(int64_t, indicesStride);
+TILING_DATA_FIELD_DEF(int64_t, indicesCastMode);
 END_TILING_DATA_DEF;
 
 REGISTER_TILING_DATA_CLASS(MoeInplaceIndexAdd_500000, MoeInplaceIndexAddSimtSortTilingData)
