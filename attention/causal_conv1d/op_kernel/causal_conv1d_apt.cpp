b2488c125e8bfec0626934207802bf7c987bd4b2
qiumingli <liqiuming4@huawei.com>
2026-03-21 16:44:30 +0800

    apt

--- attention/causal_conv1d/op_kernel/causal_conv1d_apt.cpp
+++ attention/causal_conv1d/op_kernel/causal_conv1d_apt.cpp
@@ -35,6 +35,7 @@ extern "C" __global__ __aicore__ void causal_conv1d(
     GM_ADDR workspace,            // workspace
     GM_ADDR tiling)               // tiling
 {
+    KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
     REGISTER_TILING_DEFAULT(CausalConv1dCutBSHTilingData);
     REGISTER_TILING_FOR_TILINGKEY("(TILING_KEY_VAR >= 20000)", CausalConv1dCutBHTilingData);
     TPipe pipe;
