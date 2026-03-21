cfd9d9f9665a0815255c057732eedafc5f43fea1
wangzhe123456789 <wangzhe92@huawei.com>
2026-03-21 16:43:05 +0800

    diff

--- examples/pybind/fused_infer_attention_score/gen_data.py
+++ examples/pybind/fused_infer_attention_score/gen_data.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 # ----------------------------------------------------------------------------------------------------------
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 # CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -11,21 +11,19 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------------------------------------
 
-
-import sys
-import os
+import numpy as np
+import en_dtypes
 import torch
 import torch_npu
-from torch_npu.testing.testcase import TestCase, run_tests
-import numpy as np
-sys.path.append(os.getcwd())
-import ascendc_ops
+import os
 
-ERROR_TOL = 5e-3
-DATA_TYPE = np.float32
-
-def gen_golden_data_simple(b, n1, n2, s1, s2, d):
-
+def gen_golden_data_simple():
+    b = 1
+    n1 = 1
+    n2 = 1
+    s1 = 8192
+    s2 = 8192
+    d = 128
 
     input_layout = 'BNSD'
     scale_value = 0.088388
@@ -71,99 +69,6 @@ def gen_golden_data_simple(b, n1, n2, s1, s2, d):
                                                             value_dtype=torch_npu.float8_e4m3fn)
     npu_out[0].cpu().numpy().tofile("./output/golden_out.bin")
 
-def load_bf16_bin(file_path):
-    # 读取原始字节
-    with open(file_path, 'rb') as f:
-        data_bytes = f.read()
-    
-    # 转为 uint16 数组（每个 bf16 占 2 字节）
-    uint16_array = np.frombuffer(data_bytes, dtype=np.uint16)
-
-    # 转换为 torch.bfloat16
-    # 注意：numpy 不支持 bf16，所以要用 torch.from_numpy + to
-    bf16_tensor = torch.from_numpy(uint16_array).to(torch.bfloat16)
-
-    return bf16_tensor
-
-def verify_result():
-    output = load_bf16_bin("./output/npu_out.bin")
-    golden = load_bf16_bin("./output/golden_out.bin")
-
-    print("output:")
-    print(output)
-    print("golden:")
-    print(golden)
-
-    output = output.float()
-    golden = golden.float()
-
-    # ------------------------------
-    # NaN mask
-    # ------------------------------
-
-    output_nan = np.isnan(output)
-    golden_nan = np.isnan(golden)
-
-    both_nan = output_nan & golden_nan
-    nan_mismatch = output_nan ^ golden_nan
-
-    # ------------------------------
-    # 数值误差
-    # ------------------------------
-
-    diff = np.abs(output - golden)
-
-    # 误差位置
-    diff_mask = diff > 1
-
-    # 合并错误
-    error_mask = (diff_mask | nan_mismatch) & (~both_nan)
-
-    diff_indices = np.where(error_mask)[0]
-
-    # ------------------------------
-    # 打印前100个错误
-    # ------------------------------
-
-    max_print = min(100, diff_indices.size)
-
-    for i in range(max_print):
-
-        idx = diff_indices[i]
-
-        golden_val = golden[idx]
-        output_val = output[idx]
-
-        denom = max(abs(golden_val), 1e-12)
- 
-        rdiff = abs(output_val - golden_val) / denom
-
-
-    # ------------------------------
-    # error ratio
-    # ------------------------------
-
-    error_ratio = diff_indices.size / golden.numpy().size
-
-    print("error count:", diff_indices.size)
-    print("total count:", golden.numpy().size)
-
-    return error_ratio <= ERROR_TOL
-
-class TestFia(TestCase):
-    def test_fia(self):
-        gen_golden_data_simple(1, 1, 1, 8192, 8192, 128)
-        ascendc_ops.ascendc_fia(1, 1, 1, 8192, 8192, 128)
-
-        try:
-            res = verify_result()
-            if not res:
-                raise ValueError("[ERROR] result error")
-            print("test pass")
-        except Exception as e:
-            print(e)
-            sys.exit(1)
-
 
 if __name__ == "__main__":
-    run_tests()
+    gen_golden_data_simple()
--- examples/pybind/fused_infer_attention_score/test.sh
+++ examples/pybind/fused_infer_attention_score/test.sh
@@ -1,3 +1,3 @@
 mkdir -p build; cd build
 cmake ..; make -j
-python3 fia_test.py
+python3 ../fia_test.py
