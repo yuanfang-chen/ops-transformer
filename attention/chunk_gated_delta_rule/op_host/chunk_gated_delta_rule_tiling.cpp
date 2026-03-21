87211f6289743b27460f7a19d9f1e85a26ab2c23
liusiwei5 <liusiwei5@huawei.com>
2026-03-21 10:24:09 +0800

    fix tiling

--- attention/chunk_gated_delta_rule/op_host/chunk_gated_delta_rule_tiling.cpp
+++ attention/chunk_gated_delta_rule/op_host/chunk_gated_delta_rule_tiling.cpp
@@ -113,7 +113,7 @@ namespace optiling {
         int64_t b = tilingData_.b;
         tilingData_.interWorkspaceSz += sizeHigh * nv * s;  // gCumExp
         tilingData_.interWorkspaceSz += sizeHigh * nv * s * dk;  // kCumDecay
-        tilingData_.interWorkspaceSz += sizeHigh * nv * s * dk;  // vInner
+        tilingData_.interWorkspaceSz += sizeHigh * nv * s * dv;  // vInner
         tilingData_.interWorkspaceSz += sizeHigh * nv * s * dk;  // qPrime
         tilingData_.interWorkspaceSz += sizeHigh * nv * s * dv;  // attnInter
         tilingData_.interWorkspaceSz += sizeHigh * nv * s * dk;  // kg
