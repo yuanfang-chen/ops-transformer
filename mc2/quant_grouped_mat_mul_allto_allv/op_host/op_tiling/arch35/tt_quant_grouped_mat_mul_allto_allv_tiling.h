7abf0588605d49b3843d1961d66896a5ae365813
huangshuai <huangshuai59@huawei.com>
2026-03-21 15:58:40 +0800

    fix conflict

--- mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
+++ mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/tt_quant_grouped_mat_mul_allto_allv_tiling.h
@@ -17,17 +17,7 @@
 #define TT_QUANT_GROUPED_MAT_MUL_ALLTO_ALLV_TILING_H
 
 #pragma once
-#include "securec.h"
-#include "tiling/tiling_api.h"
-#include "op_host/op_tiling/mc2_tiling_utils.h"
-#include "tiling_base/tiling_base.h"
-#include "tiling_base/tiling_templates_registry.h"
-#include "mc2_matmul_tiling_cfg.h"
-#include "op_host/op_tiling/new_mc2_tiling_utils.h"
 #include "quant_grouped_mat_mul_allto_allv_tiling_base.h"
-#include "../../../op_kernel/arch35/quant_grouped_mat_mul_allto_allv_tiling.h"
-#include "../../../op_kernel/quant_grouped_mat_mul_allto_allv_tiling_key.h"
-#include "register/tilingdata_base.h"
 
 namespace optiling {
 namespace Mc2GroupedMatmul {
@@ -44,8 +34,6 @@ protected:
     bool IsCapable() override;
     ge::graphStatus GetWorkspaceSize() override;
     uint64_t GetTilingKey() const override;
-    ge::graphStatus CheckAndSetInputOutputInfo();
-    ge::graphStatus SetGmmA2avWorkspaceInfo();
 };
 
 } // namespace Mc2GroupedMatmul
