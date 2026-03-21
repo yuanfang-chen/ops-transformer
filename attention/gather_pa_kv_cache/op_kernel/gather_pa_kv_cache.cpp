7eb38baa7e5367f26026b4c15e436ecc61218703
zhuzemao <zhuzemao1@h-partners.com>
2026-03-21 16:29:36 +0800

    1

--- attention/gather_pa_kv_cache/op_kernel/gather_pa_kv_cache.cpp
+++ attention/gather_pa_kv_cache/op_kernel/gather_pa_kv_cache.cpp
@@ -26,11 +26,6 @@ extern "C" __global__ __aicore__ void gather_pa_kv_cache(GM_ADDR keyCache, GM_AD
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
     AscendC::TPipe pipe;
     GET_TILING_DATA_WITH_STRUCT(GatherPaKvCacheTilingData, tilingData, tiling);
-    if (TILING_KEY_IS (618)) {
-        GatherPaKvCache::GatherPaKvCacheNd<int8_t> op(&pipe);
-        op.Init(keyCache, valueCache, blockTables, seqLens, seqOffset, keyOut, valueOut, &tilingData);
-        op.Process();
-    }
 #if ORIG_DTYPE_KEY_CACHE == DT_FLOAT16 || ORIG_DTYPE_KEYCACHE == DT_BFLOAT16
     if (TILING_KEY_IS (619)) {
         GatherPaKvCache::GatherPaKvCacheNd<half> op(&pipe);
@@ -40,6 +35,11 @@ extern "C" __global__ __aicore__ void gather_pa_kv_cache(GM_ADDR keyCache, GM_AD
 #else  # INT_8
     if (TILING_KEY_IS (577)) {
         GatherPaKvCache::GatherPaKvCacheNz<int8_t> op(&pipe);
+        op.Init(keyCache, valueCache, blockTables, seqLens, seqOffset, keyOut, valueOut, &tilingData);
+        op.Process();
+    }
+    else (TILING_KEY_IS (618)) {
+        GatherPaKvCache::GatherPaKvCacheNd<int8_t> op(&pipe);
         op.Init(keyCache, valueCache, blockTables, seqLens, seqOffset, keyOut, valueOut, &tilingData);
         op.Process();
     }
