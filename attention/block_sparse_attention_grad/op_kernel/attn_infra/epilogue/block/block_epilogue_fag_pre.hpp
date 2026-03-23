d3d4c9fa73e0acbb71de5e91ef7be65aa8d40dcf
tramp-ll <linglong4@huawei.com>
2026-03-23 15:49:04 +0800

    update: 更新文件 block_epilogue_fag_pre.hpp clean code pre
    Signed-off-by: tramp-ll <linglong4@huawei.com>

--- attention/block_sparse_attention_grad/op_kernel/attn_infra/epilogue/block/block_epilogue_fag_pre.hpp
+++ attention/block_sparse_attention_grad/op_kernel/attn_infra/epilogue/block/block_epilogue_fag_pre.hpp
@@ -1,11 +1,16 @@
-/*
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file block_epliogue_fag_pre.h
+ * \brief Block Epliogue Fag Pre Kernel Implementation
  */
 
 #ifndef CATLASS_EPILOGUE_BLOCK_BLOCK_EPILOGUE_FAG_PRE_HPP
