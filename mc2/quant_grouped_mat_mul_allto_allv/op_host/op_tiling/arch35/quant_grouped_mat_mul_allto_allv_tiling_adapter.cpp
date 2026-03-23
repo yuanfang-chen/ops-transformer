f1238fa8032e8dd6a769f8e471cb53189d445b8c
huangshuai <huangshuai59@huawei.com>
2026-03-23 15:14:23 +0800

    算子统一使用MC2Tiling

--- mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/quant_grouped_mat_mul_allto_allv_tiling_adapter.cpp
+++ mc2/quant_grouped_mat_mul_allto_allv/op_host/op_tiling/arch35/quant_grouped_mat_mul_allto_allv_tiling_adapter.cpp
@@ -20,8 +20,8 @@
 using namespace Mc2Log;
 using namespace AscendC;
 using namespace MC2Tiling;
-using namespace Mc2GroupedMatmulTiling;
-using namespace Mc2GroupedMatmulTiling::GmmConstant;
+using namespace optiling::Mc2GroupedMatmulTiling;
+using namespace optiling::Mc2GroupedMatmulTiling::GmmConstant;
 using namespace MC2Tiling::Mc2GroupedMatmul;
 
 namespace MC2Tiling {
@@ -60,7 +60,7 @@ ge::graphStatus QuantGroupedMatmulAllToAllvAdapter::SetGroupExpertInputParameter
         inputParams_.bQuantMode = static_cast<QuantMode>(1U << (QUANT_MODE_MP[params.gmmWeightQuantMode]));
     }
     // 是否做切分
-    inputParams_.groupType = MC2Tiling::Mc2GroupedMatmul::SPLIT_M;
+    inputParams_.groupType = optiling::Mc2GroupedMatmul::SPLIT_M;
     inputParams_.groupListType = 1;
     inputParams_.aDtype = params.gmmXDtype;
     inputParams_.bDtype = params.gmmWeightDtype;
@@ -89,7 +89,7 @@ ge::graphStatus QuantGroupedMatmulAllToAllvAdapter::SetSharedExpertInputParamete
         inputParams_.bQuantMode = static_cast<QuantMode>(1U << (QUANT_MODE_MP[params.mmWeightQuantMode]));
     }
     // 是否做切分
-    inputParams_.groupType = MC2Tiling::Mc2GroupedMatmul::SPLIT_M;
+    inputParams_.groupType = optiling::Mc2GroupedMatmul::SPLIT_M;
     // 非负递增为0，非负数列为1
     inputParams_.groupListType = 1;
     inputParams_.aDtype = params.mmXDtype;
