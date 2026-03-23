2914c1a214460f0d6e06c57ab353d3a8e3edc909
number-2-bo-bo <jinying22@huawei.com>
2026-03-23 12:47:29 +0800

    QSFAP支持kvCache非连续

--- attention/kv_quant_sparse_flash_attention_pioneer/op_host/op_api/kv_quant_sparse_flash_attention_pioneer.cpp
+++ attention/kv_quant_sparse_flash_attention_pioneer/op_host/op_api/kv_quant_sparse_flash_attention_pioneer.cpp
@@ -45,7 +45,7 @@ const aclTensor *KvQuantSparseFlashAttentionPioneer(
         int64_t ropeHeadDim,
         aclOpExecutor *executor)
 {
-    int64_t keyBlockStride = 0;
+    int64_t keyBlockStride = -1;
     auto blockStrides = key->GetViewStrides();
     keyBlockStride = blockStrides[0];
 
