42dfa9e271134a9a0e4396d06246d7cdbf8c6bc4
qiansunchi <qiansunchi@huawei.com>
2026-03-23 16:12:26 +0800

    test empty pr

--- attention/dense_lightning_indexer_grad_kl_loss/op_kernel/dense_lightning_indexer_grad_kl_loss_vector2.h
+++ attention/dense_lightning_indexer_grad_kl_loss/op_kernel/dense_lightning_indexer_grad_kl_loss_vector2.h
@@ -5,7 +5,7 @@
  * Please refer to the License for details. You may not use this file except in compliance with the License.
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+ * See LICENSE in the root of the software repository for the full text of the License. 
  */
 
 /*!
@@ -16,7 +16,7 @@
 #ifndef DENSE_LIGHTNING_INDEXER_GRAD_KL_LOSS_VECTOR2_H
 #define DENSE_LIGHTNING_INDEXER_GRAD_KL_LOSS_VECTOR2_H
 
-#include "kernel_operator.h"
+#include "kernel_operator.h" 
 #include "kernel_operator_list_tensor_intf.h"
 #include "kernel_tiling/kernel_tiling.h"
 #include "lib/matmul_intf.h"
