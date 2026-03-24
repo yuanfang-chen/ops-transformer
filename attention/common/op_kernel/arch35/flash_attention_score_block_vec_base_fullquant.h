7875443ab6ea00ffc291c21c3893f91d2b6f4f08
fanzijian <fanzijian3@huawei.com>
2026-03-24 11:27:37 +0800

    support fp8 mla layout

--- attention/common/op_kernel/arch35/flash_attention_score_block_vec_base_fullquant.h
+++ attention/common/op_kernel/arch35/flash_attention_score_block_vec_base_fullquant.h
@@ -1489,29 +1489,27 @@ __aicore__ inline void FABlockVecBaseFullquant<TEMPLATE_BASE_ARGS>::Bmm2DataCopy
     }
 
     if constexpr (isInfer) {
-        if (constInfo.isPfaGS1Merge && dSizeAligned64 - constInfo.dSizeV != 0 && (constInfo.layoutType == static_cast<uint8_t>(LayOutTypeEnum::LAYOUT_BSH) || constInfo.layoutType == static_cast<uint8_t>(LayOutTypeEnum::LAYOUT_TND))) {
-            for(int64_t i = 0; i < runInfo.vec2S1BaseSize / constInfo.gSize; i++){
-                attenOutOffset = i * constInfo.dSizeV * constInfo.gSize * constInfo.n2Size;
-                dataCopyParams.blockLen = constInfo.dSizeV * sizeof(OUTPUT_T);
-                dataCopyParams.blockCount = constInfo.gSize;
-                dataCopyParams.dstStride = 0;
-                DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + attenOutOffset],
-                    attenOut[i * constInfo.gSize * dSizeAligned64], dataCopyParams);
+        if (isMlaFullQuant) {
+            if (constInfo.transposeLayout == static_cast<uint32_t>(TransposeLayoutEnum::BSND_NBSD)) {
+                MlaTransposeDataCopyOut(runInfo, constInfo, attenOut);
+            } else {
+                DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + vec2S1Idx * runInfo.vec2S1BaseSize * attenOutOffset],
+                    attenOut, dataCopyParams);
             }
         } else {
-            DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + vec2S1Idx * runInfo.vec2S1BaseSize * attenOutOffset],
-                attenOut, dataCopyParams);
-        }
-    } else if constexpr (isInfer && isMlaNoQuant) {
-        if (constInfo.transposeLayout == static_cast<uint32_t>(TransposeLayoutEnum::BSND_NBSD) ||
-            constInfo.transposeLayout == static_cast<uint32_t>(TransposeLayoutEnum::BSH_NBSD) ||
-            constInfo.transposeLayout == static_cast<uint32_t>(TransposeLayoutEnum::TND_NTD)) {
-            MlaTransposeDataCopyOut(runInfo, constInfo, attenOut);
-        } else if (constInfo.transposeLayout == static_cast<uint32_t>(TransposeLayoutEnum::BNSD_NBSD)) {
-            MlaTranspose2DataCopyOut(runInfo, constInfo, attenOut);
-        } else {
-            DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + vec2S1Idx * runInfo.vec2S1BaseSize * attenOutOffset],
-                attenOut, dataCopyParams);
+            if (constInfo.isPfaGS1Merge && dSizeAligned64 - constInfo.dSizeV != 0 && (constInfo.layoutType == static_cast<uint8_t>(LayOutTypeEnum::LAYOUT_BSH) || constInfo.layoutType == static_cast<uint8_t>(LayOutTypeEnum::LAYOUT_TND))) {
+                for(int64_t i = 0; i < runInfo.vec2S1BaseSize / constInfo.gSize; i++){
+                    attenOutOffset = i * constInfo.dSizeV * constInfo.gSize * constInfo.n2Size;
+                    dataCopyParams.blockLen = constInfo.dSizeV * sizeof(OUTPUT_T);
+                    dataCopyParams.blockCount = constInfo.gSize;
+                    dataCopyParams.dstStride = 0;
+                    DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + attenOutOffset],
+                        attenOut[i * constInfo.gSize * dSizeAligned64], dataCopyParams);
+                }
+            } else {
+                DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + vec2S1Idx * runInfo.vec2S1BaseSize * attenOutOffset],
+                    attenOut, dataCopyParams);
+            }
         }
     } else {
         DataCopyPad(this->attentionOutGm[runInfo.attentionOutOffset + vec2S1Idx * runInfo.vec2S1BaseSize * attenOutOffset],
