7c732b0b46a70caf1ae32472df4976c6bce80110
hzp_gitcode <huzhenpeng@huawei.com>
2026-03-24 16:18:24 +0800

    all test

--- mc2/allto_allv_quant_grouped_mat_mul/op_host/op_tiling/allto_allv_quant_grouped_mat_mul_tiling_base.h
+++ mc2/allto_allv_quant_grouped_mat_mul/op_host/op_tiling/allto_allv_quant_grouped_mat_mul_tiling_base.h
@@ -90,10 +90,11 @@ constexpr uint64_t L1_ALIGN_SIZE = 32;
 // quant mode
 constexpr uint64_t NO_QUANT_MODE = 0;
 constexpr uint64_t PERTENSOR_QUANT_MODE = 1;
+constexpr uint64_t MX_QUANT_MODE = 6;
 
-class AlltoAllvGmmTilingBase : public Ops::Transformer::OpTiling::TilingBaseClass {
+class AlltoAllvQuantGmmTilingBase : public Ops::Transformer::OpTiling::TilingBaseClass {
 public:
-    explicit AlltoAllvGmmTilingBase(gert::TilingContext *context)
+    explicit AlltoAllvQuantGmmTilingBase(gert::TilingContext *context)
         : Ops::Transformer::OpTiling::TilingBaseClass(context){};
 
 protected:
