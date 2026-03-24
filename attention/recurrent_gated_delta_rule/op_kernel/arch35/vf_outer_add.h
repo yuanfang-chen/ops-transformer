f6caf4ed0767afc5020d8a645fabf6372f2e870d
huang-chuhong <huangchuhong1@h-partners.com>
2026-03-24 16:19:44 +0800

    vf

--- attention/recurrent_gated_delta_rule/op_kernel/arch35/vf_outer_add.h
+++ attention/recurrent_gated_delta_rule/op_kernel/arch35/vf_outer_add.h
@@ -21,6 +21,38 @@
 namespace AscendC {
 
 template <typename T>
+__simd_vf__ void OuterAddVFImpl(__ubuf__ T* outputBuf, __ubuf__ T* deltaBuf, __ubuf__ T* kInputBuf, __ubuf__ T* stateInBuf,
+    const uint32_t floatRepSize, uint16_t dLoops, uint32_t dTail, const uint16_t row, const uint32_t col)
+{
+    AscendC::MicroAPI::RegTensor<T> vregKInput;
+    AscendC::MicroAPI::RegTensor<T> vregDelta;
+    AscendC::MicroAPI::RegTensor<T> vregStateIn;
+    AscendC::MicroAPI::MaskReg pregAll = AscendC::MicroAPI::CreateMask<T, AscendC::MicroAPI::MaskPattern::ALL>();
+
+    for (uint16_t j = 0; j < dLoops; j++) {
+        AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + j * floatRepSize);
+        for (uint16_t i = 0; i < row; i++) {
+            uint16_t loopOffset = i * col + j * floatRepSize;
+            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
+            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
+            AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, pregAll);
+            AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, pregAll);
+        }
+    }
+
+    if (dTail > 0) {
+        AscendC::MicroAPI::MaskReg tailMask = AscendC::MicroAPI::UpdateMask<T>(dTail);
+        AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + dLoops * floatRepSize);
+        for (uint16_t i = 0; i < row; i++) {
+            uint16_t loopOffset = i * col + dLoops * floatRepSize;
+            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
+            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
+            AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, tailMask);
+            AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, tailMask);
+        }
+    }
+}
+template <typename T>
 __aicore__ inline void OuterAddVF(const LocalTensor<T>& outputUb, const LocalTensor<T>& deltaInUb,
                                   const LocalTensor<T>& kInUb, const LocalTensor<T>& stateInUb, const uint16_t row,
                                   const uint32_t col)
@@ -33,37 +65,7 @@ __aicore__ inline void OuterAddVF(const LocalTensor<T>& outputUb, const LocalTen
     constexpr uint16_t floatRepSize = 64; // 一个寄存器能够存放64个FP32
     uint16_t dLoops = col / floatRepSize;
     uint32_t dTail = col % floatRepSize;
-
-    __VEC_SCOPE__
-    {
-        AscendC::MicroAPI::RegTensor<T> vregKInput;
-        AscendC::MicroAPI::RegTensor<T> vregDelta;
-        AscendC::MicroAPI::RegTensor<T> vregStateIn;
-        AscendC::MicroAPI::MaskReg pregAll = AscendC::MicroAPI::CreateMask<T, AscendC::MicroAPI::MaskPattern::ALL>();
-
-        for (uint16_t j = 0; j < dLoops; j++) {
-            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + j * floatRepSize);
-            for (uint16_t i = 0; i < row; i++) {
-                uint16_t loopOffset = i * col + j * floatRepSize;
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
-                AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, pregAll);
-                AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, pregAll);
-            }
-        }
-
-        if (dTail > 0) {
-            AscendC::MicroAPI::MaskReg tailMask = AscendC::MicroAPI::UpdateMask<T>(dTail);
-            AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregKInput, kInputBuf + dLoops * floatRepSize);
-            for (uint16_t i = 0; i < row; i++) {
-                uint16_t loopOffset = i * col + dLoops * floatRepSize;
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_BRC_B32>(vregDelta, deltaBuf + i); // 使用DIST_BRC_B32模式搬运将首元素broadcast到整个寄存器
-                AscendC::MicroAPI::LoadAlign<T, AscendC::MicroAPI::LoadDist::DIST_NORM>(vregStateIn, stateInBuf + loopOffset);
-                AscendC::MicroAPI::MulAddDst<T, AscendC::MicroAPI::MaskMergeMode::ZEROING>(vregStateIn, vregKInput, vregDelta, tailMask);
-                AscendC::MicroAPI::StoreAlign<T, AscendC::MicroAPI::StoreDist::DIST_NORM>(outputBuf + loopOffset, vregStateIn, tailMask);
-            }
-        }
-    }
+    OuterAddVFImpl<T>(outputBuf, deltaBuf, kInputBuf, stateInBuf, floatRepSize, dLoops, dTail, row, col);
 }
 
 } // namespace
