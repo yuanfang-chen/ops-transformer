43b212e88612e5149257c03d779cd09992475c29
youngcao119 <caoyang119@huawei.com>
2026-03-24 10:17:37 +0800

    Add the new dtype float4_e2m1 for input x in the MoeInitRoutingV3 Op.

--- moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
+++ moe/moe_init_routing_v3/op_kernel/moe_init_routing_v3_apt.cpp
@@ -24,6 +24,7 @@
 #include "arch35/moe_v3_gather_hif8_pertensor_quant.h"
 #include "arch35/moe_v3_gather_hif8_pertoken_quant.h"
 #include "arch35/moe_v3_gather_hif8_quant.h"
+#include "arch35/moe_v3_gather_out_mxfp4.h"
 
 /*
  * 非量化
@@ -172,18 +173,27 @@ extern "C" __global__ __aicore__ void moe_init_routing_v3(GM_ADDR x, GM_ADDR exp
         TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTONECORE_SCATTER) ||
         TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_GATHER) ||
         TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_SCATTER)) {
-        // 非量化
-        TPipe gatherPipe;
-        MoeGatherOut<DTYPE_X> gatherOp;
-        gatherOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
-        gatherOp.Process();
-        gatherPipe.Destroy();
+        // 非量化float4_e2m1
+        if constexpr (IsSameType<DTYPE_X, fp4x2_e2m1_t>::value) {
+            TPipe gatherPipe;
+            MoeGatherOutFp4<DTYPE_X> gatherOp;
+            gatherOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
+            gatherOp.Process();
+            gatherPipe.Destroy();
+        } else {
+            TPipe gatherPipe;
+            MoeGatherOut<DTYPE_X> gatherOp;
+            gatherOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
+            gatherOp.Process();
+            gatherPipe.Destroy();
+        }
     } else if (TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTONECORE_DYNAMICQUANT_GATHER) ||
                TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTONECORE_DYNAMICQUANT_SCATTER) ||
                TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_DYNAMICQUANT_GATHER) ||
                TILING_KEY_IS(MOE_INIT_ROUTING_V3_SORTMULTICORE_DYNAMICQUANT_SCATTER)) {
         // 动态量化
-        if constexpr (!IsSameType<DTYPE_X, int8_t>::value && !IsSameType<DTYPE_EXPANDED_X, hifloat8_t>::value) {
+        if constexpr ((IsSameType<DTYPE_X, bfloat16_t>::value || IsSameType<DTYPE_X, half>::value ||
+                        IsSameType<DTYPE_X, float>::value) && (IsSameType<DTYPE_EXPANDED_X, int8_t>::value)) {
             TPipe gatherPipe;
             MoeGatherOutDynamicQuant<DTYPE_X> gatherDynamicQuantOp;
             gatherDynamicQuantOp.Init(x, scale, userWS, expandedRowIdx, expandedX, expandedScale, t, &gatherPipe);
