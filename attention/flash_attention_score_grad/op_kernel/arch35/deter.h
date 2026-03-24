fa20f1906acf3183d545580b8588d52184b969c3
dai-yuwen <daiyuwen1@huawei.com>
2026-03-24 11:16:38 +0800

    FAG确定性问题修复
    Signed-off-by: dai-yuwen <daiyuwen1@huawei.com>

--- attention/flash_attention_score_grad/op_kernel/arch35/deter.h
+++ attention/flash_attention_score_grad/op_kernel/arch35/deter.h
@@ -1389,6 +1389,8 @@ CalTNDDenseIndex(const __gm__ uint8_t *actualSeqQlenAddr, const __gm__ uint8_t *
             if constexpr (DETER_SPARSE_TYPE == DETER_CAUSAL) {
                 CalVirtualIndex(flag, m, n);
             } else if constexpr (DETER_SPARSE_TYPE == DETER_BAND) {
+                coordinateInfo.actualS1Len = actualS1Len;
+                coordinateInfo.actualS2Len = actualS2Len;
                 UpdateMNPQ<CUBE_BASEM, CUBE_BASEN, true, IS_N_EQUAL>(actualCalcS1Token, actualCalcS2Token, coordinateInfo, m, n);
                 p = coordinateInfo.p;
                 q = coordinateInfo.q;
