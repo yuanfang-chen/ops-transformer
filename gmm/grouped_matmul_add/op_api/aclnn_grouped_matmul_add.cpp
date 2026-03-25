9a8137ffcdb16a6290d6d3b08b32720b97ecc548
smdbha <panzhijie2@huawei.com>
2026-03-25 11:58:00 +0800

    refactor(gmm): move fallback and op_api out of op_host

--- gmm/grouped_matmul_add/op_api/aclnn_grouped_matmul_add.cpp
+++ gmm/grouped_matmul_add/op_api/aclnn_grouped_matmul_add.cpp
@@ -29,7 +29,7 @@
 #include "opdev/tensor_view_utils.h"
 #include "opdev/make_op_executor.h"
 
-#include "../../../grouped_matmul/op_host/op_api/grouped_matmul_util.h"
+#include "../../grouped_matmul/op_api/grouped_matmul_util.h"
 #include "grouped_matmul_add_util.h"
 #include "grouped_matmul_add.h"
 
