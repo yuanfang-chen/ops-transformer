cccf272857f0010b4f4c890327b34cd8c9309c6f
ivanchenko aleksei <amivanchenko@yandex.ru>
2026-03-25 15:49:47 +0300

    update TSQR

--- experimental/svd/ascendc/tsqr/op_host/tsqr_tiling.h
+++ experimental/svd/ascendc/tsqr/op_host/tsqr_tiling.h
@@ -28,6 +28,7 @@ BEGIN_TILING_DATA_DEF(TsqrTilingData)
     TILING_DATA_FIELD_DEF(int32_t, blockSize);
     TILING_DATA_FIELD_DEF(int32_t, numLevels);
     TILING_DATA_FIELD_DEF(int32_t, numBlocks);
+    TILING_DATA_FIELD_DEF(int32_t, batchFactor);
     TILING_DATA_FIELD_DEF(int64_t, tmpQSize);
     TILING_DATA_FIELD_DEF(int64_t, tmpRSize);
     TILING_DATA_FIELD_DEF(int64_t, bufferQSize);
