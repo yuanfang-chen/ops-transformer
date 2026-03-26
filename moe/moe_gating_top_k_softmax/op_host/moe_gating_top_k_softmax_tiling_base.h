76b6ecdccc0664be18838acfe4b3a13d695e52d5
cann-robot <songchangxia@cann.team>
2026-03-26 16:00:09 +0800

    revert//add 310p topkSoftmax

--- moe/moe_gating_top_k_softmax/op_host/moe_gating_top_k_softmax_tiling_base.h
+++ moe/moe_gating_top_k_softmax/op_host/moe_gating_top_k_softmax_tiling_base.h
@@ -101,28 +101,6 @@ REGISTER_TILING_DATA_CLASS(MoeGatingTopKSoftmax_15, MoeGatingTopKSoftmaxPerfTili
 REGISTER_TILING_DATA_CLASS(MoeGatingTopKSoftmax_16, MoeGatingTopKSoftmaxPerfTilingData);
 REGISTER_TILING_DATA_CLASS(MoeGatingTopKSoftmax_17, MoeGatingTopKSoftmaxPerfTilingData);
 
-BEGIN_TILING_DATA_DEF(MoeGatingTopKSoftmax310PTilingData)
-TILING_DATA_FIELD_DEF(uint32_t, tilingKey);
-TILING_DATA_FIELD_DEF(int32_t, row);
-TILING_DATA_FIELD_DEF(int32_t, col);
-TILING_DATA_FIELD_DEF(int32_t, k);
-TILING_DATA_FIELD_DEF(int32_t, kAlign);
-TILING_DATA_FIELD_DEF(int32_t, blockNum);
-TILING_DATA_FIELD_DEF(int32_t, workspaceSize);
-TILING_DATA_FIELD_DEF(int32_t, hasFinished);
-TILING_DATA_FIELD_DEF(int32_t, oneCoreRow);
-TILING_DATA_FIELD_DEF(int32_t, activateCore);
-TILING_DATA_FIELD_DEF(int32_t, tailRow);
-TILING_DATA_FIELD_DEF(int32_t, FormerTmpMinsize);
-TILING_DATA_FIELD_DEF(int32_t, TailTmpMinsize);
-TILING_DATA_FIELD_DEF_STRUCT(SoftMaxTiling, FormerSoftmaxTilingData); // 将SoftMaxTiling结构体参数增加至TilingData结构体
-TILING_DATA_FIELD_DEF_STRUCT(SoftMaxTiling, TailSoftmaxTilingData); // 将SoftMaxTiling结构体参数增加至TilingData结构体
-TILING_DATA_FIELD_DEF_STRUCT(TopkTiling, FormerTopkTilingData); // 将SoftMaxTiling结构体参数增加至TilingData结构体
-TILING_DATA_FIELD_DEF_STRUCT(TopkTiling, TailTopkTilingData); // 将SoftMaxTiling结构体参数增加至TilingData结构体
-END_TILING_DATA_DEF;
-
-REGISTER_TILING_DATA_CLASS(MoeGatingTopKSoftmax_18, MoeGatingTopKSoftmax310PTilingData)
-
 class MoeGatingTopKSoftmaxBaseTiling : public Ops::Transformer::OpTiling::TilingBaseClass
 {
 public:
