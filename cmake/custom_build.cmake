ee09015058d1069f178594816151b510c60a232a
y00951989 <yangshengjun3@huawei.com>
2026-03-26 20:34:42 +0800

    1

--- cmake/custom_build.cmake
+++ cmake/custom_build.cmake
@@ -92,6 +92,7 @@ if (BUILD_OPEN_PROJECT)
             ascend_dump
             ascendalog
             dl
+            hcomm
     )
     set_target_properties(cust_opapi PROPERTIES OUTPUT_NAME
             cust_opapi
