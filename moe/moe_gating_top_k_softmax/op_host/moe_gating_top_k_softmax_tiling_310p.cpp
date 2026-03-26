70ec2252f336418816d204773e38cb077b81ba99
gcw_kTZ5XeTb <2491900587@qq.com>
2026-03-26 15:50:51 +0800

    moe_gating_top_k_softmax_tiling_base.h" ->moe_gating_top_k_softmax_tiling.h
    Signed-off-by: gcw_kTZ5XeTb <2491900587@qq.com>

--- moe/moe_gating_top_k_softmax/op_host/moe_gating_top_k_softmax_tiling_310p.cpp
+++ moe/moe_gating_top_k_softmax/op_host/moe_gating_top_k_softmax_tiling_310p.cpp
@@ -20,7 +20,7 @@
 #include "moe_gating_top_k_softmax_tiling_base.h"
 #include "register/tilingdata_base.h"
 #include "tiling/tiling_api.h"
-#include "moe_gating_top_k_softmax_tiling_base.h"
+#include "moe_gating_top_k_softmax_tiling.h"
 #include "tiling/platform/platform_ascendc.h"
 #include "log/log.h"
 
