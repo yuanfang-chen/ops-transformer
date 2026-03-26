9d66f5ed97b43b3e28ca960235f66f50a1762063
y00956176 <yangyibin12@huawei.com>
2026-03-26 14:48:03 +0800

    修改GQAlayout支持范围

--- attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_check_feature.cpp
+++ attention/fused_infer_attention_score/op_host/arch32/fused_infer_attention_score_tiling_check_feature.cpp
@@ -626,7 +626,7 @@ ge::graphStatus FiaTilingCheck::CheckFeatureHeadDim() const
     return ge::GRAPH_FAILED);
 
     OP_CHECK_IF(fiaInfo_.ropeMode == RopeMode::ROPE_SPLIT && ropeHeadDim_ != MAX_ROPE_DIM,
-    OP_LOGE(opName_, "In %s situation, headDim of Rope should be equal to 64, but got %u",
+    OP_LOGE(opName_, "In %s situation, headDim of Rope should be equal to 64,  but got %u",
         QuantModeToSerialString(quantMode_).c_str(), ropeHeadDim_),
     return ge::GRAPH_FAILED);
 
