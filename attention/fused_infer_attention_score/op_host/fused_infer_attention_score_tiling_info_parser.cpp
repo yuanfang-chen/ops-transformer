c1bec768ac263242bdf1f528ca8462694285e9b5
zuoyw <wangxu350@huawei.com>
2026-03-26 23:00:43 +0800

    feature:Tiling Reconstruction

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling_info_parser.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling_info_parser.cpp
@@ -1427,10 +1427,12 @@ ge::graphStatus FiaInfoParser::ParseAxisInfo()
     }
     SetFiaShape();
     GetQueryTSize();
+    if (ge::GRAPH_SUCCESS != GetLegacyIfaFlag()) {
+        return ge::GRAPH_FAILED;
+    }
     GetUpdateInfo();
     if (ge::GRAPH_SUCCESS != GetQkHeadDim() || ge::GRAPH_SUCCESS != GetValueHeadDim() ||
-        ge::GRAPH_SUCCESS != GetLegacyIfaFlag() || ge::GRAPH_SUCCESS != GetBatchSize() ||
-        ge::GRAPH_SUCCESS != GetS1Size()) {
+        ge::GRAPH_SUCCESS != GetBatchSize() || ge::GRAPH_SUCCESS != GetS1Size()) {
         return ge::GRAPH_FAILED;
     }
     if (emptyTensorFlag_) {
