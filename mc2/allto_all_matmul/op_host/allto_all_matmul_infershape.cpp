c75706e22b477cc0f74713b45fbec80bb3eb9adf
yifux <xiongyifu1@huawei.com>
2026-03-26 11:47:25 +0800

    modify all_to_all_mm & mm_all_to_all infershape check

--- mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
+++ mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
@@ -104,7 +104,7 @@ static ge::graphStatus CheckShapeForAlltoAllMatmul(const gert::InferShapeContext
         }
     }
 
-    OP_LOGD(INNER_DEBUG, "Matmul m is: %ld, n is: %ld, k1 is: %ld, k2 is: %ld.", shape.m, shape.n, shape.k1, shape.k2);
+    OP_LOGD(INNER_DEBUG, "transX2 is: %d, Matmul m is: %ld, n is: %ld, k1 is: %ld, k2 is: %ld.", transX2, shape.m, shape.n, shape.k1, shape.k2);
     return ge::GRAPH_SUCCESS;
 }
 
