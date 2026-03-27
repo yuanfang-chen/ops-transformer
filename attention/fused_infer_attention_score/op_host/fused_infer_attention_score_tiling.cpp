db858748d2b890473219e0051c5623259fb23a05
lilongwei_hw <lilongwei11@huawei.com>
2026-03-27 17:54:16 +0800

    add batch restrict

--- attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
+++ attention/fused_infer_attention_score/op_host/fused_infer_attention_score_tiling.cpp
@@ -1370,6 +1370,7 @@ static ge::graphStatus ConvertContextToParamsFAI(gert::TilingContext *context, F
             faInfo.flashDecodeFlag = true; 
         }
         if (faInfo.pagedCacheFlag && maxQSeqlen == 1 && minQSeqlen == 1 && faInfo.maskType == MaskType::NO_MASK &&
+            (batch >= aicoreNum) &&
             !faInfo.lseFlag && !faInfo.learnableSinkFlag && (faInfo.innerPrecise == 0) && ((faInfo.numHeads / faInfo.kvHeads) <= GROUP_SIZE_128)) {
             faInfo.decodingFlag = true;
         }
