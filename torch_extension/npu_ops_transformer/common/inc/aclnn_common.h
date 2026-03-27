fd581d61d9e8df4e6196cf68037413a44ad63d19
qianzehong <qianzehong@huawei.com>
2026-03-26 22:23:27 -0400

    support loading multiple libcust_opapi.so from ASCEND_CUSTOM_OPP_PATH

--- torch_extension/npu_ops_transformer/common/inc/aclnn_common.h
+++ torch_extension/npu_ops_transformer/common/inc/aclnn_common.h
@@ -16,6 +16,9 @@
 #include <torch_npu/csrc/framework/utils/OpAdapter.h>
 #include <dlfcn.h>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <cstdlib>
 #include <functional>
 #include <type_traits>
 #include <ATen/Tensor.h>
@@ -136,11 +139,41 @@ inline void *GetOpApiLibHandler(const char *libName)
     return handler;
 }
 
+inline std::vector<void *> GetCustOpApiHandlers()
+{
+    std::vector<void *> handlers;
+    const char *env = std::getenv("ASCEND_CUSTOM_OPP_PATH");
+    if (env != nullptr) {
+        std::string envStr(env);
+        std::istringstream iss(envStr);
+        std::string path;
+        while (std::getline(iss, path, ':')) {
+            if (path.empty()) {
+                continue;
+            }
+            std::string soPath = path + "/op_api/lib/" + GetCustOpApiLibName();
+            auto handler = dlopen(soPath.c_str(), RTLD_LAZY);
+            if (handler != nullptr) {
+                handlers.push_back(handler);
+            } else {
+                ASCEND_LOGW("dlopen %s failed, error:%s.", soPath.c_str(), dlerror());
+            }
+        }
+    }
+    if (handlers.empty()) {
+        auto handler = GetOpApiLibHandler(GetCustOpApiLibName());
+        if (handler != nullptr) {
+            handlers.push_back(handler);
+        }
+    }
+    return handlers;
+}
+
 inline void *GetOpApiFuncAddr(const char *apiName)
 {
-    static auto custOpApiHandler = GetOpApiLibHandler(GetCustOpApiLibName());
-    if (custOpApiHandler != nullptr) {
-        auto funcAddr = GetOpApiFuncAddrInLib(custOpApiHandler, GetCustOpApiLibName(), apiName);
+    static auto custHandlers = GetCustOpApiHandlers();
+    for (auto handler : custHandlers) {
+        auto funcAddr = GetOpApiFuncAddrInLib(handler, GetCustOpApiLibName(), apiName);
         if (funcAddr != nullptr) {
             return funcAddr;
         }
