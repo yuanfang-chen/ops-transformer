8b3bafbc96c7466b9a18fe52fbb6b5aec7ab10ba
dai-yuwen <daiyuwen1@huawei.com>
2026-03-27 14:16:23 +0800

    tnd dense场景确定性问题修复
    Signed-off-by: dai-yuwen <daiyuwen1@huawei.com>

--- attention/flash_attention_score_grad/op_host/arch35/flash_attention_score_grad_tiling_varlen_regbase.cpp
+++ attention/flash_attention_score_grad/op_host/arch35/flash_attention_score_grad_tiling_varlen_regbase.cpp
@@ -85,11 +85,7 @@ protected:
             deterPrefixData.nNewList.push_back(actualS2Outer);
         }
         int64_t totalArea = deterPrefixData.prefix1.back() * fBaseParams.n1;
-        if (fBaseParams.g == 1) {
-            fBaseParams.deterMaxRound = std::max(CeilDivideBy(totalArea, static_cast<int64_t>(fBaseParams.aicNum)), s1Max);
-        } else {
-            fBaseParams.deterMaxRound = std::max({CeilDivideBy(totalArea, static_cast<int64_t>(fBaseParams.aicNum)), s1Max * fBaseParams.g, s2Max});
-        }
+        fBaseParams.deterMaxRound = std::max({CeilDivideBy(totalArea, static_cast<int64_t>(fBaseParams.aicNum)), s1Max * fBaseParams.g, s2Max});
 
         deterPrefixData.prefix0 = SliceVector(deterPrefixData.prefix1, fBaseParams.deterPrefixStep);
         deterPrefixData.deterPrefix = SliceVector(deterPrefixData.deterPrefix, fBaseParams.deterPrefixStep);
