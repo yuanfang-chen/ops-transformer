327f8548a10743064e01eb32c312a8fff7514615
cc-z <zhaochenchen11@huawei.com>
2026-03-27 17:06:31 +0800

    空pr

--- moe/moe_finalize_routing_v2/op_kernel/moe_finalize_routing_v2.cpp
+++ moe/moe_finalize_routing_v2/op_kernel/moe_finalize_routing_v2.cpp
@@ -82,6 +82,7 @@ extern "C" __global__ __aicore__ void moe_finalize_routing_v2(
     GM_ADDR expandedPermutedRows, GM_ADDR expandedSrcToDstRow, GM_ADDR skip1, GM_ADDR skip2, GM_ADDR bias,
     GM_ADDR scales, GM_ADDR expertForSourceRow, GM_ADDR out, GM_ADDR workspace, GM_ADDR tiling)
 {
+    
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
     GM_ADDR userWS = GetUserWorkspace(workspace);
     if (userWS == nullptr) {
