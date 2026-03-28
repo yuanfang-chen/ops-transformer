c67088b6c43970e3c7b0e507a745aa9a515c9b26
HertzHan <hanhaocheng2@huawei.com>
2026-03-28 17:42:37 +0800

    add flash_attn test scripts

--- attention/flash_attention_score/op_host/arch35/flash_attention_score_tiling_regbase.h
+++ attention/flash_attention_score/op_host/arch35/flash_attention_score_tiling_regbase.h
@@ -465,9 +465,9 @@ protected:
     uint8_t attenMaskCompressMode;
 
     int64_t s1BasicBlock;
-    int64_t s2BasicBlock;
     int64_t dBasicBlock;
     int64_t dVBasicBlock;
+    int64_t s2BasicBlock;
 
     int64_t maxValidS2Len;
     int64_t thresholdS2Size = 0;        // S2最大长度，使得当前shape下，K和V能够占满L2Cache
