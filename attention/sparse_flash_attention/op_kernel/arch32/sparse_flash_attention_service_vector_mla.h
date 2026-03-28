14a0600c2ddc5fbae3134c278c19f0dc969512ad
weixin_60515901 <changlei24@huawei.com>
2026-03-28 15:56:12 +0800

    master

--- attention/sparse_flash_attention/op_kernel/arch32/sparse_flash_attention_service_vector_mla.h
+++ attention/sparse_flash_attention/op_kernel/arch32/sparse_flash_attention_service_vector_mla.h
@@ -20,7 +20,7 @@
 #include "kernel_tiling/kernel_tiling.h"
 #include "lib/matmul_intf.h"
 #include "lib/matrix/matmul/tiling.h"
-#include "sparse_flash_attention_common.h"
+#include "../sparse_flash_attention_common.h"
 
 using AscendC::CrossCoreSetFlag;
 using AscendC::CrossCoreWaitFlag;
