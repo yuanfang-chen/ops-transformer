074577f68cbb47feef09b73cf928675898addb4a
qq_23260455 <liujiali27@h-partners.com>
2026-03-28 14:34:41 +0800

    fix

--- attention/fused_infer_attention_score/op_kernel/flash_attention_regular.h
+++ attention/fused_infer_attention_score/op_kernel/flash_attention_regular.h
@@ -618,8 +618,8 @@ namespace SplitFuse {
                 LayoutQ layoutQTemp(rowNum, embed);
                 LayoutK layoutKTemp(strideK, stackSeqTile);
                 LayoutV layoutVTemp(stackSeqTile, strideV);
-                blockMmadQK.resetBlockStart();
-                blockMmadPV.resetBlockStart();
+                lockMmadQK.resetBlockStart(kvStart, pagedBlockSize);
+                blockMmadPV.resetBlockStart(kvStart, pagedBlockSize);
                 blockMmadQK.loadQGM(gQ[gmOffsetQ], layoutQTemp, rowNum, qNBlockSize, qHeads);
 #endif
                 for (uint32_t kvSIdx = kvStart; kvSIdx < kvEnd + preKVNum; kvSIdx++) {
