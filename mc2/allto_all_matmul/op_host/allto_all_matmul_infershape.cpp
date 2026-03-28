70d34ebbd96f01a8c05ea207090f498424c84ba3
yifux <xiongyifu1@huawei.com>
2026-03-28 15:18:03 +0800

    add check tensor x2 is contiguous

--- mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
+++ mc2/allto_all_matmul/op_host/allto_all_matmul_infershape.cpp
@@ -104,7 +104,15 @@ static ge::graphStatus CheckShapeForAlltoAllMatmul(const gert::InferShapeContext
         }
     }
 
+    OP_LOGD(INNER_DEBUG, "x1 dim0 is: %ld, x1 dim1 is: %ld, x2 dim0 is: %ld, x2 dim1 is: %ld",
+            x1Shape->GetDim(0U), x1Shape->GetDim(1U), x2Shape->GetDim(0U), x2Shape->GetDim(1U));
+    OP_LOGD(INNER_DEBUG, "transX2 is: %d", transX2);
     OP_LOGD(INNER_DEBUG, "Matmul m is: %ld, n is: %ld, k1 is: %ld, k2 is: %ld.", shape.m, shape.n, shape.k1, shape.k2);
+
+    int64_t storageDim0 = x2Shape->GetStorageShape().GetDim(0);
+    int64_t storageDim1 = x2Shape->GetStorageShape().GetDim(1);
+    OP_LOGD(INNER_DEBUG, "In infershape, storageDim0 is: %ld, storageDim1 is: %ld", storageDim0, storageDim1);
+
     return ge::GRAPH_SUCCESS;
 }
 
