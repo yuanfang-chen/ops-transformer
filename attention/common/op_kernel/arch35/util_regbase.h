e42fd503f60931c2fae5828dd478fbf72baefa92
zzy__ <zhaozhiyong15@hisilicon.com>
2026-03-28 15:07:58 +0000

    FIA s1 out split

--- attention/common/op_kernel/arch35/util_regbase.h
+++ attention/common/op_kernel/arch35/util_regbase.h
@@ -486,6 +486,8 @@ struct CVSharedParams<true, false> {
     // prefix
     bool isActualSharedPrefixLenNull;
     int64_t kvPrefixSize;
+
+    int64_t totalSize;
 };
 
 template<>
@@ -524,6 +526,8 @@ struct CVSharedParams<true, true> {
     // prefix
     bool isActualSharedPrefixLenNull;
     int64_t kvPrefixSize;
+
+    int64_t totalSize;
 };
 }
 
