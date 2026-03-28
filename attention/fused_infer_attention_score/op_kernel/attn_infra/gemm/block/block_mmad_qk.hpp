cb68dca4c1fc7564b2e82b2f9da6caa098cab19e
qq_23260455 <liujiali27@h-partners.com>
2026-03-28 14:50:25 +0800

    fix

--- attention/fused_infer_attention_score/op_kernel/attn_infra/gemm/block/block_mmad_qk.hpp
+++ attention/fused_infer_attention_score/op_kernel/attn_infra/gemm/block/block_mmad_qk.hpp
@@ -181,8 +181,8 @@ public:
     }
 
     __aicore__ inline
-    void resetBlockStart(){
-        blockStartOffset = 0;
+    void resetBlockStart(uint32_t kvStart, uint32_t pagedBlockSize){
+        blockStartOffset = kvStart * maxKVStackLen % pagedBlockSize;
     }
 
     __aicore__ inline
