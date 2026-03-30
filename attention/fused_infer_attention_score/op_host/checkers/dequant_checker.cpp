005ab2138d4148953f69635e14cfd7baecf11761
wangke293 <wangke293@huawei.com>
2026-03-30 16:20:17 +0800

    拦截修改
    Signed-off-by: wangke293 <wangke293@huawei.com>

--- attention/fused_infer_attention_score/op_host/checkers/dequant_checker.cpp
+++ attention/fused_infer_attention_score/op_host/checkers/dequant_checker.cpp
@@ -1079,14 +1079,14 @@ ge::graphStatus DequantChecker::CheckInputKVTypeForAntiquant(const FiaTilingInfo
         auto keyAntiquantScaleShape = keyAntiquantScaleTensor->GetStorageShape();
         if (inputKvType == ge::DT_INT4 || inputKvType == ge::DT_INT32) {
  	        OP_CHECK_IF((keyAntiquantScaleShape == expectedShape1) && inputKvType != ge::DT_INT8,
- 	                    OP_LOGE(ifaContext_->opName,
+ 	                    OP_LOGE(fiaInfo.opName,
                                 "Datatype of key and value(INT4/INT32) is not supported. "
                                 "Datatype of key and value must be INT8 when "
                                 "keyAntiquantMode is per-tensor mode and valueAntiquantMode is per-tensor mode."),
  	                    return ge::GRAPH_FAILED);
  	    } else {
  	        OP_CHECK_IF((keyAntiquantScaleShape == expectedShape1) && inputKvType != ge::DT_INT8,
- 	                    OP_LOGE(ifaContext_->opName,
+ 	                    OP_LOGE(fiaInfo.opName,
                                 "Datatype of key and value(%s) is not supported. "
                                 "Datatype of key and value must be INT8 when "
                                 "keyAntiquantMode is per-tensor mode and valueAntiquantMode is per-tensor mode.",
