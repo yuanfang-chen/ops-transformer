c5e696b49feab1ab205bdb439df7f7a791f0cf5d
weixin_49300047 <lijianhua56@huawei.com>
2026-03-30 09:43:04 +0800

    fix codecheck

--- mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
+++ mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
@@ -89,7 +89,8 @@ REGISTER_TILING_DEFAULT(MoeDistributeDispatchV2TilingData);
     if constexpr (ArchTag == TILINGKEY_TPL_A5) {
         if constexpr (CommMode == TILINGKEY_TPL_MTE) {
             if constexpr (FullMesh == TILINGKEY_ENABLE_FULLMESH) {
-                MoeDistributeDispatchV2FullMesh<DTYPE_X, DTYPE_EXPAND_X, MoeDistributeDispatchV2Impl::UNQUANT, true, false> op;
+                MoeDistributeDispatchV2FullMesh<DTYPE_X, DTYPE_EXPAND_X, \
+                                                MoeDistributeDispatchV2Impl::UNQUANT, true, false> op;
                 op.Init(nullptr, x, expertIds, scales, xActiveMask, elasticInfo, performanceInfo, expandXOut, dynamicScalesOut, assistInfoOut, 
                         expertTokenNumsOut, epSendCountsOut, tpSendCountsOut, workspaceGM, &pipe, &tilingData);
                 op.Process();
