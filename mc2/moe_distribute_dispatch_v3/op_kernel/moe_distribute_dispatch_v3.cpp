4514f52b30959c3fdf056f65edbae33226bd3ac2
zzg_code <zengzhiguo1@huawei.com>
2026-03-30 21:05:53 +0800

    1

--- mc2/moe_distribute_dispatch_v3/op_kernel/moe_distribute_dispatch_v3.cpp
+++ mc2/moe_distribute_dispatch_v3/op_kernel/moe_distribute_dispatch_v3.cpp
@@ -128,7 +128,7 @@ __global__ __aicore__ void moe_distribute_dispatch_v3(
 //         }
 //     }
 //     AscendC::SetCtrlSpr<FLOAT_OVERFLOW_MODE_CTRL, FLOAT_OVERFLOW_MODE_CTRL>(oriOverflowMode);
-// #endif
+#endif
 #else
 #if ((ORIG_DTYPE_EXPAND_X == DT_BF16) || (ORIG_DTYPE_EXPAND_X == DT_FLOAT16))
     if constexpr (ArchTag == TILINGKEY_TPL_A3) {
