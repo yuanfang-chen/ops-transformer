9bdd3ab56780f1388dc9e57c3745df659cd73245
JimmyLam2 <linjingmin2@huawei.com>
2026-03-30 14:02:11 +0800

    1

--- moe/moe_finalize_routing_v2/op_host/moe_finalize_routing_v2_tiling.h
+++ moe/moe_finalize_routing_v2/op_host/moe_finalize_routing_v2_tiling.h
@@ -107,6 +107,7 @@ REGISTER_TILING_DATA_CLASS(MoeFinalizeRoutingV2, MoeFinalizeRoutingV2TilingData)
 struct MoeFinalizeRoutingCompileInfoV2 {
     int32_t totalCoreNum = 0;
     uint64_t ubSize = 0;
+    int32_t aivNum = 0;
 };
 
 struct ShapeParamsV2 {
