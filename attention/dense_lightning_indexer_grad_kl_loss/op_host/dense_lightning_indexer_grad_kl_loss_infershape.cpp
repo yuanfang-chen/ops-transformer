f7ea786e0eef3212de79c64e5826ee8101d3bf16
qiansunchi <qiansunchi@huawei.com>
2026-03-31 10:09:18 +0800

    [fix] add log

--- attention/dense_lightning_indexer_grad_kl_loss/op_host/dense_lightning_indexer_grad_kl_loss_infershape.cpp
+++ attention/dense_lightning_indexer_grad_kl_loss/op_host/dense_lightning_indexer_grad_kl_loss_infershape.cpp
@@ -45,7 +45,8 @@ enum OutputIdx {
 
 ge::graphStatus InferShapeDenseLightningIndexerGradKLLoss(gert::InferShapeContext *context)
 {
-    if (context == nullptr){
+    if (context == nullptr) {
+        OP_LOGE("DenseLightningIndexerGradKLLoss", "context is nullptr!");
         return ge::GRAPH_FAILED;
     }
     
@@ -85,6 +86,7 @@ ge::graphStatus InferShapeDenseLightningIndexerGradKLLoss(gert::InferShapeContex
 ge::graphStatus InferDataTypeDenseLightningIndexerGradKLLoss(gert::InferDataTypeContext *context)
 {
     if (context == nullptr) {
+        OP_LOGE("DenseLightningIndexerGradKLLoss", "context is nullptr!");
         return ge::GRAPH_FAILED;
     }
 
