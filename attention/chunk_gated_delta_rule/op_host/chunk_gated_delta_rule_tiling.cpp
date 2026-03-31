c94b50a1884e69b76303b1cbd730a67c8b625b2f
LAIM321 <laiming2@huawei.com>
2026-03-31 19:11:21 +0800

    Add ChunkGatedDeltaRule

--- attention/chunk_gated_delta_rule/op_host/chunk_gated_delta_rule_tiling.cpp
+++ attention/chunk_gated_delta_rule/op_host/chunk_gated_delta_rule_tiling.cpp
@@ -210,6 +210,16 @@ ge::graphStatus ChunkGatedDeltaRuleTiling::GetWorkspaceSize()
     return ge::GRAPH_SUCCESS;
 };
 
+ge::graphStatus ChunkGatedDeltaRuleTiling::SetScheduleConfig()
+{
+    constexpr uint32_t batchMode = 1U;
+    auto ret = context_->SetScheduleMode(batchMode);
+    OP_CHECK_IF(ret != ge::GRAPH_SUCCESS, OP_LOGE(context_->GetNodeName(), "SetScheduleMode failed, ret=%d", ret),
+                return ge::GRAPH_FAILED);
+
+    return ge::GRAPH_SUCCESS;
+}
+
 // 写回 tilingData 和 workspace 信息
 ge::graphStatus ChunkGatedDeltaRuleTiling::PostTiling()
 {
@@ -237,6 +247,9 @@ ge::graphStatus ChunkGatedDeltaRuleTiling::PostTiling()
     OP_CHECK_IF(workspaces == nullptr, OPS_REPORT_CUBE_INNER_ERR(context_->GetNodeName(), "workspaces is null"),
                 return ge::GRAPH_FAILED);
     workspaces[0] = workspaceSize_;
+
+    OP_CHECK_IF(SetScheduleConfig() != ge::GRAPH_SUCCESS, OP_LOGE(context_->GetNodeName(), "SetScheduleMode failed"),
+                return ge::GRAPH_FAILED);
 
     return ge::GRAPH_SUCCESS;
 }
