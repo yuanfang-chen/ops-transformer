d4f4aa4e38273364809dc4170000279a1f31440f
chenjiale56 <chenjiale56@huawei.com>
2026-03-31 09:09:40 +0800

    AllGatherMatmul算子样例适配主线
    Co-authored-by: 陈佳乐<chenjiale56@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3150 merge sample_agmm into master
    
    AllGatherMatmul算子样例适配主线
    
    Created-by: chenjiale56
    Commit-by: 陈佳乐
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    AllGatherMatmul算子样例v1->v2版本，与现有mc2/all_gather_matmul_v2保持一致，适配主线torch_npu
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    关联Issue [#1251](https://gitcode.com/cann/ops-transformer/issues/1251) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [x] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    
    See merge request: cann/ops-transformer!3150

---
+++ experimental/mc2/all_gather_matmul_v2/op_host/all_gather_matmul_v2_def.cpp
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file all_gather_matmul_def.cpp
+ * \brief
+ */
+#include "register/op_def_registry.h"
+
+namespace ops {
+class AllGatherMatmulV2 : public OpDef {
+ public:
+  explicit AllGatherMatmulV2(const char *name) : OpDef(name) {
+    this->Input("x1")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .FormatList({ge::FORMAT_ND}); 
+    this->Input("x2")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .FormatList({ge::FORMAT_ND})
+        .IgnoreContiguous();
+
+    this->Input("bias")
+        .ParamType(OPTIONAL)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .FormatList({ge::FORMAT_ND});
+    this->Input("x1_scale")
+        .ParamType(OPTIONAL)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
+        .FormatList({ge::FORMAT_ND});
+    this->Input("x2_scale")
+        .ParamType(OPTIONAL)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
+        .FormatList({ge::FORMAT_ND});
+    this->Input("quant_scale")
+        .ParamType(OPTIONAL)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
+        .FormatList({ge::FORMAT_ND});
+
+    this->Output("y")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .FormatList({ge::FORMAT_ND});
+    this->Output("gather_out")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .FormatList({ge::FORMAT_ND});
+    this->Output("amax_out")
+        .ParamType(OPTIONAL)
+        .DataType({ge::DT_FLOAT, ge::DT_FLOAT})
+        .FormatList({ge::FORMAT_ND});
+
+    this->Attr("group").AttrType(REQUIRED).String();
+    this->Attr("is_trans_a").AttrType(OPTIONAL).Bool(false);
+    this->Attr("is_trans_b").AttrType(OPTIONAL).Bool(false);
+    this->Attr("gather_index").AttrType(OPTIONAL).Int(0);
+    this->Attr("comm_turn").AttrType(OPTIONAL).Int(0);
+    this->Attr("rank_size").AttrType(OPTIONAL).Int(0);
+    this->Attr("block_size").AttrType(OPTIONAL).Int(0);
+    this->Attr("group_size").AttrType(OPTIONAL).Int(0);
+    this->Attr("is_gather_out").AttrType(OPTIONAL).Bool(true);
+    this->Attr("is_amax_out").AttrType(OPTIONAL).Bool(false);
+    this->Attr("y_dtype").AttrType(OPTIONAL).Int(static_cast<int>(ge::DT_UNDEFINED));
+    this->Attr("comm_mode").AttrType(REQUIRED).String("ccu");
+    
+    OpAICoreConfig aicore_config;
+    aicore_config.DynamicCompileStaticFlag(true)
+        .DynamicFormatFlag(true)
+        .DynamicRankSupportFlag(true)
+        .DynamicShapeSupportFlag(true)
+        .NeedCheckSupportFlag(false)
+        .PrecisionReduceFlag(true)
+        .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
+        .ExtendCfgInfo("jitCompile.flag", "static_false")  // 动态shape,复用二进制,后续图支持后修改
+        .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel")
+        .ExtendCfgInfo("opFile.value", "all_gather_matmul_v2_apt");
+    this->AICore().AddConfig("ascend950", aicore_config);
+    this->MC2().HcclGroup("group");
+  }
+};
+
+OP_ADD(AllGatherMatmulV2);
+}  // namespace ops
