548c11a162440231ed9574e7b92e5943c8b22627
hello_simida <wangyi206@huawei.com>
2026-03-31 19:18:29 +0800

    bugfix

--- scripts/oat_check.sh
+++ scripts/oat_check.sh
@@ -3,6 +3,15 @@
 # OAT Pre-commit Check Script (for pre-commit framework)
 # This script is called by pre-commit framework automatically
 #
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
 
 set -e
 
@@ -381,7 +390,7 @@ mkdir -p "$OAT_REPORT_DIR"
 
 echo ""
 echo "[OAT] Running compliance scan..."
-java -jar "$OAT_JAR" \
+java -cp "$OAT_JAR:$OAT_DIR/target/libs/*" ohos.oat.OatLicenseMain \
     -mode s \
     -s "$REPO_ROOT" \
     -r "$OAT_REPORT_DIR" \
