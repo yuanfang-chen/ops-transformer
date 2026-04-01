d4f47d2d3300590ce92a2f232cc0607623f8911f
hblnb <huangbolin3@huawei.com>
2026-04-01 17:17:40 +0800

    解决了部分算子如grouped_matmul_finalize_routing运行opkernel的ut时报错链接不到gert库的问题

--- cmake/ut.cmake
+++ cmake/ut.cmake
@@ -402,10 +402,22 @@ if(UT_TEST_ALL OR OP_KERNEL_UT)
                 ${ASCEND_DIR}/include
         )
       target_compile_definitions(${opName}_${socVersion}_tiling_tmp PRIVATE LOG_CPP _GLIBCXX_USE_CXX11_ABI=0)
+      set(GERT_FILE "libgert.so")
+      set(SEARCH_GERT_PATHS
+          "${ASCEND_DIR}"
+      )
+      find_file(
+          GERT_PATH
+          ${TARGET_FILE}
+          PATHS ${SEARCH_PATHS}
+          NO_DEFAULT_PATH
+          NO_CMAKE_FIND_ROOT_PATH
+      )
       target_link_libraries(
         ${opName}_${socVersion}_tiling_tmp
         PRIVATE -Wl,--no-as-needed -Wl,--as-needed -Wl,--whole-archive tiling_api
                 -Wl,--no-whole-archive gcov
+                -Wl,--no-whole-archive ${GERT_PATH}
                 $<$<BOOL:${dlog_FOUND}>:$<BUILD_INTERFACE:dlog_headers>>
         )
 
