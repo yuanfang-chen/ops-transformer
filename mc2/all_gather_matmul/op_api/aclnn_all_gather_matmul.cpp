728d32d1487e680ef8d795e5031d35e79ad625ae
hello_simida <wangyi206@huawei.com>
2026-04-01 17:22:14 +0800

    修改nc2的aclnninner调用方式

--- mc2/all_gather_matmul/op_api/aclnn_all_gather_matmul.cpp
+++ mc2/all_gather_matmul/op_api/aclnn_all_gather_matmul.cpp
@@ -23,6 +23,7 @@
 #include "opdev/platform.h"
 #include "common/op_host/op_api/matmul_util.h"
 #include "common/utils/hccl_util.h"
+#include "aclnnInner_all_gather_matmul.h"
 
 using namespace Ops::Transformer;
 using namespace op;
@@ -40,15 +41,6 @@ typedef struct {
   bool hasReg;
 } NnopbaseDfxId;
 
-extern aclnnStatus aclnnInnerAllGatherMatmulGetWorkspaceSize(const aclTensor *x1, const aclTensor *x2,
-                                                             const aclTensor *bias,
-                                                             const char *group, bool transposeX1, bool transposeX2,
-                                                             int64_t gatherIndex, int64_t commTurn, int64_t rankSize,
-                                                             bool isGatherOut, const aclTensor *output,
-                                                             const aclTensor *gatherOut, uint64_t *workspaceSize,
-                                                             aclOpExecutor **executor);
-extern aclnnStatus aclnnInnerAllGatherMatmul(void *workspace, uint64_t workspaceSize, aclOpExecutor *executor,
-                                             aclrtStream stream);
 extern "C" uint64_t NnopbaseMsprofSysTime();
 extern "C" void NnopbaseReportApiInfo(const uint64_t beginTime, NnopbaseDfxId &dfxId);
 extern "C" aclnnStatus __attribute__((weak)) NnopbaseDisableOptionalInput(void *executor, const size_t irIndex);
@@ -206,9 +198,7 @@ aclnnStatus aclnnAllGatherMatmulGetWorkspaceSize(const aclTensor *x1, const aclT
                                                   commTurn, streamMode, 0, commMode, const_cast<aclTensor *>(output),
                                                   const_cast<aclTensor *>(gatherOut), nullptr, workspaceSize, executor);
   }
-  aclnnStatus ret = aclnnInnerAllGatherMatmulGetWorkspaceSize(x1, x2, bias, group, transposeX1, transposeX2,
-                                                              gatherIndex, commTurn, rankSize, isGatherOut,
-                                                              output, gatherOut, workspaceSize, executor);
+  aclnnStatus ret = aclnnInnerAllGatherMatmulGetWorkspaceSize(x1, x2, bias, const_cast<char*>(group), transposeX1, transposeX2, gatherIndex, commTurn, rankSize, isGatherOut, output, gatherOut, workspaceSize, executor);
   OP_LOGD("AllGatherMatmul, aclnnInnerGetWorkspaceSize ret = %d.", ret);
   static NnopbaseDfxId dfxId = {0x60000, __func__, false};
   NnopbaseReportApiInfo(timeStamp, dfxId);
