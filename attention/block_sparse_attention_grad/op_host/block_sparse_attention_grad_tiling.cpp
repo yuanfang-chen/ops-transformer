bc7c9048e4129d5bfbcacd993b5b209f4d491b46
zhanglei <george.zhanglei@huawei.com>
2026-04-01 14:13:35 +0800

    fix codecheck

--- attention/block_sparse_attention_grad/op_host/block_sparse_attention_grad_tiling.cpp
+++ attention/block_sparse_attention_grad/op_host/block_sparse_attention_grad_tiling.cpp
@@ -61,6 +61,7 @@ constexpr int SCALE_VALUE_INDEX = 4;
 constexpr int VALID_HEAD_DIM_128 = 128;
 constexpr uint64_t VEC_POST_DIVISION = 3; // post 划分的空间块数， input 2份，output 1份
 constexpr uint64_t WORKSPACE_NUM_ALIGN = 256;
+constexpr uint32_t BATCH_SCHEDULE_MODE = 1;
 
 namespace optiling {
 
@@ -474,7 +475,7 @@ ge::graphStatus BSAGradTiling::FillTilingData(gert::TilingContext *context)
     tilingData_->set_tilingKey(tilingKey);
     context->SetTilingKey(tilingKey);
     context->SetBlockDim(blockDim_);
-    context->SetScheduleMode(1);
+    context->SetScheduleMode(BATCH_SCHEDULE_MODE);
 
     tilingData_->set_sOutSize(sOutSize_);
     tilingData_->set_dPOutSize(dPOutSize_);
