fb7b437b5f241a192337cc6c64affa2bc452d0e2
zhanglei <george.zhanglei@huawei.com>
2026-04-01 10:59:36 +0800

    性能优化

--- attention/block_sparse_attention_grad/op_host/block_sparse_attention_grad_tiling.cpp
+++ attention/block_sparse_attention_grad/op_host/block_sparse_attention_grad_tiling.cpp
@@ -92,7 +92,7 @@ ge::graphStatus BSAGradTiling::GetNpuInfo(gert::TilingContext *context)
     ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ubSize_);
     libapiSize_ = ascendcPlatform.GetLibApiWorkSpaceSize();
     aivNum_ = ascendcPlatform.GetCoreNumAiv();
-    aicNum_ = static_cast<uint32_t>(1);
+    aicNum_ = ascendcPlatform.GetCoreNumAic();
     return ge::GRAPH_SUCCESS;
 }
 
