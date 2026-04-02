f861e66932bc0be65bbf87cb834f17c376c052bd
lyt_claire <luyitong1@huawei.com>
2026-04-02 10:01:05 +0800

    Revert "根据检视意见修改"
    This reverts commit 5ea175f70d432eb2a0739074b02a9e508bcab092.

--- mc2/matmul_reduce_scatter_v2/op_api/aclnn_matmul_reduce_scatter_v2.cpp
+++ mc2/matmul_reduce_scatter_v2/op_api/aclnn_matmul_reduce_scatter_v2.cpp
@@ -164,8 +164,8 @@ static aclnnStatus CheckAivModeParams(const aclTensor* x1, const aclTensor* x2, 
     // 1. 检查参数是否为空指针
     CHECK_RET(CheckNotNull(x1, x2, output), ACLNN_ERR_PARAM_NULLPTR);
 
-    // 【A2、A3】检查x2矩阵非连续合法性
-    if (GetCurrentPlatformInfo().GetCurNpuArch() == NpuArch::DAV_2201) {
+    // 【A2】检查x2矩阵非连续合法性
+    if (op::GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910B) {
         if (!Ops::Transformer::IsTransposeLastTwoDims(x2) && !MC2Aclnn::IsTensorContiguous(x2)) {
             OP_LOGE(ACLNN_ERR_PARAM_INVALID, "The x2 without transpose in MatmulReduceScatter must be contiguous,"
                     "but it is non-contiguous.");
